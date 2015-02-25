0d6574cc404f7a925bcc0b914e14866009dba214
mike o'brien <mpobrien005@gmail.com>
2015-02-25 17:03:19 -0500

    mci-1694 fix up host leak detector script

--- scripts/hostcheck.py
+++ scripts/hostcheck.py
@@ -12,21 +12,26 @@ ec2 = boto.connect_ec2()
 chain = itertools.chain.from_iterable
 all_instances = list(chain([res.instances for res in ec2.get_all_instances()]))
 all_instance_ids = set(instance.id for instance in all_instances)
-#print existing_instances #gives you a list of all instances
+
+whitelist = []
 
 motuhosts = list(db.hosts.find())
 motuhostsmap = {}
 for h in motuhosts:
     motuhostsmap[h['_id']] = h['status']
-    
 
+spots = list(ec2.get_all_spot_instance_requests())
+i_to_sir = {}
+for spot in spots:
+    if spot.instance_id:
+        i_to_sir[spot.instance_id] = spot.id
 
 for instance in all_instances:
+    if instance.id in i_to_sir:
+        instance.id = i_to_sir[instance.id]
+    if instance.id in whitelist:
+        print "instance %s is in whitelist" % (instance.id))
     if instance.id not in motuhostsmap:
-        print "instance %s is NOT in motu DB (%s)" % (instance.id, len(instance.tags))
+        print "instance %s is in AWS with status 'AWS:%s' NOT in motu DB, tags are %s" % (instance.id, instance.state, repr(instance.tags))
     if instance.id in motuhostsmap:
         print "instance %s is in motu DB with status: %s" % (instance.id, motuhostsmap[instance.id])
-
-
-
-
