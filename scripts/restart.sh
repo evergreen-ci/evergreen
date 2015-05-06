c6373d2947d527782e53455ebb715886eddf9836
Kyle Erf <erf@mongodb.com>
2015-05-06 13:53:23 -0400

    MCI-1965 fix restart.sh EVGHOME

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,8 +4,8 @@ set -o verbose
 
 # restart API server
 killall -9 mci_api_server || true
-GOMAXPROCS=3 nohup $mci_home/bin/mci_api_server -conf /data/home/etc/mci_settings.yml >& $mci_home/logs/mci_api_server_nohup.log &
+GOMAXPROCS=3 nohup $EVGHOME/bin/mci_api_server -conf /data/home/etc/mci_settings.yml >& $EVGHOME/logs/mci_api_server_nohup.log &
 
 # restart UI server
 killall -9 mci_ui_server || true
-GOMAXPROCS=3 nohup $mci_home/bin/mci_ui_server -conf /data/home/etc/mci_settings.yml >& $mci_home/logs/mci_ui_server_nohup.log &
+GOMAXPROCS=3 nohup $EVGHOME/bin/mci_ui_server -conf /data/home/etc/mci_settings.yml >& $EVGHOME/logs/mci_ui_server_nohup.log &
