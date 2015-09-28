3c0dee55edd2888fe758e755e215c208ca57a69d
Shraya Ramani <shraya.ramani@10gen.com>
2015-09-28 13:54:16 -0400

    EVG-434 change manifest plugin html template

--- plugin/builtin/manifest/manifest_plugin.go
+++ plugin/builtin/manifest/manifest_plugin.go
@@ -4,12 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/evergreen-ci/evergreen/model/manifest"
-	"github.com/evergreen-ci/evergreen/model/version"
 	"github.com/evergreen-ci/evergreen/plugin"
-	"github.com/gorilla/mux"
 	"github.com/mitchellh/mapstructure"
-	"html/template"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -52,6 +48,10 @@ func (m *ManifestPlugin) Configure(conf map[string]interface{}) error {
 	return nil
 }
 
+func (m *ManifestPlugin) GetUIHandler() http.Handler {
+	return nil
+}
+
 // GetPanelConfig returns a pointer to a plugin's UI configuration.
 // or an error, if an error occur while trying to generate the config
 // A nil pointer represents a plugin without a UI presence, and is
@@ -59,19 +59,13 @@ func (m *ManifestPlugin) Configure(conf map[string]interface{}) error {
 // GetPanelConfig returns a plugin.PanelConfig struct representing panels
 // that will be added to the Version page.
 func (m *ManifestPlugin) GetPanelConfig() (*plugin.PanelConfig, error) {
-	root := plugin.StaticWebRootFromSourceFile()
-	panelHTML, err := ioutil.ReadFile(root + "/partials/version_manifest_panel.html")
-	if err != nil {
-		return nil, fmt.Errorf("Can't load panel html file, %v, %v",
-			root+"/partials/version_manifest_panel.html", err)
-	}
 	return &plugin.PanelConfig{
 		StaticRoot: plugin.StaticWebRootFromSourceFile(),
 		Panels: []plugin.UIPanel{
 			{
 				Page:      plugin.VersionPage,
 				Position:  plugin.PageRight,
-				PanelHTML: template.HTML(panelHTML),
+				PanelHTML: "<div ng-include=\"'/plugin/manifest/static/partials/version_manifest_panel.html'\"></div>",
 				DataFunc: func(context plugin.UIContext) (interface{}, error) {
 					if context.Version == nil {
 						return nil, nil
@@ -93,11 +87,7 @@ func (m *ManifestPlugin) GetPanelConfig() (*plugin.PanelConfig, error) {
 		},
 	}, nil
 }
-func (m *ManifestPlugin) GetUIHandler() http.Handler {
-	r := mux.NewRouter()
-	r.Path("/get/{project_id}/{revision}").HandlerFunc(m.GetManifest)
-	return r
-}
+
 func (m *ManifestPlugin) GetAPIHandler() http.Handler {
 	r := http.NewServeMux()
 	r.HandleFunc(fmt.Sprintf("/%v", ManifestLoadAPIEndpoint), m.ManifestLoadHandler)
@@ -113,35 +103,4 @@ func (m *ManifestPlugin) NewCommand(cmdName string) (plugin.Command, error) {
 	default:
 		return nil, fmt.Errorf("No such %v command: %v", m.Name(), cmdName)
 	}
-}
-
-func (m *ManifestPlugin) GetManifest(w http.ResponseWriter, r *http.Request) {
-	project := mux.Vars(r)["project_id"]
-	revision := mux.Vars(r)["revision"]
-
-	version, err := version.FindOne(version.ByProjectIdAndRevision(project, revision))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("error getting version for project %v with revision %v: %v",
-			project, revision, err), http.StatusBadRequest)
-		return
-	}
-	if version == nil {
-		http.Error(w, fmt.Sprintf("version not found for project %v, with revision %v", project, revision),
-			http.StatusNotFound)
-		return
-	}
-
-	foundManifest, err := manifest.FindOne(manifest.ById(version.Id))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("error getting manifest with version id %v: %v",
-			version.Id, err), http.StatusBadRequest)
-		return
-	}
-	if foundManifest == nil {
-		http.Error(w, fmt.Sprintf("manifest not found for version %v", version.Id), http.StatusNotFound)
-		return
-	}
-	plugin.WriteJSON(w, http.StatusOK, foundManifest)
-	return
-
 }
