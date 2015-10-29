796f4011a5db3c28a2ff7020ebc349ec1fa1be00
Will Banfield <william.banfield@mongodb.com>
2015-10-29 16:39:43 -0400

    EVG-685: fix rebase mistake

--- cli/main/cli.go
+++ cli/main/cli.go
@@ -10,19 +10,19 @@ func main() {
 	opts := cli.Options{}
 
 	var parser = flags.NewParser(&opts, flags.Default)
-	parser.AddCommand("get-update", "fetch the latest version of this binary", "", &cli.GetUpdateCommand{GlobalOpts: opts})
+	parser.AddCommand("get-update", "fetch the latest version of this binary", "", &cli.GetUpdateCommand{GlobalOpts: &opts})
 	parser.AddCommand("version", "display version information", "", &cli.VersionCommand{})
-	parser.AddCommand("set-module", "update or add module to an existing patch", "", &cli.SetModuleCommand{GlobalOpts: opts})
+	parser.AddCommand("set-module", "update or add module to an existing patch", "", &cli.SetModuleCommand{GlobalOpts: &opts})
 	parser.AddCommand("patch", "submit a patch", "",
-		&cli.PatchCommand{PatchCommandParams: cli.PatchCommandParams{GlobalOpts: opts}})
+		&cli.PatchCommand{PatchCommandParams: cli.PatchCommandParams{GlobalOpts: &opts}})
 	parser.AddCommand("patch-file", "submit a patch using a diff file", "",
-		&cli.PatchFileCommand{PatchCommandParams: cli.PatchCommandParams{GlobalOpts: opts}})
-	parser.AddCommand("list-patches", "show existing patches", "", &cli.ListPatchesCommand{GlobalOpts: opts})
-	parser.AddCommand("rm-module", "remove a module from an existing patch", "", &cli.RemoveModuleCommand{GlobalOpts: opts})
-	parser.AddCommand("cancel-patch", "cancel an existing patch", "", &cli.CancelPatchCommand{GlobalOpts: opts})
-	parser.AddCommand("finalize-patch", "finalize an existing patch", "", &cli.FinalizePatchCommand{GlobalOpts: opts})
-	parser.AddCommand("list-projects", "list all projects", "", &cli.ListProjectsCommand{GlobalOpts: opts})
-	parser.AddCommand("validate", "validate a config file", "", &cli.ValidateCommand{GlobalOpts: opts})
+		&cli.PatchFileCommand{PatchCommandParams: cli.PatchCommandParams{GlobalOpts: &opts}})
+	parser.AddCommand("list-patches", "show existing patches", "", &cli.ListPatchesCommand{GlobalOpts: &opts})
+	parser.AddCommand("rm-module", "remove a module from an existing patch", "", &cli.RemoveModuleCommand{GlobalOpts: &opts})
+	parser.AddCommand("cancel-patch", "cancel an existing patch", "", &cli.CancelPatchCommand{GlobalOpts: &opts})
+	parser.AddCommand("finalize-patch", "finalize an existing patch", "", &cli.FinalizePatchCommand{GlobalOpts: &opts})
+	parser.AddCommand("list-projects", "list all projects", "", &cli.ListProjectsCommand{GlobalOpts: &opts})
+	parser.AddCommand("validate", "validate a config file", "", &cli.ValidateCommand{GlobalOpts: &opts})
 	parser.AddCommand("evaluate", "display a project file's evaluated and expanded form", "",
 		&cli.EvaluateCommand{Mode: cli.All})
 	parser.AddCommand("evaluate-tasks", "display a project file's evaluated and expanded tasks", "",
