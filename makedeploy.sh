b2622b3b9d1a4bf6d011482732fde276e8019f81
Mike O'Brien <mpobrien005@gmail.com>
2016-02-13 17:02:53 -0500

    correctly include static assets and templates from plugins into tarball

--- makedeploy.sh
+++ makedeploy.sh
@@ -10,10 +10,12 @@ mkdir -p build/executables
 mkdir -p build/clients
 mkdir -p build/public
 mkdir -p build/ui/templates
+mkdir -p build/ui/plugins
 cp -R executables/* build/executables/
 cp -R clients/* build/clients/
 cp -R public/* build/public/
 cp -R ui/templates/* build/ui/templates
+cp -R ui/plugins/* build/ui/plugins
 cp bin/evergreen_api_server build/bin
 cp bin/evergreen_ui_server build/bin
 cp bin/evergreen_runner build/bin
