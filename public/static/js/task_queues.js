32913007ce19be65e5c480edbe2d5cc914610557
Shraya Ramani <shraya@mongodb.com>
2016-07-05 16:33:21 -0400

    EVG-908 add a link from task page to task queue

--- public/static/js/task_queues.js
+++ public/static/js/task_queues.js
@@ -1,81 +1,13 @@
 mciModule.controller('TaskQueuesCtrl',
-  ['$scope', '$window', '$location', 'mciTaskStatisticsRestService',
-  function($scope, $window, $location, taskStatisticsRestService) {
+  ['$scope', '$window', '$location', '$timeout', '$anchorScroll', 'mciTaskStatisticsRestService',
+  function($scope, $window, $location, $timeout, $anchorScroll, taskStatisticsRestService) {
 
   $scope.taskQueues = $window.taskQueues;
-  $scope.hostStats = $window.hostStats;
-
-  $scope.taskStats = []
-
-  // these objects store the options for the dropdown on
-  // task timing statistic aggregations. The controller automatically
-  // fills in the <select> tags in the ui
-  $scope.statOptions = {
-    fieldOptions: [
-      {display: "Start \u21E2 Finish",
-        val:["start_time", "finish_time"]},
-      {display: "Scheduled \u21E2 Start",
-        val:["scheduled_time", "start_time"]},
-      {display: "Scheduled \u21E2 Dispatched",
-        val:["scheduled_time", "dispatch_time"]},
-      {display: "Created \u21E2 Scheduled",
-        val:["create_time", "scheduled_time"]},
-    ],
-    groupOptions: [
-      {display: "Distro", val:"distro"},
-      {display: "Task Name", val:"display_name"},
-      {display: "Build Variant", val:"build_variant"},
-    ],
-    timeOptions: [
-      {display: "Today", val:"1"},
-      {display: "One Week", val:"7"},
-      {display: "One Month", val:"30"},
-      {display: "All Time", val:"-1"},
-    ]
-  }
-  $scope.fields = $scope.statOptions.fieldOptions[0];
-  $scope.group = $scope.statOptions.groupOptions[0];
-  $scope.cutoff = $scope.statOptions.timeOptions[0];
-
   $scope.loading = true;
-
-  // function for making an ajax call to the task timing aggreation
-  // ui endpoint. Updates UI with simple loading text while waiting.
-  $scope.getTimeStats = function getTimeStats() {
-    $scope.loading = true
-    taskStatisticsRestService.getTimeStatistics(
-      $scope.fields.val[0], $scope.fields.val[1],
-      $scope.group.val, $scope.cutoff.val, {
-        success: function(data, status) {
-          if (!data || data == "null") {
-            $scope.taskStats = [];
-          } else {
-            $scope.taskStats = data;
-          }
-          $scope.loading = false;
-        },
-        error: function(jqXHR, status, errorThrown) {
-          $scope.loading = false;
-          console.log('Error getting statistics ' + jqXHR);
-        }
-      });
-  };
-
-  // watch dropdowns for a change.
-  $scope.$watch('fields', function(newValue, oldValue) {
-    if (newValue != oldValue)
-      $scope.getTimeStats();
-  });
-  $scope.$watch('group', function(newValue, oldValue) {
-    if (newValue != oldValue)
-      $scope.getTimeStats();
-  });
-  $scope.$watch('cutoff', function(newValue, oldValue) {
-    if (newValue != oldValue)
-      $scope.getTimeStats();
-  });
-
   $scope.distros = [];
+
+  $anchorScroll.yOffset = 60;
+
   _.each($scope.taskQueues, function(queue) {
     $scope.distros.push(queue.distro);
   })
@@ -86,12 +18,12 @@ mciModule.controller('TaskQueuesCtrl',
     $scope.queues[queue.distro] = queue.queue;
   });
 
-  $scope.activeDistro = $location.hash();
+  $scope.activeElement = $location.hash();
 
-  $scope.setActiveDistro = function(distro) {
-    $scope.activeDistro = distro;
+  $scope.setActiveElement = function(distro) {
+    $scope.activeElement = distro;
     $location.hash(distro);
-    $('body').animate({scrollTop: $('#'+distro).offset().top-60}, 'fast');
+    $anchorScroll();
   };
 
   $scope.isPatch = function(queueItem){
@@ -107,6 +39,8 @@ mciModule.controller('TaskQueuesCtrl',
   $scope.getLength = function(distro){
     return $scope.queues[distro].length;
   }
+
+  $anchorScroll();
 
 }]);
 
