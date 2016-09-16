f6a9ea9c1c732566195f7fe9de553cb80d4dd4e4
Kyle Erf <erf@mongodb.com>
2016-09-16 14:39:57 -0400

    EVG-1203 reporting scripts for avg task cost, dynamic host time

--- scripts/report_scratch.js
+++ scripts/report_scratch.js
@@ -68,7 +68,26 @@ db.tasks.aggregate([{$match:{
                            function(result){print(result._id.project+", "+result._id.month+"/1/"+result._id.year+", "+result.total/(60*60*1000));});
 
 
+// host hours per month, csv
+db.hosts.aggregate([{$match:{
+  creation_time:{$gt:new Date(new Date().getTime() - 1000*60*60*24*400)},
+  termination_time: {$gt:new Date(new Date().getTime() - 1000*60*60*24*400)}}},
+  {$group: { _id: {month: {$month: "$creation_time"}, year: {$year: "$creation_time"}}, total: {$sum:{$subtract:["$termination_time", "$creation_time"]}}}},
+  {$sort: {"_id.year":1, "_id.month":1}}]).result.forEach(
+    function(result){print(result._id.month+"/1/"+result._id.year+", "+result.total/(60*60*1000));});
 
+
+// average task time and cost for a git and patch versions per project
+db.tasks.aggregate([{$match:{
+    status: {"$in": ["success", "failed"]},
+    start_time:{$gt:new Date(new Date().getTime() - 1000*60*60*24*7)},
+    finish_time:{$gt: new Date(new Date().getTime() - 1000*60*60*24*7)},
+    cost:{$exists:true}}},
+  {$group: {_id:{v: "$version", p: "$branch", r: "$r"}, cost: {$sum: "$cost"}, time: {$sum: {$subtract: ["$finish_time", "$start_time"]}}}}, 
+  {$group: {_id: {p: "$_id.p", r: "$_id.r"}, cost: {$avg: "$cost"}, time: {$avg: "$time"}}},
+  {$sort: {"_id.p":1, "_id.r":1}},
+]).result.forEach(
+  function(result){print(result._id.p+", "+result._id.r+", $"+result.cost+", "+result.time/(60*60*1000));});
 
 
 
