6a45ba41f0d74576840d53a3cc346d25bf4d4df4
Sam Kleinman <sam@10gen.com>
2017-07-10 18:41:17 -0400

    EVG-1851: fix compile errror on freebsed cli

--- subprocess/subtree_freebsd.go
+++ subprocess/subtree_freebsd.go
@@ -1,12 +1,14 @@
 package subprocess
 
+import "github.com/mongodb/grip"
+
 func TrackProcess(key string, pid int, logger grip.Journaler) {
 	// trackProcess is a noop on freebsd, becasue process handling
-	isn't implemented; however we expect that we can do process
-	tracking post-facto
+	// isn't implemented; however we expect that we can do process
+	// tracking post-facto
 }
 
-func cleanup(key string, logger grip.Journaler)  error {
+func cleanup(key string, logger grip.Journaler) error {
 	logger.Alert("process cleanup is not implemented on freebsd")
 
 	return nil
