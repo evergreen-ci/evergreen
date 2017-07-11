7f97bbd7a6b878801ec307fa6c658b2f6be6f8b3
tycho garen <garen@tychoish.com>
2017-07-10 20:04:44 -0400

    EVG-1851: remove plugin dependency in subtree test

--- subprocess/subtree_test.go
+++ subprocess/subtree_test.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/evergreen-ci/evergreen/plugin/plugintest"
-	"github.com/evergreen-ci/evergreen/subprocess"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/logging"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -22,7 +22,7 @@ func TestSubtreeCleanup(t *testing.T) {
 		env = append(env, "EVR_AGENT_PID=12345")
 		env = append(env, fmt.Sprintf("EVR_TASK_ID=%v", id))
 		env = append(env, fmt.Sprintf("EVR_AGENT_PID=%v", os.Getpid()))
-		localCmd := &subprocess.LocalCommand{
+		localCmd := &LocalCommand{
 			CmdString:   "while true; do sleep 1; done; echo 'finish'",
 			Stdout:      buf,
 			Stderr:      buf,
@@ -30,10 +30,10 @@ func TestSubtreeCleanup(t *testing.T) {
 			Environment: env,
 		}
 		So(localCmd.Start(), ShouldBeNil)
-		TrackProcess(id, localCmd.Cmd.Process.Pid, &plugintest.MockLogger{})
+		TrackProcess(id, localCmd.Cmd.Process.Pid, &logging.Grip{Sender: grip.GetSender()})
 
 		Convey("running KillSpawnedProcs should kill the process before it finishes", func() {
-			So(KillSpawnedProcs(id, &plugintest.MockLogger{}), ShouldBeNil)
+			So(KillSpawnedProcs(id, &logging.Grip{Sender: grip.GetSender()}), ShouldBeNil)
 			So(localCmd.Cmd.Wait(), ShouldNotBeNil)
 			So(buf.String(), ShouldNotContainSubstring, "finish")
 		})
