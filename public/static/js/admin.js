bdba85f683f3fdd1e720b8db579f838d9082c9ac
John Liu <john.liu@mongodb.com>
2017-09-06 13:41:55 -0400

    EVG-1972 add clear button

--- public/static/js/admin.js
+++ public/static/js/admin.js
@@ -24,5 +24,10 @@ mciModule.controller('AdminSettingsController', ['$scope','$window', 'mciAdminRe
     mciAdminRestService.saveSettings($scope.Settings, { success: successHandler, error: errorHandler });
   }
 
+  $scope.clearSettings = function() {
+    $scope.Settings.banner = "";
+    $scope.saveSettings();
+  }
+
   $scope.load()
 }]);
