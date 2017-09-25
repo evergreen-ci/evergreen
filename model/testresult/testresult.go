3897518cd8bda2e4190c8d09f1322e476fff1a02
Brian Samek <bsamek@users.noreply.github.com>
2017-09-25 09:59:52 -0400

    EVG-2051 Add model for testresults collection (#154)

---
+++ model/testresult/testresult.go
@@ -0,0 +1,82 @@
+package testresult
+
+import (
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/db/bsonutil"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	collection = "testresults"
+)
+
+// TestResult contains test data for a task.
+type TestResult struct {
+	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Status    string        `json:"status" bson:"status"`
+	TestFile  string        `json:"test_file" bson:"test_file"`
+	URL       string        `json:"url" bson:"url,omitempty"`
+	URLRaw    string        `json:"url_raw" bson:"url_raw,omitempty"`
+	LogID     string        `json:"log_id,omitempty" bson:"log_id,omitempty"`
+	LineNum   int           `json:"line_num,omitempty" bson:"line_num,omitempty"`
+	ExitCode  int           `json:"exit_code" bson:"exit_code"`
+	StartTime float64       `json:"start" bson:"start"`
+	EndTime   float64       `json:"end" bson:"end"`
+
+	// Together, TaskID and Execution identify the task which created this TestResult
+	TaskID    string `bson:"task_id" json:"task_id"`
+	Execution int    `bson:"task_execution" json:"task_execution"`
+
+	// LogRaw is not persisted to the database
+	LogRaw string `json:"log_raw" bson:"log_raw,omitempty"`
+}
+
+var (
+	// BSON fields for the task struct
+	testResultStatusKey    = bsonutil.MustHaveTag(TestResult{}, "Status")
+	testResultLineNumKey   = bsonutil.MustHaveTag(TestResult{}, "LineNum")
+	testResultTestFileKey  = bsonutil.MustHaveTag(TestResult{}, "TestFile")
+	testResultURLKey       = bsonutil.MustHaveTag(TestResult{}, "URL")
+	testResultLogIDKey     = bsonutil.MustHaveTag(TestResult{}, "LogID")
+	testResultURLRawKey    = bsonutil.MustHaveTag(TestResult{}, "URLRaw")
+	testResultExitCodeKey  = bsonutil.MustHaveTag(TestResult{}, "ExitCode")
+	testResultStartTimeKey = bsonutil.MustHaveTag(TestResult{}, "StartTime")
+	testResultEndTimeKey   = bsonutil.MustHaveTag(TestResult{}, "EndTime")
+	testResultTaskIDKey    = bsonutil.MustHaveTag(TestResult{}, "TaskID")
+	testResultExecutionKey = bsonutil.MustHaveTag(TestResult{}, "Execution")
+)
+
+// Insert writes a test result to the database.
+func (t *TestResult) Insert() error {
+	return db.Insert(collection, t)
+}
+
+// ByTaskIDAndExecution creates a query to return test results from the testresults collection for a given task.
+func ByTaskIDAndExecution(taskID string, execution int) ([]TestResult, error) {
+	q := db.Query(bson.M{
+		testResultTaskIDKey:    taskID,
+		testResultExecutionKey: execution,
+	})
+	return find(q)
+}
+
+// find returns all test results that satisfy the query.
+func find(query db.Q) ([]TestResult, error) {
+	tests := []TestResult{}
+	err := db.FindAllQ(collection, query, &tests)
+	if err == mgo.ErrNotFound {
+		return nil, nil
+	}
+	return tests, err
+}
+
+// FindOne returns one test result that satisfies the query.
+func findOne(query db.Q) (*TestResult, error) {
+	test := &TestResult{}
+	err := db.FindOneQ(collection, query, &test)
+	if err == mgo.ErrNotFound {
+		return nil, nil
+	}
+	return test, err
+}
---
+++ model/testresult/testresult_test.go
@@ -0,0 +1,114 @@
+package testresult
+
+import (
+	"fmt"
+	"testing"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/testutil"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestResultSuite struct {
+	suite.Suite
+	tests []TestResult
+}
+
+func TestTestResultSuite(t *testing.T) {
+	suite.Run(t, new(TestResultSuite))
+}
+
+func (s *TestResultSuite) SetupSuite() {
+	db.SetGlobalSessionProvider(db.SessionFactoryFromConfig(testutil.TestConfig()))
+
+	s.tests = []TestResult{}
+	for i := 0; i < 5; i++ {
+		s.tests = append(s.tests, TestResult{
+			ID:        bson.NewObjectId(),
+			Status:    "pass",
+			TestFile:  fmt.Sprintf("file-%d", i),
+			URL:       fmt.Sprintf("url-%d", i),
+			URLRaw:    fmt.Sprintf("urlraw-%d", i),
+			LogID:     fmt.Sprintf("logid-%d", i),
+			LineNum:   i,
+			ExitCode:  i,
+			StartTime: float64(i),
+			EndTime:   float64(i),
+			TaskID:    fmt.Sprintf("taskid-%d", i),
+			Execution: i,
+		})
+	}
+}
+
+func (s *TestResultSuite) SetupTest() {
+	err := db.Clear(collection)
+	s.Require().NoError(err)
+
+}
+
+func (s *TestResultSuite) TestInsert() {
+
+	for _, t := range s.tests {
+		err := t.Insert()
+		s.NoError(err)
+	}
+
+	for i, t := range s.tests {
+		test, err := findOne(db.Query(bson.M{
+			"_id": t.ID,
+		}))
+		s.NoError(err)
+		s.Equal("pass", test.Status)
+		s.Equal(fmt.Sprintf("file-%d", i), test.TestFile)
+		s.Equal(fmt.Sprintf("url-%d", i), test.URL)
+		s.Equal(fmt.Sprintf("urlraw-%d", i), test.URLRaw)
+		s.Equal(fmt.Sprintf("logid-%d", i), test.LogID)
+		s.Equal(i, test.LineNum)
+		s.Equal(i, test.ExitCode)
+		s.Equal(float64(i), test.StartTime)
+		s.Equal(float64(i), test.EndTime)
+		s.Equal(fmt.Sprintf("taskid-%d", i), test.TaskID)
+		s.Equal(i, test.Execution)
+	}
+}
+
+func (s *TestResultSuite) TestByTaskIDAndExecution() {
+	for _, t := range s.tests {
+		err := t.Insert()
+		s.Require().NoError(err)
+	}
+
+	additionalTests := []TestResult{}
+	for i := 5; i < 10; i++ {
+		additionalTests = append(additionalTests, TestResult{
+			ID:        bson.NewObjectId(),
+			Status:    "pass",
+			TestFile:  fmt.Sprintf("file-%d", i),
+			URL:       fmt.Sprintf("url-%d", i),
+			URLRaw:    fmt.Sprintf("urlraw-%d", i),
+			LogID:     fmt.Sprintf("logid-%d", i),
+			LineNum:   i,
+			ExitCode:  i,
+			StartTime: float64(i),
+			EndTime:   float64(i),
+			TaskID:    "taskid-3",
+			Execution: 3,
+		})
+	}
+
+	for _, t := range additionalTests {
+		err := t.Insert()
+		s.NoError(err)
+	}
+
+	tests, err := ByTaskIDAndExecution(fmt.Sprintf("taskid-3"), 3)
+	s.NoError(err)
+	s.Len(tests, 6)
+	for _, test := range tests {
+		s.Equal("pass", test.Status)
+		s.Equal("taskid-3", test.TaskID)
+		s.Equal(3, test.Execution)
+	}
+}
