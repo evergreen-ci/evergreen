cc72ee2722ec6d56f2c2d366ee392189199139c5
Sam Kleinman <sam@10gen.com>
2017-09-27 13:15:42 -0400

    iron out logic

--- command/s3_put.go
+++ command/s3_put.go
@@ -263,17 +263,23 @@ retryLoop:
 
 				err := thirdparty.PutS3File(auth, fpath, s3URL.String(), s3pc.ContentType, s3pc.Permissions)
 				if err != nil {
-					// if we encounter a problem uploading a file, then we should:
-					//   - skip if it's a single update, not marked optional, and the local file doesn't exist
-					// retry in all other cases
+					// retry errors other than "file doesn't exist", which we handle differently based on what
+					// kind of upload it is
 					if os.IsNotExist(err) {
-						if s3pc.isMulti() || s3pc.Optional {
+						if s3pc.isMulti() {
+							// try the remaining multi uploads in the group, effectively ignoring this
+							// error.
 							continue uploadLoop
+						} else if s3pc.Optional {
+							// single optional file uploads should return early.
+							return nil
 						} else {
+							// single required uploads should return an error asap.
 							return errors.Wrapf(err, "missing file %s", fpath)
 						}
 					}
 
+					// in all other cases, log an error and retry after an interval.
 					grip.Error(err)
 					timer.Reset(backoffCounter.Duration())
 					continue retryLoop
