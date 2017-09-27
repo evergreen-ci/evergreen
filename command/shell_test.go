ece4737771e2ec86d2c28a7711c7f2692761c617
Sam Kleinman <sam@10gen.com>
2017-09-27 12:01:37 -0400

    EVG-2088: shell.exec should error if directory does not exist

--- command/shell_test.go
+++ command/shell_test.go
@@ -2,12 +2,15 @@ package command
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"runtime"
 	"testing"
 
 	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/rest/client"
+	"github.com/evergreen-ci/evergreen/testutil"
 	"github.com/evergreen-ci/evergreen/util"
 	. "github.com/smartystreets/goconvey/convey"
 	"golang.org/x/net/context"
@@ -23,7 +26,7 @@ func TestShellExecuteCommand(t *testing.T) {
 	Convey("With a shell command", t, func() {
 
 		Convey("if unset, default is determined by local command", func() {
-			cmd := &shellExec{}
+			cmd := &shellExec{WorkingDir: testutil.GetDirectoryOfFile()}
 			So(cmd.Execute(ctx, comm, logger, conf), ShouldBeNil)
 			So(cmd.Shell, ShouldEqual, "")
 		})
@@ -36,10 +39,28 @@ func TestShellExecuteCommand(t *testing.T) {
 
 		for _, sh := range shells {
 			Convey(fmt.Sprintf("when set, %s is not overwritten during execution", sh), func() {
-				cmd := &shellExec{Shell: sh}
+				cmd := &shellExec{Shell: sh, WorkingDir: testutil.GetDirectoryOfFile()}
 				So(cmd.Execute(ctx, comm, logger, conf), ShouldBeNil)
 				So(cmd.Shell, ShouldEqual, sh)
 			})
 		}
+
+		Convey("command should error if working directory is unset", func() {
+			cmd := &shellExec{}
+			So(cmd.Execute(ctx, comm, logger, conf), ShouldNotBeNil)
+		})
+
+		Convey("command should error if working directory does not exist", func() {
+			path := "foo/bar/baz"
+			_, err := os.Stat(path)
+			So(os.IsNotExist(err), ShouldBeTrue)
+			_, err = os.Stat(filepath.Join(conf.WorkDir))
+			So(os.IsNotExist(err), ShouldBeTrue)
+
+			cmd := &shellExec{WorkingDir: path}
+			So(cmd.Execute(ctx, comm, logger, conf), ShouldNotBeNil)
+
+		})
+
 	})
 }
