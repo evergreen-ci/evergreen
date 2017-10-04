15eb576c9c374c727ca012975c90f6a2cde72b17
Brian Samek <brian.samek@mongodb.com>
2017-10-04 16:55:32 -0400

    fixup

--- command/initial_setup.go
+++ command/initial_setup.go
@@ -1,14 +1,14 @@
 package command
 
 import (
+	"time"
+
 	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/rest/client"
 	"golang.org/x/net/context"
 )
 
-type initialSetup struct {
-	base
-}
+type initialSetup struct{}
 
 func initialSetupFactory() Command                                    { return &initialSetup{} }
 func (*initialSetup) Type() string                                    { return model.SystemCommandType }
@@ -16,6 +16,8 @@ func (*initialSetup) SetType(s string)                                {}
 func (*initialSetup) DisplayName() string                             { return "initial task setup" }
 func (*initialSetup) SetDisplayName(s string)                         {}
 func (*initialSetup) Name() string                                    { return "setup.initial" }
+func (*initialSetup) SetIdleTimeout(d time.Duration)                  {}
+func (*initialSetup) IdleTimeout() time.Duration                      { return 0 }
 func (*initialSetup) ParseParams(params map[string]interface{}) error { return nil }
 func (*initialSetup) Execute(ctx context.Context,
 	client client.Communicator, logger client.LoggerProducer, conf *model.TaskConfig) error {
