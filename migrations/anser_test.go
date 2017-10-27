7d41443ffd5d9d53f0fd9fa5649054351efe6eb8
John Liu <john.liu@mongodb.com>
2017-10-27 16:09:03 -0400

    EVG-2052 fix migrations test

--- migrations/anser_test.go
+++ migrations/anser_test.go
@@ -11,9 +11,9 @@ import (
 
 func TestAnserBasicPlaceholder(t *testing.T) {
 	assert := assert.New(t)
-	app, err := Application(&mock.Environment{}, "mci_test")
+	app, err := Application(mock.NewEnvironment(), "mci_test")
 	assert.NoError(err)
-	assert.Len(app.Generators, 0)
+	assert.Len(app.Generators, 2)
 	assert.False(app.DryRun)
 
 	env, err := Setup(context.Background(), "mongodb://localhost:27017")
