f2a29500d8322013eae7e03ad4ec070604473873
Brian Samek <bsamek@users.noreply.github.com>
2017-11-03 13:44:05 -0400

    EVG-2298 Unify setup/teardown on spawnhosts and builders (#345)

--- taskrunner/host_gateway.go
+++ taskrunner/host_gateway.go
@@ -143,64 +143,22 @@ func (agbh *AgentHostGateway) GetAgentRevision() (string, error) {
 	return strings.TrimSpace(string(hashBytes)), nil
 }
 
-func newCappedOutputLog() *util.CappedWriter {
-	// store up to 1MB of streamed command output to print if a command fails
-	return &util.CappedWriter{
-		Buffer:   &bytes.Buffer{},
-		MaxBytes: 1024 * 1024, // 1MB
-	}
-}
-
 // Prepare the remote machine to run a task.
 // 1. Create the directories on the remote host.
 // 2. Copy the binary to the remote host.
 // 3. Run the setup script with the binary.
 func (agbh *AgentHostGateway) prepRemoteHost(hostObj host.Host, sshOptions []string, settings *evergreen.Settings) (string, error) {
 	// copy over the correct agent binary to the remote host
-	if err := runSSHCommand("curl", hostutil.CurlCommand(settings.Ui.Url, &hostObj), sshOptions, hostObj); err != nil {
+	if err := hostutil.RunSSHCommand("curl", hostutil.CurlCommand(settings.Ui.Url, &hostObj), sshOptions, hostObj); err != nil {
 		return "", errors.Wrap(err, "error downloading agent binary on remote host")
 	}
 
 	// run the setup script with the agent
-	if err := runSSHCommand("setup", hostutil.SetupCommand(&hostObj), sshOptions, hostObj); err != nil {
+	if err := hostutil.RunSSHCommand("setup", hostutil.SetupCommand(&hostObj), sshOptions, hostObj); err != nil {
 		return "", errors.Wrap(err, "error running setup script on remote host")
 	}
 
 	return agbh.GetAgentRevision()
-}
-
-func runSSHCommand(id, cmd string, sshOptions []string, host host.Host) error {
-	// compute any info necessary to ssh into the host
-	hostInfo, err := util.ParseSSHInfo(host.Host)
-	if err != nil {
-		return errors.Wrapf(err, "error parsing ssh info %v", host.Host)
-	}
-
-	output := newCappedOutputLog()
-	shellCmd := &subprocess.RemoteCommand{
-		Id:             fmt.Sprintf("%s-%s-%d", id, host.Id, rand.Int()),
-		CmdString:      cmd,
-		Stdout:         output,
-		Stderr:         output,
-		RemoteHostName: hostInfo.Hostname,
-		User:           host.User,
-		Options:        append([]string{"-p", hostInfo.Port}, sshOptions...),
-	}
-	grip.Info(message.Fields{
-		"command": shellCmd,
-		"host_id": host.Id,
-		"message": "running command over ssh",
-	})
-
-	ctx, cancel := context.WithTimeout(context.Background(), SSHTimeout)
-	defer cancel()
-	err = shellCmd.Run(ctx)
-
-	grip.Notice(shellCmd.Stop())
-	if err != nil {
-		return errors.Errorf("error running shell cmd: %s (%v)", output.String(), err)
-	}
-	return nil
 }
 
 const logAggregationEnabled = false
