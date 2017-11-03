e7690c059305b94105afa22cba8dd369ffd9bd32
John Liu <john.liu@mongodb.com>
2017-11-03 15:13:32 -0400

    EVG-2284 add logging for scheduler queue consistency (#343)

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -148,6 +148,25 @@ func (s *Scheduler) Schedule(ctx context.Context) error {
 					"span":     time.Since(distroStartTime).String(),
 					"duration": time.Since(distroStartTime),
 				})
+				if len(d.runnableTasksForDistro) != len(res.taskQueueItem) {
+					delta := make(map[string]string)
+					for _, t := range res.taskQueueItem {
+						delta[t.Id] = "res.taskQueueItem"
+					}
+					for _, i := range d.runnableTasksForDistro {
+						if delta[i.Id] == "res.taskQueueItem" {
+							delete(delta, i.Id)
+						} else {
+							delta[i.Id] = "d.runnableTasksForDistro"
+						}
+					}
+					grip.Alert(message.Fields{
+						"runner":             RunnerName,
+						"distro":             d.distroId,
+						"message":            "inconsistency with scheduler input and output",
+						"inconsistent_tasks": delta,
+					})
+				}
 			}
 		}()
 	}
