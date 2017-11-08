914f3c0d2e2102f7df5d7e19cf480722d7ab9756
Brian Samek <brian.samek@mongodb.com>
2017-11-08 15:17:33 -0500

    EVG-2242 Create patch intent model

---
+++ model/patch/github.go
@@ -0,0 +1,131 @@
+package patch
+
+import (
+	"strings"
+
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/mongodb/anser/bsonutil"
+	"github.com/pkg/errors"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	// IntentCollection is the database collection that stores patch intents.
+	IntentCollection = "patch_intent"
+
+	// GithubIntentType represents patch intents created for GitHub.
+	GithubIntentType = "github"
+)
+
+// Intent represents an intent to create a patch build and is processed by an amboy queue.
+type Intent interface {
+	// Insert inserts a patch intent in the database.
+	Insert() error
+
+	// SetProcessed should be called by an amboy queue after creating a patch from an intent.
+	SetProcessed() error
+
+	// IsProcessed returns whether a patch exists for this intent.
+	IsProcessed() bool
+
+	// GetType returns the patch intent, e.g., GithubType.
+	GetType() string
+}
+
+// githubIntent represents an intent to create a patch build as a result of a
+// PullRequestEvent webhook. These intents are processed asynchronously by an
+// amboy queue.
+type GithubIntent struct {
+	// ID is created by the driver and has no special meaning to the application.
+	ID bson.ObjectId `bson:"_id"`
+
+	// PRNumber is the PR number for the project in GitHub.
+	PRNumber int `bson:"pr_number"`
+
+	// HeadSHA is the base SHA of the patch.
+	HeadSHA string `bson:"head_sha"`
+
+	// URL is the URL of the patch in GitHub.
+	URL string `bson:"url"`
+
+	// Processed indicates whether a patch intent has been processed by the amboy queue.
+	Processed bool `bson:"processed"`
+
+	// IntentType indicates the type of the patch intent, i.e., GithubIntentType
+	IntentType string `bson:"intent_type"`
+}
+
+// BSON fields for the patches
+var (
+	IDKey         = bsonutil.MustHaveTag(GithubIntent{}, "ID")
+	PRNumberKey   = bsonutil.MustHaveTag(GithubIntent{}, "PRNumber")
+	HeadSHAKey    = bsonutil.MustHaveTag(GithubIntent{}, "HeadSHA")
+	URLKey        = bsonutil.MustHaveTag(GithubIntent{}, "URL")
+	ProcessedKey  = bsonutil.MustHaveTag(GithubIntent{}, "Processed")
+	IntentTypeKey = bsonutil.MustHaveTag(GithubIntent{}, "IntentType")
+)
+
+// NewGithubIntent return a new github patch intent.
+func NewGithubIntent(pr int, sha string, url string) (GithubIntent, error) {
+	g := GithubIntent{}
+	if pr == 0 {
+		return g, errors.New("PR number must not be 0")
+	}
+	if len(sha) != 40 {
+		return g, errors.New("Base SHA must be 40 characters long")
+	}
+	if !strings.HasPrefix(url, "http") {
+		return g, errors.Errorf("URL does not appear valid (%s)", g.URL)
+	}
+
+	g.PRNumber = pr
+	g.HeadSHA = sha
+	g.URL = url
+	g.IntentType = GithubIntentType
+	g.ID = bson.NewObjectId()
+
+	return g, nil
+}
+
+// SetProcessed should be called by an amboy queue after creating a patch from an intent.
+func (g GithubIntent) SetProcessed() error {
+	g.Processed = true
+	return updateOneIntent(
+		bson.M{IDKey: g.ID},
+		bson.M{"$set": bson.M{ProcessedKey: g.Processed}},
+	)
+}
+
+// updateOne updates one patch intent.
+func updateOneIntent(query interface{}, update interface{}) error {
+	return db.Update(
+		IntentCollection,
+		query,
+		update,
+	)
+}
+
+// IsProcessed returns whether a patch exists for this intent.
+func (g GithubIntent) IsProcessed() bool {
+	return g.Processed
+}
+
+// GetType returns the patch intent, e.g., GithubIntentType.
+func (g GithubIntent) GetType() string {
+	return g.IntentType
+}
+
+// Insert inserts a patch intent in the database.
+func (g GithubIntent) Insert() error {
+	return db.Insert(IntentCollection, g)
+}
+
+// FindUnprocessedGithubIntents finds all patch intents that have not yet been processed.
+func FindUnprocessedGithubIntents() ([]GithubIntent, error) {
+	var intents []GithubIntent
+	err := db.FindAllQ(IntentCollection, db.Query(bson.M{ProcessedKey: false}), &intents)
+	if err != nil {
+		return []GithubIntent{}, err
+	}
+	return intents, nil
+}
---
+++ model/patch/github_test.go
@@ -0,0 +1,120 @@
+package patch
+
+import (
+	"testing"
+
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/testutil"
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type GithubSuite struct {
+	suite.Suite
+	pr  int
+	sha string
+	url string
+}
+
+func TestGithubSuite(t *testing.T) {
+	suite.Run(t, new(GithubSuite))
+}
+
+func (s *GithubSuite) SetupSuite() {
+	db.SetGlobalSessionProvider(testutil.TestConfig().SessionFactory())
+	s.pr = 5
+	s.sha = "67da19930b1b18d346477e99a8e18094a672f48a"
+	s.url = "http://www.example.com"
+}
+
+func (s *GithubSuite) SetupTest() {
+	s.Require().NoError(db.Clear(IntentCollection))
+}
+
+func (s *GithubSuite) TestNewGithubIntent() {
+	intent, err := NewGithubIntent(0, s.sha, s.url)
+	s.Equal(GithubIntent{}, intent)
+	s.Error(err)
+
+	intent, err = NewGithubIntent(s.pr, "12345", s.url)
+	s.Equal(GithubIntent{}, intent)
+	s.Error(err)
+
+	intent, err = NewGithubIntent(s.pr, s.sha, "foo")
+	s.Equal(GithubIntent{}, intent)
+	s.Error(err)
+
+	intent, err = NewGithubIntent(s.pr, s.sha, s.url)
+	s.Implements((*Intent)(nil), intent)
+	s.Equal(s.pr, intent.PRNumber)
+	s.Equal(s.sha, intent.HeadSHA)
+	s.Equal(s.url, intent.URL)
+	s.False(intent.IsProcessed())
+	s.Equal(GithubIntentType, intent.GetType())
+	s.NoError(err)
+}
+
+func (s *GithubSuite) TestInsert() {
+	intent, err := NewGithubIntent(s.pr, s.sha, s.url)
+	s.NoError(err)
+	s.NoError(intent.Insert())
+
+	intents, err := FindUnprocessedGithubIntents()
+	s.NoError(err)
+	s.Len(intents, 1)
+
+	found := intents[0]
+	s.Equal(s.pr, found.PRNumber)
+	s.Equal(s.sha, found.HeadSHA)
+	s.Equal(s.url, found.URL)
+	s.False(found.IsProcessed())
+	s.Equal(GithubIntentType, found.GetType())
+}
+
+func (s *GithubSuite) TestSetProcessed() {
+	intent, err := NewGithubIntent(s.pr, s.sha, s.url)
+	s.NoError(err)
+	s.NoError(intent.Insert())
+	s.NoError(intent.SetProcessed())
+
+	found, err := FindUnprocessedGithubIntents()
+	s.NoError(err)
+	s.Len(found, 0)
+
+	var intents []GithubIntent
+	s.NoError(db.FindAllQ(IntentCollection, db.Query(bson.M{ProcessedKey: true}), &intents))
+	s.Len(intents, 1)
+	s.Equal(s.pr, intents[0].PRNumber)
+	s.Equal(s.sha, intents[0].HeadSHA)
+	s.Equal(s.url, intents[0].URL)
+	s.True(intents[0].IsProcessed())
+	s.Equal(GithubIntentType, intents[0].GetType())
+}
+
+func (s *GithubSuite) FindUnprocessedGithubIntents() {
+	intents := []GithubIntent{
+		GithubIntent{
+			Processed: true,
+		},
+		GithubIntent{
+			Processed: true,
+		},
+		GithubIntent{
+			Processed: true,
+		},
+		GithubIntent{
+			Processed: true,
+		},
+		GithubIntent{},
+		GithubIntent{},
+		GithubIntent{},
+	}
+
+	for _, intent := range intents {
+		s.NoError(intent.Insert())
+	}
+
+	found, err := FindUnprocessedGithubIntents()
+	s.NoError(err)
+	s.Len(found, 3)
+}
