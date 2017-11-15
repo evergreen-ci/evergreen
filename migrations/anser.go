e2de02f08407555247bf6c2a3b4bfcb5a8e71472
Sam Kleinman <sam@10gen.com>
2017-11-15 10:24:05 -0500

    EVG-2337: parameterize the migration entry point

--- migrations/anser.go
+++ migrations/anser.go
@@ -1,24 +1,68 @@
 package migrations
 
 import (
+	"context"
+	"time"
+
+	"github.com/mongodb/amboy/pool"
+	"github.com/mongodb/amboy/queue"
 	"github.com/mongodb/anser"
 	"github.com/pkg/errors"
 )
+
+type Options struct {
+	Limit    int
+	Target   int
+	Workers  int
+	Period   time.Duration
+	Database string
+	URI      string
+}
+
+// Setup configures the migration environment, configuring the backing
+// queue and a database session.
+func (opts Options) Setup(ctx context.Context) (anser.Environment, error) {
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithCancel(ctx)
+
+	env := anser.GetEnvironment()
+	env.RegisterCloser(func() error { cancel(); return nil })
+
+	q := queue.NewAdaptiveOrderedLocalQueue(1)
+	runner, err := pool.NewMovingAverageRateLimitedWorkers(opts.Workers, opts.Target, opts.Period, q)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err = q.SetRunner(runner); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err = q.Start(ctx); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err = env.Setup(q, opts.URI); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return env, nil
+}
 
 // Application is where the migrations are registered and defined,
 // before being handed off to another calling environment for
 // execution. See the anser documentation and the
 // anser/example_test.go for an example.
-func Application(env anser.Environment, db string, limit int) (*anser.Application, error) {
+func (opts Options) Application(env anser.Environment) (*anser.Application, error) {
 	app := &anser.Application{
-		Limit: limit,
+		Limit: opts.Limit,
 	}
 
 	if err := registerTestResultsMigrationOperations(env); err != nil {
 		return nil, errors.Wrap(err, "error registering test results migration operations")
 	}
 
-	app.Generators = append(app.Generators, testResultsGeneratorFactory(env, db, limit)...)
+	app.Generators = append(app.Generators, testResultsGeneratorFactory(env, opts.Database, opts.Limit)...)
 
 	if err := app.Setup(env); err != nil {
 		return nil, errors.WithStack(err)
