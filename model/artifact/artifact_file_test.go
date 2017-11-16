afe5071dc0853d74df68843737a54a266f264a0f
Richard Samuels <richard.samuels@mongodb.com>
2017-11-16 15:07:57 -0500

    test added

--- model/artifact/artifact_file_test.go
+++ model/artifact/artifact_file_test.go
@@ -14,7 +14,7 @@ func init() {
 }
 
 type TestArtifactFileSuite struct {
-	testEntry Entry
+	testEntries []Entry
 	suite.Suite
 }
 
@@ -25,18 +25,51 @@ func TestArtifactFile(t *testing.T) {
 
 func (s *TestArtifactFileSuite) SetupTest() {
 	s.NoError(db.Clear(Collection))
-	s.testEntry = Entry{
-		TaskId:          "task1",
-		TaskDisplayName: "Task One",
-		BuildId:         "build1",
-		Files: []File{
-			{"cat_pix", "http://placekitten.com/800/600", ""},
-			{"fast_download", "https://fastdl.mongodb.org", ""},
+	s.testEntries = []Entry{
+		{
+			TaskId:          "task1",
+			TaskDisplayName: "Task One",
+			BuildId:         "build1",
+			Files: []File{
+				{"cat_pix", "http://placekitten.com/800/600", ""},
+				{"fast_download", "https://fastdl.mongodb.org", ""},
+			},
+			Execution: 1,
 		},
-		Execution: 1,
+		{
+			TaskId:          "task2",
+			TaskDisplayName: "Task Two",
+			BuildId:         "build2",
+			Files: []File{
+				{"other", "http://example.com/other", ""},
+			},
+			Execution: 5,
+		},
 	}
 
-	s.NoError(s.testEntry.Upsert())
+	// hack to insert an entry without execution number (representative of
+	// existing data)
+	s.NoError(db.Insert(Collection, struct {
+		TaskId          string `json:"task" bson:"task"`
+		TaskDisplayName string `json:"task_name" bson:"task_name"`
+		BuildId         string `json:"build" bson:"build"`
+		Files           []File `json:"files" bson:"files"`
+	}{
+		TaskId:          "task2",
+		TaskDisplayName: "Task Two",
+		BuildId:         "build2",
+		Files: []File{
+			{"other", "http://example.com/other", ""},
+		},
+	}))
+
+	for _, entry := range s.testEntries {
+		s.NoError(entry.Upsert())
+	}
+
+	count, err := db.Count(Collection, bson.M{})
+	s.NoError(err)
+	s.Equal(3, count)
 }
 
 func (s *TestArtifactFileSuite) TestArtifactFieldsArePresent() {
@@ -55,15 +88,15 @@ func (s *TestArtifactFileSuite) TestArtifactFieldsArePresent() {
 }
 
 func (s *TestArtifactFileSuite) TestArtifactFieldsAfterUpdate() {
-	s.testEntry.Files = []File{
+	s.testEntries[0].Files = []File{
 		{"cat_pix", "http://placekitten.com/300/400", ""},
 		{"the_value_of_four", "4", ""},
 	}
-	s.NoError(s.testEntry.Upsert())
+	s.NoError(s.testEntries[0].Upsert())
 
 	count, err := db.Count(Collection, bson.M{})
 	s.NoError(err)
-	s.Equal(1, count)
+	s.Equal(3, count)
 
 	entryFromDb, err := FindOne(ByTaskId("task1"))
 	s.NoError(err)
@@ -82,4 +115,38 @@ func (s *TestArtifactFileSuite) TestArtifactFieldsAfterUpdate() {
 	s.Equal("the_value_of_four", entryFromDb.Files[3].Name)
 	s.Equal("4", entryFromDb.Files[3].Link)
 	s.Equal(1, entryFromDb.Execution)
+}
+
+func (s *TestArtifactFileSuite) TestFindByTaskIdAndExecution() {
+	entries, err := FindAll(ByTaskIdAndExecution("task1", 1))
+	s.Len(entries, 1)
+	s.NoError(err)
+	s.NotNil(entries[0])
+	s.Equal(1, entries[0].Execution)
+	s.Equal("task1", entries[0].TaskId)
+
+	entries, err = FindAll(ByTaskIdAndExecution("task2", 0))
+	s.Len(entries, 0)
+	s.NoError(err)
+	s.Empty(entries)
+
+	entries, err = FindAll(ByTaskIdAndExecution("task2", 5))
+	s.Len(entries, 1)
+	s.NoError(err)
+	s.NotNil(entries[0])
+	s.Equal(5, entries[0].Execution)
+	s.Equal("task2", entries[0].TaskId)
+}
+
+func (s *TestArtifactFileSuite) TestFindByTaskIdWithoutExecution() {
+	entries, err := FindAll(ByTaskIdWithoutExecution("task1"))
+	s.Len(entries, 0)
+	s.NoError(err)
+
+	entries, err = FindAll(ByTaskIdWithoutExecution("task2"))
+	s.Len(entries, 1)
+	s.NoError(err)
+	s.NotNil(entries[0])
+	s.Equal(0, entries[0].Execution)
+	s.Equal("task2", entries[0].TaskId)
 }
