060625b6f721a60b662f7f1c28817a9ba2f5dd59
Sam Kleinman <sam@10gen.com>
2017-12-05 16:33:47 -0500

    EVG-2372: use pooled http client in agent

--- rest/client/client.go
+++ rest/client/client.go
@@ -2,12 +2,12 @@ package client
 
 import (
 	"context"
-	"net"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/evergreen-ci/evergreen/apimodels"
+	"github.com/evergreen-ci/evergreen/util"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/logging"
 	"github.com/mongodb/grip/send"
@@ -57,18 +57,8 @@ func NewCommunicator(serverURL string) Communicator {
 		timeoutMax:   defaultTimeoutMax,
 		serverURL:    serverURL,
 	}
-	evergreen.httpClient = &http.Client{
-		Transport: &http.Transport{
-			Proxy:             http.ProxyFromEnvironment,
-			DisableKeepAlives: true,
-			Dial: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).Dial,
-			TLSHandshakeTimeout: 10 * time.Second,
-		},
-		Timeout: heartbeatTimeout,
-	}
+	evergreen.httpClient = util.GetHttpClient()
+
 	return evergreen
 }
 
