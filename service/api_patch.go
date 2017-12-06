909ee61d2bfb907a6596e3e2c9d2bfba629b6085
Richard Samuels <richard.samuels@mongodb.com>
2017-12-06 12:05:43 -0500

    change service to use patch intents

--- service/api_patch.go
+++ service/api_patch.go
@@ -6,15 +6,15 @@ import (
 	"strings"
 	"time"
 
-	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/model/patch"
-	"github.com/evergreen-ci/evergreen/model/user"
 	"github.com/evergreen-ci/evergreen/thirdparty"
+	"github.com/evergreen-ci/evergreen/units"
 	"github.com/evergreen-ci/evergreen/util"
 	"github.com/evergreen-ci/evergreen/validator"
 	"github.com/gorilla/mux"
+	"github.com/mongodb/amboy"
 	"github.com/pkg/errors"
 	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/yaml.v2"
@@ -29,173 +29,23 @@ type PatchAPIResponse struct {
 	Patch   *patch.Patch `json:"patch"`
 }
 
-// PatchAPIRequest in the input struct with which we process patch requests
-type PatchAPIRequest struct {
-	ProjectId     string
-	ModuleName    string
-	Githash       string
-	PatchContent  string
-	BuildVariants []string
-	Tasks         []string
-	Description   string
-}
-
-// CreatePatch checks an API request to see if it is safe and sane.
-// Returns the relevant patch metadata, the patch document, and any errors that occur.
-func (pr *PatchAPIRequest) CreatePatch(finalize bool, githubOauthToken string, dbUser *user.DBUser) (*patch.Patch, error) {
-
-	projectRef, err := model.FindOneProjectRef(pr.ProjectId)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Could not find project ref %v", pr.ProjectId)
-	}
-	if projectRef == nil {
-		return nil, errors.Errorf("Could not find project : %s", pr.ProjectId)
-	}
-
-	if !projectRef.Enabled {
-		return nil, errors.Errorf("project %v is disabled", projectRef.Identifier)
-	}
-
-	if len(pr.Githash) == 0 {
-		return nil, errors.New("invalid githash")
-	}
-
-	gitCommit, err := thirdparty.GetCommitEvent(githubOauthToken, projectRef.Owner, projectRef.Repo, pr.Githash)
-	if err != nil {
-		return nil, errors.Errorf("could not find base revision %v for project %v: %v",
-			pr.Githash, projectRef.Identifier, err)
-
-	}
-	if gitCommit == nil {
-		return nil, errors.Errorf("commit hash %v doesn't seem to exist", pr.Githash)
-	}
-
-	summaries, err := thirdparty.GetPatchSummaries(pr.PatchContent)
-	if err != nil {
-		return nil, err
-	}
-
-	if finalize && (len(pr.BuildVariants) == 0 || pr.BuildVariants[0] == "") {
-		return nil, errors.New("no buildvariants specified")
-	}
-
-	createTime := time.Now()
-
-	// create a new object ID to use as reference for the patch data
-	patchFileId := bson.NewObjectId().Hex()
-	patchDoc := &patch.Patch{
-		Id:            bson.NewObjectId(),
-		Description:   pr.Description,
-		Author:        dbUser.Id,
-		Project:       pr.ProjectId,
-		Githash:       pr.Githash,
-		CreateTime:    createTime,
-		Status:        evergreen.PatchCreated,
-		BuildVariants: pr.BuildVariants,
-		Tasks:         pr.Tasks,
-		Patches: []patch.ModulePatch{
-			{
-				ModuleName: "",
-				Githash:    pr.Githash,
-				PatchSet: patch.PatchSet{
-					Patch:       pr.PatchContent,
-					PatchFileId: patchFileId,
-					Summary:     summaries,
-				},
-			},
-		},
-	}
-
-	// Get and validate patched config and add it to the patch document
-	project, err := validator.GetPatchedProject(patchDoc, githubOauthToken)
-	if err != nil {
-		return nil, errors.Wrap(err, "invalid patched config")
-	}
-
-	if pr.ModuleName != "" {
-		// is there a module? validate it.
-		var module *model.Module
-		module, err = project.GetModuleByName(pr.ModuleName)
-		if err != nil {
-			return nil, errors.Wrapf(err, "could not find module %v", pr.ModuleName)
-		}
-		if module == nil {
-			return nil, errors.Errorf("no module named %v", pr.ModuleName)
-		}
-	}
-
-	// verify that all variants exists
-	for _, buildVariant := range pr.BuildVariants {
-		if buildVariant == "all" || buildVariant == "" {
-			continue
-		}
-		bv := project.FindBuildVariant(buildVariant)
-		if bv == nil {
-			return nil, errors.Errorf("No such buildvariant: %v", buildVariant)
-		}
-	}
-
-	// write the patch content into a GridFS file under a new ObjectId after validating.
-	err = db.WriteGridFile(patch.GridFSPrefix, patchFileId, strings.NewReader(pr.PatchContent))
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to write patch file to db")
-	}
-
-	// add the project config
-	projectYamlBytes, err := yaml.Marshal(project)
-	if err != nil {
-		return nil, errors.Wrap(err, "error marshaling patched config")
-	}
-
-	// set the patch number based on patch author
-	patchDoc.PatchNumber, err = dbUser.IncPatchNumber()
-	if err != nil {
-		return nil, errors.Wrap(err, "error computing patch num")
-	}
-	patchDoc.PatchedConfig = string(projectYamlBytes)
-
-	patchDoc.ClearPatchData()
-
-	model.ExpandTasksAndVariants(patchDoc, project)
-
-	var pairs []model.TVPair
-	for _, v := range patchDoc.BuildVariants {
-		for _, t := range patchDoc.Tasks {
-			if project.FindTaskForVariant(t, v) != nil {
-				pairs = append(pairs, model.TVPair{v, t})
-			}
-		}
-	}
-
-	// update variant and tasks to include dependencies
-	pairs = model.IncludePatchDependencies(project, pairs)
-
-	patchDoc.SyncVariantsTasks(model.TVPairsToVariantTasks(pairs))
-
-	return patchDoc, nil
-}
-
 // submitPatch creates the Patch document, adds the patched project config to it,
 // and saves the patches to GridFS to be retrieved
 func (as *APIServer) submitPatch(w http.ResponseWriter, r *http.Request) {
 	dbUser := MustHaveUser(r)
-	var apiRequest PatchAPIRequest
-	var finalize bool
+	var intent patch.Intent
 	if r.Header.Get("Content-Type") == formMimeType {
 		patchContent := r.FormValue("patch")
-		if patchContent == "" {
-			as.LoggedError(w, r, http.StatusBadRequest, errors.New("Error: Patch must not be empty"))
+		variants := strings.Split(r.FormValue("buildvariants"), ",")
+		finalize := strings.ToLower(r.FormValue("finalize")) == "true"
+
+		var err error
+		intent, err = patch.NewCliIntent(dbUser.Id, r.FormValue("project"), r.FormValue("githash"), r.FormValue("module"), patchContent, r.FormValue("desc"), finalize, variants, nil)
+		if err != nil {
+			as.LoggedError(w, r, http.StatusBadRequest, err)
 			return
 		}
-		apiRequest = PatchAPIRequest{
-			ProjectId:     r.FormValue("project"),
-			ModuleName:    r.FormValue("module"),
-			Githash:       r.FormValue("githash"),
-			PatchContent:  patchContent,
-			BuildVariants: strings.Split(r.FormValue("buildvariants"), ","),
-			Description:   r.FormValue("desc"),
-		}
-		finalize = strings.ToLower(r.FormValue("finalize")) == "true"
+
 	} else {
 		data := struct {
 			Description string   `json:"desc"`
@@ -210,38 +60,38 @@ func (as *APIServer) submitPatch(w http.ResponseWriter, r *http.Request) {
 			as.LoggedError(w, r, http.StatusBadRequest, err)
 			return
 		}
-		if len(data.Patch) > patch.SizeLimit {
-			as.LoggedError(w, r, http.StatusBadRequest, errors.New("Patch is too large."))
-		}
-		finalize = data.Finalize
+		finalize := data.Finalize
+		variants := strings.Split(data.Variants, ",")
 
-		apiRequest = PatchAPIRequest{
-			ProjectId:     data.Project,
-			ModuleName:    r.FormValue("module"),
-			Githash:       data.Githash,
-			PatchContent:  data.Patch,
-			BuildVariants: strings.Split(data.Variants, ","),
-			Tasks:         data.Tasks,
-			Description:   data.Description,
-		}
-	}
-
-	patchDoc, err := apiRequest.CreatePatch(finalize, as.Settings.Credentials["github"], dbUser)
-	if err != nil {
-		as.LoggedError(w, r, http.StatusBadRequest, errors.Wrap(err, "Invalid patch"))
-		return
-	}
-
-	if err = patchDoc.Insert(); err != nil {
-		as.LoggedError(w, r, http.StatusInternalServerError, errors.Wrap(err, "error inserting patch"))
-		return
-	}
-
-	if finalize {
-		if _, err = model.FinalizePatch(patchDoc, as.Settings.Credentials["github"]); err != nil {
-			as.LoggedError(w, r, http.StatusInternalServerError, err)
+		var err error
+		intent, err = patch.NewCliIntent(dbUser.Id, data.Project, data.Githash, r.FormValue("module"), data.Patch, data.Description, finalize, variants, data.Tasks)
+		if err != nil {
+			as.LoggedError(w, r, http.StatusBadRequest, err)
 			return
 		}
+	}
+
+	if err := intent.Insert(); err != nil {
+		as.LoggedError(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	patchID := bson.NewObjectId()
+	job := units.NewPatchIntentProcessor(patchID, intent)
+	as.queue.Put(job)
+	if !amboy.WaitJobInterval(job, as.queue, 10*time.Second) {
+		as.WriteJSON(w, http.StatusInternalServerError, errors.New("patch creation timed out"))
+		return
+	}
+
+	patchDoc, err := patch.FindOne(patch.ById(patchID))
+	if err != nil {
+		as.WriteJSON(w, http.StatusInternalServerError, errors.New("can't fetch patch data"))
+		return
+	}
+	if patchDoc == nil {
+		as.WriteJSON(w, http.StatusInternalServerError, errors.New("patch couldn't be found"))
+		return
 	}
 
 	as.WriteJSON(w, http.StatusCreated, PatchAPIResponse{Patch: patchDoc})
