9bebb15dd4e75b4071762725ae590246267bc9eb
tycho garen <garen@tychoish.com>
2017-12-29 11:39:49 -0500

    EVG-2460: bump cli version

--- config.go
+++ config.go
@@ -19,7 +19,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2017-12-20"
+	ClientVersion = "2017-12-29"
 )
 
 // AuthUser configures a user for our Naive authentication setup.
