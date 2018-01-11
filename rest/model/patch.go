517d8a885edee5fe227960de537315498bd6ba5a
Richard Samuels <richard.samuels@mongodb.com>
2018-01-11 14:50:02 -0500

    add github patch data

--- rest/model/patch.go
+++ rest/model/patch.go
@@ -9,23 +9,24 @@ import (
 
 // APIPatch is the model to be returned by the API whenever patches are fetched.
 type APIPatch struct {
-	Id            APIString     `json:"patch_id"`
-	Description   APIString     `json:"description"`
-	ProjectId     APIString     `json:"project_id"`
-	Branch        APIString     `json:"branch"`
-	Githash       APIString     `json:"git_hash"`
-	PatchNumber   int           `json:"patch_number"`
-	Author        APIString     `json:"author"`
-	Status        APIString     `json:"status"`
-	CreateTime    APITime       `json:"create_time"`
-	StartTime     APITime       `json:"start_time"`
-	FinishTime    APITime       `json:"finish_time"`
-	Variants      []APIString   `json:"builds"`
-	Tasks         []APIString   `json:"tasks"`
-	VariantsTasks []variantTask `json:"variants_tasks"`
-	Activated     bool          `json:"activated"`
+	Id              APIString     `json:"patch_id"`
+	Description     APIString     `json:"description"`
+	ProjectId       APIString     `json:"project_id"`
+	Branch          APIString     `json:"branch"`
+	Githash         APIString     `json:"git_hash"`
+	PatchNumber     int           `json:"patch_number"`
+	Author          APIString     `json:"author"`
+	Version         APIString     `json:"version"`
+	Status          APIString     `json:"status"`
+	CreateTime      APITime       `json:"create_time"`
+	StartTime       APITime       `json:"start_time"`
+	FinishTime      APITime       `json:"finish_time"`
+	Variants        []APIString   `json:"builds"`
+	Tasks           []APIString   `json:"tasks"`
+	VariantsTasks   []variantTask `json:"variants_tasks"`
+	Activated       bool          `json:"activated"`
+	GithubPatchData githubPatch   `json:"github_patch_data,omitempty"`
 }
-
 type variantTask struct {
 	Name  APIString   `json:"name"`
 	Tasks []APIString `json:"tasks"`
@@ -44,6 +45,7 @@ func (apiPatch *APIPatch) BuildFromService(h interface{}) error {
 	apiPatch.Githash = APIString(v.Githash)
 	apiPatch.PatchNumber = v.PatchNumber
 	apiPatch.Author = APIString(v.Author)
+	apiPatch.Version = APIString(v.Version)
 	apiPatch.Status = APIString(v.Status)
 	apiPatch.CreateTime = NewTime(v.CreateTime)
 	apiPatch.StartTime = NewTime(v.CreateTime)
@@ -71,10 +73,45 @@ func (apiPatch *APIPatch) BuildFromService(h interface{}) error {
 	}
 	apiPatch.VariantsTasks = variantTasks
 	apiPatch.Activated = v.Activated
-	return nil
+	apiPatch.GithubPatchData = githubPatch{}
+	err := apiPatch.GithubPatchData.BuildFromService(v.GithubPatchData)
+	return err
 }
 
 // ToService converts a service layer patch using the data from APIPatch
 func (apiPatch *APIPatch) ToService() (interface{}, error) {
 	return nil, errors.New("not implemented for read-only route")
+}
+
+type githubPatch struct {
+	PRNumber  int       `json:"pr_number"`
+	BaseOwner APIString `json:"base_owner"`
+	BaseRepo  APIString `json:"base_repo"`
+	HeadOwner APIString `json:"head_owner"`
+	HeadRepo  APIString `json:"head_repo"`
+	HeadHash  APIString `json:"head_hash"`
+	Author    APIString `json:"author"`
+	DiffURL   APIString `json:"diff_url"`
+}
+
+// BuildFromService converts from service level structs to an APIPatch
+func (g *githubPatch) BuildFromService(h interface{}) error {
+	v, ok := h.(patch.GithubPatch)
+	if !ok {
+		return fmt.Errorf("incorrect type when fetching converting github patch type")
+	}
+	g.PRNumber = v.PRNumber
+	g.BaseOwner = APIString(v.BaseOwner)
+	g.BaseRepo = APIString(v.BaseRepo)
+	g.HeadOwner = APIString(v.HeadOwner)
+	g.HeadRepo = APIString(v.HeadRepo)
+	g.HeadHash = APIString(v.HeadHash)
+	g.Author = APIString(v.Author)
+	g.DiffURL = APIString(v.DiffURL)
+	return nil
+}
+
+// ToService converts a service layer patch using the data from APIPatch
+func (g *githubPatch) ToService() (interface{}, error) {
+	return nil, errors.New("(*githubPatch) ToService not implemented for read-only route")
 }
