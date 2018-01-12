f2c010faa5ddb3ef2dea713bf448eed7fccc50ad
Richard Samuels <richard.samuels@mongodb.com>
2018-01-12 16:25:20 -0500

    route

---
+++ rest/route/cli_update_routes.go
@@ -0,0 +1,37 @@
+package route
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/evergreen-ci/evergreen/rest/data"
+)
+
+type cliVersion struct {
+}
+
+func getCLIVersionRouteManager(route string, version int) *RouteManager {
+	return &RouteManager{
+		Route: route,
+		Methods: []MethodHandler{
+			MethodHandler{
+				Authenticator:  &NoAuthAuthenticator{},
+				RequestHandler: &cliVersion{},
+				MethodType:     http.MethodGet,
+			},
+		},
+		Version: version,
+	}
+}
+
+func (gh *cliVersion) Handler() RequestHandler {
+	return &cliVersion{}
+}
+
+func (gh *cliVersion) ParseAndValidate(ctx context.Context, r *http.Request) error {
+	return nil
+}
+
+func (gh *cliVersion) Execute(ctx context.Context, sc data.Connector) (ResponseData, error) {
+	return ResponseData{}, nil
+}
