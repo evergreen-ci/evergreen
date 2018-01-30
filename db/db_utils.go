e235f30f44209d23f79a8bd76e29928d9d97b967
tycho garen <garen@tychoish.com>
2018-01-30 12:23:43 -0500

    EVG-2673: panic if query is wrong for update all

--- db/db_utils.go
+++ db/db_utils.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 
 	"gopkg.in/mgo.v2"
@@ -170,8 +171,30 @@ func UpdateId(collection string, id, update interface{}) error {
 }
 
 // UpdateAll updates all matching documents in the collection.
-func UpdateAll(collection string, query interface{},
-	update interface{}) (*mgo.ChangeInfo, error) {
+func UpdateAll(collection string, query interface{}, update interface{}) (*mgo.ChangeInfo, error) {
+	switch q := query.(type) {
+	case *Q, Q:
+		grip.EmergencyPanic(message.Fields{
+			"message":    "invalid query passed to update all",
+			"cause":      "programmer error",
+			"query":      query,
+			"collection": collection,
+		})
+	case nil:
+		grip.EmergencyPanic(message.Fields{
+			"message":    "nil query passed to update all",
+			"query":      query,
+			"collection": collection,
+		})
+	case map[string]interface{}:
+		if len(q) == 0 {
+			grip.EmergencyPanic(message.Fields{
+				"message":    "empty query passed to update all",
+				"collection": collection,
+				"type":       fmt.Sprintf("%T", query),
+			})
+		}
+	}
 
 	session, db, err := GetGlobalSessionFactory().GetSession()
 	if err != nil {
