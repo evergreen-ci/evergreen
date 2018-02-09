5f34ed9e4cbf7c3a54332c62aea1ef0401c1d480
John Liu <john.liu@mongodb.com>
2018-02-09 16:29:49 -0500

    add mock config function

--- rest/model/admin_test.go
+++ rest/model/admin_test.go
@@ -4,171 +4,17 @@ import (
 	"testing"
 
 	"github.com/evergreen-ci/evergreen"
-	"github.com/mongodb/grip/send"
+	"github.com/evergreen-ci/evergreen/testutil"
 	"github.com/stretchr/testify/assert"
 )
 
-var testSettings = evergreen.Settings{
-	Alerts: evergreen.AlertsConfig{
-		SMTP: &evergreen.SMTPConfig{
-			Server:     "server",
-			Port:       2285,
-			UseSSL:     true,
-			Username:   "username",
-			Password:   "password",
-			From:       "from",
-			AdminEmail: []string{"email"},
-		},
-	},
-	Amboy: evergreen.AmboyConfig{
-		Name:           "amboy",
-		DB:             "db",
-		PoolSizeLocal:  10,
-		PoolSizeRemote: 20,
-		LocalStorage:   30,
-	},
-	Api: evergreen.APIConfig{
-		HttpListenAddr:      "addr",
-		GithubWebhookSecret: "secret",
-	},
-	ApiUrl: "api",
-	AuthConfig: evergreen.AuthConfig{
-		Crowd: &evergreen.CrowdConfig{
-			Username: "crowduser",
-			Password: "crowdpw",
-			Urlroot:  "crowdurl",
-		},
-		Naive: &evergreen.NaiveAuthConfig{
-			Users: []*evergreen.AuthUser{&evergreen.AuthUser{Username: "user", Password: "pw"}},
-		},
-		Github: &evergreen.GithubAuthConfig{
-			ClientId:     "ghclient",
-			ClientSecret: "ghsecret",
-			Users:        []string{"ghuser"},
-			Organization: "ghorg",
-		},
-	},
-	Banner:             "banner",
-	BannerTheme:        "important",
-	ClientBinariesDir:  "bin_dir",
-	ConfigDir:          "cfg_dir",
-	Credentials:        map[string]string{"k1": "v1"},
-	Expansions:         map[string]string{"k2": "v2"},
-	GithubPRCreatorOrg: "org",
-	HostInit: evergreen.HostInitConfig{
-		SSHTimeoutSeconds: 10,
-	},
-	IsNonProd: true,
-	Jira: evergreen.JiraConfig{
-		Host:           "host",
-		Username:       "username",
-		Password:       "password",
-		DefaultProject: "proj",
-	},
-	Keys:    map[string]string{"k3": "v3"},
-	LogPath: "logpath",
-	NewRelic: evergreen.NewRelicConfig{
-		ApplicationName: "new_relic",
-		LicenseKey:      "key",
-	},
-	Notify: evergreen.NotifyConfig{
-		SMTP: &evergreen.SMTPConfig{
-			Server:     "server",
-			Port:       2285,
-			UseSSL:     true,
-			Username:   "username",
-			Password:   "password",
-			From:       "from",
-			AdminEmail: []string{"email"},
-		},
-	},
-	Plugins:   map[string]map[string]interface{}{"k4": map[string]interface{}{"k5": "v5"}},
-	PprofPort: "port",
-	Providers: evergreen.CloudProviders{
-		AWS: evergreen.AWSConfig{
-			Secret: "aws_secret",
-			Id:     "aws",
-		},
-		Docker: evergreen.DockerConfig{
-			APIVersion: "docker_version",
-		},
-		GCE: evergreen.GCEConfig{
-			ClientEmail:  "gce_email",
-			PrivateKey:   "gce_key",
-			PrivateKeyID: "gce_key_id",
-			TokenURI:     "gce_token",
-		},
-		OpenStack: evergreen.OpenStackConfig{
-			IdentityEndpoint: "endpoint",
-			Username:         "username",
-			Password:         "password",
-			DomainName:       "domain",
-			ProjectName:      "project",
-			ProjectID:        "project_id",
-			Region:           "region",
-		},
-		VSphere: evergreen.VSphereConfig{
-			Host:     "host",
-			Username: "vsphere",
-			Password: "vsphere_pass",
-		},
-	},
-	RepoTracker: evergreen.RepoTrackerConfig{
-		NumNewRepoRevisionsToFetch: 10,
-		MaxRepoRevisionsToSearch:   20,
-		MaxConcurrentRequests:      30,
-	},
-	Scheduler: evergreen.SchedulerConfig{
-		MergeToggle: 10,
-		TaskFinder:  "task_finder",
-	},
-	ServiceFlags: evergreen.ServiceFlags{
-		TaskDispatchDisabled:         true,
-		HostinitDisabled:             true,
-		MonitorDisabled:              true,
-		NotificationsDisabled:        true,
-		AlertsDisabled:               true,
-		TaskrunnerDisabled:           true,
-		RepotrackerDisabled:          true,
-		SchedulerDisabled:            true,
-		GithubPRTestingDisabled:      true,
-		RepotrackerPushEventDisabled: true,
-		CLIUpdatesDisabled:           true,
-		GithubStatusAPIDisabled:      true,
-	},
-	Slack: evergreen.SlackConfig{
-		Options: &send.SlackOptions{
-			Channel:   "channel",
-			Fields:    true,
-			FieldsSet: map[string]bool{},
-		},
-		Token: "token",
-		Level: "info",
-	},
-	Splunk: send.SplunkConnectionInfo{
-		ServerURL: "server",
-		Token:     "token",
-		Channel:   "channel",
-	},
-	SuperUsers: []string{"user"},
-	Ui: evergreen.UIConfig{
-		Url:            "url",
-		HelpUrl:        "helpurl",
-		HttpListenAddr: "addr",
-		Secret:         "secret",
-		DefaultProject: "mci",
-		CacheTemplates: true,
-		SecureCookies:  true,
-		CsrfKey:        "csrf",
-	},
-}
-
 func TestModelConversion(t *testing.T) {
 	assert := assert.New(t)
+	testSettings := testutil.MockConfig()
 	apiSettings := APIAdminSettings{}
 
 	// test converting from a db model to an API model
-	assert.NoError(apiSettings.BuildFromService(&testSettings))
+	assert.NoError(apiSettings.BuildFromService(testSettings))
 	assert.EqualValues(testSettings.ApiUrl, apiSettings.ApiUrl)
 	assert.EqualValues(testSettings.Banner, apiSettings.Banner)
 	assert.EqualValues(testSettings.BannerTheme, apiSettings.BannerTheme)
@@ -232,7 +78,7 @@ func TestModelConversion(t *testing.T) {
 	// test converting from the API model back to a DB model
 	dbSettings, err := apiSettings.ToService()
 	assert.NoError(err)
-	assert.Equal(testSettings, dbSettings.(evergreen.Settings))
+	assert.Equal(*testSettings, dbSettings.(evergreen.Settings))
 }
 
 func TestRestart(t *testing.T) {
--- rest/route/admin_route_test.go
+++ rest/route/admin_route_test.go
@@ -9,11 +9,11 @@ import (
 	"time"
 
 	"github.com/evergreen-ci/evergreen"
-	"github.com/mongodb/grip/send"
 
 	"github.com/evergreen-ci/evergreen/model/user"
 	"github.com/evergreen-ci/evergreen/rest/data"
 	restModel "github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/evergreen/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -23,161 +23,6 @@ type AdminRouteSuite struct {
 	suite.Suite
 	getHandler  MethodHandler
 	postHandler MethodHandler
-}
-
-var testSettings = evergreen.Settings{
-	Alerts: evergreen.AlertsConfig{
-		SMTP: &evergreen.SMTPConfig{
-			Server:     "server",
-			Port:       2285,
-			UseSSL:     true,
-			Username:   "username",
-			Password:   "password",
-			From:       "from",
-			AdminEmail: []string{"email"},
-		},
-	},
-	Amboy: evergreen.AmboyConfig{
-		Name:           "amboy",
-		DB:             "db",
-		PoolSizeLocal:  10,
-		PoolSizeRemote: 20,
-		LocalStorage:   30,
-	},
-	Api: evergreen.APIConfig{
-		HttpListenAddr:      "addr",
-		GithubWebhookSecret: "secret",
-	},
-	ApiUrl: "api",
-	AuthConfig: evergreen.AuthConfig{
-		Crowd: &evergreen.CrowdConfig{
-			Username: "crowduser",
-			Password: "crowdpw",
-			Urlroot:  "crowdurl",
-		},
-		Naive: &evergreen.NaiveAuthConfig{
-			Users: []*evergreen.AuthUser{&evergreen.AuthUser{Username: "user", Password: "pw"}},
-		},
-		Github: &evergreen.GithubAuthConfig{
-			ClientId:     "ghclient",
-			ClientSecret: "ghsecret",
-			Users:        []string{"ghuser"},
-			Organization: "ghorg",
-		},
-	},
-	Banner:             "banner",
-	BannerTheme:        "important",
-	ClientBinariesDir:  "bin_dir",
-	ConfigDir:          "cfg_dir",
-	Credentials:        map[string]string{"k1": "v1"},
-	Expansions:         map[string]string{"k2": "v2"},
-	GithubPRCreatorOrg: "org",
-	HostInit: evergreen.HostInitConfig{
-		SSHTimeoutSeconds: 10,
-	},
-	IsNonProd: true,
-	Jira: evergreen.JiraConfig{
-		Host:           "host",
-		Username:       "username",
-		Password:       "password",
-		DefaultProject: "proj",
-	},
-	Keys:    map[string]string{"k3": "v3"},
-	LogPath: "logpath",
-	NewRelic: evergreen.NewRelicConfig{
-		ApplicationName: "new_relic",
-		LicenseKey:      "key",
-	},
-	Notify: evergreen.NotifyConfig{
-		SMTP: &evergreen.SMTPConfig{
-			Server:     "server",
-			Port:       2285,
-			UseSSL:     true,
-			Username:   "username",
-			Password:   "password",
-			From:       "from",
-			AdminEmail: []string{"email"},
-		},
-	},
-	Plugins:   map[string]map[string]interface{}{"k4": map[string]interface{}{"k5": "v5"}},
-	PprofPort: "port",
-	Providers: evergreen.CloudProviders{
-		AWS: evergreen.AWSConfig{
-			Secret: "aws_secret",
-			Id:     "aws",
-		},
-		Docker: evergreen.DockerConfig{
-			APIVersion: "docker_version",
-		},
-		GCE: evergreen.GCEConfig{
-			ClientEmail:  "gce_email",
-			PrivateKey:   "gce_key",
-			PrivateKeyID: "gce_key_id",
-			TokenURI:     "gce_token",
-		},
-		OpenStack: evergreen.OpenStackConfig{
-			IdentityEndpoint: "endpoint",
-			Username:         "username",
-			Password:         "password",
-			DomainName:       "domain",
-			ProjectName:      "project",
-			ProjectID:        "project_id",
-			Region:           "region",
-		},
-		VSphere: evergreen.VSphereConfig{
-			Host:     "host",
-			Username: "vsphere",
-			Password: "vsphere_pass",
-		},
-	},
-	RepoTracker: evergreen.RepoTrackerConfig{
-		NumNewRepoRevisionsToFetch: 10,
-		MaxRepoRevisionsToSearch:   20,
-		MaxConcurrentRequests:      30,
-	},
-	Scheduler: evergreen.SchedulerConfig{
-		MergeToggle: 10,
-		TaskFinder:  "task_finder",
-	},
-	ServiceFlags: evergreen.ServiceFlags{
-		TaskDispatchDisabled:         true,
-		HostinitDisabled:             true,
-		MonitorDisabled:              true,
-		NotificationsDisabled:        true,
-		AlertsDisabled:               true,
-		TaskrunnerDisabled:           true,
-		RepotrackerDisabled:          true,
-		SchedulerDisabled:            true,
-		GithubPRTestingDisabled:      true,
-		RepotrackerPushEventDisabled: true,
-		CLIUpdatesDisabled:           true,
-		GithubStatusAPIDisabled:      true,
-	},
-	Slack: evergreen.SlackConfig{
-		Options: &send.SlackOptions{
-			Channel:   "channel",
-			Fields:    true,
-			FieldsSet: map[string]bool{},
-		},
-		Token: "token",
-		Level: "info",
-	},
-	Splunk: send.SplunkConnectionInfo{
-		ServerURL: "server",
-		Token:     "token",
-		Channel:   "channel",
-	},
-	SuperUsers: []string{"user"},
-	Ui: evergreen.UIConfig{
-		Url:            "url",
-		HelpUrl:        "helpurl",
-		HttpListenAddr: "addr",
-		Secret:         "secret",
-		DefaultProject: "mci",
-		CacheTemplates: true,
-		SecureCookies:  true,
-		CsrfKey:        "csrf",
-	},
 }
 
 func TestAdminRouteSuite(t *testing.T) {
@@ -204,7 +49,8 @@ func TestAdminRouteSuite(t *testing.T) {
 func (s *AdminRouteSuite) TestAdminRoute() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, evergreen.RequestUser, &user.DBUser{Id: "user"})
-	jsonBody, err := json.Marshal(&testSettings)
+	testSettings := testutil.MockConfig()
+	jsonBody, err := json.Marshal(testSettings)
 	s.NoError(err)
 	buffer := bytes.NewBuffer(jsonBody)
 	request, err := http.NewRequest("POST", "/admin", buffer)
@@ -225,7 +71,7 @@ func (s *AdminRouteSuite) TestAdminRoute() {
 	s.NoError(err)
 	settings, ok := settingsResp.(evergreen.Settings)
 	s.True(ok)
-	s.Equal(testSettings, settings)
+	s.Equal(*testSettings, settings)
 }
 
 func (s *AdminRouteSuite) TestGetAuthentication() {
