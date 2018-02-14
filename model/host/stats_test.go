a6e4a401df83a04e7942a1fc5863f981b8d77a3e
Sam Kleinman <sam@10gen.com>
2018-02-14 11:06:56 -0500

    wip test

---
+++ model/host/stats_test.go
@@ -0,0 +1,16 @@
+package host
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHostStatsByProvider(t *testing.T) {
+	assert := assert.New(t) // nolint
+}
+
+func TestHostStatsByDistro(t *testing.T) {
+	assert := assert.New(t) // nolint
+
+}
