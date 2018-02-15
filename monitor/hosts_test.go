3625e027edc9736eaf827c2b24a70ebcb7f888d1
Sam Kleinman <sam@10gen.com>
2018-02-15 12:20:50 -0500

    fix tests

--- monitor/hosts_test.go
+++ monitor/hosts_test.go
@@ -18,17 +18,16 @@ func TestTerminateHosts(t *testing.T) {
 	db.SetGlobalSessionProvider(testConfig.SessionFactory())
 	testutil.HandleTestingErr(db.Clear(host.Collection), t, "error clearing host collection")
 	ctx := context.Background()
-	reason := "test"
 
 	// test that trying to terminate a host that does not exist is handled gracecfully
-	h := host.Host{
+	h := &host.Host{
 		Id:       "i-12345",
 		Status:   evergreen.HostRunning,
 		Provider: evergreen.ProviderNameEc2OnDemand,
 	}
 	assert.NoError(h.Insert())
 
-	assert.NoError(terminateHosts(ctx, []host.Host{h}, testConfig, reason))
+	assert.NoError(terminateHost(ctx, h, testConfig))
 	dbHost, err := host.FindOne(host.ById(h.Id))
 	assert.NoError(err)
 	assert.NotNil(dbHost)
