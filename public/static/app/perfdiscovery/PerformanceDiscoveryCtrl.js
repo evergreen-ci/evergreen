bc57d2db28d40f15fb59d539c31639124b2df054
Ruslan Panasiuk <ruslan_panasiuk@epam.com>
2018-02-26 19:34:38 +0300

    EVG-2834: (feat) Perf. Discovery - filtering (WIP)

--- public/static/app/perfdiscovery/PerformanceDiscoveryCtrl.js
+++ public/static/app/perfdiscovery/PerformanceDiscoveryCtrl.js
@@ -37,6 +37,20 @@ mciModule.controller('PerformanceDiscoveryCtrl', function(
     vm.updateData()
   })
 
+  function extractFilterOptions(items, idKey, nameKey) {
+    return _.map(items, function(d) {
+      return {id: d[idKey], name: d[nameKey]}
+    })
+  }
+
+  function colByField(gridOptions) {
+    return function(fieldName) {
+      return _.findWhere(gridOptions.columnDefs, {
+        field: fieldName
+      })
+    }
+  }
+
   vm.updateData = function() {
     var revision = vm.revisionSelect.selected.id;
     var baselineRev = vm.tagSelect.selected.id;
@@ -45,23 +59,22 @@ mciModule.controller('PerformanceDiscoveryCtrl', function(
       revision: revision
     })
 
-    var buildTaskData = _.reduce(version.builds, function(items, build) {
-      var taskItems = _.map(build.tasks, function(v, k) {
-        return {
-          build: build.name,
-          task: k,
-          task_id: v.task_id,
-        }
-      })
-      return items.concat(taskItems)
-    }, [])
-
-
     PerfDiscoveryService.getData(
       version, baselineRev
     ).then(function(res) {
       vm.gridOptions.data = res
+      var col = colByField(vm.gridOptions)
+      col('build').filter.options = _.unique(_.pluck(res, 'build'))
+      col('storageEngine').filter.options = _.unique(_.pluck(res, 'storageEngine'))
+      col('task').filter.options = _.unique(_.pluck(res, 'task'))
+      col('threads').filter.options = _.unique(_.pluck(res, 'threads'))
     })
+  }
+
+  function conditionFn(term, val, row, col) {
+    if (typeof(term) == 'undefined') return true
+    if (term.length === 0) return true
+    return _.contains(term, val)
   }
 
   vm.gridOptions = {
@@ -71,49 +84,118 @@ mciModule.controller('PerformanceDiscoveryCtrl', function(
     columnDefs: [{
         name: 'Link',
         field: 'link',
-        enableFiltering: true,
+        enableFiltering: false,
+        width: 60,
       }, {
         name: 'Build',
         field: 'build',
-        enableFiltering: true,
+        filterHeaderTemplate: 'evg-ui-select/header-filter',
+        filter: {
+          noTerm: true,
+          rawTerm: true,
+          condition: conditionFn,
+          options: []
+        },
       }, {
         name: 'Storage Engine',
         field: 'storageEngine',
-        enableFiltering: true,
+        filterHeaderTemplate: 'evg-ui-select/header-filter',
+        filter: {
+          noTerm: true,
+          condition: conditionFn,
+          options: []
+        },
       }, {
         name: 'Task',
         field: 'task',
-        enableFiltering: true,
+        filterHeaderTemplate: 'evg-ui-select/header-filter',
+        filter: {
+          noTerm: true,
+          condition: conditionFn,
+          options: []
+        },
       }, {
         name: 'Test',
         field: 'test',
-        enableFiltering: true,
       }, {
         name: 'Threads',
         field: 'threads',
-        enableFiltering: true,
+        filterHeaderTemplate: 'evg-ui-select/header-filter',
+        filter: {
+          noTerm: true,
+          condition: conditionFn,
+          options: []
+        },
+        width: 130,
       }, {
         name: 'Ratio',
         field: 'ratio',
         cellFilter: 'number:2',
+        enableFiltering: false,
+        width: 70,
       }, {
         name: 'Trend',
         field: 'trendData',
-        enableSorting: false,
         width: PERF_DISCOVERY.TREND_COL_WIDTH,
+        enableSorting: false,
+        enableFiltering: false,
       }, {
         name: 'Avg and Self',
         field: 'avgVsSelf',
         enableSorting: false,
+        enableFiltering: false,
       }, {
         name: 'ops/sec',
         field: 'speed',
         cellFilter: 'number:2',
+        enableFiltering: false,
+        width: 100,
       }, {
         name: 'Baseline',
         field: 'baseSpeed',
         cellFilter: 'number:2',
+        enableFiltering: false,
+        width: 120,
       },
     ]
   }
 })
+
+mciModule.run(function($templateCache) {
+  $templateCache.put('perf-discovery/multiselect-filter',
+    '<ui-select multiple append-to-body="true" ng-model="vm.col.filters[0].term" ng-change="vm.triggerFiltering()">' +
+      '<ui-select-match placeholder="Choose {{vm.field}}">{{$item}}</ui-select-match>' +
+      '<ui-select-choices repeat="item in vm.filters">' +
+        '{{item}}' +
+      '</ui-select-choices>' +
+    '</ui-select>'
+  )
+
+  $templateCache.put('evg-ui-select/header-filter',
+    '<mselect field="col.field" filters="col.filters[0].options" grid="grid" col="col"></mselect>'
+  )
+})
+
+mciModule.directive('mselect', function(uiGridConstants) { return {
+  restrict: 'E',
+  scope: {
+    filters: '=',
+    field: '=',
+    grid: '=',
+    col: '=',
+  },
+  bindToController: true,
+  controllerAs: 'vm',
+  templateUrl: 'perf-discovery/multiselect-filter',
+  controller: function() {
+    var vm = this
+
+    vm.triggerFiltering = function() {
+      vm.grid.notifyDataChange(uiGridConstants.dataChange.COLUMN)
+    }
+
+    vm.model = []//vm.col.filters[0].term
+
+    return vm
+  }
+}})
