2bfec8bb40c4f9d06973de14fc138551faf54999
Jim OLeary <jim.oleary@gmail.com>
2018-03-15 15:26:24 +0000

    space and quote removal

--- public/static/plugins/perf/js/trend_chart.js
+++ public/static/plugins/perf/js/trend_chart.js
@@ -593,11 +593,11 @@ var drawSingleTrendChart = function(params) {
     })
 
     focusedText
-      .attr({'y': function(d, i) { return opsLabelsY[i] },
-             'transform': function(){
+      .attr({y: function(d, i) { return opsLabelsY[i] },
+             transform: function(){
                // transform the hover text location based on the list index
                var x = 0;
-               if(series){
+               if (series) {
                  x = (cfg.focus.labelOffset.x + this.getBBox().width) * idx / series.length
                }
                return d3Translate(-x, 0)
