b83c9b1d062f2630eb128ebed44569f730db4786
tycho garen <garen@tychoish.com>
2018-03-19 21:38:49 -0400

    better tests

--- util/yaml.go
+++ util/yaml.go
@@ -1,8 +1,6 @@
 package util
 
 import (
-	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -40,77 +38,4 @@ func ReadFromYAMLFile(fn string, data interface{}) error {
 	}
 
 	return errors.Wrap(yaml.Unmarshal(bytes, data), "problem reading yaml")
-}
-
-type StringOrBool string
-
-func (v *StringOrBool) MarshalYAML() (interface{}, error) {
-	bv, err := v.Bool()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	out, err := yaml.Marshal(fmt.Sprintf("%t", bv))
-
-	return out, errors.WithStack(err)
-}
-
-func (v *StringOrBool) UnmarshalYAML(um func(interface{}) error) error {
-	var val interface{}
-	err := um(val)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	return v.setVal(val)
-}
-
-func (v *StringOrBool) setVal(val interface{}) error {
-	switch input := val.(type) {
-	case bool:
-		*v = StringOrBool(fmt.Sprintf("%t", input))
-	case string:
-		*v = StringOrBool(input)
-	default:
-		return errors.Errorf("'%s' must be a string or a bool [%T]", input, input)
-	}
-
-	return nil
-}
-
-func (v *StringOrBool) UnmarshalJSON(in []byte) error {
-	var val interface{}
-
-	err := json.Unmarshal(in, val)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	return v.setVal(val)
-}
-
-func (v *StringOrBool) MarshalJSON() ([]byte, error) {
-	bv, err := v.Bool()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	out, err := json.Marshal(fmt.Sprintf("%t", bv))
-	if err != nil {
-		return nil, err
-	}
-
-	return out, nil
-}
-
-func (v *StringOrBool) Bool() (bool, error) {
-	str := string(*v)
-
-	if StringSliceContains([]string{"", "false", "False", "FALSE", "F", "f", "0", "n", "no", "!true", "!t", "!True"}, str) {
-		return false, nil
-	} else if StringSliceContains([]string{"true", "True", "TRUE", "T", "t", "1", "y", "yes", "!false", "!f", "!False"}, str) {
-		return true, nil
-	}
-
-	return false, errors.Errorf("'%s' is not a valid string bool form", str)
 }
