bb8330009fc5cd858363ff66fc381e842b8ee94d
Brian Samek <brian.samek@mongodb.com>
2018-03-21 15:23:18 -0400

    missed an import

--- rest/data/generate_test.go
+++ rest/data/generate_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/model/build"
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/model/version"
