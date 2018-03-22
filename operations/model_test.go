11d45a25cfd7a8549d560ff817eadf574f82b540
Marcus Todd <malexander.todd@gmail.com>
2018-03-22 15:43:46 -0400

    test FindDefaultAlias and SetDefaultAlias instead of loadAlias

--- operations/model_test.go
+++ operations/model_test.go
@@ -6,10 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadAlias(t *testing.T) {
+func TestFindDefaultAlias(t *testing.T) {
 	assert := assert.New(t)
 
-	// Normal test, use the default alias
+	// Find the default alias for a project when one is present
 	conf1 := ClientProjectConf{
 		Name:     "test",
 		Default:  true,
@@ -39,31 +39,13 @@ func TestLoadAlias(t *testing.T) {
 		ShowSummary: false,
 	}
 
-	mockLoadAlias(p1, client1, false)
-	assert.Equal(p1.Alias, "testAlias")
+	assert.Equal(client1.FindDefaultAlias(p1.Project), "testAlias")
 
-	// Test that, given no default alias, the user can write a passed-in alias as default
-	conf2 := ClientProjectConf{
-		Name:     "test2",
-		Default:  true,
-		Alias:    "",
-		Variants: []string{},
-		Tasks:    []string{},
-	}
-
-	client2 := &ClientSettings{
-		APIServerHost: "apiserverhost",
-		UIServerHost:  "uiserverhost",
-		APIKey:        "apikey",
-		User:          "user",
-		Projects:      []ClientProjectConf{conf2},
-		LoadedFrom:    "-",
-	}
-
+	// Find an empty string when no project with the given alias exists
 	p2 := &patchParams{
 		Project:     "test2",
-		Variants:    conf2.Variants,
-		Tasks:       conf2.Tasks,
+		Variants:    conf1.Variants,
+		Tasks:       conf1.Tasks,
 		Description: "",
 		Alias:       "testAlias2",
 		SkipConfirm: false,
@@ -72,14 +54,17 @@ func TestLoadAlias(t *testing.T) {
 		ShowSummary: false,
 	}
 
-	mockLoadAlias(p2, client2, true)
-	assert.Equal(client2.Projects[0].Alias, "testAlias2")
+	assert.Empty(client1.FindDefaultAlias(p2.Project))
+}
 
-	// Test that a passed in alias doesn't replace the default
+func TestSetDefaultAlias(t *testing.T) {
+	assert := assert.New(t)
+
+	// Set the default alias for a project that doesn't have one
 	conf3 := ClientProjectConf{
-		Name:     "test3",
+		Name:     "test",
 		Default:  true,
-		Alias:    "defaultAlias",
+		Alias:    "",
 		Variants: []string{},
 		Tasks:    []string{},
 	}
@@ -94,32 +79,17 @@ func TestLoadAlias(t *testing.T) {
 	}
 
 	p3 := &patchParams{
-		Project:     "test3",
+		Project:     "test",
 		Variants:    conf3.Variants,
 		Tasks:       conf3.Tasks,
 		Description: "",
-		Alias:       "newAlias",
+		Alias:       "defaultAlias",
 		SkipConfirm: false,
 		Finalize:    false,
 		Large:       false,
 		ShowSummary: false,
 	}
 
-	mockLoadAlias(p3, client3, true)
-	assert.Equal(p3.Alias, "newAlias")
+	client3.SetDefaultAlias(p3.Project, p3.Alias)
 	assert.Equal(client3.Projects[0].Alias, "defaultAlias")
-}
-
-// Mock version of loadAlias that removes the command line prompt and writing to file
-func mockLoadAlias(p *patchParams, conf *ClientSettings, isNewDefault bool) {
-	if p.Alias != "" {
-		defaultAlias := conf.FindDefaultAlias(p.Project)
-		if defaultAlias == "" && !p.SkipConfirm && isNewDefault {
-			conf.SetDefaultAlias(p.Project, p.Alias)
-		}
-	} else {
-		p.Alias = conf.FindDefaultAlias(p.Project)
-	}
-
-	return
 }
