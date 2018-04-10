94386af39ad753da700a1b65f12c69ab9cd7a2fc
Brian Samek <bsamek@users.noreply.github.com>
2018-04-10 09:53:08 -0400

    EVG-3119 Remove new_start, new_end endpoints (#1028)

--- service/api_task_test.go
+++ service/api_task_test.go
@@ -78,7 +78,7 @@ func getEndTaskEndpoint(t *testing.T, as *APIServer, hostId, taskId string, deta
 	if err != nil {
 		t.Fatalf("creating test API handler: %v", err)
 	}
-	url := fmt.Sprintf("/api/2/task/%v/new_end", taskId)
+	url := fmt.Sprintf("/api/2/task/%s/end", taskId)
 
 	request, err := http.NewRequest("POST", url, nil)
 	if err != nil {
