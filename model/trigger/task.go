b70beaeb8b00a9d682c41a0ba07f2a81178275d4
Richard Samuels <richard.samuels@mongodb.com>
2018-05-23 12:29:10 -0400

    port simple alerts

--- model/trigger/task.go
+++ model/trigger/task.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"github.com/evergreen-ci/evergreen"
+	"github.com/evergreen-ci/evergreen/model/alertrecord"
 	"github.com/evergreen-ci/evergreen/model/event"
 	"github.com/evergreen-ci/evergreen/model/task"
 	restModel "github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func init() {
@@ -135,4 +139,89 @@ func taskSuccess(e *event.TaskEventData, t *task.Task) (*notificationGenerator, 
 	}
 
 	return generatorFromTask(name, t, e.Status)
+}
+
+func taskFirstFailureInBuild(e *event.TaskEventData, t *task.Task) (*notificationGenerator, error) {
+	const name = "first-failure-in-build"
+
+	if e.Status != evergreen.TaskFailed {
+		return nil, nil
+	}
+	rec, err := alertrecord.FindOne(alertrecord.ByFirstFailureInVariant(t.Version, t.BuildVariant))
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("failed to fetch alertrecord (%s)", name))
+	}
+	if rec != nil {
+		return nil, nil
+	}
+
+	return generatorFromTaskWithAlertRecord(name, t, e.Status, alertrecord.FirstVariantFailureId)
+}
+
+func taskFirstFailureInVersion(e *event.TaskEventData, t *task.Task) (*notificationGenerator, error) {
+	const name = "first-failure-in-version"
+
+	if e.Status != evergreen.TaskFailed {
+		return nil, nil
+	}
+	rec, err := alertrecord.FindOne(alertrecord.ByFirstFailureInVersion(t.Project, t.Version))
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("failed to fetch alertrecord (%s)", name))
+	}
+	if rec != nil {
+		return nil, nil
+	}
+
+	return generatorFromTaskWithAlertRecord(name, t, e.Status, alertrecord.FirstVersionFailureId)
+}
+
+func taskFirstFailureWithName(e *event.TaskEventData, t *task.Task) (*notificationGenerator, error) {
+	const name = "first-failure-with-name"
+
+	if e.Status != evergreen.TaskFailed {
+		return nil, nil
+	}
+	rec, err := alertrecord.FindOne(alertrecord.ByFirstFailureInTaskType(t.Version, t.DisplayName))
+	if err != nil {
+		return false, nil
+	}
+	if rec != nil {
+		return nil, nil
+	}
+
+	return generatorFromTaskWithAlertRecord(name, t, e.Status, alertrecord.FirstTaskTypeFailureId)
+}
+
+func generatorFromTaskWithAlertRecord(triggerName string, t *task.Task, status, alertType string) (*notificationGenerator, error) {
+	gen, err := generatorFromTask(name, t, e.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	rec := newAlertRecord(t, alertType)
+	grip.Error(message.WrapError(rec.Insert(), message.Fields{
+		"source":  "alert-record",
+		"type":    alertType,
+		"task_id": t.Id,
+	}))
+
+	return gen, nil
+}
+
+// newAlertRecord creates an instance of an alert record for the given alert type, populating it
+// with as much data from the triggerContext as possible
+func newAlertRecord(t *task.Task, alertType string) *alertrecord.AlertRecord {
+	record := &alertrecord.AlertRecord{
+		Id:   bson.NewObjectId(),
+		Type: alertType,
+	}
+	record.ProjectId = t.Project
+	record.VersionId = t.Version
+	record.RevisionOrderNumber = t.RevisionOrderNumber
+	record.TaskName = t.DisplayName
+	record.Variant = t.BuildVariant
+	record.TaskId = t.Id
+	record.HostId = t.HostId
+
+	return record
 }
