564b5cdbb5ce5cef37cfd04991e837cc9ace5107
Brian Samek <bsamek@users.noreply.github.com>
2018-06-06 17:02:03 -0400

    EVG-3474 Log size of log messages (#1226)

--- util/json.go
+++ util/json.go
@@ -10,12 +10,19 @@ import (
 
 // ReadJSONInto reads JSON from an io.ReadCloser into the data pointer.
 func ReadJSONInto(r io.ReadCloser, data interface{}) error {
+	_, err := ReadJSONIntoWithLength(r, data)
+	return err
+}
+
+// ReadJSONInto reads JSON from an io.ReadCloser into the data pointer.
+func ReadJSONIntoWithLength(r io.ReadCloser, data interface{}) (int, error) {
 	defer r.Close()
 	bytes, err := ioutil.ReadAll(r)
 	if err != nil {
-		return errors.Wrapf(err, "error reading JSON (%s)", string(bytes))
+		return 0, errors.Wrapf(err, "error reading JSON (%s)", string(bytes))
 	}
-	return errors.Wrapf(json.Unmarshal(bytes, data), "error attempting to unmarshal into %T", data)
+	length := len(bytes)
+	return length, errors.Wrapf(json.Unmarshal(bytes, data), "error attempting to unmarshal into %T", data)
 }
 
 func WriteJSONInto(fn string, data interface{}) error {
