87c39f7cd3da183731917d58c6ba9e04abbe2ae4
Thomas Zembowicz <tzembowicz@gmail.com>
2018-06-20 15:26:43 -0400

    EVG-3423 create jobs to decommission excess parent hosts (#1255)

--- operations/service.go
+++ operations/service.go
@@ -82,7 +82,8 @@ func startSystemCronJobs(ctx context.Context, env evergreen.Environment) {
 		units.PopulateTaskMonitoring(),
 		units.PopulateEventAlertProcessing(1),
 		units.PopulateBackgroundStatsJobs(env, 0),
-		units.PopulateLastContainerFinishTimeJobs()))
+		units.PopulateLastContainerFinishTimeJobs(),
+		units.PopulateParentDecommissionJobs()))
 
 	amboy.IntervalQueueOperation(ctx, env.RemoteQueue(), 15*time.Second, time.Now(), opts, amboy.GroupQueueOperationFactory(
 		units.PopulateHostSetupJobs(env, 0),
