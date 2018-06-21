3c08ab7a061cd87aeb464ea1d6eb0b9a549ccc97
Richard Samuels <richard.samuels@mongodb.com>
2018-06-21 10:39:10 -0400

    host spawn trigger w/o email

--- model/trigger/host.go
+++ model/trigger/host.go
@@ -13,6 +13,7 @@ import (
 
 func init() {
 	registry.registerEventHandler(event.ResourceTypeHost, event.EventHostProvisioned, makeHostTriggers)
+	registry.registerEventHandler(event.ResourceTypeHost, event.EventHostProvisionError, makeHostTriggers)
 }
 
 type hostTriggers struct {
@@ -71,16 +72,20 @@ func (t *hostTriggers) Selectors() []event.Selector {
 }
 
 func (t *hostTriggers) hostSpawnOutcome(sub *event.Subscription) (*notification.Notification, error) {
-	if !t.host.UserHost || t.event.EventType == event.EventHostProvisioned || t.event.EventType == event.EventHostProvisionError {
+	if !t.host.UserHost {
 		return nil, nil
 	}
 
+	return t.generate(sub)
+}
+
+func (t *hostTriggers) slack() *notification.SlackPayload {
 	text := fmt.Sprintf("Host with distro '%s' has spawned", t.host.Distro)
 
 	var attachment message.SlackAttachment
 	if t.event.EventType == event.EventHostProvisioned {
 		attachment = message.SlackAttachment{
-			Title:     "Requested host has succesfully spawned",
+			Title:     "Evergreen Host",
 			TitleLink: fmt.Sprintf("%s/host/%s", t.uiConfig.Url, t.host.Id),
 			Color:     "good",
 			Fields: []*message.SlackAttachmentField{
@@ -97,8 +102,9 @@ func (t *hostTriggers) hostSpawnOutcome(sub *event.Subscription) (*notification.
 		}
 
 	} else if t.event.EventType == event.EventHostProvisionError {
+		text = fmt.Sprintf("Host with distro '%s' has failed to spawn", t.host.Distro)
 		attachment = message.SlackAttachment{
-			Title:     "Requested host has failed to provision",
+			Title:     "Click here to spawn another host",
 			TitleLink: fmt.Sprintf("%s/spawn", t.uiConfig.Url),
 			Color:     "danger",
 			Fields: []*message.SlackAttachmentField{
@@ -111,5 +117,33 @@ func (t *hostTriggers) hostSpawnOutcome(sub *event.Subscription) (*notification.
 		}
 	}
 
+	return &notification.SlackPayload{
+		Body:        text,
+		Attachments: []message.SlackAttachment{attachment},
+	}
+}
+
+func (t *hostTriggers) email() (*notification.SlackPayload, error) {
 	return nil, nil
+}
+
+func (t *hostTriggers) makePayload(sub *event.Subscription) interface{} {
+	var payload interface{}
+	if sub.Type == event.SlackSubscriberType {
+		payload = t.slack()
+
+	} else if sub.Type == event.EmailSubscriberType {
+		payload, _ = t.email()
+	}
+
+	return payload
+}
+
+func (t *hostTriggers) generate(sub *event.Subscription) (*notification.Notification, error) {
+	payload := t.makePayload(sub)
+	if payload == nil {
+		return nil, nil
+	}
+
+	return notification.New(t.event, sub.Trigger, &sub.Subscriber, payload)
 }
