8e3b915a7dcc571ece1a01327500369ac3cce7da
Richard Samuels <richard.samuels@mongodb.com>
2018-06-21 10:38:54 -0400

    allow subscriptions to provisioned/provisionfail

--- model/event/host_event.go
+++ model/event/host_event.go
@@ -14,6 +14,7 @@ import (
 func init() {
 	registry.AddType(ResourceTypeHost, hostEventDataFactory)
 	registry.AllowSubscription(ResourceTypeHost, EventHostProvisioned)
+	registry.AllowSubscription(ResourceTypeHost, EventHostProvisionFailed)
 }
 
 const (
