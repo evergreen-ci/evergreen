0c6f6b87c1f32d17d936d8ffdf94e6d3a0788a4f
Thomas Zembowicz <thomas.zembowicz@10gen.com>
2018-06-25 16:08:36 -0400

    EVG-3550 change name of container pool validation test

--- validator/distro_validator_test.go
+++ validator/distro_validator_test.go
@@ -267,7 +267,7 @@ func TestEnsureNonZeroID(t *testing.T) {
 	assert.Nil(ensureHasNonZeroID(ctx, &distro.Distro{Id: " "}, conf))
 }
 
-func TestEnsureContainerPoolHasValidDistro(t *testing.T) {
+func TestEnsureValidContainerPool(t *testing.T) {
 	assert := assert.New(t)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
