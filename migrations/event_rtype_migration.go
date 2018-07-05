679754b8bcf5b044efc387def8a4ee4b78b93d21
Sam Kleinman <garen@tychoish.com>
2018-07-05 16:55:16 -0400

    MAKE-413: custom anser config (#1358)

--- migrations/event_rtype_migration.go
+++ migrations/event_rtype_migration.go
@@ -6,7 +6,6 @@ import (
 	"github.com/mongodb/anser"
 	"github.com/mongodb/anser/bsonutil"
 	"github.com/mongodb/anser/model"
-	"github.com/pkg/errors"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -14,50 +13,47 @@ import (
 const (
 	migrationEventRtypeRestructureAllLogs  = "event-rtype-to-root-alllogs"
 	migrationEventRtypeRestructureTaskLogs = "event-rtype-to-root-tasklogs"
+	migrationTime                          = "2015-10-21T16:29:00-07:00"
+	allLogCollection                       = "event_log"
 )
 
-const migrationTime = "2015-10-21T16:29:00-07:00"
+func eventRTypeMigration(env anser.Environment, args migrationGeneratorFactoryOptions) (anser.Generator, error) {
+	const (
+		resourceTypeKey = "r_type"
+		dataKey         = "data"
+		processedAtKey  = "processed_at"
+	)
 
-func makeEventRTypeMigration(collection string) migrationGeneratorFactory { //nolint: deadcode, megacheck
 	loc, _ := time.LoadLocation("UTC")
 	bttf, err := time.ParseInLocation(time.RFC3339, migrationTime, loc)
 	if err != nil {
-		return func(env anser.Environment, args migrationGeneratorFactoryOptions) (anser.Generator, error) {
-			return nil, errors.Wrap(err, "time is invalid")
-		}
+		return nil, err
 	}
 
-	return func(env anser.Environment, args migrationGeneratorFactoryOptions) (anser.Generator, error) {
-		const (
-			resourceTypeKey = "r_type"
-			dataKey         = "data"
-			processedAtKey  = "processed_at"
-		)
-		var embeddedResourceTypeKey = bsonutil.GetDottedKeyName(dataKey, resourceTypeKey)
+	var embeddedResourceTypeKey = bsonutil.GetDottedKeyName(dataKey, resourceTypeKey)
 
-		notExists := bson.M{
-			"$exists": false,
-		}
-		opts := model.GeneratorOptions{
-			NS: model.Namespace{
-				DB:         args.db,
-				Collection: collection,
-			},
-			Limit: args.limit,
-			Query: bson.M{
-				processedAtKey:  notExists,
-				resourceTypeKey: notExists,
-			},
-			JobID: args.id,
-		}
-
-		return anser.NewSimpleMigrationGenerator(env, opts, bson.M{
-			"$set": bson.M{
-				processedAtKey: bttf,
-			},
-			"$rename": bson.M{
-				embeddedResourceTypeKey: resourceTypeKey,
-			},
-		}), nil
+	notExists := bson.M{
+		"$exists": false,
 	}
+	opts := model.GeneratorOptions{
+		NS: model.Namespace{
+			DB:         args.db,
+			Collection: allLogCollection,
+		},
+		Limit: args.limit,
+		Query: bson.M{
+			processedAtKey:  notExists,
+			resourceTypeKey: notExists,
+		},
+		JobID: args.id,
+	}
+
+	return anser.NewSimpleMigrationGenerator(env, opts, bson.M{
+		"$set": bson.M{
+			processedAtKey: bttf,
+		},
+		"$rename": bson.M{
+			embeddedResourceTypeKey: resourceTypeKey,
+		},
+	}), nil
 }
