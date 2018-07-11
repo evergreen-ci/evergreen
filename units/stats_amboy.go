9a84032b48bef4e808a744a386c0e5e0077f5f00
Sam Kleinman <garen@tychoish.com>
2018-07-11 09:51:02 -0400

    EVG-3632: log queue state aggregations (#1383)

--- units/stats_amboy.go
+++ units/stats_amboy.go
@@ -3,12 +3,15 @@ package units
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/evergreen-ci/evergreen"
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/dependency"
 	"github.com/mongodb/amboy/job"
+	"github.com/mongodb/amboy/queue"
 	"github.com/mongodb/amboy/registry"
+	"github.com/mongodb/amboy/reporting"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/logging"
 	"github.com/mongodb/grip/message"
@@ -67,7 +70,7 @@ func makeAmboyStatsCollector() *amboyStatsCollector {
 	return j
 }
 
-func (j *amboyStatsCollector) Run(_ context.Context) {
+func (j *amboyStatsCollector) Run(ctx context.Context) {
 	defer j.MarkComplete()
 
 	if j.env == nil {
@@ -91,5 +94,46 @@ func (j *amboyStatsCollector) Run(_ context.Context) {
 			"message": "amboy remote queue stats",
 			"stats":   remoteQueue.Stats(),
 		})
+
+		settings := j.env.Settings()
+
+		opts := queue.DefaultMongoDBOptions()
+		opts.URI = settings.Database.Url
+		opts.DB = settings.Amboy.DB
+		opts.Priority = true
+
+		reporter, err := reporting.MakeDBQueueState(settings.Amboy.Name, opts, j.env.Session())
+		if err != nil {
+			j.AddError(err)
+			return
+		}
+
+		r := message.Fields{
+			"message": "amboy remote queue report",
+		}
+
+		pending, err := reporter.JobStatus(ctx, reporting.Pending)
+		j.AddError(err)
+		if pending != nil {
+			r["pending"] = pending
+		}
+		inprog, err := reporter.JobStatus(ctx, reporting.InProgress)
+		j.AddError(err)
+		if inprog != nil {
+			r["inprog"] = inprog
+		}
+		stale, err := reporter.JobStatus(ctx, reporting.Stale)
+		j.AddError(err)
+		if stale != nil {
+			r["stale"] = stale
+		}
+
+		recentErrors, err := reporter.RecentErrors(ctx, time.Minute, reporting.StatsOnly)
+		j.AddError(err)
+		if recentErrors != nil {
+			r["errors"] = recentErrors
+		}
+
+		j.logger.InfoWhen(len(r) > 1, r)
 	}
 }
