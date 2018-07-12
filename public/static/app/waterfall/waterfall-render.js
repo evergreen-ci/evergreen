4a1a690d67cbb22be135efa5eb503cc6bddfdad2
Ruslan <Ruslan.Panasiuk@gmail.com>
2018-07-12 17:40:30 +0300

    EVG-3171 (feat) Waterfall - collapsed variant search (#1390)

--- public/static/app/waterfall/waterfall-render.js
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-  React.createElement(Root, {data: window.serverData, project: window.project, userTz: window.userTz, jiraHost: window.jiraHost}),
-  document.getElementById('root')
-);
