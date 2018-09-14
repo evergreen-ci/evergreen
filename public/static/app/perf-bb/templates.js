04542866d6098090a541e21d8908bc0a567c035a
Ruslan <Ruslan.Panasiuk@gmail.com>
2018-09-14 16:26:20 +0300

    EVG-5177 (feat) Aggregation for grouped items (#1638)

--- public/static/app/perf-bb/templates.js
+++ public/static/app/perf-bb/templates.js
@@ -2,8 +2,9 @@ mciModule.run(function($templateCache) {
   $templateCache.put('hazard-level-cell',
     '<div>' +
       '<div class="ui-grid-cell-contents" style="text-align: center;">' +
-        '<div ng-if="row.groupHeader" style="font-weight: bold;" ng-style="{color: level.color}">' +
-          '{{level.label}} {{cnt}} {{sv | number:2}}' +
+        '<div ng-if="row.groupHeader" >' +
+          '<span style="font-weight: bold;" ng-style="{color: level.color}">{{level.label}} </span>' +
+          '({{count}})' +
         '</div>' +
         '<div ng-if="!row.groupHeader" ng-style="{color: color}">' +
           '{{ratio > 0 ? "+" : ""}}{{ratio | number : 0}}%' +
