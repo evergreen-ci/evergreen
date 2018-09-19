35868ed2ef17758b59eae9bcc0182dd3e78ef37f
Ruslan Panasiuk <ruslan_panasiuk@epam.com>
2018-09-19 14:31:57 +0300

    EVG-5346 (fix) Perf. discovery - correct ratio calculation

--- public/static/app/perfdiscovery/perfDiscoveryRatio.js
+++ public/static/app/perfdiscovery/perfDiscoveryRatio.js
@@ -14,6 +14,13 @@ mciModule.directive('perfDiscoveryRatio', function() {
       // This $watch is required due to known bug with ui-grid
       // https://github.com/angular-ui/ui-grid/issues/4869
       scope.$watch('ratio', function(val) {
+        var val = Math.round(
+          (scope.ratio > 1 ? scope.ratio - 1 : 1 - 1 / scope.ratio) * 100
+        )
+        scope.normRatio = Math.abs(val)
+        scope.sign = val <  0 ? '-' :
+                     val == 0 ? ''  :
+                     /* else */ '+'
         scope.color = scale(scope.ratio)
       })
     }
