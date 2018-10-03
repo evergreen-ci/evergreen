edb238ceb632d4118c5a87b4e5bca2076dc1009d
John Liu <john.liu@mongodb.com>
2018-10-03 10:43:25 -0400

    cr

--- agent/logging.go
+++ agent/logging.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/evergreen-ci/evergreen"
+	"github.com/evergreen-ci/evergreen/subprocess"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/level"
 	"github.com/mongodb/grip/send"
@@ -36,13 +37,17 @@ func GetSender(ctx context.Context, prefix, taskId string) (send.Sender, error) 
 		senders []send.Sender
 	)
 
-	if splunk := send.GetSplunkConnectionInfo(); splunk.Populated() {
-		grip.Info("configuring splunk sender")
-		sender, err = send.NewSplunkLogger("evergreen.agent", splunk, send.LevelInfo{Default: level.Alert, Threshold: level.Alert})
-		if err != nil {
-			return nil, errors.Wrap(err, "problem creating the splunk logger")
+	if os.Getenv(subprocess.MarkerAgentPID) == "" { // this var is set if the agent is started via a command
+		if splunk := send.GetSplunkConnectionInfo(); splunk.Populated() {
+			grip.Info("configuring splunk sender")
+			sender, err = send.NewSplunkLogger("evergreen.agent", splunk, send.LevelInfo{Default: level.Alert, Threshold: level.Alert})
+			if err != nil {
+				return nil, errors.Wrap(err, "problem creating the splunk logger")
+			}
+			senders = append(senders, sender)
 		}
-		senders = append(senders, sender)
+	} else {
+		grip.Notice("agent started via command - not configuring external logger")
 	}
 
 	if prefix == "" {
