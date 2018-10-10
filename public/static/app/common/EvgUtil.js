5a3d370a3b63f445e308b82cfa8b6318ad342a56
Ruslan Panasiuk <ruslan_panasiuk@epam.com>
2018-10-10 13:30:42 +0300

    simplify the code

--- public/static/app/common/EvgUtil.js
+++ public/static/app/common/EvgUtil.js
@@ -7,26 +7,26 @@ mciModule.factory('EvgUtil', function() {
   }
 
   // internal function which generates evergreen id from given tokens
-  function generateId() {
-    return _.map(Array.prototype.slice.call(arguments), normalize).join('_')
+  function generateId(tokens) {
+    return _.map(tokens, normalize).join('_')
   }
 
   return {
     // Generates version id from project_id and revision
     // :param params: {project, revision, buildVariant, createTime}
     generateBuildId: function (params) {
-      return generateId(
+      return generateId([
         params.project,
         params.buildVariant,
         params.revision,
         moment.utc(params.createTime).format(DATE_ID_FORMAT)
-      )
+      ])
     },
 
     // Generates version id from project_id and revision
     // :param params: {project, revision}
     generateVersionId: function (params) {
-      return generateId(params.project, params.revision)
+      return generateId([params.project, params.revision])
     },
   }
 })
