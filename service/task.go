0831121a09e8ca4aa05b19067b4db39cde0b434e
Brian Samek <bsamek@users.noreply.github.com>
2018-10-25 11:26:39 -0400

    EVG-5465 Check circular dependencies on task page, not in notifications (#1724)

--- service/task.go
+++ service/task.go
@@ -420,6 +420,9 @@ func getTaskDependencies(t *task.Task) ([]uiDep, string, error) {
 		})
 	}
 
+	if err = t.CircularDependencies(); err != nil {
+		return nil, "", err
+	}
 	status, err := t.BlockedState(nil)
 	if err != nil {
 		return nil, "", err
