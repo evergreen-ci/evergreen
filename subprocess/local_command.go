b1551fc3faba6c3ed35729ff7342ab36f283b5a0
Brian Samek <brian.samek@mongodb.com>
2018-11-13 17:14:51 -0500

    EVG-5531 Only use interactive shell in ScriptMode

--- subprocess/local_command.go
+++ subprocess/local_command.go
@@ -98,14 +98,7 @@ func (lc *localCmd) Start(ctx context.Context) error {
 		}
 		cmd.Stdin = strings.NewReader(lc.CmdString)
 	} else {
-		// If the shell is a shell (e.g., "bash" or "dash", not "python") pass "-i" to force
-		// it to be interactive. This makes agents not started over SSH, like in a Docker
-		// container, backwards-compatible with the behavior of agents started over SSH.
-		if strings.HasSuffix(lc.Shell, "sh") {
-			cmd = exec.CommandContext(ctx, lc.Shell, "-i", "-c", lc.CmdString)
-		} else {
-			cmd = exec.CommandContext(ctx, lc.Shell, "-c", lc.CmdString)
-		}
+		cmd = exec.CommandContext(ctx, lc.Shell, "-c", lc.CmdString)
 	}
 
 	// create the command, set the options
