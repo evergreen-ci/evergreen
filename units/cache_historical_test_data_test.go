65eec4583005c6ca9f837713c3fbf80db952cb82
David Bradford <david.bradford@mongodb.com>
2018-11-13 17:47:33 -0500

    EVG-5516: Ensure test data for caching historical test data fits in an hour (#1775)

--- units/cache_historical_test_data_test.go
+++ units/cache_historical_test_data_test.go
@@ -395,7 +395,10 @@ func (s *cacheHistoryTestDataSuite) TestCacheHistoricalTestDataJob() {
 	err := clearStatsData()
 	s.Nil(err)
 
-	baseTime := time.Now().Add(-4*7*24*time.Hour + 2*time.Hour)
+	// Our sample data will be 30 minutes apart and the Job will aggregation 1 hours worth of data.
+	// So need to be sure the sample data does not cross an hour boundary or only part of it will
+	// be aggregated. So, truncate the base date to an hour.
+	baseTime := time.Now().Add(-4*7*24*time.Hour + 2*time.Hour).Truncate(time.Hour)
 
 	ref := &model.ProjectRef{
 		Repo:       "evergreen",
