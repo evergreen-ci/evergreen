e3f78fc08df9debda3bce56cb3e5a3c2bd176e7a
Jonathan Brill <Jonathan.Brill@mongodb.com>
2018-11-29 17:34:46 -0500

    incrementing ClientVersion for auto-update

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2018-11-17"
+	ClientVersion = "2018-11-29"
 
 	errNotFound = "not found"
 )
