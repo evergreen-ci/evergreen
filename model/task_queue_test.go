ed70c43c7c4700e01917e14818f64a0ad3085405
tycho garen <garen@tychoish.com>
2018-12-13 05:52:08 -0500

    avoid whitespace

--- model/task_queue_test.go
+++ model/task_queue_test.go
@@ -81,7 +81,6 @@ func TestDequeueTask(t *testing.T) {
 			// make sure the db representation was updated
 			taskQueue, err := LoadTaskQueue(distroId)
 			So(err, ShouldBeNil)
-
 			So(taskQueue.Length(), ShouldEqual, 2)
 			So(taskQueue.Queue[0].Id, ShouldEqual, taskIds[0])
 			So(taskQueue.Queue[1].Id, ShouldEqual, taskIds[2])
