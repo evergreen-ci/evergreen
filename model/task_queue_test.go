3e8dacc08b89c00cafb7e8cc3787d97e0640b72f
tycho garen <garen@tychoish.com>
2018-12-13 05:39:51 -0500

    EVG-5663: avoid expensive update operation during task dispatching

--- model/task_queue_test.go
+++ model/task_queue_test.go
@@ -81,6 +81,10 @@ func TestDequeueTask(t *testing.T) {
 			// make sure the db representation was updated
 			taskQueue, err := LoadTaskQueue(distroId)
 			So(err, ShouldBeNil)
+			for _, it := range taskQueue.Queue {
+				fmt.Printf("%T: %+v\n", it.Id, string(it.Id))
+			}
+
 			So(taskQueue.Length(), ShouldEqual, 2)
 			So(taskQueue.Queue[0].Id, ShouldEqual, taskIds[0])
 			So(taskQueue.Queue[1].Id, ShouldEqual, taskIds[2])
