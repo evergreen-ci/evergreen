1ed09cbc6bff37e8816a22ecd1d154fec3cb5d3b
Brian Samek <brian.samek@mongodb.com>
2019-02-06 14:32:09 -0500

    fix tests

--- rest/data/generate_test.go
+++ rest/data/generate_test.go
@@ -16,19 +16,22 @@ func TestGeneratePoll(t *testing.T) {
 	assert := assert.New(t)
 	gc := &GenerateConnector{}
 	q := queue.NewLocalUnordered(1)
-	finished, err := gc.GeneratePoll(context.Background(), "1", q)
+	finished, errs, err := gc.GeneratePoll(context.Background(), "1", q)
+	assert.Empty(errs)
 	assert.False(finished)
 	assert.Error(err)
 	j := &mockJob{}
 	j.SetID("generate-tasks-1")
 	assert.NoError(q.Start(context.Background()))
 	assert.NoError(q.Put(j))
-	finished, err = gc.GeneratePoll(context.Background(), "1", q)
+	finished, errs, err = gc.GeneratePoll(context.Background(), "1", q)
 	assert.False(finished)
+	assert.Empty(errs)
 	assert.NoError(err)
 	time.Sleep(20 * time.Millisecond)
-	finished, err = gc.GeneratePoll(context.Background(), "1", q)
+	finished, errs, err = gc.GeneratePoll(context.Background(), "1", q)
 	assert.True(finished)
+	assert.Empty(errs)
 	assert.NoError(err)
 }
 
