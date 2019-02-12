ca1aa9142cc79f25debb6afc21c4ead07bc25991
Stuart Butler <butler.stuart@gmail.com>
2019-02-12 08:43:39 -0500

    EVG-5325 rest endpoint for custom notifications (#1979)

---
+++ rest/route/notification.go
@@ -0,0 +1,303 @@
+package route
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/evergreen-ci/evergreen"
+	"github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/evergreen/util"
+	"github.com/evergreen-ci/gimlet"
+	"github.com/mongodb/grip/message"
+	"github.com/mongodb/grip/send"
+	"github.com/pkg/errors"
+)
+
+///////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/notifications/{type}
+
+type notificationPostHandler struct {
+	handler     gimlet.RouteHandler
+	environment evergreen.Environment
+}
+
+func makeNotification(environment evergreen.Environment) gimlet.RouteHandler {
+	return &notificationPostHandler{
+		environment: environment,
+	}
+}
+
+func (h *notificationPostHandler) Factory() gimlet.RouteHandler {
+	return &notificationPostHandler{
+		environment: h.environment,
+	}
+}
+
+// Parse fetches the notification type from the http request.
+func (h *notificationPostHandler) Parse(ctx context.Context, r *http.Request) error {
+	t := gimlet.GetVars(r)["type"]
+	switch t {
+	case "jira_comment":
+		h.handler = makeJiraCommentNotification(h.environment)
+	case "jira_issue":
+		h.handler = makeJiraIssueNotification(h.environment)
+	case "slack":
+		h.handler = makeSlackNotification(h.environment)
+	case "email":
+		h.handler = makeEmailNotification(h.environment)
+	default:
+		return errors.Errorf("'%s' is not a supported {type}", t)
+	}
+
+	return h.handler.Parse(ctx, r)
+}
+
+// Run dispatches the notification.
+func (h *notificationPostHandler) Run(ctx context.Context) gimlet.Responder {
+	return h.handler.Run(ctx)
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/notifications/jira_comment
+
+type jiraCommentNotificationPostHandler struct {
+	APIJiraComment *model.APIJiraComment
+	composer       message.Composer
+	sender         send.Sender
+	environment    evergreen.Environment
+}
+
+func makeJiraCommentNotification(environment evergreen.Environment) gimlet.RouteHandler {
+	return &jiraCommentNotificationPostHandler{
+		environment: environment,
+	}
+}
+
+func (h *jiraCommentNotificationPostHandler) Factory() gimlet.RouteHandler {
+	return &jiraCommentNotificationPostHandler{
+		environment: h.environment,
+	}
+}
+
+// Parse fetches the JSON payload from the and unmarshals it to an APIJiraComment.
+func (h *jiraCommentNotificationPostHandler) Parse(ctx context.Context, r *http.Request) error {
+	body := util.NewRequestReader(r)
+	h.APIJiraComment = &model.APIJiraComment{}
+	if err := gimlet.GetJSON(body, h.APIJiraComment); err != nil {
+		return errors.Wrap(err, "API error while unmarshalling JSON to model.APIJiraComment")
+	}
+
+	return nil
+}
+
+// Run dispatches the notification.
+func (h *jiraCommentNotificationPostHandler) Run(ctx context.Context) gimlet.Responder {
+	i, err := h.APIJiraComment.ToService()
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "API error converting from model.APIJiraComment to message.JIRAComment"))
+	}
+	comment, ok := i.(*message.JIRAComment)
+	if !ok {
+		return gimlet.MakeJSONErrorResponder(gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("Unexpected type %T for message.JIRAComment", i),
+		})
+	}
+
+	h.composer = message.MakeJIRACommentMessage(comment.IssueID, comment.Body)
+	h.sender, err = h.environment.GetSender(evergreen.SenderJIRAComment)
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "Error fetching sender key for evergreen.SenderJIRAComment"))
+	}
+
+	h.sender.Send(h.composer)
+
+	return gimlet.NewJSONResponse(struct{}{})
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/notifications/jira_issue
+
+type jiraIssueNotificationPostHandler struct {
+	APIJiraIssue *model.APIJiraIssue
+	composer     message.Composer
+	sender       send.Sender
+	environment  evergreen.Environment
+}
+
+func makeJiraIssueNotification(environment evergreen.Environment) gimlet.RouteHandler {
+	return &jiraIssueNotificationPostHandler{
+		environment: environment,
+	}
+}
+
+func (h *jiraIssueNotificationPostHandler) Factory() gimlet.RouteHandler {
+	return &jiraIssueNotificationPostHandler{
+		environment: h.environment,
+	}
+}
+
+// Parse fetches the JSON payload from the and unmarshals it to an APIJiraIssue.
+func (h *jiraIssueNotificationPostHandler) Parse(ctx context.Context, r *http.Request) error {
+	body := util.NewRequestReader(r)
+	h.APIJiraIssue = &model.APIJiraIssue{}
+	if err := gimlet.GetJSON(body, h.APIJiraIssue); err != nil {
+		return errors.Wrap(err, "API error while unmarshalling JSON to model.APIJiraIssue")
+	}
+
+	return nil
+}
+
+// Run dispatches the notification.
+func (h *jiraIssueNotificationPostHandler) Run(ctx context.Context) gimlet.Responder {
+	i, err := h.APIJiraIssue.ToService()
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "API error converting from model.APIJiraIssue to message.JiraIssue"))
+	}
+	issue, ok := i.(*message.JiraIssue)
+	if !ok {
+		return gimlet.MakeJSONErrorResponder(gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("Unexpected type %T for message.JiraIssue", i),
+		})
+	}
+
+	h.composer = message.MakeJiraMessage(issue)
+	h.sender, err = h.environment.GetSender(evergreen.SenderJIRAIssue)
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "Error fetching sender key for evergreen.SenderJIRAIssue"))
+	}
+
+	h.sender.Send(h.composer)
+
+	return gimlet.NewJSONResponse(struct{}{})
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/notifications/slack
+
+type slackNotificationPostHandler struct {
+	APISlack    *model.APISlack
+	composer    message.Composer
+	sender      send.Sender
+	environment evergreen.Environment
+}
+
+func makeSlackNotification(environment evergreen.Environment) gimlet.RouteHandler {
+	return &slackNotificationPostHandler{
+		environment: environment,
+	}
+}
+
+func (h *slackNotificationPostHandler) Factory() gimlet.RouteHandler {
+	return &slackNotificationPostHandler{
+		environment: h.environment,
+	}
+}
+
+// Parse fetches the JSON payload from the and unmarshals it to an APISlack.
+func (h *slackNotificationPostHandler) Parse(ctx context.Context, r *http.Request) error {
+	body := util.NewRequestReader(r)
+	h.APISlack = &model.APISlack{}
+	if err := gimlet.GetJSON(body, h.APISlack); err != nil {
+		return errors.Wrap(err, "API error while unmarshalling JSON to model.APISlack")
+	}
+
+	return nil
+}
+
+// Run dispatches the notification.
+func (h *slackNotificationPostHandler) Run(ctx context.Context) gimlet.Responder {
+	attachments := []message.SlackAttachment{}
+	for _, a := range h.APISlack.Attachments {
+		i, err := a.ToService()
+		if err != nil {
+			return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "API error converting from model.APISlackAttachment to message.SlackAttachment"))
+		}
+		attachment, ok := i.(*message.SlackAttachment)
+		if !ok {
+			return gimlet.MakeJSONErrorResponder(gimlet.ErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Message:    fmt.Sprintf("Unexpected type %T for message.SlackAttachment", i),
+			})
+		}
+		attachments = append(attachments, *attachment)
+	}
+	target := model.FromAPIString(h.APISlack.Target)
+	msg := model.FromAPIString(h.APISlack.Msg)
+
+	h.composer = message.MakeSlackMessage(target, msg, attachments)
+	s, err := h.environment.GetSender(evergreen.SenderSlack)
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "Error fetching sender key for evergreen.SenderSlack"))
+	}
+
+	h.sender = s
+	h.sender.Send(h.composer)
+
+	return gimlet.NewJSONResponse(struct{}{})
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/notifications/email
+
+type emailNotificationPostHandler struct {
+	APIEmail    *model.APIEmail
+	composer    message.Composer
+	sender      send.Sender
+	environment evergreen.Environment
+}
+
+func makeEmailNotification(environment evergreen.Environment) gimlet.RouteHandler {
+	return &emailNotificationPostHandler{
+		environment: environment,
+	}
+}
+
+func (h *emailNotificationPostHandler) Factory() gimlet.RouteHandler {
+	return &emailNotificationPostHandler{
+		environment: h.environment,
+	}
+}
+
+// Parse fetches the JSON payload from the and unmarshals it to an APIEmail.
+func (h *emailNotificationPostHandler) Parse(ctx context.Context, r *http.Request) error {
+	body := util.NewRequestReader(r)
+	h.APIEmail = &model.APIEmail{}
+	if err := gimlet.GetJSON(body, h.APIEmail); err != nil {
+		return errors.Wrap(err, "API error while unmarshalling JSON to model.APIEmail")
+	}
+
+	return nil
+}
+
+// Run dispatches the notification.
+func (h *emailNotificationPostHandler) Run(ctx context.Context) gimlet.Responder {
+	i, err := h.APIEmail.ToService()
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "API error converting from model.APIEmail to message.Email"))
+	}
+	email, ok := i.(*message.Email)
+	if !ok {
+		return gimlet.MakeJSONErrorResponder(gimlet.ErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message:    fmt.Sprintf("Unexpected type %T for message.Email", i),
+		})
+	}
+
+	h.composer = message.MakeEmailMessage(*email)
+	h.sender, err = h.environment.GetSender(evergreen.SenderEmail)
+	if err != nil {
+		return gimlet.MakeJSONInternalErrorResponder(errors.Wrap(err, "Error fetching sender key for evergreen.SenderEmail"))
+	}
+
+	h.sender.Send(h.composer)
+
+	return gimlet.NewJSONResponse(struct{}{})
+}
---
+++ rest/route/notification_test.go
@@ -0,0 +1,243 @@
+package route
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/evergreen-ci/evergreen"
+	"github.com/evergreen-ci/evergreen/db"
+	restModel "github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/evergreen/testutil"
+	"github.com/evergreen-ci/gimlet"
+	"github.com/stretchr/testify/suite"
+)
+
+///////////////////////////////////////////////////////////////////////
+//
+// Tests for POST /rest/v2/notifications/jira_comment
+
+type JiraCommentNotificationSuite struct {
+	rm          gimlet.RouteHandler
+	environment evergreen.Environment
+
+	suite.Suite
+}
+
+func TestJiraCommentNotificationSuite(t *testing.T) {
+	db.SetGlobalSessionProvider(testutil.TestConfig().SessionFactory())
+	suite.Run(t, new(JiraCommentNotificationSuite))
+}
+
+func (s *JiraCommentNotificationSuite) SetupSuite() {
+	s.rm = makeJiraCommentNotification(evergreen.GetEnvironment())
+}
+
+func (s *JiraCommentNotificationSuite) TestParseValidJSON() {
+	ctx := context.Background()
+	json := []byte(`{
+		"issue_id": "This is the JIRA issue's ID",
+		"body": "This is the JIRA comment's body"
+  }`)
+	req, _ := http.NewRequest("POST", "http://example.com/api/rest/v2/notifications/jira_comment", bytes.NewBuffer(json))
+	err := s.rm.Parse(ctx, req)
+	s.NoError(err)
+
+	apiJiraComment := s.rm.(*jiraCommentNotificationPostHandler).APIJiraComment
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's ID"), apiJiraComment.IssueID)
+	s.Equal(restModel.ToAPIString("This is the JIRA comment's body"), apiJiraComment.Body)
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// Tests for POST /rest/v2/notifications/jira_issue
+
+type JiraIssueNotificationSuite struct {
+	rm          gimlet.RouteHandler
+	environment evergreen.Environment
+
+	suite.Suite
+}
+
+func TestJiraIssueNotificationSuite(t *testing.T) {
+	db.SetGlobalSessionProvider(testutil.TestConfig().SessionFactory())
+	suite.Run(t, new(JiraIssueNotificationSuite))
+}
+
+func (s *JiraIssueNotificationSuite) SetupSuite() {
+	s.rm = makeJiraIssueNotification(evergreen.GetEnvironment())
+}
+
+func (s *JiraIssueNotificationSuite) TestParseValidJSON() {
+	ctx := context.Background()
+	json := []byte(`{
+		"issue_key": "This is the JIRA issue's key",
+		"project": "This is the JIRA issue's project",
+		"summary": "This is the JIRA issue's summary",
+		"description": "This is the JIRA issue's description",
+		"reporter": "This is the JIRA issue's reporter",
+		"assignee": "This is the JIRA issue's assignee",
+		"type": "This is the JIRA issue's type",
+		"components": ["c1", "c2"],
+		"labels": ["l1", "l2"],
+		"fields": {"f1": true, "f2": 12.34, "f3": "string"}
+  }`)
+	req, _ := http.NewRequest("POST", "http://example.com/api/rest/v2/notifications/jira_issue", bytes.NewBuffer(json))
+	err := s.rm.Parse(ctx, req)
+	s.NoError(err)
+
+	apiJiraIssue := s.rm.(*jiraIssueNotificationPostHandler).APIJiraIssue
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's key"), apiJiraIssue.IssueKey)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's project"), apiJiraIssue.Project)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's summary"), apiJiraIssue.Summary)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's description"), apiJiraIssue.Description)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's reporter"), apiJiraIssue.Reporter)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's assignee"), apiJiraIssue.Assignee)
+	s.Equal(restModel.ToAPIString("This is the JIRA issue's type"), apiJiraIssue.Type)
+	s.Equal([]string{"c1", "c2"}, apiJiraIssue.Components)
+	s.Equal([]string{"l1", "l2"}, apiJiraIssue.Labels)
+	mock := map[string]interface{}{"f1": true, "f2": 12.34, "f3": "string"}
+	s.Equal(len(mock), len(apiJiraIssue.Fields))
+	s.Equal(mock["f1"], apiJiraIssue.Fields["f1"])
+	s.Equal(mock["f2"], apiJiraIssue.Fields["f2"])
+	s.Equal(mock["f3"], apiJiraIssue.Fields["f3"])
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// Tests for POST /rest/v2/notifications/slack
+
+type SlackNotificationSuite struct {
+	rm          gimlet.RouteHandler
+	environment evergreen.Environment
+
+	suite.Suite
+}
+
+func TestSlackNotificationSuite(t *testing.T) {
+	db.SetGlobalSessionProvider(testutil.TestConfig().SessionFactory())
+	suite.Run(t, new(SlackNotificationSuite))
+}
+
+func (s *SlackNotificationSuite) SetupSuite() {
+	s.rm = makeSlackNotification(evergreen.GetEnvironment())
+}
+
+func (s *SlackNotificationSuite) TestParseValidJSON() {
+	ctx := context.Background()
+	json := []byte(`{
+		"target": "This is the Slack's target",
+		"msg": "This the Slack's message",
+		"attachments": [
+			{
+				"color": "I'm the first attachment's color",
+				"fallback": "I'm the first attachment's fallback",
+				"author_name": "I'm the first attachment's author's name",
+				"author_icon": "I'm the first attachment's author's icon",
+				"title": "I'm the first attachment's title",
+				"text": "I'm the first attachment's text",
+				"fields":	[
+										{
+											"title": "I'm the first attachment's first field title",
+											"value": "I'm the first attachment's first field value",
+											"short": true
+										}
+				],
+				"mrkdwn_in": ["m11", "m12"],
+				"footer": "I'm the first attachment's footer"
+			},
+			{
+				"color": "I'm the second attachment's color",
+				"fallback": "I'm the second attachment's fallback",
+				"author_name": "I'm the second attachment's author's name",
+				"author_icon": "I'm the second attachment's author's icon",
+				"title": "I'm the second attachment's title",
+				"text": "I'm the second attachment's text",
+				"fields":	[
+										{
+											"title": "I'm the second attachment's first field title",
+											"value": "I'm the second attachment's first field value",
+											"short": false
+										}
+				],
+				"mrkdwn_in": ["m21", "m22"],
+				"footer": "I'm the second attachment's footer"
+			}
+		]
+	}`)
+	req, _ := http.NewRequest("POST", "http://example.com/api/rest/v2/notifications/slack", bytes.NewBuffer(json))
+	err := s.rm.Parse(ctx, req)
+	s.NoError(err)
+
+	apiSlack := s.rm.(*slackNotificationPostHandler).APISlack
+	s.Equal(restModel.ToAPIString("This is the Slack's target"), apiSlack.Target)
+	s.Equal(restModel.ToAPIString("This the Slack's message"), apiSlack.Msg)
+
+	s.Equal(restModel.ToAPIString("I'm the first attachment's color"), apiSlack.Attachments[0].Color)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's fallback"), apiSlack.Attachments[0].Fallback)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's author's name"), apiSlack.Attachments[0].AuthorName)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's author's icon"), apiSlack.Attachments[0].AuthorIcon)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's title"), apiSlack.Attachments[0].Title)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's text"), apiSlack.Attachments[0].Text)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's first field title"), apiSlack.Attachments[0].Fields[0].Title)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's first field value"), apiSlack.Attachments[0].Fields[0].Value)
+	s.Equal(true, apiSlack.Attachments[0].Fields[0].Short)
+	s.Equal([]string{"m11", "m12"}, apiSlack.Attachments[0].MarkdownIn)
+	s.Equal(restModel.ToAPIString("I'm the first attachment's footer"), apiSlack.Attachments[0].Footer)
+
+	s.Equal(restModel.ToAPIString("I'm the second attachment's color"), apiSlack.Attachments[1].Color)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's fallback"), apiSlack.Attachments[1].Fallback)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's author's name"), apiSlack.Attachments[1].AuthorName)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's author's icon"), apiSlack.Attachments[1].AuthorIcon)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's title"), apiSlack.Attachments[1].Title)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's text"), apiSlack.Attachments[1].Text)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's first field title"), apiSlack.Attachments[1].Fields[0].Title)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's first field value"), apiSlack.Attachments[1].Fields[0].Value)
+	s.Equal(false, apiSlack.Attachments[1].Fields[0].Short)
+	s.Equal([]string{"m21", "m22"}, apiSlack.Attachments[1].MarkdownIn)
+	s.Equal(restModel.ToAPIString("I'm the second attachment's footer"), apiSlack.Attachments[1].Footer)
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// Tests for POST /rest/v2/notifications/email
+
+type EmailNotificationSuite struct {
+	rm          gimlet.RouteHandler
+	environment evergreen.Environment
+
+	suite.Suite
+}
+
+func TestEmailNotificationSuite(t *testing.T) {
+	db.SetGlobalSessionProvider(testutil.TestConfig().SessionFactory())
+	suite.Run(t, new(EmailNotificationSuite))
+}
+
+func (s *EmailNotificationSuite) SetupSuite() {
+	s.rm = makeEmailNotification(evergreen.GetEnvironment())
+}
+
+func (s *EmailNotificationSuite) TestParseValidJSON() {
+	ctx := context.Background()
+	json := []byte(`{
+		"from": "me",
+  	"recipients": ["Tom", "Dick", "Harry"],
+  	"subject": "This is the email's subject",
+  	"body": "This is the email's body",
+  	"is_plain_text": true,
+		"headers": {"h1": ["v11", "v12"], "h2": ["v21", "v22"]}
+	}`)
+	req, _ := http.NewRequest("POST", "http://example.com/api/rest/v2/notifications/email", bytes.NewBuffer(json))
+	err := s.rm.Parse(ctx, req)
+	s.NoError(err)
+
+	apiEmail := s.rm.(*emailNotificationPostHandler).APIEmail
+	s.Equal(restModel.ToAPIString("me"), apiEmail.From)
+	s.Equal([]string{"Tom", "Dick", "Harry"}, apiEmail.Recipients)
+	s.Equal(restModel.ToAPIString("This is the email's subject"), apiEmail.Subject)
+	s.Equal(restModel.ToAPIString("This is the email's body"), apiEmail.Body)
+	s.Equal(true, apiEmail.PlainTextContents)
+	s.Equal(map[string][]string{"h1": []string{"v11", "v12"}, "h2": []string{"v21", "v22"}}, apiEmail.Headers)
+}
