ca0986ff3738cdb99c6e903f31e5246203a777e6
StuartButler <butler.stuart@gmail.com>
2019-02-25 09:09:17 -0500

    EVG-5684 refactor inputs to host allocation to be public (WIP #3)

--- scheduler/host_allocator.go
+++ scheduler/host_allocator.go
@@ -10,13 +10,6 @@ import (
 )
 
 // HostAllocator is responsible for determining how many new hosts should be spun up.
-// Parameters:
-//  taskQueueItems: a map of distro name -> task queue items for that distro (a TaskQueue object)
-//  distros: a map of distro name -> information on that distro (a model.Distro object)
-//  existingDistroHosts: a map of distro name -> currently running hosts on that distro
-//  projectTaskDurations: the expected duration of tasks by project and variant
-//  taskRunDistros: a map of task id -> distros the task is allowed to run on
-// Returns a map of distro name -> how many hosts need to be spun up for that distro.
 type HostAllocator func(context.Context, HostAllocatorData) (int, error)
 
 type HostAllocatorData struct {
@@ -27,15 +20,12 @@ type HostAllocatorData struct {
 	UsesContainers       bool
 	ContainerPool        *evergreen.ContainerPool
 	DistroQueueInfo      DistroQueueInfo
-	TaskRunDistros       []string // used by duration_based_host_allocator
 }
 
 func GetHostAllocator(name string) HostAllocator {
 	switch name {
 	case "deficit":
 		return DeficitBasedHostAllocator
-	case "duration":
-		return DurationBasedHostAllocator
 	case "utilization":
 		return UtilizationBasedHostAllocator
 	default:
