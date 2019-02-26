2c21ff01b64a2e7f21dfd4dada309d62123d6e78
StuartButler <butler.stuart@gmail.com>
2019-02-26 09:55:56 -0500

    EVG-5684 refactor inputs to host allocation to be public (WIP #5)

--- model/task_queue.go
+++ model/task_queue.go
@@ -21,6 +21,22 @@ const (
 	TaskQueuesCollection = "task_queues"
 )
 
+type TaskGroupInfo struct {
+	Name                  string        `bson:"name" json:"name"`
+	Count                 int           `bson:"count" json:"count"`
+	MaxHosts              int           `bson:"max_hosts" json:"max_hosts"`
+	ExpectedDuration      time.Duration `bson:"expected_duration" json:"expected_duration"`
+	CountOverThreshold    int           `bson:"count_over_threshold" json:"count_over_threshold"`
+	DurationOverThreshold time.Duration `bson:"duration_over_threshold" json:"duration_over_threshold"`
+}
+
+type DistroQueueInfo struct {
+	Length             int             `bson:"length" json:"length"`
+	ExpectedDuration   time.Duration   `bson:"expected_duration" json:"expected_duration"`
+	CountOverThreshold int             `bson:"count_over_threshold" json:"count_over_threshold"`
+	TaskGroupInfos     []TaskGroupInfo `bson:"task_group_infos" json:"task_group_infos"`
+}
+
 // represents the next n tasks to be run on hosts of the distro
 type TaskQueue struct {
 	Id          bson.ObjectId   `bson:"_id,omitempty" json:"_id"`
