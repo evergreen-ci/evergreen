9ddd09a55fe8ad04274a23bde3a9c6d54c261691
Kim Tao <kim.tao@mongodb.com>
2019-03-01 17:21:28 -0500

    Change client version

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2019-01-22"
+	ClientVersion = "2019-03-01"
 
 	errNotFound = "not found"
 )
