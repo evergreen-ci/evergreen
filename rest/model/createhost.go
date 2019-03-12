0bdb17c5bd63b33fcad64e15e6de4870efbcaea5
ablack12 <annie.black@10gen.com>
2019-03-12 16:06:48 -0400

    EVG-5865 add containers to host.list

--- rest/model/createhost.go
+++ rest/model/createhost.go
@@ -9,16 +9,44 @@ type CreateHost struct {
 	DNSName    string `json:"dns_name"`
 	IP         string `json:"ip_address"`
 	InstanceID string `json:"instance_id"`
+
+	ContainerID string `json:"container_id"`
+	Host        string `json:"host"`
+	Image       string `json:"image"`
+	Command     string `json:"command"`
+	Port        int    `json:"port"`
 }
 
 func (createHost *CreateHost) BuildFromService(h interface{}) error {
 	switch v := h.(type) {
 	case host.Host:
+		// container
+		if v.ParentID != "" {
+			createHost.ContainerID = v.ExternalIdentifier
+			createHost.Host = v.Id
+			createHost.Image = v.DockerOptions.Image
+			createHost.Command = v.DockerOptions.Command
+			if v.ContainerPoolSettings != nil {
+				createHost.Port = int(v.ContainerPoolSettings.Port)
+			}
+			return nil
+		}
 		createHost.DNSName = v.Host
 		createHost.InstanceID = v.Id
 		createHost.IP = v.IP
 		createHost.InstanceID = v.ExternalIdentifier
 	case *host.Host:
+		// container
+		if v.ParentID != "" {
+			createHost.ContainerID = v.ExternalIdentifier
+			createHost.Host = v.Id
+			createHost.Image = v.DockerOptions.Image
+			createHost.Command = v.DockerOptions.Command
+			if v.ContainerPoolSettings != nil {
+				createHost.Port = int(v.ContainerPoolSettings.Port)
+			}
+			return nil
+		}
 		createHost.DNSName = v.Host
 		createHost.InstanceID = v.Id
 		createHost.IP = v.IP
