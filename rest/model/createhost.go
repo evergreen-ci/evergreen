a761920ea24cced294ffa3a2edcecc12be6d5436
Annie Black <annie.black@10gen.com>
2019-03-13 09:42:26 -0400

    EVG-5865 add containers to host.list (#2102)

--- rest/model/createhost.go
+++ rest/model/createhost.go
@@ -9,16 +9,38 @@ type CreateHost struct {
 	DNSName    string `json:"dns_name"`
 	IP         string `json:"ip_address"`
 	InstanceID string `json:"instance_id"`
+
+	ContainerID string `json:"container_id"`
+	ParentID    string `json:"parent_id"`
+	Image       string `json:"image"`
+	Command     string `json:"command"`
+	Port        int    `json:"port"`
 }
 
 func (createHost *CreateHost) BuildFromService(h interface{}) error {
 	switch v := h.(type) {
 	case host.Host:
+		// container
+		if v.ParentID != "" {
+			createHost.ContainerID = v.ExternalIdentifier
+			createHost.ParentID = v.ParentID
+			createHost.Image = v.DockerOptions.Image
+			createHost.Command = v.DockerOptions.Command
+			return nil
+		}
 		createHost.DNSName = v.Host
 		createHost.InstanceID = v.Id
 		createHost.IP = v.IP
 		createHost.InstanceID = v.ExternalIdentifier
 	case *host.Host:
+		// container
+		if v.ParentID != "" {
+			createHost.ContainerID = v.ExternalIdentifier
+			createHost.ParentID = v.ParentID
+			createHost.Image = v.DockerOptions.Image
+			createHost.Command = v.DockerOptions.Command
+			return nil
+		}
 		createHost.DNSName = v.Host
 		createHost.InstanceID = v.Id
 		createHost.IP = v.IP
