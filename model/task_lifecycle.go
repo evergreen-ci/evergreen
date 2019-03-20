049461213a2a15f59493d4dd39cf9b8cce814b5e
Kim Tao <kim.tao@mongodb.com>
2019-03-20 14:50:17 -0400

    Wrap errors

--- model/task_lifecycle.go
+++ model/task_lifecycle.go
@@ -423,27 +423,27 @@ func MarkEnd(t *task.Task, caller string, finishTime time.Time, detail *apimodel
 			return err
 		}
 		if err = build.UpdateCachedTask(t.DisplayTask.BuildId, t.DisplayTask.Id, t.DisplayTask.Status, t.TimeTaken); err != nil {
-			grip.Error(message.Fields{
+			grip.Error(message.WrapError(err, message.Fields{
 				"message":    "failed to update cached display task",
 				"function":   "MarkEnd",
 				"build_id":   t.DisplayTask.BuildId,
 				"task_id":    t.DisplayTask.Id,
 				"status":     t.DisplayTask.Status,
 				"time_taken": t.TimeTaken,
-			})
-			return err
+			}))
+			return errors.Wrap(err, "error updating cached display task")
 		}
 	} else {
 		err = build.SetCachedTaskFinished(t.BuildId, t.Id, detail.Status, detail, t.TimeTaken)
 		if err != nil {
-			grip.Error(message.Fields{
+			grip.Error(message.WrapError(err, message.Fields{
 				"message":    "failed to set cached task finished",
 				"function":   "MarkEnd",
 				"build_id":   t.BuildId,
 				"task_id":    t.Id,
 				"status":     detail.Status,
 				"time_taken": t.TimeTaken,
-			})
+			}))
 			return errors.Wrap(err, "error updating build")
 		}
 	}
@@ -592,15 +592,15 @@ func UpdateBuildAndVersionStatusForTask(taskId string, updates *StatusChanges) e
 		}
 		err = b.SetCachedTaskFinished(t.Id, status, &t.Details, t.TimeTaken)
 		if err != nil {
-			grip.Error(message.Fields{
+			grip.Error(message.WrapError(err, message.Fields{
 				"message":    "failed to set cached task finished",
 				"function":   "UpdateBuildAndVersionStatusForTask",
 				"build_id":   b.Id,
 				"task_id":    t.Id,
 				"status":     status,
 				"time_taken": t.TimeTaken,
-			})
-			return fmt.Errorf("error updating build: %v", err.Error())
+			}))
+			return errors.Wrap(err, "error updating cached task")
 		}
 	}
 
