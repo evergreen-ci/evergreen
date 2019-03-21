6ac7e25d3bd23df50b688e94b23fc7423bff66f4
Brian Samek <brian.samek@mongodb.com>
2019-03-21 09:55:23 -0400

    test commit

--- config.go
+++ config.go
@@ -24,7 +24,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2019-03-14"
+	ClientVersion = "2019-03-15"
 
 	errNotFound = "not found"
 )
