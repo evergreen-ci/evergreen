31d61bed8b890675bfda102b5ff6330dd1a93217
Brian Samek <brian.samek@mongodb.com>
2019-03-26 11:02:44 -0400

    Revert "EVG-5972 Deploy agent with get-update command (#2128)"
    This reverts commit c4674543957b396313110657ef00c8b20ba42bbe.

--- model/host/hostutil_test.go
+++ model/host/hostutil_test.go
@@ -3,7 +3,6 @@ package host
 import (
 	"testing"
 
-	"github.com/evergreen-ci/evergreen/model/distro"
 	"github.com/evergreen-ci/evergreen/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -20,16 +19,4 @@ func TestOutputOptions(t *testing.T) {
 
 	require.NoError(t, opts.Validate())
 	assert.Equal(t, opts.GetOutput(), opts.GetError())
-}
-
-func TestCurlCommand(t *testing.T) {
-	assert := assert.New(t)
-	h := &Host{Distro: distro.Distro{Arch: "windows_amd64"}}
-	url := "www.example.com"
-	expected := "cd ~ && curl -LO 'www.example.com/clients/windows_amd64/evergreen.exe' && chmod +x evergreen.exe"
-	assert.Equal(expected, h.CurlCommand(url))
-
-	h = &Host{Distro: distro.Distro{Arch: "linux_amd64"}}
-	expected = "cd ~ && if [ -f evergreen ]; then ./evergreen get-update --install --force; else curl -LO 'www.example.com/clients/linux_amd64/evergreen' && chmod +x evergreen; fi"
-	assert.Equal(expected, h.CurlCommand(url))
 }
