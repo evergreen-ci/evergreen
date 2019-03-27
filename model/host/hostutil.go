b8ebe44d7ea2d1056fb6d3b5a832740b2323f145
Brian Samek <brian.samek@mongodb.com>
2019-03-27 10:03:36 -0400

    EVG-5972 Add a backstop to the teardown script

--- model/host/hostutil.go
+++ model/host/hostutil.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/subprocess"
 	"github.com/evergreen-ci/evergreen/util"
 	"github.com/mongodb/grip"
@@ -29,6 +32,35 @@ func (h *Host) SetupCommand() string {
 // TearDownCommand returns a command for running a teardown script on a host.
 func (h *Host) TearDownCommand() string {
 	return fmt.Sprintf("%s host teardown", filepath.Join("~", h.Distro.BinaryName()))
+}
+
+// TearDownCommandOverSSH returns a command for running a teardown script on a host. This command
+// runs if there is a problem running host teardown with the agent and is intended only as a
+// backstop if multiple agent deploys interfere with one another
+// (https://jira.mongodb.org/browse/EVG-5972). It likely can be removed after work to improve amboy
+// job locking or the SSH dependency.
+func TearDownCommandOverSSH() string {
+	chmod := ChmodCommandWithSudo(context.Background(), evergreen.TeardownScriptName, false).Args
+	chmodString := strings.Join(chmod, " ")
+	sh := ShCommandWithSudo(context.Background(), evergreen.TeardownScriptName, false).Args
+	shString := strings.Join(sh, " ")
+	return fmt.Sprintf("%s && %s", chmodString, shString)
+}
+
+func ShCommandWithSudo(ctx context.Context, script string, sudo bool) *exec.Cmd {
+	if sudo {
+		return exec.CommandContext(ctx, "sudo", "sh", script)
+	}
+	return exec.CommandContext(ctx, "sh", script)
+}
+
+func ChmodCommandWithSudo(ctx context.Context, script string, sudo bool) *exec.Cmd {
+	args := []string{}
+	if sudo {
+		args = append(args, "sudo")
+	}
+	args = append(args, "chmod", "+x", script)
+	return exec.CommandContext(ctx, args[0], args[1:]...)
 }
 
 func (h *Host) CurlCommand(url string) string {
