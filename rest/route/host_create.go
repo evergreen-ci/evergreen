951b96415d82850a56c4c8ab3ea736156a67a1b9
Annie Black <annie.black@10gen.com>
2019-04-03 15:39:58 -0400

    EVG-5895 move creating/assigning parents out of scheduler (#2174)

--- rest/route/host_create.go
+++ rest/route/host_create.go
@@ -11,6 +11,7 @@ import (
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/apimodels"
 	dbModel "github.com/evergreen-ci/evergreen/model"
+	"github.com/evergreen-ci/evergreen/model/distro"
 	"github.com/evergreen-ci/evergreen/model/host"
 	"github.com/evergreen-ci/evergreen/rest/data"
 	"github.com/evergreen-ci/evergreen/rest/model"
@@ -68,6 +69,28 @@ func (h *hostCreateHandler) Run(ctx context.Context) gimlet.Responder {
 	numHosts, err := strconv.Atoi(h.createHost.NumHosts)
 	if err != nil {
 		return gimlet.MakeJSONErrorResponder(err)
+	}
+
+	// need to first generate the number of container hosts if it's relevant
+	if h.createHost.CloudProvider == evergreen.ProviderNameDocker {
+		distro, err := distro.FindOne(distro.ById(h.createHost.Distro))
+		if err != nil {
+			return gimlet.NewJSONErrorResponse(errors.Wrapf(err, "error getting distro '%s'", h.createHost.Distro))
+		}
+		settings, err := evergreen.GetConfig()
+		if err != nil {
+			return gimlet.NewJSONErrorResponse(errors.Wrap(err, "error getting settings config"))
+		}
+
+		containerPool := settings.ContainerPools.GetContainerPool(distro.ContainerPool)
+		if containerPool == nil {
+			return gimlet.NewJSONErrorResponse(errors.Errorf("container pool '%s' not found", distro.ContainerPool))
+		}
+
+		_, numHosts, err = host.InsertParentIntentsAndGetNumHostsToSpawn(containerPool, 1, true)
+		if err != nil {
+			return gimlet.NewJSONErrorResponse(errors.Wrap(err, "error creating parent intents and number of hosts to spawn"))
+		}
 	}
 
 	ids := []string{}
