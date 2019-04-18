60dc6f14d00c044ed81658691bd0b18504d26d82
Brian Samek <brian.samek@mongodb.com>
2019-04-18 09:23:43 -0400

    pr

--- environment.go
+++ environment.go
@@ -339,8 +339,7 @@ func (e *envState) createGenerateTasksQueue(ctx context.Context) error {
 	e.remoteQueueGroup = remoteQueueGroup
 
 	e.closers["generate-tasks"] = func(ctx context.Context) error {
-		closerCtx, _ := context.WithTimeout(context.Background(), 30*time.Second) // nolint
-		e.remoteQueueGroup.Close(closerCtx)
+		e.remoteQueueGroup.Close(ctx)
 		return nil
 	}
 
