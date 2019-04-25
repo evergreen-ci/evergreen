4ff501bf0c56f5ce628f842bf45c103e59a2443d
Brian Samek <brian.samek@mongodb.com>
2019-04-25 16:27:56 -0400

    actually let's keep that log message

--- service/api_task.go
+++ service/api_task.go
@@ -268,6 +268,13 @@ func (as *APIServer) EndTask(w http.ResponseWriter, r *http.Request) {
 		endTaskResp.ShouldExit = true
 	}
 
+	grip.Info(message.Fields{
+		"message":   "Successfully marked task as finished",
+		"task":      t.Id,
+		"execution": t.Execution,
+		"operation": "mark end",
+		"duration":  time.Since(finishTime),
+	})
 	gimlet.WriteJSON(w, endTaskResp)
 }
 
