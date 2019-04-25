5a8ffad3f5eb0dd91bdc9751c75beb61cc910d00
Brian Samek <brian.samek@mongodb.com>
2019-04-25 13:44:10 -0400

    more robust generate poll testing

--- rest/data/generate_test.go
+++ rest/data/generate_test.go
@@ -2,37 +2,86 @@ package data
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/dependency"
 	"github.com/mongodb/amboy/job"
+	"github.com/mongodb/amboy/pool"
 	"github.com/mongodb/amboy/queue"
-	"github.com/stretchr/testify/assert"
+	"github.com/mongodb/amboy/registry"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func TestGeneratePoll(t *testing.T) {
-	assert := assert.New(t)
+	registry.AddJobType("mock", func() amboy.Job { return newMockJob() })
+	require := require.New(t)
 	gc := &GenerateConnector{}
-	q := queue.NewLocalUnordered(1)
+
+	// Set up the queue
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	opts := queue.DefaultMongoDBOptions()
+	opts.DB = "amboy_test_generate"
+	client, err := mongo.NewClient(options.Client().ApplyURI(opts.URI))
+	require.NoError(err)
+	require.NoError(client.Connect(ctx))
+	require.NoError(client.Database(opts.DB).Drop(ctx))
+	singlemdb, err := queue.OpenNewMongoDriver(ctx, opts.DB, opts, client)
+	require.NoError(err)
+	q := queue.NewRemoteUnordered(8)
+	require.NoError(q.SetDriver(singlemdb))
+	require.NoError(q.SetRunner(pool.NewAbortablePool(1, q)))
+	require.NoError(q.Start(context.Background()))
+
+	// Insert some jobs
+	var j *mockJob
+	for i := 2; i < 10; i++ {
+		j = newMockJob()
+		j.SetID(fmt.Sprintf("generate-tasks-%d", i))
+		require.NoError(q.Put(j))
+	}
+
+	// Queue does not contain job we're interested in
 	finished, errs, err := gc.GeneratePoll(context.Background(), "1", q)
-	assert.Empty(errs)
-	assert.False(finished)
-	assert.Error(err)
-	j := &mockJob{}
+	require.Empty(errs)
+	require.False(finished)
+	require.Error(err)
+
+	// Insert some more jobs
+	for i := 10; i < 20; i++ {
+		j = newMockJob()
+		j.SetID(fmt.Sprintf("generate-tasks-%d", i))
+		require.NoError(q.Put(j))
+	}
+
+	// Queue still does not contain job we're interested in
+	finished, errs, err = gc.GeneratePoll(context.Background(), "1", q)
+	require.Empty(errs)
+	require.False(finished)
+	require.Error(err)
+
+	// Put the job
+	j = newMockJob()
 	j.SetID("generate-tasks-1")
-	assert.NoError(q.Start(context.Background()))
-	assert.NoError(q.Put(j))
+	require.NoError(q.Put(j))
+
+	// Job should be there, but not finished
 	finished, errs, err = gc.GeneratePoll(context.Background(), "1", q)
-	assert.False(finished)
-	assert.Empty(errs)
-	assert.NoError(err)
-	time.Sleep(20 * time.Millisecond)
+	require.False(finished)
+	require.Empty(errs)
+	require.NoError(err)
+
+	// Job should be there, and finished
+	time.Sleep(time.Second)
 	finished, errs, err = gc.GeneratePoll(context.Background(), "1", q)
-	assert.True(finished)
-	assert.Empty(errs)
-	assert.NoError(err)
+	require.True(finished)
+	require.Empty(errs)
+	require.NoError(err)
 }
 
 type mockJob struct {
