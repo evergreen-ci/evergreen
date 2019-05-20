e86ef3085ba2b02dc09f56b61d2b13cbadccc7ef
John Liu <john.liu@mongodb.com>
2019-05-20 16:35:01 -0400

    more compile

--- model/task/task_test.go
+++ model/task/task_test.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -1051,7 +1052,7 @@ func TestBulkInsert(t *testing.T) {
 		Version: "version",
 	}
 	tasks := Tasks{&t1, &t2, &t3}
-	assert.NoError(tasks.InsertUnordered())
+	assert.NoError(tasks.InsertUnordered(context.Background()))
 	dbTasks, err := Find(ByVersion("version"))
 	assert.NoError(err)
 	assert.Len(dbTasks, 3)
