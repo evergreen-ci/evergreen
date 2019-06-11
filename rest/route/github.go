cd26b5e28081b12380e32b3b4d897812af7d846d
Jonathan Brill <jonathan.brill@10gen.com>
2019-06-11 16:39:23 -0400

    EVG-6154: PR Status (#2362)

--- rest/route/github.go
+++ rest/route/github.go
@@ -2,7 +2,6 @@ package route
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -11,6 +10,7 @@ import (
 	"github.com/evergreen-ci/evergreen/model/patch"
 	"github.com/evergreen-ci/evergreen/rest/data"
 	"github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/evergreen/thirdparty"
 	"github.com/evergreen-ci/evergreen/units"
 	"github.com/evergreen-ci/gimlet"
 	"github.com/google/go-github/github"
@@ -25,6 +25,8 @@ const (
 	githubActionOpened      = "opened"
 	githubActionSynchronize = "synchronize"
 	githubActionReopened    = "reopened"
+
+	retryComment = "evergreen retry"
 )
 
 type githubHookApi struct {
@@ -131,41 +133,29 @@ func (gh *githubHookApi) Run(ctx context.Context) gimlet.Responder {
 
 		if *event.Action == githubActionOpened || *event.Action == githubActionSynchronize ||
 			*event.Action == githubActionReopened {
-			ghi, err := patch.NewGithubIntent(gh.msgID, event)
-			if err != nil {
-				grip.Error(message.WrapError(err, message.Fields{
-					"source":  "github hook",
-					"msg_id":  gh.msgID,
-					"event":   gh.eventType,
-					"action":  event.Action,
-					"message": "failed to create intent",
-				}))
-				return gimlet.NewJSONErrorResponse(gimlet.ErrorResponse{
-					StatusCode: http.StatusBadRequest,
-					Message:    err.Error(),
-				})
-			}
-
 			grip.Info(message.Fields{
 				"source":    "github hook",
 				"msg_id":    gh.msgID,
 				"event":     gh.eventType,
-				"action":    event.Action,
+				"repo":      *event.PullRequest.Base.Repo.FullName,
+				"ref":       *event.PullRequest.Base.Ref,
+				"pr_number": *event.PullRequest.Number,
+				"hash":      *event.PullRequest.Head.SHA,
 				"message":   "pr accepted, attempting to queue",
-				"repo":      event.Repo.FullName,
-				"ref":       event.PullRequest.Base.Ref,
-				"pr_number": event.Number,
-				"creator":   event.Sender.Login,
-				"hash":      event.PullRequest.Head.SHA,
 			})
-
-			if err := gh.sc.AddPatchIntent(ghi, gh.queue); err != nil {
-				return gimlet.NewJSONErrorResponse(gimlet.ErrorResponse{
-					StatusCode: http.StatusInternalServerError,
-					Message:    err.Error(),
-				})
+			if err := gh.AddIntentForPR(event.PullRequest); err != nil {
+				grip.Error(message.WrapError(err, message.Fields{
+					"source":    "github hook",
+					"msg_id":    gh.msgID,
+					"event":     gh.eventType,
+					"action":    event.Action,
+					"repo":      *event.PullRequest.Base.Repo.FullName,
+					"ref":       *event.PullRequest.Base.Ref,
+					"pr_number": *event.PullRequest.Number,
+					"message":   "can't add intent",
+				}))
+				return gimlet.NewJSONErrorResponse(err)
 			}
-
 		} else if *event.Action == githubActionClosed {
 			grip.Info(message.Fields{
 				"source":  "github hook",
@@ -176,14 +166,14 @@ func (gh *githubHookApi) Run(ctx context.Context) gimlet.Responder {
 			})
 
 			err := gh.sc.AbortPatchesFromPullRequest(event)
-			grip.ErrorWhen(err != nil, message.WrapError(err, message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  *event.Action,
-				"message": "failed to abort patches",
-			}))
 			if err != nil {
+				grip.Error(message.WrapError(err, message.Fields{
+					"source":  "github hook",
+					"msg_id":  gh.msgID,
+					"event":   gh.eventType,
+					"action":  *event.Action,
+					"message": "failed to abort patches",
+				}))
 				return gimlet.MakeJSONErrorResponder(err)
 			}
 
@@ -197,147 +187,154 @@ func (gh *githubHookApi) Run(ctx context.Context) gimlet.Responder {
 		return gimlet.NewJSONResponse(struct{}{})
 
 	case *github.IssueCommentEvent:
-		isPullRequestComment := event.Issue.IsPullRequest()
-		triggersCommitq := commitqueue.TriggersCommitQueue(*event.Action, *event.Comment.Body)
-		if !(isPullRequestComment && triggersCommitq) {
-			return gimlet.NewJSONResponse(struct{}{})
+		if event.Issue.IsPullRequest() {
+			if commitqueue.TriggersCommitQueue(*event.Action, *event.Comment.Body) {
+				grip.Info(message.Fields{
+					"source":    "github hook",
+					"msg_id":    gh.msgID,
+					"event":     gh.eventType,
+					"repo":      *event.Repo.FullName,
+					"pr_number": *event.Issue.Number,
+					"user":      *event.Sender.Login,
+					"message":   "commit queue triggered",
+				})
+				if err := gh.commitQueueEnqueue(ctx, event); err != nil {
+					grip.Error(message.WrapError(err, message.Fields{
+						"source":    "github hook",
+						"msg_id":    gh.msgID,
+						"event":     gh.eventType,
+						"action":    event.Action,
+						"repo":      *event.Repo.FullName,
+						"pr_number": *event.Issue.Number,
+						"user":      *event.Sender.Login,
+						"message":   "can't enqueue on commit queue",
+					}))
+					return gimlet.MakeJSONErrorResponder(err)
+				}
+			}
+			if triggersRetry(*event.Action, *event.Comment.Body) {
+				grip.Info(message.Fields{
+					"source":    "github hook",
+					"msg_id":    gh.msgID,
+					"event":     gh.eventType,
+					"repo":      *event.Repo.FullName,
+					"pr_number": *event.Issue.Number,
+					"user":      *event.Sender.Login,
+					"message":   "retry triggered",
+				})
+				if err := gh.retryPRPatch(ctx, event.Repo.Owner.GetLogin(), event.Repo.GetName(), event.Issue.GetNumber()); err != nil {
+					grip.Error(message.WrapError(err, message.Fields{
+						"source":    "github hook",
+						"msg_id":    gh.msgID,
+						"event":     gh.eventType,
+						"action":    event.Action,
+						"repo":      *event.Repo.FullName,
+						"pr_number": *event.Issue.Number,
+						"user":      *event.Sender.Login,
+						"message":   "can't retry PR",
+					}))
+					return gimlet.MakeJSONErrorResponder(err)
+				}
+			}
 		}
-
-		userRepo := data.UserRepoInfo{
-			Username: *event.Comment.User.Login,
-			Owner:    *event.Repo.Owner.Login,
-			Repo:     *event.Repo.Name,
-		}
-		authorized, err := gh.sc.IsAuthorizedToPatchAndMerge(ctx, gh.settings, userRepo)
-		if err != nil {
-			grip.Error(message.WrapError(err, message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"user":    userRepo.Username,
-				"message": "get authorized failed",
-			}))
-			return gimlet.MakeJSONErrorResponder(errors.Wrap(err, "can't get user info from GitHub API"))
-		}
-		if !authorized {
-			grip.Error(message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"user":    userRepo.Username,
-				"message": "user is not authorized to merge",
-			})
-			return gimlet.MakeJSONErrorResponder(gimlet.ErrorResponse{
-				StatusCode: http.StatusUnauthorized,
-				Message:    fmt.Sprintf("user '%s' is not authorized to merge", userRepo.Username),
-			})
-		}
-
-		PRNum := *event.Issue.Number
-		pr, err := gh.sc.GetGitHubPR(ctx, userRepo.Owner, userRepo.Repo, PRNum)
-		if err != nil {
-			grip.Error(message.WrapError(err, message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"item":    PRNum,
-				"message": "get pr failed",
-			}))
-			return gimlet.MakeJSONErrorResponder(errors.Wrap(err, "can't get PR from GitHub API"))
-		}
-
-		if pr == nil || pr.Base == nil || pr.Base.Ref == nil {
-			grip.Error(message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"item":    PRNum,
-				"message": "PR contains no base branch ref",
-			})
-			return gimlet.MakeJSONErrorResponder(errors.New("PR contains no base branch label"))
-		}
-
-		modules := model.ParseGitHubCommentModules(*event.Comment.Body)
-		baseBranch := *pr.Base.Ref
-		projectRef, err := gh.sc.GetProjectWithCommitQueueByOwnerRepoAndBranch(userRepo.Owner, userRepo.Repo, baseBranch)
-		if err != nil {
-			grip.Error(message.WrapError(err, message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"branch":  baseBranch,
-				"item":    PRNum,
-				"message": "can't find project",
-			}))
-			return gimlet.MakeJSONErrorResponder(errors.Wrap(err, "can't find project"))
-		}
-		item := model.APICommitQueueItem{
-			Issue:   model.ToAPIString(strconv.Itoa(PRNum)),
-			Modules: modules,
-		}
-		_, err = gh.sc.EnqueueItem(projectRef.Identifier, item)
-		if err != nil {
-			grip.Error(message.WrapError(err, message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"item":    PRNum,
-				"message": "commit queue enqueue failed",
-			}))
-			return gimlet.MakeJSONErrorResponder(errors.Wrap(err, "can't enqueue item on commit queue"))
-		}
-
-		if pr == nil || pr.Head == nil || pr.Head.SHA == nil {
-			grip.Error(message.Fields{
-				"source":  "github hook",
-				"msg_id":  gh.msgID,
-				"event":   gh.eventType,
-				"action":  event.Action,
-				"owner":   userRepo.Owner,
-				"repo":    userRepo.Repo,
-				"item":    PRNum,
-				"message": "PR contains no head branch SHA",
-			})
-			return gimlet.MakeJSONErrorResponder(errors.New("PR contains no head branch SHA"))
-		}
-		pushJob := units.NewGithubStatusUpdateJobForPushToCommitQueue(userRepo.Owner, userRepo.Repo, *pr.Head.SHA, PRNum)
-		q := evergreen.GetEnvironment().LocalQueue()
-		grip.Error(message.WrapError(q.Put(ctx, pushJob), message.Fields{
-			"source":  "github hook",
-			"msg_id":  gh.msgID,
-			"event":   gh.eventType,
-			"action":  event.Action,
-			"owner":   userRepo.Owner,
-			"repo":    userRepo.Repo,
-			"item":    PRNum,
-			"message": "failed to queue notification for commit queue push",
-		}))
-
-		grip.Info(message.Fields{
-			"source":  "github hook",
-			"msg_id":  gh.msgID,
-			"event":   gh.eventType,
-			"action":  event.Action,
-			"comment": event.Comment.Body,
-			"message": "finished processing comment",
-		})
 	}
 
 	return gimlet.NewJSONResponse(struct{}{})
+}
+
+func (gh *githubHookApi) retryPRPatch(ctx context.Context, owner, repo string, prNumber int) error {
+	settings, err := gh.sc.GetEvergreenSettings()
+	if err != nil {
+		return errors.Wrap(err, "can't get Evergreen settings")
+	}
+	githubToken, err := settings.GetGithubOauthToken()
+	if err != nil {
+		return errors.Wrap(err, "can't get GitHub token from settings")
+	}
+
+	pr, err := thirdparty.GetGithubPullRequest(ctx, githubToken, owner, repo, prNumber)
+	if err != nil {
+		return errors.Wrapf(err, "can't get PR for repo %s:%s, PR #%d", owner, repo, prNumber)
+	}
+
+	return gh.AddIntentForPR(pr)
+}
+
+func (gh *githubHookApi) AddIntentForPR(pr *github.PullRequest) error {
+	ghi, err := patch.NewGithubIntent(gh.msgID, pr)
+	if err != nil {
+		return errors.Wrap(err, "failed to create intent")
+	}
+
+	if err := gh.sc.AddPatchIntent(ghi, gh.queue); err != nil {
+		return errors.Wrap(err, "error saving intent")
+	}
+
+	return nil
+}
+
+func (gh *githubHookApi) commitQueueEnqueue(ctx context.Context, event *github.IssueCommentEvent) error {
+	userRepo := data.UserRepoInfo{
+		Username: *event.Comment.User.Login,
+		Owner:    *event.Repo.Owner.Login,
+		Repo:     *event.Repo.Name,
+	}
+	authorized, err := gh.sc.IsAuthorizedToPatchAndMerge(ctx, gh.settings, userRepo)
+	if err != nil {
+		return errors.Wrap(err, "can't get user info from GitHub API")
+	}
+	if !authorized {
+		return errors.Errorf("user '%s' is not authorized to merge", userRepo.Username)
+	}
+
+	PRNum := *event.Issue.Number
+	pr, err := gh.sc.GetGitHubPR(ctx, userRepo.Owner, userRepo.Repo, PRNum)
+	if err != nil {
+		return errors.Wrap(err, "can't get PR from GitHub API")
+	}
+
+	if pr == nil || pr.Base == nil || pr.Base.Ref == nil {
+		return errors.New("PR contains no base branch label")
+	}
+
+	modules := model.ParseGitHubCommentModules(*event.Comment.Body)
+	baseBranch := *pr.Base.Ref
+	projectRef, err := gh.sc.GetProjectWithCommitQueueByOwnerRepoAndBranch(userRepo.Owner, userRepo.Repo, baseBranch)
+	if err != nil {
+		return errors.Wrapf(err, "can't find project for '%s:%s' tracking branch '%s'", userRepo.Owner, userRepo.Repo, baseBranch)
+	}
+	item := model.APICommitQueueItem{
+		Issue:   model.ToAPIString(strconv.Itoa(PRNum)),
+		Modules: modules,
+	}
+	_, err = gh.sc.EnqueueItem(projectRef.Identifier, item)
+	if err != nil {
+		return errors.Wrap(err, "can't enqueue item on commit queue")
+	}
+
+	if pr == nil || pr.Head == nil || pr.Head.SHA == nil {
+		return errors.New("PR contains no head branch SHA")
+	}
+	pushJob := units.NewGithubStatusUpdateJobForPushToCommitQueue(userRepo.Owner, userRepo.Repo, *pr.Head.SHA, PRNum)
+	q := evergreen.GetEnvironment().LocalQueue()
+	grip.Error(message.WrapError(q.Put(ctx, pushJob), message.Fields{
+		"source":  "github hook",
+		"msg_id":  gh.msgID,
+		"event":   gh.eventType,
+		"action":  event.Action,
+		"owner":   userRepo.Owner,
+		"repo":    userRepo.Repo,
+		"item":    PRNum,
+		"message": "failed to queue notification for commit queue push",
+	}))
+
+	return nil
+}
+
+func triggersRetry(action, comment string) bool {
+	if action == "deleted" {
+		return false
+	}
+
+	return comment == retryComment
 }
