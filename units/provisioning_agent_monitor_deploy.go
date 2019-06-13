edcc7c7f5e5717917291ac96f7771beeb0926805
Kim Tao <kim.tao@mongodb.com>
2019-06-13 10:21:50 -0400

    Clean up agent monitor deploy job

--- units/provisioning_agent_monitor_deploy.go
+++ units/provisioning_agent_monitor_deploy.go
@@ -13,7 +13,6 @@ import (
 	"github.com/evergreen-ci/evergreen/model/event"
 	"github.com/evergreen-ci/evergreen/model/host"
 	"github.com/evergreen-ci/evergreen/util"
-	"github.com/google/shlex"
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/dependency"
 	"github.com/mongodb/amboy/job"
@@ -74,13 +73,11 @@ func NewAgentMonitorDeployJob(env evergreen.Environment, h host.Host, id string)
 func (j *agentMonitorDeployJob) Run(ctx context.Context) {
 	defer j.MarkComplete()
 
-	flags, err := evergreen.GetServiceFlags()
+	disabled, err := j.agentStartDisabled()
 	if err != nil {
 		j.AddError(err)
 		return
-	}
-
-	if flags.AgentStartDisabled {
+	} else if disabled {
 		grip.Debug(message.Fields{
 			"mode":     "degraded",
 			"host":     j.HostID,
@@ -90,18 +87,12 @@ func (j *agentMonitorDeployJob) Run(ctx context.Context) {
 		return
 	}
 
-	if j.host == nil {
-		j.host, err = host.FindOneId(j.HostID)
-		if err != nil {
-			j.AddError(err)
-			return
-		}
-		if j.host == nil {
-			j.AddError(fmt.Errorf("could not find host %s for job %s", j.HostID, j.TaskID))
-			return
-		}
+	if err := j.populateIfUnset(); err != nil {
+		j.AddError(err)
+		return
 	}
-	if util.StringSliceContains(evergreen.DownHostStatus, j.host.Status) {
+
+	if j.hostDown() {
 		grip.Debug(message.Fields{
 			"host_id": j.host.Id,
 			"status":  j.host.Status,
@@ -110,15 +101,10 @@ func (j *agentMonitorDeployJob) Run(ctx context.Context) {
 		return
 	}
 
-	if j.env == nil {
-		j.env = evergreen.GetEnvironment()
-	}
-
-	// Occasionally an agent deploy job will be dispatched around the same time that
-	// PopulateAgentDeployJobs creates another job. These jobs can race. An atomic update on the
-	// needs new agent field will cause the job to fail early here. Updating the last
-	// communicated time prevents the other branches of the PopulateAgentDeployJobs from
-	// immediately applying. Instead MaxLCTInterval must pass.
+	// An atomic update on the needs new agent monitor field will cause
+	// concurrent jobs to fail early here. Updating the last communicated time
+	// prevents PopulateAgentMonitorDeployJobs from immediately running unless
+	// MaxLCTInterval passes.
 	if err = j.host.SetNeedsNewAgentMonitorAtomically(false); err != nil {
 		grip.Info(message.WrapError(err, message.Fields{
 			"message": "needs new agent monitor flag is already false, not deploying new agent monitor",
@@ -131,9 +117,30 @@ func (j *agentMonitorDeployJob) Run(ctx context.Context) {
 	if err = j.host.UpdateLastCommunicated(); err != nil {
 		j.AddError(errors.Wrapf(err, "error setting LCT on host %s", j.host.Id))
 	}
+
 	defer func() {
 		if j.HasErrors() {
-			if err = j.host.SetNeedsNewAgentMonitor(true); err != nil {
+			event.LogHostAgentMonitorDeployFailed(j.host.Id, j.Error())
+
+			noRetries, err := j.checkNoRetries()
+			if err != nil {
+				j.AddError(err)
+			} else if noRetries {
+				if disableErr := j.host.DisablePoisonedHost(fmt.Sprintf("failed %d times to put agent monitor on host", agentMonitorPutRetries)); disableErr != nil {
+					j.AddError(errors.Wrapf(disableErr, "error terminating host %s", j.host.Id))
+					return
+				}
+
+				job := NewDecoHostNotifyJob(j.env, j.host, nil, "error starting agent monitor on host")
+				grip.Error(message.WrapError(j.env.RemoteQueue().Put(ctx, job),
+					message.Fields{
+						"message": fmt.Sprintf("tried %d times to put agent monitor on host", agentMonitorPutRetries),
+						"host_id": j.host.Id,
+						"distro":  j.host.Distro,
+					}))
+				return
+			}
+			if err := j.host.SetNeedsNewAgentMonitor(true); err != nil {
 				grip.Info(message.WrapError(err, message.Fields{
 					"message": "problem setting needs new agent monitor flag to true",
 					"distro":  j.host.Distro,
@@ -145,80 +152,107 @@ func (j *agentMonitorDeployJob) Run(ctx context.Context) {
 	}()
 
 	if j.host.Distro.CommunicationMethod == distro.CommunicationMethodSSH {
-		j.AddError(j.sshStartAgentMonitorOnHost(ctx))
-	} else {
-		// EVG-XXXX: allow agent monitor to be redeployed over RPC.
-	}
-
-	stat, err := event.GetRecentAgentMonitorDeployStatuses(j.HostID, agentMonitorPutRetries)
-	j.AddError(err)
-	if err != nil {
-		return
-	}
-
-	if stat.LastAttemptFailed() && stat.AllAttemptsFailed() && stat.Count >= agentMonitorPutRetries {
-		if disableErr := j.host.DisablePoisonedHost(fmt.Sprintf("failed %d times to put agent monitor on host", agentMonitorPutRetries)); disableErr != nil {
-			j.AddError(errors.Wrapf(disableErr, "error terminating host %s", j.host.Id))
+		sshOpts, err := j.sshOptions(ctx)
+		if err != nil {
+			j.AddError(err)
 			return
 		}
 
-		job := NewDecoHostNotifyJob(j.env, j.host, nil, "error starting agent on host")
-		grip.Error(message.WrapError(j.env.RemoteQueue().Put(ctx, job),
-			message.Fields{
-				"message": fmt.Sprintf("tried %d times to put agent monitor on host", agentMonitorPutRetries),
-				"host_id": j.host.Id,
-				"distro":  j.host.Distro,
-			}))
+		if err := j.sshFetchClient(ctx, sshOpts); err != nil {
+			j.AddError(err)
+			return
+		}
+
+		if err := j.sshRunSetup(ctx, sshOpts); err != nil {
+			j.AddError(err)
+			return
+		}
+
+		j.AddError(j.sshStartAgentMonitor(ctx, sshOpts))
+	} else {
+		// TODO: EVG-6232: allow agent monitor to be redeployed over RPC.
 	}
+
 }
 
-func (j *agentMonitorDeployJob) getHostMessage() message.Fields {
-	m := message.Fields{
-		"message":  "starting agent on host",
-		"runner":   "taskrunner",
-		"host":     j.host.Host,
-		"distro":   j.host.Distro.Id,
-		"provider": j.host.Distro.Provider,
-	}
-
-	if j.host.InstanceType != "" {
-		m["instance"] = j.host.InstanceType
-	}
-
-	sinceLCT := time.Since(j.host.LastCommunicationTime)
-	if j.host.NeedsNewAgentMonitor {
-		m["reason"] = "flagged for new agent monitor"
-	} else if j.host.LastCommunicationTime.IsZero() {
-		m["reason"] = "new host"
-	} else if sinceLCT > host.MaxLCTInterval {
-		m["reason"] = "host has exceeded last communication threshold"
-		m["threshold"] = host.MaxLCTInterval
-		m["threshold_span"] = host.MaxLCTInterval.String()
-		m["last_communication_at"] = sinceLCT
-		m["last_communication_at_time"] = sinceLCT.String()
-	}
-
-	return m
+// hostDown checks if the host is down.
+func (j *agentMonitorDeployJob) hostDown() bool {
+	return util.StringSliceContains(evergreen.DownHostStatus, j.host.Status)
 }
 
-func (j *agentMonitorDeployJob) sshStartAgentMonitorOnHost(ctx context.Context) error {
+// disableHost changes the host so that it is down and enqueues a job to
+// terminate it.
+func (j *agentMonitorDeployJob) disableHost() error {
+	return nil
+}
+
+// checkNoRetries checks if the job has exhausted the maximum allowed attempts
+// to deploy the agent monitor.
+func (j *agentMonitorDeployJob) checkNoRetries() (bool, error) {
+	stat, err := event.GetRecentAgentMonitorDeployStatuses(j.HostID, agentMonitorPutRetries)
+	if err != nil {
+		return false, errors.Wrap(err, "could not get recent agent monitor deploy statuses")
+	}
+
+	return stat.LastAttemptFailed() && stat.AllAttemptsFailed() && stat.Count >= agentMonitorPutRetries, nil
+}
+
+// sshOptions gets this host's SSH options.
+func (j *agentMonitorDeployJob) sshOptions(ctx context.Context) ([]string, error) {
 	cloudHost, err := cloud.GetCloudHost(ctx, j.host, j.env.Settings())
 	if err != nil {
-		return errors.Wrapf(err, "Failed to get cloud host for %s", j.host.Id)
+		return nil, errors.Wrapf(err, "failed to get cloud host for %s", j.host.Id)
 	}
 
-	sshOptions, err := cloudHost.GetSSHOptions()
+	sshOpts, err := cloudHost.GetSSHOptions()
 	if err != nil {
-		return errors.Wrapf(err, "Error getting ssh options for host %s", j.host.Id)
+		return nil, errors.Wrapf(err, "error getting ssh options for host %s", j.host.Id)
 	}
+	return sshOpts, nil
+}
 
+// sshFetchClient gets the latest version of the client on the host over SSH.
+func (j *agentMonitorDeployJob) sshFetchClient(ctx context.Context, sshOpts []string) error {
 	grip.Info(message.Fields{
-		"message": "prepping host for agent monitor",
+		"message": "fetching latest evergreen client version",
 		"host":    j.host.Id,
 	})
 
-	if logs, err := j.runSetupScript(ctx, sshOptions); err != nil {
+	if logs, err := j.fetchClient(ctx, sshOpts); err != nil {
+		return errors.Wrapf(err, "error downloading agent monitor binary on remote host: %s", logs)
+	}
+
+	return nil
+}
+
+// fetchClient fetches the client on the host through the host's Jasper service.
+func (j *agentMonitorDeployJob) fetchClient(ctx context.Context, sshOpts []string) (string, error) {
+	settings := j.env.Settings()
+	cmd := j.host.CurlCommand(settings.Ui.Url)
+	input := jaspercli.CommandInput{Commands: [][]string{[]string{cmd}}}
+
+	output, err := j.host.RunSSHJasperRequest(ctx, j.env, "create-process", input, sshOpts)
+	if err != nil {
+		return output, errors.Wrap(err, "problem creating command")
+	}
+
+	if _, err := jaspercli.ExtractOutcomeResponse([]byte(output)); err != nil {
+		return output, errors.Wrap(err, "error in request outcome")
+	}
+
+	return output, nil
+}
+
+// sshRunSetup runs the setup script on the host over SSH.
+func (j *agentMonitorDeployJob) sshRunSetup(ctx context.Context, sshOpts []string) error {
+	grip.Info(message.Fields{
+		"message": "running setup script",
+		"host":    j.host.Id,
+	})
+
+	if logs, err := j.runSetupScript(ctx, sshOpts); err != nil {
 		event.LogProvisionFailed(j.host.Id, logs)
+
 		errMsg := "error running setup script on host"
 		msg := message.Fields{
 			"message": errMsg,
@@ -241,9 +275,37 @@ func (j *agentMonitorDeployJob) sshStartAgentMonitorOnHost(ctx context.Context) 
 			"distro":  j.host.Distro,
 		}))
 
-		event.LogHostAgentMonitorDeployFailed(j.host.Id, err)
 		return err
 	}
+
+	return nil
+}
+
+// runSetupScript runs the setup script on the host through the host's Jasper
+// service.
+func (j *agentMonitorDeployJob) runSetupScript(ctx context.Context, sshOpts []string) (string, error) {
+	cmd := j.host.SetupCommand()
+	input := jaspercli.CommandInput{Commands: [][]string{[]string{cmd}}}
+
+	output, err := j.host.RunSSHJasperRequest(ctx, j.env, "create-command", input, sshOpts)
+	if err != nil {
+		return output, errors.Wrap(err, "problem creating command")
+	}
+
+	if _, err := jaspercli.ExtractOutcomeResponse([]byte(output)); err != nil {
+		return output, errors.Wrap(err, "error in request outcome")
+	}
+
+	return output, nil
+}
+
+/// sshStartAgentMonitor starts the agent monitor on the host.
+func (j *agentMonitorDeployJob) sshStartAgentMonitor(ctx context.Context, sshOpts []string) error {
+
+	grip.Info(message.Fields{
+		"message": "prepping host for agent monitor",
+		"host":    j.host.Id,
+	})
 
 	grip.Info(message.Fields{
 		"message": "prepping host finished successfully",
@@ -253,13 +315,13 @@ func (j *agentMonitorDeployJob) sshStartAgentMonitorOnHost(ctx context.Context) 
 
 	// Generate the host secret if none exists.
 	if j.host.Secret == "" {
-		if err = j.host.CreateSecret(); err != nil {
+		if err := j.host.CreateSecret(); err != nil {
 			return errors.Wrapf(err, "creating secret for %s", j.host.Id)
 		}
 	}
 
-	grip.Info(j.getHostMessage())
-	output, err := j.host.RunSSHJasperRequest(ctx, j.env, "create-command", j.agentMonitorRequestInput(), sshOptions)
+	grip.Info(j.deployMessage())
+	output, err := j.host.RunSSHJasperRequest(ctx, j.env, "create-command", j.agentMonitorRequestInput(), sshOpts)
 	if err != nil {
 		grip.Error(message.WrapError(err, message.Fields{
 			"message": "failed to start agent monitor on host",
@@ -273,25 +335,6 @@ func (j *agentMonitorDeployJob) sshStartAgentMonitorOnHost(ctx context.Context) 
 	event.LogHostAgentMonitorDeployed(j.host.Id)
 
 	return nil
-}
-
-func (j *agentMonitorDeployJob) runSetupScript(ctx context.Context, sshOptions []string) (string, error) {
-	cmd, err := shlex.Split(j.host.SetupCommand())
-	if err != nil {
-		return "", errors.Wrap(err, "problem parsing setup command")
-	}
-
-	input := jasper.CreateOptions{Args: cmd}
-	output, err := j.host.RunSSHJasperRequest(ctx, j.env, "create-process", input, sshOptions)
-	if err != nil {
-		return output, errors.Wrap(err, "failed to create command")
-	}
-
-	if _, err := jaspercli.ExtractOutcomeResponse([]byte(output)); err != nil {
-		return output, errors.Wrap(err, "error getting command outcome")
-	}
-
-	return output, nil
 }
 
 // agentMonitorRequestInput assembles the input to a Jasper request to create
@@ -313,7 +356,7 @@ func (j *agentMonitorDeployJob) agentMonitorRequestInput() jaspercli.CommandInpu
 		"--cleanup",
 		"monitor",
 		fmt.Sprintf("--client_url='%s'", clientURL),
-		fmt.Sprintf("--client_path='%s'", filepath.Join("kim: TODO: EVG-6282", j.host.Distro.BinaryName())),
+		fmt.Sprintf("--client_path='%s'", filepath.Join(j.host.Distro.ClientDir, j.host.Distro.BinaryName())),
 	}
 
 	input := jaspercli.CommandInput{
@@ -345,4 +388,64 @@ func (j *agentMonitorDeployJob) agentEnv(settings *evergreen.Settings) map[strin
 	}
 
 	return env
+}
+
+// deployMessage builds the message containing information preceding an agent
+// monitor deploy.
+func (j *agentMonitorDeployJob) deployMessage() message.Fields {
+	m := message.Fields{
+		"message":  "starting agent monitor on host",
+		"runner":   "taskrunner",
+		"host":     j.host.Host,
+		"distro":   j.host.Distro.Id,
+		"provider": j.host.Distro.Provider,
+	}
+
+	if j.host.InstanceType != "" {
+		m["instance"] = j.host.InstanceType
+	}
+
+	sinceLCT := time.Since(j.host.LastCommunicationTime)
+	if j.host.NeedsNewAgentMonitor {
+		m["reason"] = "flagged for new agent monitor"
+	} else if j.host.LastCommunicationTime.IsZero() {
+		m["reason"] = "new host"
+	} else if sinceLCT > host.MaxLCTInterval {
+		m["reason"] = "host has exceeded last communication threshold"
+		m["threshold"] = host.MaxLCTInterval
+		m["threshold_span"] = host.MaxLCTInterval.String()
+		m["last_communication_at"] = sinceLCT
+		m["last_communication_at_time"] = sinceLCT.String()
+	}
+
+	return m
+}
+
+// agentStartDisabled checks if the agent (and consequently, the agent monitor)
+// should be deployed.
+func (j *agentMonitorDeployJob) agentStartDisabled() (bool, error) {
+	flags, err := evergreen.GetServiceFlags()
+	if err != nil {
+		j.AddError(err)
+		return false, errors.New("could not get service flags")
+	}
+
+	return flags.AgentStartDisabled, nil
+}
+
+// populateIfUnset populates the unset job fields.
+func (j *agentMonitorDeployJob) populateIfUnset() error {
+	if j.host == nil {
+		host, err := host.FindOneId(j.HostID)
+		if err != nil || host == nil {
+			return errors.Wrapf(err, "could not find host %s for job %s", j.HostID, j.TaskID)
+		}
+		j.host = host
+	}
+
+	if j.env == nil {
+		j.env = evergreen.GetEnvironment()
+	}
+
+	return nil
 }
