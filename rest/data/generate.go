662124b9e4d764faf60628899fe52bf7e86f737e
Brian Samek <brian.samek@mongodb.com>
2019-06-18 17:23:17 -0400

    fix compile

--- rest/data/generate.go
+++ rest/data/generate.go
@@ -29,7 +29,7 @@ func (gc *GenerateConnector) GenerateTasks(ctx context.Context, taskID string, j
 	if err != nil {
 		return errors.Wrapf(err, "problem getting queue for version %s", t.Version)
 	}
-	err := q.Put(ctx, units.NewGenerateTasksJob(taskID, jsonBytes))
+	err = q.Put(ctx, units.NewGenerateTasksJob(taskID, jsonBytes))
 	grip.DebugWhen(err != nil, errors.Wrapf(err, "problem saving new generate tasks job for task '%s'", taskID))
 	return nil
 }
