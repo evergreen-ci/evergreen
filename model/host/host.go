ee871638754055e58ddd808fba4ebd5ab9718da9
Sam Kleinman <sam@10gen.com>
2019-06-24 17:24:48 -0400

    EVG-6331: Host Pool Stats (#2420)

--- model/host/host.go
+++ model/host/host.go
@@ -1245,6 +1245,39 @@ func (hosts HostGroup) GetHostIds() []string {
 	return ids
 }
 
+type HostGroupStats struct {
+	Quarantined    int `bson:"quarantined" json:"quarantined" yaml:"quarantined"`
+	Decommissioned int `bson:"decommissioned" json:"decommissioned" yaml:"decommissioned"`
+	Idle           int `bson:"idle" json:"idle" yaml:"idle"`
+	Active         int `bson:"active" json:"active" yaml:"active"`
+	Provisioning   int `bson:"provisioning" json:"provisioning" yaml:"provisioning"`
+	Total          int `bson:"total" json:"total" yaml:"total"`
+}
+
+func (hosts HostGroup) Stats() HostGroupStats {
+	out := HostGroupStats{}
+
+	for _, h := range hosts {
+		out.Total++
+
+		if h.Status == evergreen.HostQuarantined {
+			out.Quarantined++
+		} else if h.Status == evergreen.HostDecommissioned {
+			out.Decommissioned++
+		} else if h.Status != evergreen.HostRunning {
+			out.Provisioning++
+		} else if h.Status == evergreen.HostRunning {
+			if h.RunningTask == "" {
+				out.Idle++
+			} else {
+				out.Active++
+			}
+		}
+	}
+
+	return out
+}
+
 // getNumContainersOnParents returns a slice of uphost parents and their respective
 // number of current containers currently running in order of longest expected
 // finish time
