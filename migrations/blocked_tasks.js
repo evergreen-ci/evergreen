756549649d9f29af3e28d7e3e758e5fa633eae32
Jonathan Brill <Jonathan.Brill@mongodb.com>
2019-07-08 14:40:12 -0400

    remove create_time filter

--- migrations/blocked_tasks.js
+++ migrations/blocked_tasks.js
@@ -48,63 +48,59 @@ function completeVersions(tasks) {
 //
 // example invocation:
 //
-// startingDate = ISODate("2013-01-01T00:00:00Z")
-// millisecondsPerDay = 86400000
-// delta = millisecondsPerDay * 1
 // sleepMilliseconds = 100
 // limit = 1000
-// migrate(startingDate, delta, sleepMilliseconds, limit)
+// migrate(sleepMilliseconds, limit)
 
-function migrate(startingDate, delta, sleepMilliseconds, limit) {
-    while(startingDate < new Date()) {
-        printjson(startingDate)
-        loops = 0
-        tasksSize = 0
-        while (true) {
-            tasks = db.tasks.find({"create_time": {"$gte": startingDate, "$lte": new Date(startingDate.getTime() + delta)}, "status":"undispatched", "depends_on.status": {"$in": ["success", "failed", "", "*"]}, "depends_on": {"$elemMatch":{"unattainable": {"$exists": false}}}}, {"depends_on":1}).limit(limit).toArray()
-            tasks = completeVersions(tasks)
-            if (tasks.length == 0 || tasks.length == tasksSize) {
-                break
-            }
-            tasksSize = tasks.length
-            printjson(loops++)
+function migrate(sleepMilliseconds, limit) {
+    var loops = 0
+    while(true) {
+        var tasks = db.tasks.find({"status":"undispatched", "depends_on.status": {"$in": ["success", "failed", "", "*"]}, "depends_on": {"$elemMatch":{"unattainable": {"$exists": false}}}}, {"depends_on":1}).limit(limit).toArray()
+        tasks = completeVersions(tasks)
+        if (tasks.length == 0) {
+            break
+        }
+        printjson(loops++)
 
-            for (i=0; i < tasks.length; i++) {
-                taskUpdated = false
-                dependsOn = tasks[i].depends_on
-                for (j=0; j < dependsOn.length; j++) {
-                    if(dependsOn[j].unattainable != null) {
-                        continue
-                    }
-                    
-                    depTasksMap = getDepTaskMap(task)
-                    if (!(dependsOn[j]._id in depTasksMap)) {
-                        break
-                    }
-                    depTask = depTasksMap[dependsOn[j]._id]
-                    if (finishedStatuses.includes(depTask.status)) {
-                        taskUpdated = true
-                        // 1st degree blocked
-                        if(dependsOn[j].status != "*" && depTask.status != dependsOn[j].status) {
-                            dependsOn[j].unattainable = true
-                        } else {
-                            dependsOn[j].unattainable = false
-                        }
-                    } else if (dependsOn[j].status != "*" && taskIsBlocked(depTask._id)) {
-                        taskUpdated = true
+        var tasksUpdated = 0
+        for (i=0; i < tasks.length; i++) {
+            taskUpdated = false
+            dependsOn = tasks[i].depends_on
+            for (j=0; j < dependsOn.length; j++) {
+                if(dependsOn[j].unattainable != null) {
+                    continue
+                }
+                
+                depTasksMap = getDepTaskMap(task)
+                if (!(dependsOn[j]._id in depTasksMap)) {
+                    break
+                }
+                depTask = depTasksMap[dependsOn[j]._id]
+                if (finishedStatuses.includes(depTask.status)) {
+                    taskUpdated = true
+                    // 1st degree blocked
+                    if(dependsOn[j].status != "*" && depTask.status != dependsOn[j].status) {
                         dependsOn[j].unattainable = true
-                    } else if (unattainableResolved(depTask._id)) {
-                        taskUpdated = true
+                    } else {
                         dependsOn[j].unattainable = false
                     }
-                }
-                if(taskUpdated) {
-                    db.tasks.updateOne({"_id": tasks[i]._id}, {"$set": {"depends_on": dependsOn}})
+                } else if (dependsOn[j].status != "*" && taskIsBlocked(depTask._id)) {
+                    taskUpdated = true
+                    dependsOn[j].unattainable = true
+                } else if (unattainableResolved(depTask._id)) {
+                    taskUpdated = true
+                    dependsOn[j].unattainable = false
                 }
             }
-
+            if(taskUpdated) {
+                tasksUpdated++
+                db.tasks.updateOne({"_id": tasks[i]._id}, {"$set": {"depends_on": dependsOn}})
+            }
         }
-        startingDate = new Date(startingDate.getTime() + delta)
+        if(tasksUpdated == 0) {
+            break
+        }
+
         sleep(sleepMilliseconds)
     }
 }
