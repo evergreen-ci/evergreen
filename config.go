a5524154e484ec758ac47056b35711234746548a
Annie Black <annie.black@10gen.com>
2019-07-16 13:48:09 -0400

    EVG-6333 use different client (#2463)

--- config.go
+++ config.go
@@ -26,7 +26,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2019-07-11"
+	ClientVersion = "2019-07-16"
 )
 
 // ConfigSection defines a sub-document in the evegreen config
