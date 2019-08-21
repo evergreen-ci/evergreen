4410f687812cba100eccc77fd150283fe7c60992
Kim Tao <kim.tao@mongodb.com>
2019-08-21 16:56:36 -0400

    EVG-6636: increase allowed cost delta (#2584)

--- units/host_termination_test.go
+++ units/host_termination_test.go
@@ -108,7 +108,7 @@ func TestHostCosts(t *testing.T) {
 	assert.InDelta(5, dbHost.TotalCost, 0.002)
 	dbTask, err := task.FindOneId(t1.Id)
 	assert.NoError(err)
-	assert.InDelta(5, dbTask.SpawnedHostCost, 0.001)
+	assert.InDelta(5, dbTask.SpawnedHostCost, 0.002)
 }
 
 ////////////////////////////////////////////////////////////////////////
