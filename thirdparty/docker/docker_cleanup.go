de6342a5b86e434a8c6c6ff9df20be48a0cd70f3
Jonathan Brill <Jonathan.Brill@mongodb.com>
2019-08-21 11:41:58 -0400

    fix for code review

--- thirdparty/docker/docker_cleanup.go
+++ thirdparty/docker/docker_cleanup.go
@@ -7,6 +7,7 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 )
 
@@ -28,48 +29,65 @@ func Cleanup(ctx context.Context, logger grip.Journaler) error {
 		return errors.Wrap(err, "can't get Docker info")
 	}
 
-	logger.Infof("Removing: %d containers (%d running, %d paused, %d stopped), %d images",
-		info.Containers,
-		info.ContainersRunning,
-		info.ContainersPaused,
-		info.ContainersStopped,
-		info.Images,
-	)
+	logger.Info(message.Fields{
+		"message":            "removing docker artifacts",
+		"containers_total":   info.Containers,
+		"containers_running": info.ContainersRunning,
+		"containers_paused":  info.ContainersPaused,
+		"containers_stopped": info.ContainersStopped,
+		"images":             info.Images,
+	})
 
 	catcher := grip.NewBasicCatcher()
+	catcher.Add(cleanContainers(ctx, dockerClient))
+	catcher.Add(cleanImages(ctx, dockerClient))
+	catcher.Add(cleanVolumes(ctx, dockerClient, logger))
 
-	// clean up containers
+	return catcher.Resolve()
+}
+
+func cleanContainers(ctx context.Context, dockerClient *client.Client) error {
 	containers, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
-		catcher.Add(errors.Wrap(err, "can't get list of containers"))
-	}
-	for _, container := range containers {
-		catcher.Add(errors.Wrapf(dockerClient.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{Force: true}), "can't remove container '%s'", container.ID))
+		return errors.Wrap(err, "can't get list of containers")
 	}
 
-	// clean up images
+	catcher := grip.NewBasicCatcher()
+	for _, container := range containers {
+		catcher.Wrapf(dockerClient.ContainerRemove(ctx, container.ID, types.ContainerRemoveOptions{Force: true}), "can't remove container '%s'", container.ID)
+	}
+
+	return catcher.Resolve()
+}
+
+func cleanImages(ctx context.Context, dockerClient *client.Client) error {
 	images, err := dockerClient.ImageList(ctx, types.ImageListOptions{All: true})
 	if err != nil {
-		catcher.Add(errors.Wrap(err, "can't get image list"))
+		return errors.Wrap(err, "can't get image list")
 	}
+
+	catcher := grip.NewBasicCatcher()
 	for _, image := range images {
 		resp, err := dockerClient.ImageRemove(ctx, image.ID, types.ImageRemoveOptions{Force: true})
-		if err != nil {
-			catcher.Add(errors.Wrapf(err, "can't remove image '%s'", image.ID))
-		}
+		catcher.Wrapf(err, "can't remove image '%s'", image.ID)
 		if len(resp) != 1 || resp[0].Deleted != image.ID {
-			catcher.Add(errors.Wrapf(err, "image '%s' wasn't deleted", image.ID))
+			catcher.Errorf("image '%s' wasn't deleted", image.ID)
 		}
 	}
 
-	// clean up volumes
+	return catcher.Resolve()
+}
+
+func cleanVolumes(ctx context.Context, dockerClient *client.Client, logger grip.Journaler) error {
 	volumes, err := dockerClient.VolumeList(ctx, filters.Args{})
 	if err != nil {
-		catcher.Add(errors.Wrap(err, "can't get volume list"))
+		return errors.Wrap(err, "can't get volume list")
 	}
+
 	logger.Infof("Removing %d volumes", len(volumes.Volumes))
+	catcher := grip.NewBasicCatcher()
 	for _, volume := range volumes.Volumes {
-		catcher.Add(errors.Wrapf(dockerClient.VolumeRemove(ctx, volume.Name, true), "can't remove volume '%s'", volume.Name))
+		catcher.Wrapf(dockerClient.VolumeRemove(ctx, volume.Name, true), "can't remove volume '%s'", volume.Name)
 	}
 
 	return catcher.Resolve()
---
+++ thirdparty/docker/docker_cleanup_test.go
@@ -0,0 +1,100 @@
+package docker
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/volume"
+	"github.com/docker/docker/client"
+	"github.com/mongodb/grip"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCleanup(t *testing.T) {
+	dockerClient, err := client.NewClientWithOpts()
+	require.NoError(t, err)
+	ctx := context.Background()
+
+	for name, test := range map[string]func(*testing.T){
+		"cleanContainers": func(*testing.T) {
+			resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
+				Image: "hello-world",
+			}, nil, nil, "")
+			require.NoError(t, err)
+			require.NoError(t, dockerClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}))
+			info, err := dockerClient.Info(ctx)
+			require.NoError(t, err)
+			require.True(t, info.ContainersRunning > 0)
+
+			assert.NoError(t, cleanContainers(context.Background(), dockerClient))
+
+			info, err = dockerClient.Info(ctx)
+			assert.NoError(t, err)
+			assert.Equal(t, 0, info.Containers)
+		},
+		"cleanImages": func(*testing.T) {
+			assert.NoError(t, cleanImages(context.Background(), dockerClient))
+
+			info, err := dockerClient.Info(ctx)
+			assert.NoError(t, err)
+			assert.Equal(t, 0, info.Images)
+		},
+		"cleanVolumes": func(*testing.T) {
+			_, err = dockerClient.VolumeCreate(ctx, volume.VolumeCreateBody{})
+			require.NoError(t, err)
+			volumes, err := dockerClient.VolumeList(ctx, filters.Args{})
+			require.NoError(t, err)
+			require.True(t, len(volumes.Volumes) > 0)
+
+			assert.NoError(t, cleanVolumes(context.Background(), dockerClient, grip.NewJournaler("")))
+
+			volumes, err = dockerClient.VolumeList(ctx, filters.Args{})
+			assert.NoError(t, err)
+			assert.Len(t, volumes.Volumes, 0)
+		},
+		"Cleanup": func(*testing.T) {
+			resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
+				Image: "hello-world",
+			}, nil, nil, "")
+			require.NoError(t, err)
+			require.NoError(t, dockerClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}))
+			info, err := dockerClient.Info(ctx)
+			require.NoError(t, err)
+			require.True(t, info.ContainersRunning > 0)
+			require.True(t, info.Images > 0)
+
+			_, err = dockerClient.VolumeCreate(ctx, volume.VolumeCreateBody{})
+			require.NoError(t, err)
+			volumes, err := dockerClient.VolumeList(ctx, filters.Args{})
+			require.NoError(t, err)
+			require.True(t, len(volumes.Volumes) > 0)
+
+			assert.NoError(t, Cleanup(context.Background(), grip.NewJournaler("")))
+
+			info, err = dockerClient.Info(ctx)
+			assert.NoError(t, err)
+			assert.Equal(t, 0, info.Containers)
+			assert.Equal(t, 0, info.Images)
+
+			volumes, err = dockerClient.VolumeList(ctx, filters.Args{})
+			assert.NoError(t, err)
+			assert.Len(t, volumes.Volumes, 0)
+		},
+	} {
+		out, err := dockerClient.ImagePull(ctx, "hello-world", types.ImagePullOptions{})
+		require.NoError(t, err)
+		defer out.Close()
+		io.Copy(ioutil.Discard, out)
+		info, err := dockerClient.Info(ctx)
+		require.NoError(t, err)
+		require.Equal(t, 1, info.Images)
+
+		t.Run(name, test)
+	}
+}
