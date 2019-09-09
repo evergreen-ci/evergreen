60788eca2454682f882ae00e4e9b4659d651e119
Sam Kleinman <sam@10gen.com>
2019-09-09 17:48:05 -0400

    EVG-6701: legacy task group order sorting (#2638)

--- scheduler/setup_funcs.go
+++ scheduler/setup_funcs.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/model/task"
-	"github.com/mongodb/grip"
 	"github.com/pkg/errors"
 )
 
@@ -28,8 +27,6 @@ func PopulateCaches(id string, distroID string, tasks []task.Task) ([]task.Task,
 		tasks:     tasks,
 		runtimeID: id,
 		setupFuncs: []sortSetupFunc{
-			cacheTaskGroups,
-			backfillTaskGroups,
 			cacheExpectedDurations,
 		},
 	}
@@ -38,67 +35,6 @@ func PopulateCaches(id string, distroID string, tasks []task.Task) ([]task.Task,
 	}
 
 	return cmp.tasks, nil
-}
-
-// cacheTaskGroups caches task groups by version. It uses yaml.Unmarshal instead
-// of model.LoadProjectInto and only unmarshals task groups for efficiency.
-func cacheTaskGroups(comparator *CmpBasedTaskComparator) error {
-	comparator.projects = make(map[string]project)
-	for _, v := range comparator.versions {
-		fullP := &model.Project{}
-		if err := model.LoadProjectInto([]byte(v.Config), "", fullP); err != nil {
-			return errors.Wrapf(err, "error getting task groups from version %s", v.Id)
-		}
-		comparator.projects[v.Id] = project{TaskGroups: fullP.TaskGroups}
-	}
-	return nil
-}
-
-func backfillTaskGroups(comparator *CmpBasedTaskComparator) error {
-	catcher := grip.NewBasicCatcher()
-
-OUTERLOOP:
-	for _, t := range comparator.tasks {
-		if t.TaskGroup != "" && t.TaskGroupOrder == 0 {
-			proj := comparator.projects[t.Version]
-			for _, g := range proj.TaskGroups {
-				if g.Name == t.TaskGroup {
-					for idx, tn := range g.Tasks {
-						if t.DisplayName == tn {
-							t.TaskGroupOrder = idx + 1
-							t.TaskGroupMaxHosts = g.MaxHosts
-							catcher.Add(t.SetTaskGroupInfo())
-							continue OUTERLOOP
-						}
-					}
-					catcher.Errorf("task '%s' is missing from task group '%s' in version '%s'",
-						t.DisplayName, t.TaskGroup, t.Version)
-				}
-			}
-		}
-	}
-
-	return catcher.Resolve()
-}
-
-// groupTaskGroups puts tasks that have the same build and task group next to
-// each other in the queue. This ensures that, in a stable sort,
-// byTaskGroupOrder sorts task group members relative to each other.
-func groupTaskGroups(comparator *CmpBasedTaskComparator) error {
-	taskMap := make(map[string]task.Task)
-	taskKeys := []string{}
-	for _, t := range comparator.tasks {
-		k := fmt.Sprintf("%s-%s-%s", t.BuildId, t.TaskGroup, t.Id)
-		taskMap[k] = t
-		taskKeys = append(taskKeys, k)
-	}
-	// Reverse sort to sort task groups to the top, so that they are more
-	// quickly pinned to hosts.
-	sort.Sort(sort.Reverse(sort.StringSlice(taskKeys)))
-	for i, k := range taskKeys {
-		comparator.tasks[i] = taskMap[k]
-	}
-	return nil
 }
 
 func cacheExpectedDurations(comparator *CmpBasedTaskComparator) error {
@@ -132,5 +68,25 @@ func cacheExpectedDurations(comparator *CmpBasedTaskComparator) error {
 
 	comparator.tasks = tasks
 
+	return nil
+}
+
+// groupTaskGroups puts tasks that have the same build and task group next to
+// each other in the queue. This ensures that, in a stable sort,
+// byTaskGroupOrder sorts task group members relative to each other.
+func groupTaskGroups(comparator *CmpBasedTaskComparator) error {
+	taskMap := make(map[string]task.Task)
+	taskKeys := []string{}
+	for _, t := range comparator.tasks {
+		k := fmt.Sprintf("%s-%s-%s", t.BuildId, t.TaskGroup, t.Id)
+		taskMap[k] = t
+		taskKeys = append(taskKeys, k)
+	}
+	// Reverse sort to sort task groups to the top, so that they are more
+	// quickly pinned to hosts.
+	sort.Sort(sort.Reverse(sort.StringSlice(taskKeys)))
+	for i, k := range taskKeys {
+		comparator.tasks[i] = taskMap[k]
+	}
 	return nil
 }
