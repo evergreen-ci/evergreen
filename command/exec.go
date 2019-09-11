c70541df2af2652358bd2de9cfc548938df261cd
Sam Kleinman <sam@10gen.com>
2019-09-10 21:29:34 -0400

    EVG-6650: improve path handling for absolute paths (#2646)

--- command/exec.go
+++ command/exec.go
@@ -220,6 +220,10 @@ func (c *subprocessExec) Execute(ctx context.Context, comm client.Communicator, 
 		return errors.WithStack(err)
 	}
 
+	logger.Execution().WarningWhenf(filepath.IsAbs(c.WorkingDir) && !strings.HasPrefix(c.WorkingDir, conf.WorkDir),
+		"the working directory is an absolute path [%s], which isn't supported except when prefixed by '%s'",
+		c.WorkingDir, conf.WorkDir)
+
 	c.WorkingDir, err = conf.GetWorkingDirectory(c.WorkingDir)
 	if err != nil {
 		logger.Execution().Warning(err.Error())
@@ -240,6 +244,12 @@ func (c *subprocessExec) Execute(ctx context.Context, comm client.Communicator, 
 			}
 		}
 	}
+
+	logger.Execution().Debug(message.Fields{
+		"working_directory": c.WorkingDir,
+		"background":        c.Background,
+		"binary":            c.Binary,
+	})
 
 	err = errors.WithStack(c.runCommand(ctx, conf.Task.Id, c.getProc(ctx, conf.Task.Id, logger), logger))
 
