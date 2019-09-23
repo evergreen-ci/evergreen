45d05e5cee47e5ea792f6406a5e48175ed5325e7
John Liu <john.liu@mongodb.com>
2019-09-23 14:52:21 -0400

    EVG-6568 use the old patch page for now

--- service/ui.go
+++ service/ui.go
@@ -246,9 +246,9 @@ func (uis *UIServer) GetServiceApp() *gimlet.APIApp {
 
 	app.AddRoute("/timeline/{project_id}").Wrap(needsContext).Handler(uis.timeline).Get()
 	app.AddRoute("/timeline").Wrap(needsContext).Handler(uis.timeline).Get()
-	app.AddRoute("/json/timeline/{project_id}").Wrap(needsContext, allowsCORS).Handler(uis.timelineJson).Get()
-	app.AddRoute("/json/patches/project/{project_id}").Wrap(needsContext, allowsCORS).Handler(uis.patchTimelineJson).Get()
-	app.AddRoute("/json/patches/user/{user_id}").Wrap(needsContext, allowsCORS).Handler(uis.patchTimelineJson).Get()
+	app.AddRoute("/json/timeline/{project_id}").Wrap(needsContext, needsLogin, allowsCORS).Handler(uis.timelineJson).Get()
+	app.AddRoute("/json/patches/project/{project_id}").Wrap(needsContext, needsLogin, allowsCORS).Handler(uis.patchTimelineJson).Get()
+	app.AddRoute("/json/patches/user/{user_id}").Wrap(needsContext, needsLogin, allowsCORS).Handler(uis.patchTimelineJson).Get()
 
 	// Grid page
 	app.AddRoute("/grid").Wrap(needsContext).Handler(uis.grid).Get()
