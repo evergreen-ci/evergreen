65f289e80ce91970a5ad0cf83ca81993c0c90a45
Brian Samek <brian.samek@mongodb.com>
2019-10-14 16:30:35 -0400

    config string

--- config.go
+++ config.go
@@ -26,7 +26,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2019-10-10"
+	ClientVersion = "2019-10-14"
 )
 
 // ConfigSection defines a sub-document in the evegreen config
