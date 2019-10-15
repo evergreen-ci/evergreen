a6b2158695761a066360d667891c6421d5ed8862
Julian Edwards <julian.edwards@mongodb.com>
2019-10-15 17:51:29 -0400

    use cached settings in api server

--- service/api_creds.go
+++ service/api_creds.go
@@ -3,9 +3,7 @@ package service
 import (
 	"net/http"
 
-	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/gimlet"
-	"github.com/pkg/errors"
 )
 
 type Credentials struct {
@@ -14,15 +12,9 @@ type Credentials struct {
 }
 
 func (as *APIServer) BuildloggerV3Credentials(w http.ResponseWriter, r *http.Request) {
-	conf, err := evergreen.GetConfig()
-	if err != nil {
-		as.LoggedError(w, r, http.StatusInternalServerError, errors.Wrapf(err, "Error getting evergreen LDAP credentials for buildloggerv3"))
-		return
-	}
-
 	creds := &Credentials{
-		Username: conf.LoggerConfig.BuildloggerV3User,
-		Password: conf.LoggerConfig.BuildloggerV3Password,
+		Username: as.Settings.LoggerConfig.BuildloggerV3User,
+		Password: as.Settings.LoggerConfig.BuildloggerV3Password,
 	}
 	gimlet.WriteJSON(w, creds)
 }
