758e5365f429473f8c3d3a8404b7b1fff42e1f0d
Sam Kleinman <sam@10gen.com>
2019-10-23 17:06:32 -0400

    EVG-6046: revendor gimlet

--- vendor/github.com/evergreen-ci/gimlet/framework_response_simple.go
+++ vendor/github.com/evergreen-ci/gimlet/framework_response_simple.go
@@ -2,124 +2,111 @@ package gimlet
 
 import (
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
-func NewTextResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusOK,
-		format: TEXT,
+func newResponder(data interface{}, code int, of OutputFormat) Responder {
+	switch in := data.(type) {
+	case error:
+		var eresp ErrorResponse
+		switch err := errors.Cause(in).(type) {
+		case *ErrorResponse:
+			if http.StatusText(eresp.StatusCode) == "" {
+				eresp.StatusCode = code
+			}
+
+			eresp = *err
+		case ErrorResponse:
+			if http.StatusText(eresp.StatusCode) == "" {
+				eresp.StatusCode = code
+			}
+
+			eresp = err
+		default:
+			eresp = ErrorResponse{
+				StatusCode: code,
+				Message:    err.Error(),
+			}
+		}
+
+		if of == TEXT {
+			return &responderImpl{
+				data:   eresp.Error(),
+				status: eresp.StatusCode,
+				format: of,
+			}
+		}
+		return &responderImpl{
+			data:   eresp,
+			status: eresp.StatusCode,
+			format: of,
+		}
+	default:
+		return &responderImpl{
+			data:   data,
+			status: code,
+			format: of,
+		}
 	}
+}
+
+func NewTextResponse(data interface{}) Responder {
+	return newResponder(data, http.StatusOK, TEXT)
 }
 
 func NewTextErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusBadRequest,
-		format: TEXT,
-	}
+	return newResponder(data, http.StatusBadRequest, TEXT)
 }
 
 func NewTextInternalErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusInternalServerError,
-		format: TEXT,
-	}
+	return newResponder(data, http.StatusInternalServerError, TEXT)
 }
 
 func NewJSONResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusOK,
-		format: JSON,
-	}
+	return newResponder(data, http.StatusOK, JSON)
 }
 
 func NewJSONErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusBadRequest,
-		format: JSON,
-	}
+	return newResponder(data, http.StatusBadRequest, JSON)
 }
 
 func NewJSONInternalErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusInternalServerError,
-		format: JSON,
-	}
+	return newResponder(data, http.StatusInternalServerError, JSON)
 }
 
 func NewBinaryResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusOK,
-		format: BINARY,
-	}
+	return newResponder(data, http.StatusOK, BINARY)
 }
 
 func NewBinaryErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusBadRequest,
-		format: BINARY,
-	}
+	return newResponder(data, http.StatusBadRequest, BINARY)
 }
 
 func NewBinaryInternalErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusInternalServerError,
-		format: BINARY,
-	}
+	return newResponder(data, http.StatusInternalServerError, BINARY)
 }
 
 func NewHTMLResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusOK,
-		format: HTML,
-	}
+	return newResponder(data, http.StatusOK, HTML)
 }
 
 func NewHTMLErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusBadRequest,
-		format: HTML,
-	}
+	return newResponder(data, http.StatusBadRequest, HTML)
 }
 
 func NewHTMLInternalErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusInternalServerError,
-		format: HTML,
-	}
+	return newResponder(data, http.StatusInternalServerError, HTML)
 }
 
 func NewYAMLResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusOK,
-		format: YAML,
-	}
+	return newResponder(data, http.StatusOK, YAML)
 }
 
 func NewYAMLErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusBadRequest,
-		format: YAML,
-	}
+	return newResponder(data, http.StatusBadRequest, YAML)
 }
 
 func NewYAMLInternalErrorResponse(data interface{}) Responder {
-	return &responderImpl{
-		data:   data,
-		status: http.StatusInternalServerError,
-		format: YAML,
-	}
+	return newResponder(data, http.StatusInternalServerError, YAML)
 }
