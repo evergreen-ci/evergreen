a6c9557f151261074e0608077dd396cb2d6bad70
StuartButler <butler.stuart@gmail.com>
2019-11-12 14:16:40 -0500

    EVG-6792: Adding back in 3 fields to PlannerSettings to simplify any potential rollback

--- model/distro/distro.go
+++ model/distro/distro.go
@@ -125,13 +125,16 @@ type FinderSettings struct {
 }
 
 type PlannerSettings struct {
-	Version               string        `bson:"version" json:"version" mapstructure:"version"`
-	TargetTime            time.Duration `bson:"target_time" json:"target_time" mapstructure:"target_time,omitempty"`
-	GroupVersions         *bool         `bson:"group_versions" json:"group_versions" mapstructure:"group_versions,omitempty"`
-	TaskOrdering          string        `bson:"task_ordering" json:"task_ordering" mapstructure:"task_ordering,omitempty"`
-	PatchFactor           int64         `bson:"patch_factor" json:"patch_factor" mapstructure:"patch_factor"`
-	TimeInQueueFactor     int64         `bson:"time_in_queue_factor" json:"time_in_queue_factor" mapstructure:"time_in_queue_factor"`
-	ExpectedRuntimeFactor int64         `bson:"expected_runtime_factor" json:"expected_runtime_factor" mapstructure:"expected_runtime_factor"`
+	Version                string        `bson:"version" json:"version" mapstructure:"version"`
+	MinimumHosts           int           `bson:"minimum_hosts" json:"minimum_hosts,omitempty" mapstructure:"minimum_hosts,omitempty"`
+	MaximumHosts           int           `bson:"maximum_hosts" json:"maximum_hosts,omitempty" mapstructure:"maximum_hosts,omitempty"`
+	TargetTime             time.Duration `bson:"target_time" json:"target_time" mapstructure:"target_time,omitempty"`
+	AcceptableHostIdleTime time.Duration `bson:"acceptable_host_idle_time" json:"acceptable_host_idle_time" mapstructure:"acceptable_host_idle_time,omitempty"`
+	GroupVersions          *bool         `bson:"group_versions" json:"group_versions" mapstructure:"group_versions,omitempty"`
+	TaskOrdering           string        `bson:"task_ordering" json:"task_ordering" mapstructure:"task_ordering,omitempty"`
+	PatchFactor            int64         `bson:"patch_zipper_factor" json:"patch_factor" mapstructure:"patch_factor"`
+	TimeInQueueFactor      int64         `bson:"time_in_queue_factor" json:"time_in_queue_factor" mapstructure:"time_in_queue_factor"`
+	ExpectedRuntimeFactor  int64         `bson:"expected_runtime_factor" json:"expected_runtime_factor" mapstructure:"expected_runtime_factor"`
 
 	maxDurationPerHost time.Duration
 }
