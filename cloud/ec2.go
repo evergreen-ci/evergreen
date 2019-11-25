7ec5673cc640eb02cc15f486233dee2212d5889f
Annie Black <annie.black@10gen.com>
2019-11-25 09:22:30 -0500

    EVG-6272 enforce 30 day uptime for expirable spawn hosts (#2904)

--- cloud/ec2.go
+++ cloud/ec2.go
@@ -585,7 +585,7 @@ func (m *ec2Manager) setNoExpiration(ctx context.Context, h *host.Host, noExpira
 	if err != nil {
 		return errors.Wrap(err, "error getting host resources")
 	}
-	expireOnValue := expireInDays(spawnHostExpireDays)
+	expireOnValue := expireInDays(evergreen.SpawnHostExpireDays)
 	_, err = m.client.CreateTags(ctx, &ec2.CreateTagsInput{
 		Resources: aws.StringSlice(resources),
 		Tags: []*ec2.Tag{
@@ -641,7 +641,11 @@ func (m *ec2Manager) ModifyHost(ctx context.Context, h *host.Host, opts host.Hos
 		catcher.Add(m.setNoExpiration(ctx, h, *opts.NoExpiration))
 	}
 	if opts.AddHours > 0 {
-		catcher.Add(m.extendExpiration(ctx, h, opts.AddHours))
+		if err := h.PastMaxExpiration(opts.AddHours); err != nil {
+			catcher.Add(err)
+		} else {
+			catcher.Add(m.extendExpiration(ctx, h, opts.AddHours))
+		}
 	}
 
 	return catcher.Resolve()
--- model/host/host.go
+++ model/host/host.go
@@ -1497,6 +1497,23 @@ func (h *Host) IsIdleParent() (bool, error) {
 	return num == 0, nil
 }
 
+// For spawn hosts that have never been set unexpirable, this will
+// prevent spawn hosts from being set further than 30 days.
+// For unexpirable hosts, this will prevent them from being extended any further
+// than the new 30 day expiration (unless it is set to unexpirable again #loophole)
+func (h *Host) PastMaxExpiration(extension time.Duration) error {
+	maxExpirationTime := h.CreationTime.Add(evergreen.SpawnHostExpireDays * time.Hour * 24)
+	if h.ExpirationTime.After(maxExpirationTime) {
+		return errors.New("Spawn host cannot be extended further")
+	}
+
+	proposedTime := h.ExpirationTime.Add(extension)
+	if proposedTime.After(maxExpirationTime) {
+		return errors.Errorf("Spawn host cannot be extended more than %d days past creation", evergreen.SpawnHostExpireDays)
+	}
+	return nil
+}
+
 // UpdateLastContainerFinishTime updates latest finish time for a host with containers
 func (h *Host) UpdateLastContainerFinishTime(t time.Time) error {
 	selector := bson.M{
