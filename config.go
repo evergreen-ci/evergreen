f24e62e1c8fae2a6fbf3f74022c53e226b1648b7
Kim Tao <kim.tao@mongodb.com>
2019-12-03 09:11:51 -0500

    Update binary version

--- config.go
+++ config.go
@@ -26,7 +26,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2019-11-12"
+	ClientVersion = "2019-12-03"
 )
 
 // ConfigSection defines a sub-document in the evegreen config
