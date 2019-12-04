33acf08907bd3b26685c418960bbe31d727c69aa
Jonathan Brill <jonathan.brill@10gen.com>
2019-12-04 15:51:56 -0500

    EVG-6408 fix CostCalculator not implemented errors (#2933)

--- cloud/ec2.go
+++ cloud/ec2.go
@@ -1187,12 +1187,7 @@ func (m *ec2Manager) CostForDuration(ctx context.Context, h *host.Host, start, e
 	if end.Before(start) || util.IsZeroTime(start) || util.IsZeroTime(end) {
 		return 0, errors.New("task timing data is malformed")
 	}
-	ec2Settings := &EC2ProviderSettings{}
-	err := ec2Settings.fromDistroSettings(h.Distro)
-	if err != nil {
-		return 0, errors.Wrap(err, "problem getting region from host")
-	}
-	if err = m.client.Create(m.credentials, m.region); err != nil {
+	if err := m.client.Create(m.credentials, m.region); err != nil {
 		return 0, errors.Wrap(err, "error creating client")
 	}
 	defer m.client.Close()
