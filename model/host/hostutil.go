6609ad71d41024f035dcce338acd60b17f58e688
Kim Tao <kim.tao@mongodb.com>
2019-12-06 08:30:35 -0500

    Update hostutil.go

--- model/host/hostutil.go
+++ model/host/hostutil.go
@@ -306,7 +306,7 @@ func (h *Host) ForceReinstallJasperCommand(settings *evergreen.Settings) string 
 	}
 
 	for _, envVar := range h.Distro.BootstrapSettings.Env {
-		params = append(params, fmt.Sprintf("--env %s=%s", envVar.Key, envVar.Value))
+		params = append(params, fmt.Sprintf("--env '%s=%s'", envVar.Key, envVar.Value))
 	}
 
 	if os, _ := h.Distro.Platform(); os == "linux" {
