c4a99c30d21b89128a25e03e83e52e1128f5ca25
julianedwards <julian.edwards@mongodb.com>
2019-12-09 11:16:46 -0500

    auth route

---
+++ rest/route/auth.go
@@ -0,0 +1,55 @@
+package route
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/evergreen-ci/gimlet"
+	"github.com/pkg/errors"
+)
+
+////////////////////////////////////////////////////////////////////////
+//
+// POST /rest/v2/auth
+
+type authPermissionGetHandler struct {
+	resource      string
+	resourceType  string
+	permission    string
+	requiredLevel int
+}
+
+func makeGetAuthPermissionHandler() gimlet.RouteHandler {
+	return &authPermissionGetHandler{}
+}
+
+func (h *authPermissionGetHandler) Factory() gimlet.RouteHandler { return h }
+func (h *authPermissionGetHandler) Parse(ctx context.Context, r *http.Request) error {
+	var err error
+
+	vals := r.URL.Query()
+	h.resource = vals.Get("resource")
+	h.resourceType = vals.Get("resource_type")
+	h.permission = vals.Get("permission")
+	h.requiredLevel, err = strconv.Atoi(vals.Get("required_level"))
+
+	return err
+}
+
+func (h *authPermissionGetHandler) Run(ctx context.Context) gimlet.Responder {
+	u := MustHaveUser(ctx)
+
+	opts := gimlet.PermissionOpts{
+		Resource:      h.resource,
+		ResourceType:  h.resourceType,
+		Permission:    h.permission,
+		RequiredLevel: h.requiredLevel,
+	}
+	ok, err := u.HasPermission(opts)
+	if err != nil {
+		return gimlet.MakeJSONErrorResponder(errors.Wrap(err, "Error checking permission for user"))
+	}
+
+	return gimlet.NewTextResponse(ok)
+}
