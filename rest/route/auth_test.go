ff5c2db32dc2ca04df82f11435379f2fee80e682
julianedwards <julian.edwards@10gen.com>
2019-12-09 17:32:29 -0500

    EVG-7067: Add rest route to get user permissions (#2946)

---
+++ rest/route/auth_test.go
@@ -0,0 +1,47 @@
+package route
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type AuthRouteSuite struct {
+	suite.Suite
+}
+
+func TestAuthRouteSuite(t *testing.T) {
+	s := new(AuthRouteSuite)
+	suite.Run(t, s)
+}
+
+func (s *AuthRouteSuite) TestParse() {
+	h := &authPermissionGetHandler{}
+	urlString := "http://evergreen.mongodb.com/rest/v2/auth"
+	urlString += "?resource=abc&resource_type=project&permission=read&required_level=10"
+	req := &http.Request{Method: "GET"}
+	req.URL, _ = url.Parse(urlString)
+	err := h.Parse(context.TODO(), req)
+	s.Require().NoError(err)
+	s.Equal("abc", h.resource)
+	s.Equal("project", h.resourceType)
+	s.Equal("read", h.permission)
+	s.Equal(10, h.requiredLevel)
+}
+
+func (s *AuthRouteSuite) TestParseFail() {
+	h := &authPermissionGetHandler{}
+	urlString := "http://evergreen.mongodb.com/rest/v2/auth"
+	urlString += "?resource=abc&resource_type=project&permission=read&required_level=notAnumber"
+	req := &http.Request{Method: "GET"}
+	req.URL, _ = url.Parse(urlString)
+	err := h.Parse(context.TODO(), req)
+	s.Error(err)
+	s.Equal("abc", h.resource)
+	s.Equal("project", h.resourceType)
+	s.Equal("read", h.permission)
+	s.Equal(0, h.requiredLevel)
+}
