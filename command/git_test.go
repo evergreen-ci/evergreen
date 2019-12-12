4b17149e8c737f6ea7eec4267dd9773db9e25d4f
Brian Samek <brian.samek@mongodb.com>
2019-12-12 16:24:04 -0500

    fix test

--- command/git_test.go
+++ command/git_test.go
@@ -552,8 +552,8 @@ func (s *GitGetProjectSuite) TestBuildCommandForCLIMergeTests() {
 	s.modelData2.TaskConfig.Task.Requester = evergreen.MergeTestRequester
 	cmds, err := c.buildCloneCommand(conf, opts)
 	s.NoError(err)
-	s.Len(cmds, 9)
-	s.True(strings.HasPrefix(cmds[8], fmt.Sprintf("git checkout '%s'", s.modelData2.TaskConfig.ProjectRef.Branch)))
+	s.Len(cmds, 8)
+	s.True(strings.HasSuffix(cmds[5], fmt.Sprintf("--branch '%s'", s.modelData2.TaskConfig.ProjectRef.Branch)))
 }
 
 func (s *GitGetProjectSuite) TestBuildModuleCommand() {
