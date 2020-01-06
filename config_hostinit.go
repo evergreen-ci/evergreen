a583e004ddf974e9229155f8334c5aafd8c8eb7f
John Liu <john.liu@mongodb.com>
2020-01-06 10:42:51 -0500

    Revert "Revert "EVG-7102 upgrade go driver to 1.3.0-prerelease (#2977)""
    This reverts commit 3291c64eaa01a62477e010105710b3d96abd534e.

--- config_hostinit.go
+++ config_hostinit.go
@@ -21,15 +21,13 @@ func (c *HostInitConfig) Get(env Environment) error {
 
 	res := coll.FindOne(ctx, byId(c.SectionId()))
 	if err := res.Err(); err != nil {
-		return errors.Wrapf(err, "error retrieving section %s", c.SectionId())
-	}
-
-	if err := res.Decode(c); err != nil {
 		if err == mongo.ErrNoDocuments {
 			*c = HostInitConfig{}
 			return nil
 		}
-
+		return errors.Wrapf(err, "error retrieving section %s", c.SectionId())
+	}
+	if err := res.Decode(c); err != nil {
 		return errors.Wrap(err, "problem decoding result")
 	}
 
