67f5f3994eb9760c5e35e4336be23adc6683c78d
julianedwards <julian.edwards@mongodb.com>
2020-01-17 14:37:12 -0500

    remove TODO

--- agent/logging.go
+++ agent/logging.go
@@ -131,8 +131,6 @@ func (a *Agent) prepLogger(tc *taskContext, c *model.LoggerConfig, commandName s
 	var defaultLogger string
 	if tc.taskConfig != nil && tc.taskConfig.ProjectRef != nil {
 		defaultLogger = tc.taskConfig.ProjectRef.DefaultLogger
-		// TODO: remove after testing
-		grip.Info(fmt.Sprintf("project ref logger is %s", tc.taskConfig.ProjectRef.DefaultLogger))
 	}
 	if !model.IsValidDefaultLogger(defaultLogger) {
 		grip.Warningf("default logger '%s' is not valid, setting Evergreen logger as default", defaultLogger)
