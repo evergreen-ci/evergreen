05b514d1e99cdf7d420e4a581bb798cddd36a2df
Arjun Patel <arjun453@gmail.com>
2020-01-21 09:11:19 -0500

    build api task with api url

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -56,14 +56,18 @@ func (r *queryResolver) Task(ctx context.Context, taskID string) (*model.APITask
 	if err != nil {
 		return nil, errors.Wrap(err, "error converting task")
 	}
+	err = apiTask.BuildFromService(r.sc.GetURL())
+	if err != nil {
+		return nil, errors.Wrap(err, "error converting task")
+	}
 	return &apiTask, nil
 }
 
 // New injects resources into the resolvers, such as the data connector
-func New() Config {
+func New(apiURL string) Config {
 	return Config{
 		Resolvers: &Resolver{
-			sc: &data.DBConnector{},
+			sc: &data.DBConnector{URL: apiURL},
 		},
 	}
 }
