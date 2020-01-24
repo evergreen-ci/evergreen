9646d1441e9e508d682323d7726431b18d465218
Arjun Patel <arjun453@gmail.com>
2020-01-24 15:18:45 -0500

    tests for TestsByTaskIdFilterSortPaginate

--- rest/data/test_test.go
+++ rest/data/test_test.go
@@ -84,6 +84,110 @@ func TestFindTestsByTaskId(t *testing.T) {
 	assert.Equal(http.StatusNotFound, apiErr.StatusCode)
 }
 
+func TestFindTestsByTaskIdFilterSortPaginate(t *testing.T) {
+	assert := assert.New(t)
+	assert.NoError(db.ClearCollections(task.Collection, testresult.Collection))
+
+	serviceContext := &DBConnector{}
+	numTests := 10
+	numTasks := 2
+	testObjects := make([]string, numTests)
+
+	for ix := range testObjects {
+		testObjects[ix] = fmt.Sprintf("TestSuite/TestNum%d", ix)
+	}
+	last := len(testObjects) - 1
+	sort.StringSlice(testObjects).Sort()
+
+	for i := 0; i < numTasks; i++ {
+		id := fmt.Sprintf("task_%d", i)
+		testTask := &task.Task{
+			Id: id,
+		}
+		tests := make([]testresult.TestResult, numTests)
+		for j := 0; j < numTests; j++ {
+			status := "pass"
+			if j%2 == 0 {
+				status = "fail"
+			}
+			tests[j] = testresult.TestResult{
+				TaskID:    id,
+				Execution: 0,
+				Status:    status,
+				TestFile:  testObjects[j],
+				EndTime:   float64(j),
+				StartTime: 0,
+			}
+		}
+		assert.NoError(testTask.Insert())
+		for _, test := range tests {
+			assert.NoError(test.Insert())
+		}
+	}
+
+	for i := 0; i < numTasks; i++ {
+		taskId := fmt.Sprintf("task_%d", i)
+		foundTests, err := serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "test_file", 1, 0, 0, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, numTests)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "pass", "test_file", 1, 0, 0, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, numTests/2)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "fail", "test_file", 1, 0, 0, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, numTests/2)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "TestSuite/TestNum1", "test_file", 1, 0, 0, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, 1)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "TestSuite/TestNum2", "test_file", 1, 0, 0, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, 1)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "test_file", 1, 0, 5, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, 5)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "test_file", 1, 1, 5, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, 5)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "test_file", 1, 2, 5, 0)
+		assert.NoError(err)
+		assert.Len(foundTests, 0)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "test_file", -1, 0, 0, 0)
+		assert.NoError(err)
+
+		for i := range foundTests {
+			assert.True(foundTests[i].TestFile == testObjects[last-i])
+		}
+		assert.Len(foundTests, 10)
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "duration", -1, 0, 0, 0)
+		assert.NoError(err)
+		for i, test := range foundTests {
+			assert.True(test.EndTime == float64(last-i))
+		}
+
+		foundTests, err = serviceContext.FindTestsByTaskIdFilterSortPaginate(taskId, "", "duration", 1, 0, 0, 0)
+		assert.NoError(err)
+		for i, test := range foundTests {
+			assert.True(test.EndTime == float64(i))
+		}
+	}
+	foundTests, err := serviceContext.FindTestsByTaskIdFilterSortPaginate("fake_task", "", "duration", 1, 0, 0, 0)
+	assert.Error(err)
+	assert.Len(foundTests, 0)
+	apiErr, ok := err.(gimlet.ErrorResponse)
+	assert.True(ok)
+	assert.Equal(http.StatusNotFound, apiErr.StatusCode)
+
+}
+
 func TestFindTestsByDisplayTaskId(t *testing.T) {
 	assert := assert.New(t)
 	assert.NoError(db.ClearCollections(task.Collection, testresult.Collection))
