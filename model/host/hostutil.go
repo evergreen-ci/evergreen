44e5d733448f44966c227a29d4504a04747654c5
Kim Tao <kim.tao@mongodb.com>
2020-01-30 10:00:55 -0500

    Change owner for cli_bin so evergreen binary has executable permissions

--- model/host/hostutil.go
+++ model/host/hostutil.go
@@ -997,6 +997,7 @@ func (h *Host) SetupSpawnHostCommands(settings *evergreen.Settings) (string, err
 		fmt.Sprintf("echo '%s' > %s", confJSON, confPath),
 		fmt.Sprintf("cp %s %s", binaryPath, binDir),
 		fmt.Sprintf("(echo '\nexport PATH=\"${PATH}:%s\"\n' >> %s/.profile || true; echo '\nexport PATH=\"${PATH}:%s\"\n' >> %s/.bash_profile || true)", binDir, h.Distro.HomeDir(), binDir, h.Distro.HomeDir()),
+		fmt.Sprintf("chown -R %s %s", h.Distro.User, binDir),
 	}, " && ")
 
 	script := setupBinDirCmds
