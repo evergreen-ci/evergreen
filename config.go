c7df4f17f428c47b895c5e3470594726770d35c3
Kim Tao <kim.tao@mongodb.com>
2020-02-04 14:13:10 -0500

    Update version string

--- config.go
+++ config.go
@@ -26,7 +26,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-01-27"
+	ClientVersion = "2020-02-04"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
