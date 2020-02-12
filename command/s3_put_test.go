88d226cc84d92d76211e5d4c71a7fb805d44cc1a
Brian Samek <brian.samek@mongodb.com>
2020-02-12 16:48:33 -0500

    EVG-7226 Check if LocalFile is absolute after expanding values
    Prior to this commit s3.put checked if LocalFile was an absolute path prior to
    expanding values. This means that if a user provided an expansion in LocalPath
    which made it absolute, s3.put would behave as if it were local. This commit
    inverts the order, expanding values before checking if the path is absolute.

--- command/s3_put_test.go
+++ command/s3_put_test.go
@@ -247,6 +247,7 @@ func TestExpandS3PutParams(t *testing.T) {
 			cmd.ResourceDisplayName = "${display_name}"
 			cmd.Visibility = "${visibility}"
 			cmd.Optional = "${optional}"
+			cmd.LocalFile = "${workdir}/foo"
 
 			conf.Expansions.Update(
 				map[string]string{
@@ -269,9 +270,10 @@ func TestExpandS3PutParams(t *testing.T) {
 			So(cmd.ContentType, ShouldEqual, "ct")
 			So(cmd.ResourceDisplayName, ShouldEqual, "file")
 			So(cmd.Visibility, ShouldEqual, "private")
-			So(cmd.workDir, ShouldEqual, "working_directory")
 			So(cmd.Optional, ShouldEqual, "true")
 
+			// EVG-7226 Since LocalFile is an absolute path, workDir should be empty
+			So(cmd.workDir, ShouldEqual, "")
 		})
 
 		Convey("the expandParams function should error for invalid optional values", func() {
