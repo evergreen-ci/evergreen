211da2dfdc07a27ca2bbd7a30fb4d69e23436cf3
Arjun Patel <arjun453@gmail.com>
2020-02-12 17:20:12 -0500

    EVG-7271 & EVG-7272: schedule and unschedule task mutations (#3154)

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -254,6 +254,46 @@ func (r *queryResolver) TaskTests(ctx context.Context, taskID string, sortCatego
 	return testPointers, nil
 }
 
+func setScheduled(ctx context.Context, sc data.Connector, taskID string, isActive bool) (*restModel.APITask, error) {
+	usr := route.MustHaveUser(ctx)
+	if err := model.SetActiveState(taskID, usr.Username(), isActive); err != nil {
+		return nil, InternalServerError.Send(ctx, err.Error())
+	}
+	task, err := task.FindOneId(taskID)
+	if err != nil {
+		return nil, ResourceNotFound.Send(ctx, err.Error())
+	}
+	if task == nil {
+		return nil, ResourceNotFound.Send(ctx, err.Error())
+	}
+	apiTask := restModel.APITask{}
+	err = apiTask.BuildFromService(task)
+	if err != nil {
+		return nil, InternalServerError.Send(ctx, err.Error())
+	}
+	err = apiTask.BuildFromService(sc.GetURL())
+	if err != nil {
+		return nil, InternalServerError.Send(ctx, err.Error())
+	}
+	return &apiTask, nil
+}
+
+func (r *mutationResolver) ScheduleTask(ctx context.Context, taskID string) (*restModel.APITask, error) {
+	task, err := setScheduled(ctx, r.sc, taskID, true)
+	if err != nil {
+		return nil, err
+	}
+	return task, nil
+}
+
+func (r *mutationResolver) UnscheduleTask(ctx context.Context, taskID string) (*restModel.APITask, error) {
+	task, err := setScheduled(ctx, r.sc, taskID, false)
+	if err != nil {
+		return nil, err
+	}
+	return task, nil
+}
+
 // New injects resources into the resolvers, such as the data connector
 func New(apiURL string) Config {
 	return Config{
