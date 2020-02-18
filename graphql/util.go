3f5fef2af1208348374b33b4eb9f7c537ee56a1e
Trey Granderson <treygranderson@Treys-MacBook-Pro.local>
2020-02-18 13:03:17 -0500

    changes

--- graphql/util.go
+++ graphql/util.go
@@ -5,18 +5,6 @@ import (
 	"time"
 )
 
-// GetFormattedDuration returns a time.Duration type in "33h 33m 33s" format
-func GetFormattedDuration(d time.Duration) *string {
-	h := d / time.Hour
-	d -= h * time.Hour
-	m := d / time.Minute
-	d -= m * time.Minute
-	s := d / time.Second
-
-	formatted := fmt.Sprintf("%02dh %02dm %02ds", h, m, s)
-	return &formatted
-}
-
 // GetFormattedDate returns a time.Time type in the format "Dec 13, 2020, 11:58:04 pm"
 func GetFormattedDate(t *time.Time, timezone string) (*string, error) {
 	if t == nil {
