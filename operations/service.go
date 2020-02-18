a9d49674dbb1acb4385268e2c133262fe4698eab
Kim Tao <kim.tao@mongodb.com>
2020-02-18 09:03:40 -0500

    Wrap job enqueueing in message

--- operations/service.go
+++ operations/service.go
@@ -103,7 +103,9 @@ func startSystemCronJobs(ctx context.Context, env evergreen.Environment) error {
 
 	// Enqueue jobs to ensure each app server has the correct SSH key files.
 	ts := util.RoundPartOfHour(30).Format(units.TSFormat)
-	grip.Error(local.Put(ctx, units.NewLocalUpdateSSHKeysJob(ts)))
+	grip.Error(message.WrapError(local.Put(ctx, units.NewLocalUpdateSSHKeysJob(ts), message.Fields{
+		"message": "could not enqueue jobs to update app server's local SSH keys",
+	}))
 
 	return nil
 }
