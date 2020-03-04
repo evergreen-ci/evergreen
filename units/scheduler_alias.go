1f2746d421c5048fa83ff904c414042f2206bc94
Kim Tao <kim.tao@mongodb.com>
2020-03-04 11:27:24 -0500

    Fix units

--- units/scheduler_alias.go
+++ units/scheduler_alias.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/model/distro"
-	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/scheduler"
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/dependency"
@@ -74,7 +73,7 @@ func (j *distroAliasSchedulerJob) Run(ctx context.Context) {
 	}
 
 	startAt := time.Now()
-	tasks, err := task.FindSchedulableForAlias(j.DistroID)
+	tasks, err := scheduler.FindSchedulableForAlias(j.DistroID)
 	j.AddError(errors.Wrap(err, "problem finding tasks"))
 	if tasks == nil {
 		return
