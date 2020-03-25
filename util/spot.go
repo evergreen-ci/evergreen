196406b78edf4f2e45eb427e0342575ad3798b01
Brian Samek <brian.samek@mongodb.com>
2020-03-25 15:23:32 -0400

    forgot a file

---
+++ util/spot.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"net/http"
+
+	"github.com/mongodb/grip"
+	"github.com/pkg/errors"
+)
+
+// SpotHostWillTerminateSoon returns true if the EC2 spot host it is running on will terminate soon.
+func SpotHostWillTerminateSoon() bool {
+	const url = "http://169.254.169.254/latest/meta-data/spot/termination-time"
+	resp, err := http.Get(url)
+	if err != nil {
+		grip.Info(errors.Wrap(err, "problem getting termination endpoint"))
+		return false
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		return true
+	}
+	return false
+}
