d591c30497ce4f831b8494b4d601df2e4490e84c
Arjun Patel <arjun453@gmail.com>
2020-03-26 10:50:00 -0400

    refactor

--- graphql/util.go
+++ graphql/util.go
@@ -96,8 +96,8 @@ func IsURL(str string) bool {
 // BaseTaskStatuses represents the format {buildVariant: {displayName: status}} for base task statuses
 type BaseTaskStatuses map[string]map[string]string
 
-// GetBaseTaskStatusesFromPatchID gets the status of each base build associated with a task
-func GetBaseTaskStatusesFromPatchID(r *queryResolver, patchID string) (BaseTaskStatuses, error) {
+// GetBaseTasksFromPatchID returns the base tasks of a patch
+func GetBaseTasksFromPatchID(r *queryResolver, patchID string) ([]task.Task, error) {
 	version, err := r.sc.FindVersionById(patchID)
 	if err != nil {
 		return nil, fmt.Errorf("Error getting version %s: %s", patchID, err.Error())
@@ -119,7 +119,15 @@ func GetBaseTaskStatusesFromPatchID(r *queryResolver, patchID string) (BaseTaskS
 	if baseTasks == nil {
 		return nil, fmt.Errorf("No tasks found for version %s", baseVersion.Id)
 	}
+	return baseTasks, nil
+}
 
+// GetBaseTaskStatusesFromPatchID gets the status of each base build associated with a task
+func GetBaseTaskStatusesFromPatchID(r *queryResolver, patchID string) (BaseTaskStatuses, error) {
+	baseTasks, err := GetBaseTasksFromPatchID(r, patchID)
+	if err != nil {
+		return nil, err
+	}
 	baseTaskStatusesByDisplayNameByVariant := make(map[string]map[string]string)
 	for _, task := range baseTasks {
 		if _, ok := baseTaskStatusesByDisplayNameByVariant[task.BuildVariant]; !ok {
