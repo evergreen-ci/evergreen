a637733036faaea13ca12cbe35404735ccf203dc
ablack12 <annie.black@10gen.com>
2020-04-03 08:30:15 -0700

    nit

--- cloud/ec2_integration_test.go
+++ cloud/ec2_integration_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/evergreen-ci/birch"
-
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/evergreen-ci/evergreen/model/distro"
