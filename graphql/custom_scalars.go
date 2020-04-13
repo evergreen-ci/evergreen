084254b16fea27dd2825bb95db07c87c56622b70
Arjun Patel <arjun453@gmail.com>
2020-04-13 18:44:00 -0400

    return nil if error

--- graphql/custom_scalars.go
+++ graphql/custom_scalars.go
@@ -23,12 +23,12 @@ func UnmarshalStringMap(v interface{}) (map[string]string, error) {
 	stringMap := make(map[string]string)
 	stringInterface, ok := v.(map[string]interface{})
 	if !ok {
-		return stringMap, fmt.Errorf("%T is not a StringMap", v)
+		return nil, fmt.Errorf("%T is not a StringMap", v)
 	}
 	for key, value := range stringInterface {
 		_, ok := value.(string)
 		if !ok {
-			return stringMap, fmt.Errorf("%v is not a StringMap. Value %v for key %v should be type string but got %T", v, value, key, value)
+			return nil, fmt.Errorf("%v is not a StringMap. Value %v for key %v should be type string but got %T", v, value, key, value)
 		}
 		strValue := fmt.Sprintf("%v", value)
 		stringMap[key] = strValue
