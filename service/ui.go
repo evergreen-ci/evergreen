c5516e996766d00a887aec24d01e6835fc0faa91
Trey Granderson <treygranderson@Treys-MBP.fios-router.home>
2020-04-13 15:03:15 -0400

    EVG-7432: enable CORS for login and logout endpoints

--- service/ui.go
+++ service/ui.go
@@ -264,10 +264,10 @@ func (uis *UIServer) GetServiceApp() *gimlet.APIApp {
 	app.NoVersions = true
 
 	// User login and logout
-	app.AddRoute("/login").Handler(uis.loginPage).Get()
-	app.AddRoute("/login").Handler(uis.login).Post()
+	app.AddRoute("/login").Wrap(allowsCORS).Handler(uis.loginPage).Get()
+	app.AddRoute("/login").Wrap(allowsCORS).Handler(uis.login).Post()
 	app.AddRoute("/login/key").Handler(uis.userGetKey).Post()
-	app.AddRoute("/logout").Handler(uis.logout).Get()
+	app.AddRoute("/logout").Wrap(allowsCORS).Handler(uis.logout).Get()
 
 	app.AddRoute("/robots.txt").Get().Handler(func(rw http.ResponseWriter, r *http.Request) {
 		_, err := rw.Write([]byte(strings.Join([]string{
