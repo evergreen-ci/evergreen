48ee7b29982cf0390a4a9b21ba08ee7583a12280
Jonathan Brill <jonathan.brill@10gen.com>
2020-05-04 17:54:34 -0400

    remove lookup

--- model/host/volume_test.go
+++ model/host/volume_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,35 +14,15 @@ func TestFindVolumesToDelete(t *testing.T) {
 
 	volumes := []Volume{
 		{ID: "v0", Expiration: time.Date(2010, time.December, 10, 23, 0, 0, 0, time.UTC)},
-		{ID: "v1", Expiration: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)},
+		{ID: "v1", Expiration: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC), Host: "h0"},
 		{ID: "v2", Expiration: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)},
-		{ID: "v3", Expiration: time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)},
 	}
 	for _, vol := range volumes {
 		require.NoError(t, vol.Insert())
 	}
 
-	hosts := []Host{
-		{
-			Id:      "h0",
-			Status:  evergreen.HostRunning,
-			Volumes: []VolumeAttachment{{VolumeID: "v1"}, {VolumeID: "other"}},
-		},
-		{
-			Id:      "h1",
-			Status:  evergreen.HostTerminated,
-			Volumes: []VolumeAttachment{{VolumeID: "v2"}, {VolumeID: "other"}},
-		},
-	}
-	for _, h := range hosts {
-		require.NoError(t, h.Insert())
-	}
-
 	toDelete, err := FindVolumesToDelete(time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC))
 	assert.NoError(t, err)
-	assert.Len(t, toDelete, 2)
-	expectedVolumeIDs := []string{"v2", "v3"}
-	for _, vol := range toDelete {
-		assert.Contains(t, expectedVolumeIDs, vol.ID)
-	}
+	assert.Len(t, toDelete, 1)
+	assert.Equal(t, "v2", toDelete[0].ID)
 }
