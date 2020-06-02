4be7200c64ed3960bddd0ac958035e8672311000
Annie Black <annie.black@10gen.com>
2020-06-01 18:23:39 -0700

    EVG-8101 use correct flag type in host configure (#3639)

--- config.go
+++ config.go
@@ -29,7 +29,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-05-14"
+	ClientVersion = "2020-06-02"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-05-14"
