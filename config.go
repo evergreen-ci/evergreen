13dfe70c0821b682ee2f4ad13c6ed46dec5a01a8
Chaya Malik <chaya.malik@10gen.com>
2020-06-05 10:26:10 -0400

    update config

--- config.go
+++ config.go
@@ -29,7 +29,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-06-01"
+	ClientVersion = "2020-06-05"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-05-14"
