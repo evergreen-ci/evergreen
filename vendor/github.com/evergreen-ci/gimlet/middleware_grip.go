e9ff4607e0c11c975cce5b05435f81d2f9166790
Jonathan Brill <jonathan.brill@10gen.com>
2020-06-17 16:28:26 -0400

    revendor gimlet

--- vendor/github.com/evergreen-ci/gimlet/middleware_grip.go
+++ vendor/github.com/evergreen-ci/gimlet/middleware_grip.go
@@ -170,18 +170,33 @@ func (l *appRecoveryLogger) ServeHTTP(rw http.ResponseWriter, r *http.Request, n
 	defer func() {
 		if err := recover(); err != nil {
 			if rw.Header().Get("Content-Type") == "" {
-				rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				rw.Header().Set("Content-Type", "application/json; charset=utf-8")
 			}
 			rw.WriteHeader(http.StatusInternalServerError)
 
-			_ = recovery.SendMessageWithPanicError(err, nil, l.Journaler, message.Fields{
-				"action":   "aborted",
-				"request":  GetRequestID(ctx),
-				"duration": time.Since(getRequestStartAt(ctx)),
-				"path":     r.URL.Path,
-				"remote":   r.RemoteAddr,
-				"length":   r.ContentLength,
-			})
+			// handlers panic with http.ErrAbortHandler to abort and suppress logging
+			// (https://golang.org/pkg/net/http/#Handler)
+			// log at a lower level
+			if err == http.ErrAbortHandler {
+				l.Debug(message.Fields{
+					"message":  "hit suppressed abort panic",
+					"action":   "aborted",
+					"request":  GetRequestID(ctx),
+					"duration": time.Since(getRequestStartAt(ctx)),
+					"path":     r.URL.Path,
+					"remote":   r.RemoteAddr,
+					"length":   r.ContentLength,
+				})
+			} else {
+				_ = recovery.SendMessageWithPanicError(err, nil, l.Journaler, message.Fields{
+					"action":   "aborted",
+					"request":  GetRequestID(ctx),
+					"duration": time.Since(getRequestStartAt(ctx)),
+					"path":     r.URL.Path,
+					"remote":   r.RemoteAddr,
+					"length":   r.ContentLength,
+				})
+			}
 
 			WriteJSONInternalError(rw, ErrorResponse{
 				StatusCode: http.StatusInternalServerError,
