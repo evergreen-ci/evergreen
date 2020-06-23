c2718716d542f59601fc6b5e86cb4b8fcda0b3f1
Jonathan Brill <jonathan.brill@10gen.com>
2020-06-23 17:29:24 -0400

    EVG-8204 increment version string

--- config.go
+++ config.go
@@ -29,7 +29,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-06-16"
+	ClientVersion = "2020-06-23"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-06-05"
