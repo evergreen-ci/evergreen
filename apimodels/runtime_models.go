23d78dd856de7cb3f24f154c74199001658e69d1
Jonathan Brill <jonathan.brill@10gen.com>
2020-06-23 16:57:31 -0400

    pass through standard streams

--- apimodels/runtime_models.go
+++ apimodels/runtime_models.go
@@ -1,7 +1,5 @@
 package apimodels
 
-import "github.com/mongodb/grip/send"
-
 // Struct for reporting process timeouts
 type ProcessTimeoutResponse struct {
 	Status        string      `json:"status"`
@@ -12,5 +10,4 @@ type WorkstationSetupCommandOptions struct {
 	Directory string
 	Quiet     bool
 	DryRun    bool
-	Output    send.Sender
 }
