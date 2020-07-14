bdf4520f1118a3393c276ae5c4a13c03cb7c5a31
Kim Tao <kim.tao@mongodb.com>
2020-07-14 15:08:45 -0400

    Revendor utility to 8a885e291da3d271f58d396ee2b313ab1aec21a8

--- vendor/github.com/evergreen-ci/utility/file_test.go
+++ vendor/github.com/evergreen-ci/utility/file_test.go
@@ -24,25 +24,32 @@ func TestFile(t *testing.T) {
 	})
 }
 
-func TestBuildFileList(t *testing.T) {
+func TestFileListBuilder(t *testing.T) {
 	t.Run("DoesNotPanicForMissingRoot", func(t *testing.T) {
 		assert.NotPanics(t, func() {
-			list, err := BuildFileList("this/path/does/not/exist", "*")
+			dir := "this/path/does/not/exist"
+			m, err := NewGitignoreFileMatcher(dir, "*")
+			require.NoError(t, err)
+			b := FileListBuilder{
+				WorkingDir: dir,
+				Include:    m,
+			}
+			files, err := b.Build()
 			require.Error(t, err)
-			assert.Empty(t, list)
+			assert.Empty(t, files)
 		})
 	})
 
-	wd, err := ioutil.TempDir("", t.Name())
-	require.NoError(t, err, "error getting working directory")
+	tmpDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
 	defer func() {
-		assert.NoError(t, os.RemoveAll(wd))
+		assert.NoError(t, os.RemoveAll(tmpDir))
 	}()
 
-	fnames := []string{
+	fileNames := []string{
 		"testFile1",
 		"testFile2",
-		"testFile.go",
+		"testFile1.go",
 		"testFile2.go",
 		"testFile3.yml",
 		"built.yml",
@@ -53,86 +60,201 @@ func TestBuildFileList(t *testing.T) {
 		"dir1",
 		"dir2",
 	}
-	// create all the files in the current directory
-	for _, fname := range fnames {
-		f, err := os.Create(filepath.Join(wd, fname))
+
+	var allFiles []string
+
+	for _, fileName := range fileNames {
+		f, err := os.Create(filepath.Join(tmpDir, fileName))
 		require.NoError(t, err, "error creating test file")
 		require.NoError(t, f.Close(), "error closing test file")
+		allFiles = append(allFiles, fileName)
 	}
 
-	// create all the files in the sub directories
 	for _, dirName := range dirNames {
-		err := os.Mkdir(filepath.Join(wd, dirName), 0777)
+		err := os.Mkdir(filepath.Join(tmpDir, dirName), 0777)
 		require.NoError(t, err, "error creating test directory")
-		for _, fname := range fnames {
-			path := filepath.Join(wd, dirName, fname)
+		for _, fileName := range fileNames {
+			path := filepath.Join(tmpDir, dirName, fileName)
 			f, err := os.Create(path)
 			require.NoError(t, err, "error creating test file")
 			require.NoError(t, f.Close(), "error closing test file")
+			allFiles = append(allFiles, filepath.Join(dirName, fileName))
 		}
 	}
-	defer func() {
-		require.NoError(t, os.RemoveAll(wd), "error removing test directory")
-	}()
 
-	t.Run("MatchesOneFileName", func(t *testing.T) {
-		files, err := BuildFileList(wd, fnames[0])
-		require.NoError(t, err)
-		assert.Contains(t, files[0], fnames[0])
-		for i := 1; i < len(fnames); i++ {
-			assert.NotContains(t, files, fnames[i])
-		}
-	})
-	t.Run("MatchesAllFilesWithPrefix", func(t *testing.T) {
-		files, err := BuildFileList(wd, "/testFile*")
-		require.NoError(t, err)
-		for i, fname := range fnames {
-			if i <= 4 {
-				assert.Contains(t, files, fname)
-			} else {
-				assert.NotContains(t, files, fname)
-			}
-		}
-	})
-	t.Run("MatchesAllFilesWithSuffix", func(t *testing.T) {
-		files, err := BuildFileList(wd, "/*.go")
-		require.NoError(t, err)
-		for i, fname := range fnames {
-			if i == 2 || i == 3 || i == 6 {
-				assert.Contains(t, files, fname)
-			} else {
-				assert.NotContains(t, files, fname)
-			}
-		}
-	})
-	t.Run("MatchesAllFilesInSubdirectoryWithSuffix", func(t *testing.T) {
-		files, err := BuildFileList(wd, "/dir1/*.go")
-		require.NoError(t, err)
-		for i, fname := range fnames {
-			path := filepath.Join("dir1", fname)
-			if i == 2 || i == 3 || i == 6 {
-				assert.Contains(t, files, path)
-				assert.NotContains(t, files, fname)
-			} else {
-				assert.NotContains(t, files, path)
-				assert.NotContains(t, files, fname)
-			}
-		}
-	})
-	t.Run("MatchesAllFilesInWildcardSubdirectoryWithSuffix", func(t *testing.T) {
-		files, err := BuildFileList(wd, "/*/*.go")
-		require.NoError(t, err)
-		for i, fname := range fnames {
-			for _, dirName := range dirNames {
-				path := filepath.Join(dirName, fname)
-				if i == 2 || i == 3 || i == 6 {
-					assert.Contains(t, files, path)
-					assert.NotContains(t, files, fname)
-				} else {
-					assert.NotContains(t, files, path)
-					assert.NotContains(t, files, fname)
+	t.Run("GitignoreFileBuilder", func(t *testing.T) {
+		for testName, testCase := range map[string]struct {
+			includeExprs []string
+			excludeExprs []string
+			expected     []string
+		}{
+			"MatchesOneFileName": {
+				includeExprs: []string{"testFile1"},
+				expected: []string{
+					"testFile1",
+					filepath.Join("dir1", "testFile1"),
+					filepath.Join("dir2", "testFile1"),
+				},
+			},
+			"MatchesOneFileWhenSubdirectoriesExcluded": {
+				includeExprs: []string{"testFile1"},
+				excludeExprs: []string{"/*/testFile1"},
+				expected:     []string{"testFile1"},
+			},
+			"MatchesAllFilesWithPrefix": {
+				includeExprs: []string{"/testFile*"},
+				expected: []string{
+					"testFile1",
+					"testFile2",
+					"testFile1.go",
+					"testFile2.go",
+					"testFile3.yml",
+				},
+			},
+			"MatchesAllFilesWithPrefixExcludingSuffix": {
+				includeExprs: []string{"/testFile*"},
+				excludeExprs: []string{"/*.go"},
+				expected: []string{
+					"testFile1",
+					"testFile2",
+					"testFile3.yml",
+				},
+			},
+			"MatchesAllFilesWithSuffix": {
+				includeExprs: []string{"/*.go"},
+				expected: []string{
+					"testFile1.go",
+					"testFile2.go",
+					"built.go",
+				},
+			},
+			"MatchesAllFilesWithSuffixExcludingPrefix": {
+				includeExprs: []string{"/*.go"},
+				excludeExprs: []string{"/built*"},
+				expected: []string{
+					"testFile1.go",
+					"testFile2.go",
+				},
+			},
+			"MatchesAllFilesInSubdirectoryWithSuffix": {
+				includeExprs: []string{"/dir1/*.go"},
+				expected: []string{
+					filepath.Join("dir1", "testFile1.go"),
+					filepath.Join("dir1", "testFile2.go"),
+					filepath.Join("dir1", "built.go"),
+				},
+			},
+			"MatchesAllFilesInWildcardSubdirectoryWithSuffix": {
+				includeExprs: []string{"/*/*.go"},
+				expected: []string{
+					filepath.Join("dir1", "testFile1.go"),
+					filepath.Join("dir1", "testFile2.go"),
+					filepath.Join("dir1", "built.go"),
+					filepath.Join("dir2", "testFile1.go"),
+					filepath.Join("dir2", "testFile2.go"),
+					filepath.Join("dir2", "built.go"),
+				},
+			},
+			"MatchesAllFilesInWildcardSubdirectoryWithSuffixExcludingPrefix": {
+				includeExprs: []string{"/*/*.go"},
+				excludeExprs: []string{"/*/built*"},
+				expected: []string{
+					filepath.Join("dir1", "testFile1.go"),
+					filepath.Join("dir1", "testFile2.go"),
+					filepath.Join("dir2", "testFile1.go"),
+					filepath.Join("dir2", "testFile2.go"),
+				},
+			},
+		} {
+			t.Run(testName, func(t *testing.T) {
+				include, err := NewGitignoreFileMatcher(tmpDir, testCase.includeExprs...)
+				require.NoError(t, err)
+				var exclude FileMatcher
+				if len(testCase.excludeExprs) != 0 {
+					exclude, err = NewGitignoreFileMatcher(tmpDir, testCase.excludeExprs...)
+					require.NoError(t, err)
 				}
-			}
+				b := FileListBuilder{
+					WorkingDir: tmpDir,
+					Include:    include,
+					Exclude:    exclude,
+				}
+
+				files, err := b.Build()
+				require.NoError(t, err)
+				assert.ElementsMatch(t, testCase.expected, files)
+			})
 		}
+	})
+
+	t.Run("NewFileListBuilder", func(t *testing.T) {
+		t.Run("MatchesAllFiles", func(t *testing.T) {
+			b := NewFileListBuilder(tmpDir)
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.ElementsMatch(t, allFiles, files)
+		})
+	})
+	t.Run("AlwaysMatch", func(t *testing.T) {
+		t.Run("IncludesAllFilesAndDirectories", func(t *testing.T) {
+			b := NewFileListBuilder(tmpDir)
+			b.Include = AlwaysMatch{}
+			files, err := b.Build()
+			require.NoError(t, err)
+			allFilesAndDirs := append(allFiles, dirNames...)
+			allFilesAndDirs = append(allFilesAndDirs, "")
+			assert.ElementsMatch(t, allFilesAndDirs, files)
+		})
+		t.Run("ExcludesAllFilesAndDirectories", func(t *testing.T) {
+			b := NewFileListBuilder(tmpDir)
+			b.Exclude = AlwaysMatch{}
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.Empty(t, files)
+		})
+	})
+
+	t.Run("FileAlwaysMatch", func(t *testing.T) {
+		t.Run("IncludesAllFiles", func(t *testing.T) {
+			b := FileListBuilder{
+				Include:    FileAlwaysMatch{},
+				WorkingDir: tmpDir,
+			}
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.ElementsMatch(t, allFiles, files)
+		})
+		t.Run("ExcludesAllFiles", func(t *testing.T) {
+			b := FileListBuilder{
+				Include:    AlwaysMatch{},
+				Exclude:    FileAlwaysMatch{},
+				WorkingDir: tmpDir,
+			}
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.ElementsMatch(t, append(dirNames, ""), files)
+		})
+	})
+
+	t.Run("NeverMatch", func(t *testing.T) {
+		t.Run("IncludesNoFiles", func(t *testing.T) {
+			b := FileListBuilder{
+				Include:    NeverMatch{},
+				WorkingDir: tmpDir,
+			}
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.Empty(t, files)
+		})
+		t.Run("ExcludesNoFiles", func(t *testing.T) {
+			b := FileListBuilder{
+				Include:    FileAlwaysMatch{},
+				Exclude:    NeverMatch{},
+				WorkingDir: tmpDir,
+			}
+			files, err := b.Build()
+			require.NoError(t, err)
+			assert.ElementsMatch(t, allFiles, files)
+		})
 	})
 }
