10a4cd33ea2e9df929a771bfc18c23c45cf853f6
hhoke <harris.hoke@10gen.com>
2020-07-17 16:47:20 -0400

    removed test logging

--- loggers_linux.go
+++ loggers_linux.go
@@ -3,13 +3,10 @@
 package evergreen
 
 import (
-	"github.com/mongodb/grip"
-	"github.com/mongodb/grip/message"
 	"github.com/mongodb/grip/send"
 )
 
 func getSystemLogger() send.Sender {
-	grip.Critical(message.Fields{"test": "ignore pls"})
 	sender, err := send.MakeSystemdLogger()
 	if err != nil {
 		return send.MakeNative()
