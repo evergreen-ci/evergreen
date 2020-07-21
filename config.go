41003d0f5e0ee32fcf6796e94d5b50d505a63be5
Kim Tao <kim.tao@mongodb.com>
2020-07-21 09:49:28 -0400

    EVG-8159: include response body in error message from REST client (#3802)

--- config.go
+++ config.go
@@ -29,7 +29,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-07-15"
+	ClientVersion = "2020-07-20"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-07-18"
