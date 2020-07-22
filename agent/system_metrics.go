f3f6b458b1c3241e381e264b837318f5748e89a3
Colin Brown <colin.brown@mongodb.com>
2020-07-22 16:28:54 -0400

    Fix error handling and Kim comments

--- agent/system_metrics.go
+++ agent/system_metrics.go
@@ -2,16 +2,16 @@ package agent
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/rest/client"
 	"github.com/evergreen-ci/timber"
-	system_metrics "github.com/evergreen-ci/timber/system_metrics"
+	metrics "github.com/evergreen-ci/timber/system_metrics"
 	"github.com/mongodb/ftdc"
 	"github.com/mongodb/grip"
-	"github.com/mongodb/grip/message"
 	"github.com/mongodb/grip/recovery"
 	"github.com/pkg/errors"
 )
@@ -23,22 +23,21 @@ type MetricCollector interface {
 }
 
 type SystemMetricsCollector struct {
-	mu          sync.Mutex
-	wg          sync.WaitGroup
-	innerCancel context.CancelFunc
-	logger      client.LoggerProducer
-	taskOpts    *system_metrics.SystemMetricsOptions
-	connOpts    system_metrics.ConnectionOptions
-	interval    time.Duration
-	collectors  []MetricCollector
-	id          string
-	client      *system_metrics.SystemMetricsClient
-	errored     bool
-	closed      bool
+	mu              sync.Mutex
+	wg              sync.WaitGroup
+	streamingCancel context.CancelFunc
+	taskOpts        *metrics.SystemMetricsOptions
+	connOpts        metrics.ConnectionOptions
+	interval        time.Duration
+	collectors      []MetricCollector
+	id              string
+	client          *metrics.SystemMetricsClient
+	catcher         grip.Catcher
+	closed          bool
 }
 
-func NewSystemMetricsCollector(ctx context.Context, logger client.LoggerProducer, interval time.Duration,
-	c client.Communicator, t *task.Task, collectors []MetricCollector) (*SystemMetricsCollector, error) {
+func NewSystemMetricsCollector(ctx context.Context, interval time.Duration, c client.Communicator,
+	t *task.Task, collectors []MetricCollector) (*SystemMetricsCollector, error) {
 
 	if interval < 0 {
 		return nil, errors.New("interval cannot be negative")
@@ -68,121 +67,93 @@ func NewSystemMetricsCollector(ctx context.Context, logger client.LoggerProducer
 		APIKey:      bi.APIKey,
 		Retries:     10,
 	}
-	connOpts := system_metrics.ConnectionOptions{
+	connOpts := metrics.ConnectionOptions{
 		DialOpts: dialOpts,
 	}
 
 	return &SystemMetricsCollector{
-		logger:     logger,
 		interval:   interval,
 		collectors: collectors,
 		taskOpts:   taskOpts,
 		connOpts:   connOpts,
+		catcher:    grip.NewBasicCatcher(),
 	}, nil
 }
 
-func (s *SystemMetricsCollector) Start(ctx context.Context) {
-	catcher := grip.NewBasicCatcher()
+func (s *SystemMetricsCollector) Start(ctx context.Context) (returnedErr error) {
 	defer func() {
 		s.mu.Lock()
 		defer s.mu.Unlock()
-		s.errored = true
-		err := catcher.Resolve()
-		s.logger.System().Error(message.WrapError(err, message.Fields{
-			"message":  "error setting up system metrics collection",
-			"id":       s.id,
-			"connOpts": s.connOpts,
-			"taskOpts": *s.taskOpts,
-		}))
+		if s.catcher.HasErrors() {
+			returnedErr = s.cleanup()
+		}
+		return
 	}()
 
 	var err error
-	s.client, err = system_metrics.NewSystemMetricsClient(ctx, s.connOpts)
-	if err != nil {
-		catcher.Add(err)
+	s.client, err = metrics.NewSystemMetricsClient(ctx, s.connOpts)
+	if s.logError(err, "problem creating new system metrics client") {
 		return
 	}
 
 	s.id, err = s.client.CreateSystemMetricRecord(ctx, *s.taskOpts)
-	if err != nil {
-		catcher.Add(err)
+	if s.logError(err, "problem creating cedar system metrics metadata object") {
 		return
 	}
 
-	innerCtx := context.Background()
-	innerCtx, s.innerCancel = context.WithCancel(innerCtx)
+	var streamingCtx context.Context
+	streamingCtx, s.streamingCancel = context.WithCancel(ctx)
 
 	for _, collector := range s.collectors {
 		s.wg.Add(1)
-		stream, err := s.client.StreamSystemMetrics(innerCtx, s.id, collector.Name(), system_metrics.StreamOpts{})
-		if err != nil {
-			catcher.Add(err)
+		stream, err := s.client.StreamSystemMetrics(streamingCtx, s.id, collector.Name(), metrics.StreamOpts{})
+		if s.logError(err, fmt.Sprintf("problem creating system metrics stream for id %s and metricType %s", s.id, collector.Name())) {
 			return
 		}
 		ftdcCollector := ftdc.NewStreamingCollector(1, stream)
 		err = ftdcCollector.SetMetadata(collector.Metadata())
-		if err != nil {
-			catcher.Add(err)
+		if s.logError(err, fmt.Sprintf("problem setting metadata on stream for id %s and metricType %s", s.id, collector.Name())) {
 			return
 		}
-		go s.timedCollect(innerCtx, collector, stream, ftdcCollector)
+		go s.timedCollect(streamingCtx, collector, stream, ftdcCollector)
 	}
-	go s.closeOnCancel(ctx)
+	go s.closeOnCancel(ctx, streamingCtx)
+	return
 }
 
-func (s *SystemMetricsCollector) timedCollect(ctx context.Context, mc MetricCollector, stream *system_metrics.SystemMetricsWriteCloser, collector ftdc.Collector) {
+func (s *SystemMetricsCollector) timedCollect(ctx context.Context, mc MetricCollector, stream *metrics.SystemMetricsWriteCloser, collector ftdc.Collector) {
 	defer s.wg.Done()
-
-	catcher := grip.NewBasicCatcher()
-	defer func() {
-		s.mu.Lock()
-		defer s.mu.Unlock()
-		s.errored = true
-		err := catcher.Resolve()
-		s.logger.System().Error(message.WrapError(err, message.Fields{
-			"message":    "error while collecting system metrics",
-			"id":         s.id,
-			"metricType": mc.Name(),
-		}))
-	}()
 
 	timer := time.NewTimer(0)
 	defer timer.Stop()
-	defer catcher.Add(stream.Close())
-	defer ftdc.FlushCollector(collector, stream)
-	defer catcher.Add(recovery.HandlePanicWithError(recover(), nil, "panic in system metrics collector"))
+	defer s.logError(stream.Close(), fmt.Sprintf("problem closing system metrics stream for id %s and metricType %s", s.id, mc.Name()))
+	defer s.logError(ftdc.FlushCollector(collector, stream), fmt.Sprintf("problem flushing system metrics data collector for id %s and metricType %s", s.id, mc.Name()))
+	defer s.logError(recovery.HandlePanicWithError(recover(), nil, fmt.Sprintf("panic in system metrics stream for id %s and metricType %s", s.id, mc.Name())), "")
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-timer.C:
-			func() {
-				data, err := mc.Collect()
-				if err != nil {
-					catcher.Add(err)
-					return
-				}
-				err = collector.Add(data)
-				if err != nil {
-					catcher.Add(err)
-					return
-				}
-				_ = timer.Reset(s.interval)
-			}()
+			data, err := mc.Collect()
+			if s.logError(err, fmt.Sprintf("problem collecting system metrics data for id %s and metricType %s", s.id, mc.Name())) {
+				return
+			}
+			if s.logError(collector.Add(data), fmt.Sprintf("problem adding system metrics data to collector for id %s and metricType %s", s.id, mc.Name())) {
+				return
+			}
+			_ = timer.Reset(s.interval)
 		}
 	}
 }
 
-func (s *SystemMetricsCollector) closeOnCancel(outerCtx context.Context) {
+func (s *SystemMetricsCollector) closeOnCancel(outerCtx, streamingCtx context.Context) {
 	for {
 		select {
 		case <-outerCtx.Done():
-			if s.cleanup() != nil {
-				s.mu.Lock()
-				defer s.mu.Unlock()
-				s.errored = true
-			}
+			grip.Error(s.cleanup())
+			return
+		case <-streamingCtx.Done():
 			return
 		}
 	}
@@ -190,53 +161,57 @@ func (s *SystemMetricsCollector) closeOnCancel(outerCtx context.Context) {
 
 func (s *SystemMetricsCollector) Close(outerCtx context.Context) error {
 	s.mu.Lock()
-	if s.closed && s.errored {
-		return errors.New("system metrics collector already cancelled or closed with errors, see system logs")
-	}
 	if s.closed {
+		s.mu.Unlock()
 		return errors.New("system metrics collector already cancelled or closed")
 	}
 	s.mu.Unlock()
 
-	err := s.cleanup()
-	if err != nil || s.errored {
-		err = errors.New("system metrics collector encountered errors, see system logs.")
-	}
-	return err
+	return s.cleanup()
 }
 
 func (s *SystemMetricsCollector) cleanup() error {
-	s.innerCancel()
+	s.streamingCancel()
 	s.wg.Wait()
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	catcher := grip.NewBasicCatcher()
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
-	err := s.client.CloseSystemMetrics(ctx, s.id)
-	catcher.Add(err)
-	s.logger.System().Error(message.WrapError(err, message.Fields{
-		"message":  "error closing system metrics collector",
-		"id":       s.id,
-		"interval": s.interval,
-	}))
-	err = s.client.CloseClient()
-	catcher.Add(err)
-	s.logger.System().Error(message.WrapError(err, message.Fields{
-		"message":  "error closing system metrics collector client",
-		"id":       s.id,
-		"interval": s.interval,
-	}))
+	s.logError(s.client.CloseSystemMetrics(ctx, s.id), fmt.Sprintf("error closing out system metrics object for id %s", s.id))
+	s.logError(s.client.CloseClient(), fmt.Sprintf("error closing system metrics client for id %s", s.id))
 	s.closed = true
 
-	return catcher.Resolve()
+	return s.catcher.Resolve()
 }
 
-func getTaskInfo(t *task.Task) (*system_metrics.SystemMetricsOptions, error) {
-	// convert from task to SystemMetrics optiobn
-	return nil, nil
+func (s *SystemMetricsCollector) logError(err error, msg string) bool {
+	if err != nil {
+		s.mu.Lock()
+		defer s.mu.Unlock()
+		if msg != "" {
+			s.catcher.Add(errors.Wrap(err, msg))
+		} else {
+			s.catcher.Add(err)
+		}
+		return true
+	}
+	return false
+}
+
+func getTaskInfo(t *task.Task) (*metrics.SystemMetricsOptions, error) {
+	opts := &metrics.SystemMetricsOptions{
+		Project:     t.Project,
+		Version:     t.Version,
+		Variant:     t.BuildVariant,
+		TaskName:    t.DisplayName,
+		TaskId:      t.Id,
+		Execution:   int32(t.Execution),
+		Mainline:    t.CommitQueueMerge,
+		Compression: metrics.CompressionTypeNone,
+		Schema:      metrics.SchemaTypeRawEvents,
+	}
+	return opts, nil
 }
 
 // TODO
