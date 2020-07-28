990f51dd01a3a9ebf10c344f6a8ae6dccdd2990b
hhoke <harris.hoke@10gen.com>
2020-07-28 15:21:33 -0400

    added timeout to err flush

--- agent/agent.go
+++ agent/agent.go
@@ -503,7 +503,7 @@ func (a *Agent) finishTask(ctx context.Context, tc *taskContext, status string) 
 		err := a.uploadToS3(ctx, tc)
 		tc.logger.Execution().Error(errors.Wrap(err, "error uploading log files"))
 		tc.logger.Execution().Infof("Sending final status as: %v", detail.Status)
-		ctx, cancel = a.withCallbackTimeout(ctx, tc)
+		ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
 		defer cancel()
 		grip.Error(tc.logger.Flush(ctx))
 	}
