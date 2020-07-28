20979298d171743e420a62f46bf262f386783e0b
Jonathan Brill <jonathan.brill@10gen.com>
2020-07-28 12:12:33 -0400

    EVG-12681 fix for Apple's git (#3834)

--- config.go
+++ config.go
@@ -29,7 +29,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-07-24"
+	ClientVersion = "2020-07-28"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-07-30"
