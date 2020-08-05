af0f68b7c91c8e2c3e583eed6dba3083d5b94b9f
Colin Brown <colin.brown@mongodb.com>
2020-08-05 16:59:14 -0400

    Fix agent version

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	ClientVersion = "2020-07-31"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2020-07-31"
+	AgentVersion = "2020-08-05"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
