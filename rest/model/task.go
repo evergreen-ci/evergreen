7c56f5ebef25da056b60ed558392d4d6d031d858
Annie Black <annie.black@10gen.com>
2020-08-11 14:16:05 -0700

    EVG-12654 tweak how AMI is returned from task API (#3877)

--- rest/model/task.go
+++ rest/model/task.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/mongodb/grip"
-
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/apimodels"
 	"github.com/evergreen-ci/evergreen/model/artifact"
 	"github.com/evergreen-ci/evergreen/model/host"
 	"github.com/evergreen-ci/evergreen/model/patch"
 	"github.com/evergreen-ci/evergreen/model/task"
+	"github.com/mongodb/grip"
 	"github.com/pkg/errors"
 )
 
@@ -69,7 +68,7 @@ type APITask struct {
 	Aborted            bool                `json:"aborted"`
 	CanSync            bool                `json:"can_sync,omitempty"`
 	SyncAtEndOpts      APISyncAtEndOptions `json:"sync_at_end_opts"`
-	Ami                *string             `json:ami`
+	Ami                *string             `json:"ami"`
 }
 
 type LogLinks struct {
@@ -220,22 +219,16 @@ func (at *APITask) BuildFromService(t interface{}) error {
 		if v.HostId != "" {
 			hostLink := fmt.Sprintf("%s/host/%s", evergreen.GetEnvironment().Settings().Ui.Url, v.HostId)
 			at.HostLink = &hostLink
-		}
-
-		h, err := host.FindOneId(v.HostId)
-		if err != nil {
-			return errors.Wrap(err, fmt.Sprintf("Error while finding distro for host: %s", v.HostId))
-		}
-		if h != nil {
-			var ami *string = nil
-			for _, providerSetting := range h.Distro.ProviderSettingsList {
-				providerSettingAmi := providerSetting.Lookup("ami")
-				if providerSettingAmi != nil {
-					amiString := providerSettingAmi.StringValue()
-					ami = &amiString
+			h, err := host.FindOneId(v.HostId)
+			if err != nil {
+				return errors.Wrapf(err, "error finding host '%s' for task", v.HostId)
+			}
+			if h != nil && len(h.Distro.ProviderSettingsList) == 1 {
+				ami, ok := h.Distro.ProviderSettingsList[0].Lookup("ami").StringValueOK()
+				if ok {
+					at.Ami = ToStringPtr(ami)
 				}
 			}
-			at.Ami = ami
 		}
 
 		if len(v.ExecutionTasks) > 0 {
