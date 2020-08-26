cb2af1a67db96992730fc20ab10afa84cc6a5320
Jonathan Brill <jonathan.brill@10gen.com>
2020-08-26 18:34:50 -0400

    increment client version

--- config.go
+++ config.go
@@ -31,7 +31,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2020-08-19"
+	ClientVersion = "2020-08-26"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2020-08-17"
