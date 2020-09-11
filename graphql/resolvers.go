4355d4c6229f8dcddb6f8b8b73c2d4be04bae3f6
Chaya Malik <chaya.malik@10gen.com>
2020-09-11 16:54:32 -0400

    12910: Don't return an error if it's not configured (#3981)

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -1961,14 +1961,17 @@ func (r *queryResolver) BuildBaron(ctx context.Context, taskId string, exec int)
 	execString := strconv.Itoa(exec)
 	searchReturnInfo, projectNotFound, err := GetSearchReturnInfo(taskId, execString)
 	if projectNotFound {
-		return nil, ResourceNotFound.Send(ctx, err.Error())
+		return &BuildBaron{
+			SearchReturnInfo:     searchReturnInfo,
+			BuildBaronConfigured: false,
+		}, nil
 	}
 	if err != nil {
 		return nil, InternalServerError.Send(ctx, err.Error())
 	}
 	return &BuildBaron{
 		SearchReturnInfo:     searchReturnInfo,
-		BuildBaronConfigured: !projectNotFound,
+		BuildBaronConfigured: true,
 	}, nil
 }
 
