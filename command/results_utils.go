d84c8531e02005c3bdf416c5f723accb7e7b996a
Kim Tao <kim.tao@mongodb.com>
2020-09-11 15:14:27 -0400

    Refactor sending test results for attach.results so it only sends the raw log test results for the one relevant test results command

--- command/results_utils.go
+++ command/results_utils.go
@@ -10,8 +10,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-// sendTestResults sends the test results to the API server. For test results
-// that use raw logs, it also sends the test logs.
+// sendTestResults sends the test results to the API server.
 func sendTestResults(ctx context.Context, conf *model.TaskConfig,
 	logger client.LoggerProducer, comm client.Communicator,
 	results *task.LocalTestResults) error {
@@ -20,35 +19,6 @@ func sendTestResults(ctx context.Context, conf *model.TaskConfig,
 		return errors.New("cannot send nil results")
 	}
 
-	td := client.TaskData{ID: conf.Task.Id, Secret: conf.Task.Secret}
-
-	for i, res := range results.Results {
-		if ctx.Err() != nil {
-			return errors.Errorf("operation canceled during uploading")
-		}
-
-		if res.LogRaw != "" {
-			logger.Execution().Info("Attaching raw test logs")
-			testLogs := &model.TestLog{
-				Name:          res.TestFile,
-				Task:          conf.Task.Id,
-				TaskExecution: conf.Task.Execution,
-				Lines:         []string{res.LogRaw},
-			}
-
-			id, err := comm.SendTestLog(ctx, td, testLogs)
-			if err != nil {
-				logger.Execution().Errorf("problem posting raw logs from results %s", err.Error())
-			} else {
-				results.Results[i].LogId = id
-			}
-
-			// clear the logs from the TestResult struct after it has been saved in the test logs. Since they are
-			// being saved in the test_logs collection, we can clear them to prevent them from being saved in the task
-			// collection.
-			results.Results[i].LogRaw = ""
-		}
-	}
 	logger.Execution().Info("attaching test results")
 
 	if err := comm.SendTestResults(ctx, client.TaskData{ID: conf.Task.Id, Secret: conf.Task.Secret}, results); err != nil {
