3908fd24b73523c77ea3ea0fc6cb038446a0b0a5
Mohamed <mohamed.khelif@mongodb.com>
2020-09-18 15:34:36 -0400

    Add volume id as a spawn host option

--- graphql/generated.go
+++ graphql/generated.go
@@ -4051,6 +4051,7 @@ input SpawnHostInput {
   setUpScript: String
   isVirtualWorkStation: Boolean!
   homeVolumeSize: Int
+  volumeId: String
 }
 
 input EditSpawnHostInput {
@@ -20359,6 +20360,12 @@ func (ec *executionContext) unmarshalInputSpawnHostInput(ctx context.Context, ob
 		case "homeVolumeSize":
 			var err error
 			it.HomeVolumeSize, err = ec.unmarshalOInt2ᚖint(ctx, v)
+			if err != nil {
+				return it, err
+			}
+		case "volumeId":
+			var err error
+			it.VolumeID, err = ec.unmarshalOString2ᚖstring(ctx, v)
 			if err != nil {
 				return it, err
 			}
