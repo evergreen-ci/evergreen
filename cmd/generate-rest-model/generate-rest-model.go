099b777cbfdda0c490bf1e8a0fed00bedebdbbf2
John Liu <john.m.liu2@gmail.com>
2020-09-22 09:56:43 -0400

    cr

--- cmd/generate-rest-model/generate-rest-model.go
+++ cmd/generate-rest-model/generate-rest-model.go
@@ -18,10 +18,11 @@ func main() {
 		modelFile  string
 		helperFile string
 	)
-	flag.StringVar(&configFile, "config", "", "path to the config yml file with struct mappings")
-	flag.StringVar(&schemaFile, "schema", "", "path to the graphql schema to use for the output")
-	flag.StringVar(&modelFile, "model", "", "file path to write the generated model code <TODO relative>")
-	flag.StringVar(&helperFile, "helper", "", "file path to write the generated helper functions")
+	const pathToRestModels = "rest/model"
+	flag.StringVar(&configFile, "config", "", "path to the config yml file with struct mappings, either an absolute path or relative to the evergreen folder")
+	flag.StringVar(&schemaFile, "schema", "", "path to the graphql schema to use for the output, either an absolute path or relative to the evergreen folder")
+	flag.StringVar(&modelFile, "model", "", "file path to write the generated model code, either an absolute path or relative to the evergreen folder")
+	flag.StringVar(&helperFile, "helper", "", "file path to write the generated helper functions, either an absolute path or relative to the evergreen folder")
 	flag.Parse()
 
 	configData, err := ioutil.ReadFile(configFile)
@@ -36,7 +37,7 @@ func main() {
 	for dbModel, info := range gqlConfig.Models {
 		mapping[dbModel] = info.Model[0]
 	}
-	model.SetGeneratePathPrefix("rest/model")
+	model.SetGeneratePathPrefix(pathToRestModels)
 	model, helper, err := model.Codegen(string(schema), mapping)
 	grip.EmergencyFatal(errors.Wrap(err, "error generating code"))
 	err = ioutil.WriteFile(modelFile, model, 0644)
