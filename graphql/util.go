48f365905734c0af43b7b3cf07c8eddb13b5b709
Mohamed <mohamed.khelif@mongodb.com>
2020-09-24 13:00:30 -0400

    Use correct function

--- graphql/util.go
+++ graphql/util.go
@@ -299,7 +299,7 @@ func GetVariantsAndTasksFromProject(patchedConfig string, patchProject string) (
 	for _, variant := range project.BuildVariants {
 		tasksForVariant := []model.BuildVariantTaskUnit{}
 		for _, TaskFromVariant := range variant.Tasks {
-			if *TaskFromVariant.Patchable {
+			if !util.IsPtrSetToFalse(TaskFromVariant.Patchable) {
 				if TaskFromVariant.IsGroup {
 					tasksForVariant = append(tasksForVariant, model.CreateTasksFromGroup(TaskFromVariant, project)...)
 				} else {
