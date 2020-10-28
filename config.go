9e3e54eb8c5c1927599642e2f7ddc8f38cc5bfd9
John Liu <john.liu@mongodb.com>
2020-10-28 14:47:15 -0400

    EVG-12966 more logging in task setup (#4108)

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 	ClientVersion = "2020-10-19"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2020-10-22"
+	AgentVersion = "2020-10-27"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
