9cae9e4ca58f2ff00dacd23b0ec16700705da77d
julianedwards <julian.edwards@mongodb.com>
2020-10-30 15:16:23 -0400

    update agent version

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 	ClientVersion = "2020-10-19"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2020-10-27"
+	AgentVersion = "2020-10-30"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
