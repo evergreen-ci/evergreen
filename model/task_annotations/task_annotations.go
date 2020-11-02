8e0a561bdf8d61e073564fff96ed447e19a3a973
Chaya Malik <chaya.malik@10gen.com>
2020-11-02 16:29:28 -0500

    add user sourced info

--- model/task_annotations/task_annotations.go
+++ model/task_annotations/task_annotations.go
@@ -10,14 +10,10 @@ type TaskAnnotation struct {
 	Id            string `bson:"_id" json:"id"`
 	TaskId        string `bson:"task_id" json:"task_id"`
 	TaskExecution int    `bson:"task_execution" json:"task_execution"`
-	// comment about the failure
-	Note string `bson:"note,omitempty" json:"note,omitempty"`
-	// links to tickets definitely related.
-	Issues []IssueLink `bson:"issues,omitempty" json:"issues,omitempty"`
-	// links to tickets possibly related
-	SuspectedIssues []IssueLink `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
-	// annotation attribution
-	Source AnnotationSource `bson:"source" json:"source"`
+	// the task annotation that is api sourced and will be overwritten when updated
+	APIAnnotation Annotation `bson:"api_annotation,omitempty" json:"api_annotation,omitempty"`
+	// user edits to the annotation that will be preserved
+	UserAnnotation Annotation `bson:"user_annotation,omitempty" json:"user_annotation,omitempty"`
 	// structured data about the task (not displayed in the UI, but available in the API)
 	Metadata *birch.Document `bson:"metadata,omitempty" json:"metadata,omitempty"`
 }
@@ -31,4 +27,15 @@ type IssueLink struct {
 type AnnotationSource struct {
 	Author string    `bson:"author,omitempty" json:"author,omitempty"`
 	Time   time.Time `bson:"time,omitempty" json:"time,omitempty"`
+}
+
+type Annotation struct {
+	// comment about the failure
+	Note string `bson:"note,omitempty" json:"note,omitempty"`
+	// links to tickets definitely related.
+	Issues []IssueLink `bson:"issues,omitempty" json:"issues,omitempty"`
+	// links to tickets possibly related
+	SuspectedIssues []IssueLink `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
+	// annotation attribution
+	Source AnnotationSource `bson:"source" json:"source"`
 }
--- rest/model/task_annotations.go
+++ rest/model/task_annotations.go
@@ -3,57 +3,50 @@ package model
 import (
 	"time"
 
-	"github.com/evergreen-ci/birch"
 	"github.com/evergreen-ci/evergreen/model/task_annotations"
 )
 
 type APITaskAnnotation struct {
-	Id              *string             `bson:"_id" json:"id"`
-	TaskId          *string             `bson:"task_id" json:"task_id"`
-	TaskExecution   int                 `bson:"task_execution" json:"task_execution"`
-	Note            *string             `bson:"note" json:"note"`
-	Issues          []APIIssueLink      `bson:"issues" json:"issues"`
-	SuspectedIssues []APIIssueLink      `bson:"suspected_issues" json:"suspected_issues"`
-	Source          APIAnnotationSource `bson:"source" json:"source"`
-	Metadata        *birch.Document     `bson:"metadata" json:"metadata"`
+	Id             *string       `bson:"_id" json:"id"`
+	TaskId         *string       `bson:"task_id" json:"task_id"`
+	TaskExecution  *int          `bson:"task_execution" json:"task_execution"`
+	APIAnnotation  APIAnnotation `bson:"api_annotation,omitempty" json:"api_annotation,omitempty"`
+	UserAnnotation APIAnnotation `bson:"user_annotation,omitempty" json:"user_annotation,omitempty"`
 }
 
+type APIAnnotation struct {
+	Note          *string             `bson:"note,omitempty" json:"note,omitempty"`
+	Issues        []APIIssueLink      `bson:"issues,omitempty" json:"issues,omitempty"`
+	SuspectIssues []APIIssueLink      `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
+	Source        APIAnnotationSource `bson:"source" json:"source"`
+}
 type APIAnnotationSource struct {
-	Author *string    `json:"author"`
-	Time   *time.Time `json:"time"`
-}
-type APIIssueLink struct {
-	URL      *string `json:"url"`
-	IssueKey *string `json:"issue_key"`
+	Author *string    `bson:"author,omitempty" json:"author,omitempty"`
+	Time   *time.Time `bson:"time,omitempty" json:"time,omitempty"`
 }
 
-// APITaskAnnotationBuildFromService takes the task_annotations.TaskAnnotation DB struct and
-// returns the REST struct *APITaskAnnotation with the corresponding fields populated
-func APITaskAnnotationBuildFromService(t task_annotations.TaskAnnotation) *APITaskAnnotation {
-	m := APITaskAnnotation{}
-	m.Id = StringStringPtr(t.Id)
-	m.TaskId = StringStringPtr(t.TaskId)
-	m.TaskExecution = t.TaskExecution
+type APIIssueLink struct {
+	URL      *string `bson:"url" json:"url"`
+	IssueKey *string `bson:"issue_key,omitempty" json:"issue_key,omitempty"`
+}
+
+// APIAnnotationBuildFromService takes the task_annotations.Annotation DB struct and
+// returns the REST struct *APIAnnotation with the corresponding fields populated
+func APIAnnotationBuildFromService(t task_annotations.Annotation) *APIAnnotation {
+	m := APIAnnotation{}
 	m.Issues = ArrtaskannotationsIssueLinkArrAPIIssueLink(t.Issues)
 	m.Note = StringStringPtr(t.Note)
-	m.SuspectedIssues = ArrtaskannotationsIssueLinkArrAPIIssueLink(t.SuspectedIssues)
 	m.Source = *APIAnnotationSourceBuildFromService(t.Source)
-	m.Metadata = t.Metadata
 	return &m
 }
 
-// APITaskAnnotationToService takes the APITaskAnnotation REST struct and returns the DB struct
-// *task_annotations.TaskAnnotation with the corresponding fields populated
-func APITaskAnnotationToService(m APITaskAnnotation) *task_annotations.TaskAnnotation {
-	out := &task_annotations.TaskAnnotation{}
-	out.TaskId = StringPtrString(m.TaskId)
-	out.TaskExecution = m.TaskExecution
-	out.Id = StringPtrString(m.Id)
+// APIAnnotationToService takes the APIAnnotation REST struct and returns the DB struct
+// *task_annotations.Annotation with the corresponding fields populated
+func APIAnnotationToService(m APIAnnotation) *task_annotations.Annotation {
+	out := &task_annotations.Annotation{}
 	out.Issues = ArrAPIIssueLinkArrtaskannotationsIssueLink(m.Issues)
 	out.Note = StringPtrString(m.Note)
-	out.SuspectedIssues = ArrAPIIssueLinkArrtaskannotationsIssueLink(m.SuspectedIssues)
 	out.Source = *APIAnnotationSourceToService(m.Source)
-	out.Metadata = m.Metadata
 	return out
 }
 
@@ -97,7 +90,30 @@ func APIIssueLinkToService(m APIIssueLink) *task_annotations.IssueLink {
 	return out
 }
 
-//generated helper functions
+// APITaskAnnotationBuildFromService takes the task_annotations.TaskAnnotation DB struct and
+// returns the REST struct *APITaskAnnotation with the corresponding fields populated
+func APITaskAnnotationBuildFromService(t task_annotations.TaskAnnotation) *APITaskAnnotation {
+	m := APITaskAnnotation{}
+	m.APIAnnotation = *APIAnnotationBuildFromService(t.APIAnnotation)
+	m.Id = StringStringPtr(t.Id)
+	m.TaskExecution = IntIntPtr(t.TaskExecution)
+	m.TaskId = StringStringPtr(t.TaskId)
+	m.UserAnnotation = *APIAnnotationBuildFromService(t.UserAnnotation)
+	return &m
+}
+
+// APITaskAnnotationToService takes the APITaskAnnotation REST struct and returns the DB struct
+// *task_annotations.TaskAnnotation with the corresponding fields populated
+func APITaskAnnotationToService(m APITaskAnnotation) *task_annotations.TaskAnnotation {
+	out := &task_annotations.TaskAnnotation{}
+	out.APIAnnotation = *APIAnnotationToService(m.APIAnnotation)
+	out.Id = StringPtrString(m.Id)
+	out.TaskExecution = IntPtrInt(m.TaskExecution)
+	out.TaskId = StringPtrString(m.TaskId)
+	out.UserAnnotation = *APIAnnotationToService(m.UserAnnotation)
+	return out
+}
+
 func ArrtaskannotationsIssueLinkArrAPIIssueLink(t []task_annotations.IssueLink) []APIIssueLink {
 	m := []APIIssueLink{}
 	for _, e := range t {
@@ -112,4 +128,21 @@ func ArrAPIIssueLinkArrtaskannotationsIssueLink(t []APIIssueLink) []task_annotat
 		m = append(m, *APIIssueLinkToService(e))
 	}
 	return m
+}
+
+func IntInt(in int) int {
+	return int(in)
+}
+
+func IntIntPtr(in int) *int {
+	out := int(in)
+	return &out
+}
+
+func IntPtrInt(in *int) int {
+	var out int
+	if in == nil {
+		return out
+	}
+	return int(*in)
 }
