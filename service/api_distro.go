2dedd9dc46e3ba614c19ba4ff0af96268e5e45fb
Kim Tao <kim.tao@mongodb.com>
2020-11-11 10:43:56 -0500

    EVG-13239: remove old route to get agent's distro (#4158)

--- service/api_distro.go
+++ service/api_distro.go
@@ -1,36 +1,11 @@
 package service
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/evergreen-ci/evergreen/apimodels"
-	"github.com/evergreen-ci/evergreen/model/host"
 	"github.com/evergreen-ci/gimlet"
 )
-
-// TODO (EVG-13239): remove once agents have updated.
-// GetDistro loads the task's distro and sends it to the requester.
-func (as *APIServer) GetDistro(w http.ResponseWriter, r *http.Request) {
-	t := MustHaveTask(r)
-
-	// Get the distro for this task
-	h, err := host.FindOne(host.ByRunningTaskId(t.Id))
-	if err != nil {
-		as.LoggedError(w, r, http.StatusInternalServerError, err)
-		return
-	}
-
-	if h == nil {
-		message := fmt.Errorf("No host found running task %v", t.Id)
-		as.LoggedError(w, r, http.StatusInternalServerError, message)
-		return
-	}
-
-	// agent can't properly unmarshal provider settings
-	h.Distro.ProviderSettingsList = nil
-	gimlet.WriteJSON(w, h.Distro)
-}
 
 func (as *APIServer) GetDistroView(w http.ResponseWriter, r *http.Request) {
 	h := MustHaveHost(r)
