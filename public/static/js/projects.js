b2f74ce1ce5632bf069d60a25e9dde25c9b07cab
Jonathan Brill <jonathan.brill@10gen.com>
2020-11-18 17:36:07 -0500

    add more ways to select tasks with trigger aliases

--- public/static/js/projects.js
+++ public/static/js/projects.js
@@ -887,9 +887,34 @@ mciModule.controller('ProjectCtrl', function ($scope, $window, $http, $location,
   };
 
   function newPatchTriggerAliasController($scope, $mdDialog) {
+    $scope.new_task_specifier = {};
+    $scope.alias = {task_specifiers: []};
+
     if ($scope.data.aliasToEdit) {
       $scope.alias = $scope.data.aliasToEdit;
+      $scope.alias.task_specifiers = $scope.alias.task_specifiers || [];
       $scope.aliasIndex = $scope.data.index;
+    }
+
+    $scope.removeTaskSpecifier = function(index) {
+      $scope.alias.task_specifiers.splice(index, 1);
+    }
+
+    $scope.addTaskSpecifier = function() {
+      if (!$scope.validTaskSpecifier($scope.new_task_specifier)) {
+        return
+      }
+
+      $scope.alias.task_specifiers.push($scope.new_task_specifier);
+      $scope.new_task_specifier = {};
+    }
+
+    $scope.validTaskSpecifier = function(specifier) {
+      if (!specifier.patch_alias && (!specifier.variant_regex || !specifier.task_regex)) {
+        return false
+      }
+
+      return true;
     }
 
     $scope.closeDialog = function (save) {
@@ -914,7 +939,7 @@ mciModule.controller('ProjectCtrl', function ($scope, $window, $http, $location,
     };
 
     $scope.validTriggerAlias = function () {
-      if (!$scope.alias.definition_alias || !$scope.alias.child_project) {
+      if (!$scope.alias.alias || !$scope.alias.child_project) {
         return false;
       }
 
