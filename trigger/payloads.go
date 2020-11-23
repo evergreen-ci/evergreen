94be951489238a29b33d02b56d4063958ff6ca8c
John Liu <john.liu@mongodb.com>
2020-11-23 15:27:11 -0500

    EVG-12542 handle absolute urls in task notifications (#4199)

--- trigger/payloads.go
+++ trigger/payloads.go
@@ -451,7 +451,20 @@ func getFailedTestsFromTemplate(t task.Task) ([]task.TestResult, error) {
 	for i := range t.LocalTestResults {
 		if t.LocalTestResults[i].Status == evergreen.TestFailedStatus {
 			testResult := t.LocalTestResults[i]
-			testResult.URL = settings.Ui.Url + testResult.URL
+			if testResult.URL != "" {
+				logURL, err := url.Parse(testResult.URL)
+				if err != nil {
+					return nil, errors.Wrapf(err, "unable to parse URL %s", testResult.URL)
+				}
+				if logURL.Host == "" {
+					testResult.URL = settings.Ui.Url + testResult.URL
+				} else {
+					testResult.URL = logURL.String()
+				}
+			} else if testResult.LogId != "" {
+				testResult.URL = logURL(testResult, settings.Ui.Url)
+			}
+
 			result = append(result, testResult)
 		}
 	}
