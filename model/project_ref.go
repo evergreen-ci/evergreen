aa6d695d61682262e578949e04bd9eb809be12f4
Annie Black <annie.black@10gen.com>
2021-01-14 08:57:17 -0800

    move

--- model/project_ref.go
+++ model/project_ref.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"math"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -20,6 +19,7 @@ import (
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/model/user"
 	"github.com/evergreen-ci/evergreen/thirdparty"
+	"github.com/evergreen-ci/evergreen/util"
 	"github.com/evergreen-ci/gimlet"
 	"github.com/evergreen-ci/utility"
 	"github.com/mongodb/anser/bsonutil"
@@ -414,7 +414,7 @@ func mergeBranchAndRepoSettings(pRef *ProjectRef, repoRef *RepoRef) *ProjectRef 
 	for i := 0; i < reflectedBranch.Elem().NumField(); i++ {
 
 		branchField := reflectedBranch.Elem().Field(i)
-		if isFieldUndefined(branchField) {
+		if util.IsFieldUndefined(branchField) {
 			reflectedField := reflectedRepo.Field(i)
 			branchField.Set(reflectedField)
 		}
@@ -426,43 +426,6 @@ func mergeBranchAndRepoSettings(pRef *ProjectRef, repoRef *RepoRef) *ProjectRef 
 	}
 
 	return pRef
-}
-
-// isFieldUndefined is an adaptation of IsZero https://golang.org/src/reflect/value.go?s=34297:34325#L1090
-func isFieldUndefined(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return math.Float64bits(v.Float()) == 0
-	case reflect.Complex64, reflect.Complex128:
-		c := v.Complex()
-		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
-	case reflect.Array:
-		return v.Len() == 0
-	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
-		return v.IsNil()
-	case reflect.String:
-		return v.Len() == 0
-	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			if !isFieldUndefined(v.Field(i)) {
-				return false
-			}
-		}
-		return true
-	default:
-		// this should never happen
-		grip.Error(message.Fields{
-			"message": "field has no valid type",
-			"value":   v,
-		})
-		return false
-	}
 }
 
 func FindIdForProject(identifier string) (string, error) {
