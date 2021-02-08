ee459c32d6f3ed6a732429fb2b704ea1aeb6be45
John Liu <john.liu@mongodb.com>
2021-02-08 13:37:14 -0500

    EVG-13690 fix incorrect check for expansions (#4376)

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 	ClientVersion = "2021-02-01"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-02-04"
+	AgentVersion = "2021-02-08"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
