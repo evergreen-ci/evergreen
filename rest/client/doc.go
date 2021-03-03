1e5968efb41a086f5e8188545062ab76f6c33b2c
Brian Samek <bsamek@users.noreply.github.com>
2021-03-03 11:41:06 -0500

    EVG-14182 Add docs to rest subpackages (#4448)
    * EVG-14182 Add docs to rest subpackages
    
    * pr
    
    * missed a spot
    
    * mention graphql

---
+++ rest/client/doc.go
@@ -0,0 +1,2 @@
+// Package client provides a REST client for API requests to the server.
+package client
---
+++ rest/route/doc.go
@@ -0,0 +1,2 @@
+// Package route handles incoming REST requests to the server.
+package route
