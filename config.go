3facf0f7c321a62118a4c0d0ce35b18c8cd3d6f9
Brian Samek <brian.samek@mongodb.com>
2021-03-04 14:52:58 -0500

    Revert "EVG-14208 Bump agent version"
    This reverts commit 687841a1b65841f36e140e6872de8204c81118b2.

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 	ClientVersion = "2021-02-22"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-03-04"
+	AgentVersion = "2021-03-02"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
