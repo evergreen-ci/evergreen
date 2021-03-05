a28cec12954d2e86fdb90fa5f418087ce54f2099
julianedwards <julian.edwards@mongodb.com>
2021-03-05 17:57:50 -0500

    update agent version to monday cause that it when I am going to merge this

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2021-03-05"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-03-05"
+	AgentVersion = "2021-03-08"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
