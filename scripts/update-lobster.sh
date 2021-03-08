2a7ac6932d62e33d599eaf0b259c2bf9fd7bb6fb
Arjun Patel <arjun.patel@mongodb.com>
2021-03-08 11:05:34 -0500

    refactor EVERGREEN => EVG; LOB => LOBSTER

--- scripts/update-lobster.sh
+++ scripts/update-lobster.sh
@@ -14,9 +14,9 @@ LOBSTER_REPO=https://github.com/evergreen-ci/lobster.git
 LOBSTER_TEMP_ASSETS_DIR="$LOBSTER_TEMP_DIR/build"
 LOBSTER_STATIC_DIR="$LOBSTER_TEMP_ASSETS_DIR/static"
 LOBSTER_HTML="$LOBSTER_TEMP_ASSETS_DIR/index.html"
-EVG_LOB_MANIFEST_DIR="$EVGHOME/public/static/lobster"
-EVG_LOB_STATIC_DIR="$EVG_LOB_MANIFEST_DIR/static"
-EVERGREEN_TEMPLATES_DIR="$EVGHOME/service/templates"
+EVG_LOBSTER_MANIFEST_DIR="$EVGHOME/public/static/lobster"
+EVG_LOBSTER_STATIC_DIR="$EVG_LOBSTER_MANIFEST_DIR/static"
+EVG_TEMPLATES_DIR="$EVGHOME/service/templates"
 
 # clone lobster repo
 mkdir -p $LOBSTER_TEMP_DIR
@@ -33,15 +33,15 @@ export REACT_APP_EVERGREEN_BASE=https://evergreen.mongodb.com
 npm run build 
 
 # delete lobster static files in evergreen repo
-rm -rf $EVG_LOB_STATIC_DIR
-mkdir -p $EVG_LOB_STATIC_DIR
+rm -rf $EVG_LOBSTER_STATIC_DIR
+mkdir -p $EVG_LOBSTER_STATIC_DIR
 
 # copy files from lobster to evergreen
-cp -R $LOBSTER_STATIC_DIR/ $EVG_LOB_STATIC_DIR/
+cp -R $LOBSTER_STATIC_DIR/ $EVG_LOBSTER_STATIC_DIR/
 cp $LOBSTER_HTML "$LOBSTER_TEMP_DIR/temp.html"
-cp "$LOBSTER_TEMP_ASSETS_DIR/manifest.json" $EVG_LOB_MANIFEST_DIR
+cp "$LOBSTER_TEMP_ASSETS_DIR/manifest.json" $EVG_LOBSTER_MANIFEST_DIR
 # surround the html with go template tags
-cd $EVERGREEN_TEMPLATES_DIR
+cd $EVG_TEMPLATES_DIR
 echo -e "{{define \"base\"}}\n$(cat $LOBSTER_TEMP_DIR/temp.html)" > lobster.html
 echo "{{end}}" >> lobster.html
 echo "finished updating lobster"
