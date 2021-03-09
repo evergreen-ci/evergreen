a200dc55ae8afec57b9aa006825794716665fb3d
Annie Black <annie.black@10gen.com>
2021-03-09 14:41:21 -0500

    EVG-14197 remove user from log (#4468)
    * EVG-14197 check nil explicitly
    
    * remove user from log

--- service/task_history.go
+++ service/task_history.go
@@ -271,8 +271,6 @@ func (uis *UIServer) taskHistoryTestNames(w http.ResponseWriter, r *http.Request
 	taskName := gimlet.GetVars(r)["task_name"]
 
 	projCtx := MustHaveProjectContext(r)
-	u := gimlet.GetUser(r.Context())
-
 	project, err := projCtx.GetProject()
 	if err != nil || project == nil {
 		http.Error(w, "not found", http.StatusNotFound)
@@ -289,9 +287,9 @@ func (uis *UIServer) taskHistoryTestNames(w http.ResponseWriter, r *http.Request
 		http.Error(w, fmt.Sprintf("Error finding test names: `%v`", err.Error()), http.StatusInternalServerError)
 		return
 	}
-	grip.DebugWhen(u != nil, message.Fields{
+
+	grip.Debug(message.Fields{
 		"message":               "got test names",
-		"user":                  u.Username(),
 		"test_names_query_secs": testNamesQueryDuration.Seconds(),
 		"num_test_names":        len(results),
 	})
