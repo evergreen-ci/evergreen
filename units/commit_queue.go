2a9e4f89bfe91b701ec1f06841e7242525f72cb4
John Liu <john.liu@mongodb.com>
2021-03-23 10:04:47 -0400

    even more

--- units/commit_queue.go
+++ units/commit_queue.go
@@ -281,28 +281,20 @@ func (j *commitQueueJob) TryUnstick(ctx context.Context, cq *commitqueue.CommitQ
 	if mergeTask != nil {
 		// check that the merge task can run. Assume that if we're here the merge task
 		// should in fact run (ie. has not been dequeued due to a task failure)
-		if !mergeTask.Activated || mergeTask.Priority < 0 {
+		blocked := mergeTask.Blocked()
+		if !mergeTask.Activated || mergeTask.Priority < 0 || blocked {
 			grip.Error(message.Fields{
 				"message":  "merge task is not dispatchable",
 				"project":  mergeTask.Project,
 				"task":     mergeTask.Id,
 				"active":   mergeTask.Activated,
 				"priority": mergeTask.Priority,
+				"blocked":  blocked,
 				"source":   "commit queue",
 				"job_id":   j.ID(),
 			})
 			j.dequeue(cq, nextItem)
 			event.LogCommitQueueConcludeTest(nextItem.Version, evergreen.EnqueueFailed)
-		}
-		if mergeTask.Blocked() {
-			grip.Error(message.Fields{
-				"message":      "merge task is blocked",
-				"project":      mergeTask.Project,
-				"task":         mergeTask.Id,
-				"dependencies": mergeTask.DependsOn,
-				"source":       "commit queue",
-				"job_id":       j.ID(),
-			})
 		}
 	}
 
