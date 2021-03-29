7e94c2e658a99b1c2f12c00101a9d5096f8359fa
Annie Black <annie.black@10gen.com>
2021-03-29 10:40:08 -0400

    EVG-13405 add finalize option to set-module

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-03-23"
+	ClientVersion = "2021-03-26"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-03-23"
