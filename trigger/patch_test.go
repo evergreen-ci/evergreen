b6293bebe5b104ccba54bc163a1116acef4e5237
Chaya Malik <chaya.malik@10gen.com>
2021-04-09 12:35:48 -0400

    try to fix test

--- trigger/patch_test.go
+++ trigger/patch_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 
+	dbModel "github.com/evergreen-ci/evergreen/model"
+
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/evergreen-ci/evergreen/model/event"
@@ -34,7 +36,7 @@ func (s *patchSuite) SetupSuite() {
 }
 
 func (s *patchSuite) SetupTest() {
-	s.NoError(db.ClearCollections(event.AllLogCollection, patch.Collection, event.SubscriptionsCollection))
+	s.NoError(db.ClearCollections(event.AllLogCollection, patch.Collection, event.SubscriptionsCollection, dbModel.ProjectRefCollection))
 	startTime := time.Now().Truncate(time.Millisecond)
 
 	patchID := mgobson.ObjectIdHex("5aeb4514f27e4f9984646d97")
