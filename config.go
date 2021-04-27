f05af38101a3a76668019cb14cde334c3e4ccdf0
John Liu <john.liu@mongodb.com>
2021-04-27 13:34:17 -0400

    version

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-03-29"
+	ClientVersion = "2021-04-27"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-04-27"
