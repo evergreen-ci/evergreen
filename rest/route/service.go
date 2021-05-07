69157419f0a68b269eb1d6f5691f8f7dcfb65816
Bynn <bynn.lee@mongodb.com>
2021-05-07 10:39:40 -0400

    add checkUser to wrap

--- rest/route/service.go
+++ rest/route/service.go
@@ -121,7 +121,7 @@ func AttachHandler(app *gimlet.APIApp, opts HandlerOpts) {
 	app.AddRoute("/hosts/{host_id}/attach").Version(2).Post().Wrap(checkUser).RouteHandler(makeAttachVolume(sc, env))
 	app.AddRoute("/hosts/{host_id}/detach").Version(2).Post().Wrap(checkUser).RouteHandler(makeDetachVolume(sc, env))
 	app.AddRoute("/hosts/{host_id}/provisioning_options").Version(2).Get().Wrap(checkHost).RouteHandler(makeHostProvisioningOptionsGetHandler(sc))
-	app.AddRoute("/hosts/ip_address/{ip_address}").Version(2).Get().Wrap().RouteHandler(makeGetHostByIpAddress(sc))
+	app.AddRoute("/hosts/ip_address/{ip_address}").Version(2).Get().Wrap(checkUser).RouteHandler(makeGetHostByIpAddress(sc))
 	app.AddRoute("/volumes").Version(2).Get().Wrap(checkUser).RouteHandler(makeGetVolumes(sc))
 	app.AddRoute("/volumes").Version(2).Post().Wrap(checkUser).RouteHandler(makeCreateVolume(sc, env))
 	app.AddRoute("/volumes/{volume_id}").Version(2).Wrap(checkUser).Delete().RouteHandler(makeDeleteVolume(sc, env))
