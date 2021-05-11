d1051b3b685f6a88b88d007357bf2ff7510b6c31
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-05-11 12:01:26 -0400

    fix

--- graphql/http_handler.go
+++ graphql/http_handler.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/apollotracing"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
 )
 
 // Handler returns a gimlet http handler func used as the gql route handler
@@ -14,7 +12,6 @@ func Handler(apiURL string) func(w http.ResponseWriter, r *http.Request) {
 	srv := handler.NewDefaultServer(NewExecutableSchema(New(apiURL)))
 
 	// Apollo tracing support https://github.com/apollographql/apollo-tracing
-	srv.Use(extension.AutomaticPersistedQuery{Cache: lru.New(100)})
 	srv.Use(apollotracing.Tracer{})
 
 	return srv.ServeHTTP
