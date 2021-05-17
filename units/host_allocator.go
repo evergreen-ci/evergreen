36419be8d62a0451df526d0f7a3b60dd7ec5b230
Kim Tao <kim.tao@mongodb.com>
2021-05-17 11:37:59 -0400

    Fix panic in host allocator due to uninitialized environment

--- units/host_allocator.go
+++ units/host_allocator.go
@@ -68,6 +68,8 @@ func NewHostAllocatorJob(env evergreen.Environment, distroID string, timestamp t
 func (j *hostAllocatorJob) Run(ctx context.Context) {
 	defer j.MarkComplete()
 
+	j.env = evergreen.GetEnvironment()
+
 	config, err := evergreen.GetConfig()
 	if err != nil {
 		j.AddError(errors.Wrap(err, "Can't get evergreen configuration"))
