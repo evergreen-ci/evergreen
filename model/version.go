0da46d6259143eefb152506c54c8982398756b2d
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-05-18 15:18:31 -0400

    CR

--- model/version.go
+++ model/version.go
@@ -139,7 +139,7 @@ func (self *Version) SetActivated() error {
 }
 
 func (self *Version) SetNotActivated() error {
-	if !utility.FromBoolPtr(self.Activated) {
+	if !utility.FromBoolTPtr(self.Activated) {
 		return nil
 	}
 	self.Activated = utility.FalsePtr()
