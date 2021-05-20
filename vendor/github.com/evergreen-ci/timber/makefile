58c28f7616b4bf709e880768f2ba925d53b49829
julianedwards <julian.edwards@10gen.com>
2021-05-20 12:10:38 -0400

    EVG-14557: Revendor timber and add windows system cert workaround for cedar gRPC connections (#4693)

--- vendor/github.com/evergreen-ci/timber/makefile
+++ vendor/github.com/evergreen-ci/timber/makefile
@@ -21,6 +21,7 @@ endif
 export GOPATH := $(gopath)
 export GOCACHE := $(gocache)
 export GOROOT := $(goroot)
+export GO111MODULE := off
 # end environment setup
 
 
@@ -31,7 +32,7 @@ $(shell mkdir -p $(buildDir))
 # start lint setup targets
 lintDeps := $(buildDir)/run-linter $(buildDir)/golangci-lint
 $(buildDir)/golangci-lint:
-	@curl --retry 10 --retry-max-time 60 -sSfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(buildDir) v1.30.0 >/dev/null 2>&1
+	@curl --retry 10 --retry-max-time 60 -sSfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(buildDir) v1.40.0 >/dev/null 2>&1
 $(buildDir)/run-linter:cmd/run-linter/run-linter.go $(buildDir)/golangci-lint
 	@$(gobin) build -o $@ $<
 # end lint setup targets
--- vendor/github.com/evergreen-ci/timber/vendor/github.com/evergreen-ci/aviation/tls_utils_test.go
+++ vendor/github.com/evergreen-ci/timber/vendor/github.com/evergreen-ci/aviation/tls_utils_test.go
@@ -5,6 +5,7 @@ import (
 	"crypto/x509"
 	"io/ioutil"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,47 +15,55 @@ import (
 func TestGetClientTLSConfig(t *testing.T) {
 	for _, test := range []struct {
 		name   string
-		ca     string
+		cas    []string
 		usrCrt string
 		usrKey string
 		hasErr bool
 	}{
 		{
 			name:   "CACertDNE",
-			ca:     "DNE",
+			cas:    []string{"DNE"},
 			usrCrt: filepath.Join("testdata", "usr.crt"),
 			usrKey: filepath.Join("testdata", "usr.key"),
 			hasErr: true,
 		},
 		{
 			name:   "UserCrtDNE",
-			ca:     filepath.Join("testdata", "ca.crt"),
+			cas:    []string{filepath.Join("testdata", "ca.crt")},
 			usrCrt: "DNE",
 			usrKey: "DNE",
 			hasErr: true,
 		},
 		{
 			name:   "ClientConfig",
-			ca:     filepath.Join("testdata", "ca.crt"),
+			cas:    []string{filepath.Join("testdata", "ca.crt")},
 			usrCrt: filepath.Join("testdata", "user.crt"),
 			usrKey: filepath.Join("testdata", "user.key"),
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-			conf, err := GetClientTLSConfigFromFiles(test.ca, test.usrCrt, test.usrKey)
+			conf, err := GetClientTLSConfigFromFiles(test.cas, test.usrCrt, test.usrKey)
 			if test.hasErr {
 				assert.Error(t, err)
 				assert.Nil(t, conf)
 			} else {
 				assert.NoError(t, err)
+
 				expectedCrt, err := tls.LoadX509KeyPair(test.usrCrt, test.usrKey)
 				require.NoError(t, err)
 				require.Len(t, conf.Certificates, 1)
 				assert.Equal(t, expectedCrt, conf.Certificates[0])
-				ca, err := ioutil.ReadFile(test.ca)
-				require.NoError(t, err)
+
 				cp := x509.NewCertPool()
-				require.True(t, cp.AppendCertsFromPEM(ca))
+				if runtime.GOOS != "windows" {
+					cp, err = x509.SystemCertPool()
+					require.NoError(t, err)
+				}
+				for _, ca := range test.cas {
+					ca, err := ioutil.ReadFile(ca)
+					require.NoError(t, err)
+					require.True(t, cp.AppendCertsFromPEM(ca))
+				}
 				assert.Equal(t, cp, conf.RootCAs)
 			}
 		})
