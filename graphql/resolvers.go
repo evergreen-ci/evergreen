b01e0e03e06c0d5a2d4ace87601ec3cd10aac289
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-06-07 17:36:26 -0400

    Resolve tasks with display statuses

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -1025,7 +1025,7 @@ func (r *queryResolver) UserPatches(ctx context.Context, limit *int, page *int, 
 }
 
 func (r *queryResolver) Task(ctx context.Context, taskID string, execution *int) (*restModel.APITask, error) {
-	dbTask, err := task.FindByIdExecution(taskID, execution)
+	dbTask, err := task.FindOneIdAndExecutionWithDisplayStatus(taskID, execution)
 	if err != nil {
 		return nil, ResourceNotFound.Send(ctx, err.Error())
 	}
@@ -1036,11 +1036,7 @@ func (r *queryResolver) Task(ctx context.Context, taskID string, execution *int)
 	if err != nil {
 		return nil, InternalServerError.Send(ctx, "error converting task")
 	}
-	start, err := model.GetEstimatedStartTime(*dbTask)
-	if err != nil {
-		return nil, InternalServerError.Send(ctx, "error getting estimated start time")
-	}
-	apiTask.EstimatedStart = restModel.NewAPIDuration(start)
+
 	return apiTask, err
 }
 
@@ -2223,6 +2219,22 @@ func (r *taskResolver) DisplayTask(ctx context.Context, obj *restModel.APITask) 
 	}
 	return apiTask, nil
 }
+func (r *taskResolver) EstimatedStart(ctx context.Context, obj *restModel.APITask) (*restModel.APIDuration, error) {
+	i, err := obj.ToService()
+	if err != nil {
+		return nil, InternalServerError.Send(ctx, fmt.Sprintf("Error while converting task %s to service", *obj.Id))
+	}
+	t, ok := i.(*task.Task)
+	if !ok {
+		return nil, InternalServerError.Send(ctx, fmt.Sprintf("Unable to convert APITask %s to Task", *obj.Id))
+	}
+	start, err := model.GetEstimatedStartTime(*t)
+	if err != nil {
+		return nil, InternalServerError.Send(ctx, "error getting estimated start time")
+	}
+	duration := restModel.NewAPIDuration(start)
+	return &duration, nil
+}
 func (r *taskResolver) TotalTestCount(ctx context.Context, obj *restModel.APITask) (int, error) {
 	tests, err := r.sc.GetTestCountByTaskIdAndFilters(*obj.Id, "", nil, obj.Execution)
 	if err != nil {
@@ -2332,26 +2344,7 @@ func (r *taskResolver) CanSetPriority(ctx context.Context, obj *restModel.APITas
 }
 
 func (r *taskResolver) Status(ctx context.Context, obj *restModel.APITask) (string, error) {
-	updatedStatus := *obj.DisplayStatus
-	if updatedStatus == evergreen.TaskUndispatched {
-		i, err := obj.ToService()
-		if err != nil {
-			return updatedStatus, nil
-		}
-		t, ok := i.(*task.Task)
-		if !ok {
-			return updatedStatus, nil
-		}
-		// Lets return blocked as a status https://jira.mongodb.org/browse/EVG-14039 will evaluate whether or not it should remain as a status
-		if t.Blocked() {
-			return evergreen.TaskStatusBlocked, nil
-		} else if t.Activated {
-			return evergreen.TaskWillRun, nil
-		} else {
-			return evergreen.TaskWillNotRun, nil
-		}
-	}
-	return updatedStatus, nil
+	return *obj.DisplayStatus, nil
 }
 
 func (r *taskResolver) LatestExecution(ctx context.Context, obj *restModel.APITask) (int, error) {
@@ -2464,7 +2457,7 @@ func (r *taskResolver) ExecutionTasksFull(ctx context.Context, obj *restModel.AP
 	}
 	executionTasks := []*restModel.APITask{}
 	for _, execTaskID := range t.ExecutionTasks {
-		execT, err := task.FindByIdExecution(execTaskID, &t.Execution)
+		execT, err := task.FindOneIdAndExecutionWithDisplayStatus(execTaskID, &t.Execution)
 		if err != nil {
 			return nil, InternalServerError.Send(ctx, fmt.Sprintf("Error while getting execution task with id: %s : %s", execTaskID, err.Error()))
 		}
