5255128db1b567ee9917e85cc8426b63ac1931b8
Annie Black <annie.black@10gen.com>
2021-06-09 13:07:18 -0400

    better all configs

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-05-25"
+	ClientVersion = "2021-06-09"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-06-08"
