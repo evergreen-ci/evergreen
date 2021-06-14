e54af6db02f3ed388132709d6be7333cc88b76c5
Annie Black <annie.black@10gen.com>
2021-06-14 09:43:06 -0400

    formatting

--- operations/admin_all_configs.go
+++ operations/admin_all_configs.go
@@ -82,30 +82,30 @@ func fetchAndWriteConfigs(c *legacyClient, projects []model.ProjectRef, useLegac
 			ConfigFile: p.RemotePath,
 		}
 		if exists := configDownloaded[repo]; exists {
-			grip.Infof("Configuration for project %s already downloaded", p.Identifier)
+			grip.Infof("Configuration for project '%s' already downloaded", p.Identifier)
 			continue
 		}
-		grip.Infof("Downloading configuration for %s", p.Identifier)
+		grip.Infof("Downloading configuration for '%s'", p.Identifier)
 		versions, err := c.GetRecentVersions(p.Id)
 		if err != nil {
-			catcher.Wrapf(err, "failed to fetch recent versions for %s", p.Identifier)
+			catcher.Wrapf(err, "failed to fetch recent versions for '%s'", p.Identifier)
 			continue
 		}
 		if len(versions) == 0 {
-			catcher.Errorf("WARNING: project %s has no versions", p.Identifier)
+			catcher.Errorf("WARNING: project '%s' has no versions", p.Identifier)
 			continue
 		}
 
 		config, err := c.GetConfig(versions[0], useLegacy)
 		if err != nil {
-			catcher.Wrapf(err, "failed to fetch config for project %s, version %s", p.Identifier, versions[0])
+			catcher.Wrapf(err, "failed to fetch config for project '%s', version '%s'", p.Identifier, versions[0])
 			continue
 		}
 		configDownloaded[repo] = true
 
 		err = ioutil.WriteFile(p.Identifier+".yml", config, 0644)
 		if err != nil {
-			catcher.Wrapf(err, "failed to write configuration for project %s", p.Identifier)
+			catcher.Wrapf(err, "failed to write configuration for project '%s'", p.Identifier)
 		}
 	}
 
