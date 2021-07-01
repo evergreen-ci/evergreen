e6b30baa535dc8c4b9333a9a219427c93b72d41f
malik.hadjri <malik.hadjri@mongodb.com>
2021-07-01 18:55:49 -0400

    EVG-13811: Ensure default security group is added in unit test

--- rest/data/host_create_test.go
+++ rest/data/host_create_test.go
@@ -308,8 +308,9 @@ buildvariants:
 			assert.NoError(t, ec2Settings.FromDistroSettings(h.Distro, ""))
 			assert.NotEmpty(t, ec2Settings.KeyName)
 			assert.InDelta(t, time.Now().Add(evergreen.DefaultSpawnHostExpiration).Unix(), h.ExpirationTime.Unix(), float64(1*time.Millisecond))
-			require.Len(t, ec2Settings.SecurityGroupIDs, 1)
+			require.Len(t, ec2Settings.SecurityGroupIDs, 2)
 			assert.Equal(t, "sg-distro", ec2Settings.SecurityGroupIDs[0]) // if not overridden, stick with ec2 security group
+			assert.Equal(t, "sg-097bff6dd0d1d31d0", ec2Settings.SecurityGroupIDs[1])
 			assert.Equal(t, distro.BootstrapMethodNone, h.Distro.BootstrapSettings.Method, "host provisioning should be set to none by default")
 		}
 	})
