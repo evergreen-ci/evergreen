1fe84781e8aa6f6f3ae9e60cc2481790cbc1135c
julianedwards <julian.edwards@mongodb.com>
2021-07-08 14:18:06 -0400

    fix query

--- model/task/task.go
+++ model/task/task.go
@@ -3191,7 +3191,7 @@ func (t *Task) hasCedarResults() bool {
 			// This is a display task from the old task collection,
 			// we need to look there for its execution tasks.
 			execTasks, err = FindAllOld(db.Query(bson.M{
-				IdKey:        bson.M{"$in": t.ExecutionTasks},
+				OldTaskIdKey: bson.M{"$in": t.ExecutionTasks},
 				ExecutionKey: t.Execution,
 			}))
 		} else {
