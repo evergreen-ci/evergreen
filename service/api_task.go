5212cb995a68c6cc5d1ad33ead27ed38f81fbd0f
malik.hadjri <malik.hadjri@mongodb.com>
2021-07-13 10:16:18 -0400

    Revert PR #4822

--- service/api_task.go
+++ service/api_task.go
@@ -310,20 +310,14 @@ func (as *APIServer) EndTask(w http.ResponseWriter, r *http.Request) {
 		endTaskResp.ShouldExit = true
 	}
 
-	msg := message.Fields{
+	grip.Info(message.Fields{
 		"message":     "Successfully marked task as finished",
 		"task_id":     t.Id,
 		"execution":   t.Execution,
 		"operation":   "mark end",
 		"duration":    time.Since(finishTime),
 		"should_exit": endTaskResp.ShouldExit,
-	}
-
-	if t.IsPartOfDisplay() {
-		msg["display_task_id"] = t.DisplayTask.Id
-	}
-
-	grip.Info(msg)
+	})
 	gimlet.WriteJSON(w, endTaskResp)
 }
 
