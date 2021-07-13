70dc16d158bc33ad1d8cae95dc996efaed6be58e
julianedwards <julian.edwards@mongodb.com>
2021-07-13 10:24:21 -0400

    Revert "Revert "EVG-14924: Fetch cedar test results for display tasks (#4808)""
    This reverts commit 217c3dc8d62f370401569d9e99aac9bb2bee7360.

--- rest/data/version.go
+++ rest/data/version.go
@@ -250,7 +250,7 @@ func (vc *DBVersionConnector) GetVersionsAndVariants(skip, numVersionElements in
 // addFailedAndStartedTests adds all of the failed tests associated with a task
 func addFailedAndStartedTests(rows map[string]restModel.BuildList, failedAndStartedTasks []task.Task) error {
 	for i := range failedAndStartedTasks {
-		if err := failedAndStartedTasks[i].MergeNewTestResults(); err != nil {
+		if err := failedAndStartedTasks[i].MergeTestResults(); err != nil {
 			return errors.Wrap(err, "error merging test results")
 		}
 	}
