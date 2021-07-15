3836d23575095dd160a1ad315eeb39de56771a10
julianedwards <julian.edwards@10gen.com>
2021-07-15 11:30:28 -0400

    EVG-15006: Revendor timber (#4837)

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-07-07"
+	ClientVersion = "2021-07-14"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-07-14-a"
