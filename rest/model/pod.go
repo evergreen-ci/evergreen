33502ae8970d18f28dad2594ba8dd5b9bcdf1b43
jessfeng <jessicafengyq@gmail.com>
2021-07-16 14:59:10 -0400

    Remove BuildFromService

--- rest/model/pod.go
+++ rest/model/pod.go
@@ -6,7 +6,6 @@ import (
 	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/model/pod"
 	"github.com/evergreen-ci/utility"
-	"github.com/pkg/errors"
 )
 
 type APISecretOpts struct {
@@ -40,33 +39,6 @@ type APIPod struct {
 	TimeInfo APITimeInfo
 }
 
-// toAPIEnvVars converts a string-string map of environment variables and a string-string map of secrets to a slice of APIPodEnvVars.
-func toAPIEnvVars(envVars map[string]string, secrets map[string]string) []*APIPodEnvVar {
-	var podEnvVars []*APIPodEnvVar
-
-	for k, v := range envVars {
-		apiEnvVar := &APIPodEnvVar{
-			Name:  &k,
-			Value: &v,
-		}
-		podEnvVars = append(podEnvVars, apiEnvVar)
-	}
-
-	for k, v := range secrets {
-		apiEnvVar := &APIPodEnvVar{
-			SecretOpts: &APISecretOpts{
-				Name:   &k,
-				Value:  &v,
-				Owned:  utility.TruePtr(),
-				Exists: utility.FalsePtr(),
-			},
-		}
-		podEnvVars = append(podEnvVars, apiEnvVar)
-	}
-
-	return podEnvVars
-}
-
 // fromAPIEnvVars converts a slice of APIPodEnvVars to a string-string map of environment variables and a string-string map of secrets.
 func fromAPIEnvVars(api []*APIPodEnvVar) (map[string]string, map[string]string) {
 	var envVars map[string]string
@@ -81,29 +53,6 @@ func fromAPIEnvVars(api []*APIPodEnvVar) (map[string]string, map[string]string) 
 	}
 
 	return envVars, secrets
-}
-
-// BuildFromService converts from service level pod.Pod to APIPod.
-func (p *APIPod) BuildFromService(h interface{}) error {
-	var podData pod.Pod
-	switch v := h.(type) {
-	case pod.Pod:
-		podData = v
-	case *pod.Pod:
-		podData = *v
-	default:
-		return errors.Errorf("%T is not a supported expansion type", h)
-	}
-
-	p.CPU = &podData.TaskContainerCreationOpts.CPU
-	p.Cluster = utility.ToStringPtr(string(podData.TaskContainerCreationOpts.Platform))
-	p.EnvVars = toAPIEnvVars(podData.TaskContainerCreationOpts.EnvVars, podData.TaskContainerCreationOpts.EnvSecrets)
-	p.Image = &podData.TaskContainerCreationOpts.Image
-	p.Memory = &podData.TaskContainerCreationOpts.MemoryMB
-	p.Name = &podData.ExternalID
-	p.Secret = &podData.Secret
-
-	return nil
 }
 
 // ToService returns a service layer pod.Pod using the data from APIPod.
