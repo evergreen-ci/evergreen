c1c82bd52ad283f0b7e5ec727a8c6667a093da7f
malik.hadjri <malik.hadjri@mongodb.com>
2021-07-22 12:01:27 -0400

    EVG-14088 Add unit test to confirm expansion of params

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2021-07-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-07-20"
+	AgentVersion = "2021-07-22"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
