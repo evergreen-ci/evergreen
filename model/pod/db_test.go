088d0a2605da3b24249cc92bb099f5bac113855d
jessfeng <jessicafengyq@gmail.com>
2021-07-23 16:04:13 -0400

    Implement finding initialized pods

--- model/pod/db_test.go
+++ model/pod/db_test.go
@@ -1,1 +1,41 @@
 package pod
+
+import (
+	"testing"
+
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/utility"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindByInitializing(t *testing.T) {
+	require.NoError(t, db.Clear(Collection))
+
+	p := &Pod{
+		ID:     utility.RandomString(),
+		Status: StatusInitializing,
+		Secret: "secret",
+	}
+	require.NoError(t, p.Insert())
+
+	p = &Pod{
+		ID:     utility.RandomString(),
+		Status: StatusStarting,
+		Secret: "secret",
+	}
+	require.NoError(t, p.Insert())
+
+	p = &Pod{
+		ID:     utility.RandomString(),
+		Status: StatusInitializing,
+		Secret: "secret",
+	}
+	require.NoError(t, p.Insert())
+
+	pods, err := FindByInitializing()
+	require.NoError(t, err)
+	require.Len(t, pods, 2)
+	assert.Equal(t, StatusInitializing, pods[0].Status)
+	assert.Equal(t, StatusInitializing, pods[1].Status)
+}
