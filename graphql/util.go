331e843c5e83441559d79d035994b97d25188c73
Chaya Malik <chaya.malik@10gen.com>
2021-07-28 17:44:36 -0400

    remove p == nil check

--- graphql/util.go
+++ graphql/util.go
@@ -714,9 +714,6 @@ func ModifyVersion(version model.Version, user user.DBUser, proj *model.ProjectR
 		if err != nil {
 			return http.StatusInternalServerError, errors.Wrapf(err, "error getting patch '%s'", version.Id)
 		}
-		if p == nil {
-			return http.StatusNotFound, errors.Errorf("patch %s does not exist", version.Id)
-		}
 		for _, childPatchId := range p.Triggers.ChildPatches {
 			if err := model.SetVersionPriority(childPatchId, modifications.Priority, user.Id); err != nil {
 				return http.StatusInternalServerError, errors.Wrapf(err, "error setting priority for child patch '%s'", childPatchId)
