ec968efd631157f1cedbc9051fefe71d15cbec20
Chaya Malik <chaya.malik@10gen.com>
2021-07-28 14:18:53 -0400

    update priority for child patches

--- graphql/util.go
+++ graphql/util.go
@@ -709,6 +709,19 @@ func ModifyVersion(version model.Version, user user.DBUser, proj *model.ProjectR
 		if err := model.SetVersionPriority(version.Id, modifications.Priority, user.Id); err != nil {
 			return http.StatusInternalServerError, errors.Errorf("error setting version priority: %s", err)
 		}
+		// update priority for child patches
+		p, err := patch.FindOneId(version.Id)
+		if err != nil {
+			return http.StatusInternalServerError, errors.Wrapf(err, "error getting patch '%s'", version.Id)
+		}
+		if p == nil {
+			return http.StatusNotFound, errors.Errorf("patch %s does not exist", version.Id)
+		}
+		for _, childPatchId := range p.Triggers.ChildPatches {
+			if err := model.SetVersionPriority(childPatchId, modifications.Priority, user.Id); err != nil {
+				return http.StatusInternalServerError, errors.Wrapf(err, "error setting priority for child patch '%s'", childPatchId)
+			}
+		}
 	default:
 		return http.StatusBadRequest, errors.Errorf("Unrecognized action: %v", modifications.Action)
 	}
