028a6c5a9e1ef62ac54b5d23f25c4a6a1d0aa0f8
Alexander Nicholas Costas <alexander.costas@mongodb.com>
2021-08-09 12:49:18 -0400

    TIG-3231: New trend charts in evergreen (#4910)

--- public/static/app/perf/perf.js
+++ public/static/app/perf/perf.js
@@ -12,7 +12,7 @@ const findIndex = function (list, predicate) {
 };
 
 mciModule.controller('PerfController', function PerfController(
-  $scope, $window, $http, $location, $filter, ChangePointsService,
+  $scope, $window, $http, $location, $filter, ChangePointsService, PERFORMANCE_ANALYSIS_AND_TRIAGE_API,
   DrawPerfTrendChart, PROCESSED_TYPE, Settings,
   TestSample, CANARY_EXCLUSION_REGEX, ApiTaskdata,
   loadBuildFailures, loadChangePoints, loadTrendData,
@@ -184,6 +184,7 @@ $http.get(templateUrl).success(function(template) {
   $scope.Math = $window.Math;
   $scope.conf = $window.plugins["perf"];
   $scope.task = $window.task_data;
+  $scope.newTrendChartsUi = PERFORMANCE_ANALYSIS_AND_TRIAGE_API.UI + "/task/" + ($scope.task ? $scope.task.id : null) + "/performanceData";
   $scope.tablemode = 'maxthroughput';
   $scope.threadLevelsRadio = {
     options: [{
