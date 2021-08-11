459f6075dc009d46e2d361b994c352945b3a98a5
Annie Black <annie.black@10gen.com>
2021-08-11 09:35:08 -0400

    EVG-13272 consider repo for github check aliases

--- model/lifecycle.go
+++ model/lifecycle.go
@@ -524,7 +524,7 @@ func addTasksToBuild(ctx context.Context, b *build.Build, project *Project, v *V
 			return nil, nil, errors.Errorf("project '%s' not found", project.Identifier)
 		}
 		if pRef.IsGithubChecksEnabled() {
-			githubCheckAliases, err = FindAliasInProject(v.Identifier, evergreen.GithubChecksAlias)
+			githubCheckAliases, err = FindAliasInProjectOrRepo(v.Identifier, evergreen.GithubChecksAlias)
 			grip.Error(message.WrapError(err, message.Fields{
 				"message":            "error getting github check aliases when adding tasks to build",
 				"project":            v.Identifier,
