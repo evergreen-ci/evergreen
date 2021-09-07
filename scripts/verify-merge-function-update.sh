e719c8d3afb9a235a6971cf17781d3d016510311
Bynn <bynn.lee@mongodb.com>
2021-09-07 17:46:56 -0400

    check which side changed

--- scripts/verify-merge-function-update.sh
+++ scripts/verify-merge-function-update.sh
@@ -6,12 +6,15 @@ if [[ "${BRANCH_NAME}" == "" ]]; then
     BRANCH_NAME=main;
 fi
 
+merge_function_changed=false
+parser_project_changed=false
+
 # Find the common ancestor between the current set of changes and the upstream branch, then see if any source code files
 # have changed in the merge functions.
 common_ancestor=$(git merge-base ${BRANCH_NAME}@{upstream} HEAD);
 files_changed="$(git diff --name-only "${common_ancestor}" -- 'model/project_parser_merge_functions.go')"
 if [[ "${files_changed}" == "" ]]; then
-    exit 0;
+    merge_function_changed=true
 fi
 
 # Find the latest commit when the ParserProject struct was updated.
@@ -21,17 +24,25 @@ commit_project_parser_main=$(git blame -l -L ${project_parser_start_line_num},${
 last_commit_project_parser_updated=$(git blame -l -L ${project_parser_start_line_num},${project_parser_end_line_num} model/project_parser.go | cut -d ' ' -f 1);
 
 # Check if the last commit when the ParserProject struct was updated is in the list of committed changes.
-if [[ "${commit_project_parser_main}" == "${last_commit_project_parser_updated}" ]]; then
-    exit 0;
+if [[ "${commit_project_parser_main}" != "${last_commit_project_parser_updated}" ]]; then
+    parser_project_changed=true
 fi
 
 for line in ${last_commit_project_parser_updated}
 do 
     # If the ParserProject struct was updated but not committed yet, it still counts as a change.
     if [[ "${line}" == "0000000000000000000000000000000000000000" ]]; then
-        exit 0;
+        parser_project_changed=true
     fi
 done
 
-echo "Only one of either ParserProject struct and project_parser_merge_functions has been updated. Please update both." >&2
+if [[ "${merge_function_changed}" == "${parser_project_changed}" ]]; then
+    exit 0
+fi
+
+if ${merge_function_changed}; then 
+    echo "project_parser_merge_functions has been changed but ParserProject struct has not been updated. Please update project_parser_merge_functions." >&2
+elif ${parser_project_changed}; then
+    echo "ParserProject struct has been changed but project_parser_merge_functions has not been updated. Please update the ParserProject struct." >&2
+fi
 exit 1
