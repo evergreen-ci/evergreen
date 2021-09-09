aa1608f4d907f0605f972e435a1e16da7ec826e1
Jonathan Brill <jonathan.brill@10gen.com>
2021-09-09 17:38:13 -0400

    dramatically increase timeout

--- agent/util/subtree_unix.go
+++ agent/util/subtree_unix.go
@@ -19,7 +19,7 @@ const (
 	cleanupCheckAttempts   = 10
 	cleanupCheckTimeoutMin = 100 * time.Millisecond
 	cleanupCheckTimeoutMax = time.Second
-	contextTimeout         = 30 * time.Second
+	contextTimeout         = time.Hour
 )
 
 // TrackProcess is a noop by default if we don't need to do any special
