46ac83927ebac1aa423228dafd6a3521d598bb23
Bynn <bynn.lee@mongodb.com>
2021-09-09 10:59:06 -0400

    order wrong

--- model/task_lifecycle.go
+++ model/task_lifecycle.go
@@ -128,14 +128,14 @@ func SetActiveState(t *task.Task, caller string, active bool) error {
 		return nil
 	}
 
-	if err := UpdateBuildAndVersionStatusForTask(t); err != nil {
-		return errors.Wrap(err, "problem updating build and version status for task")
-	}
-
 	if t.IsPartOfDisplay() {
 		if err := UpdateDisplayTaskForTask(t); err != nil {
 			return errors.Wrap(err, "problem updating display task")
 		}
+	}
+
+	if err := UpdateBuildAndVersionStatusForTask(t); err != nil {
+		return errors.Wrap(err, "problem updating build and version status for task")
 	}
 
 	return nil
