8aad95babf031b159b5c56f8b725510563187ecc
Annie Black <annie.black@10gen.com>
2021-09-16 09:51:06 -0400

    EVG-5312 add improvements to task group restart logic

--- units/host_termination.go
+++ units/host_termination.go
@@ -188,8 +188,8 @@ func (j *hostTerminationJob) Run(ctx context.Context) {
 			if err != nil {
 				j.AddError(errors.Wrapf(err, "error finding last task '%s'", j.host.LastTask))
 			}
-
-			if lastTask != nil && lastTask.IsPartOfSingleHostTaskGroup() {
+			// Only try to restart the task group if it was successful and should have continued executing.
+			if lastTask != nil && lastTask.IsPartOfSingleHostTaskGroup() && lastTask.Status == evergreen.TaskSucceeded {
 				tasks, err := task.FindTaskGroupFromBuild(lastTask.BuildId, lastTask.TaskGroup)
 				if err != nil {
 					j.AddError(errors.Wrapf(err, "can't get task group for task '%s'", lastTask.Id))
