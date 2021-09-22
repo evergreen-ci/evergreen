81fb8974a5201a90877e30811161be111d9e6bb2
Kim Tao <kim.tao@mongodb.com>
2021-09-22 14:15:17 -0400

    EVG-15225: remove single quotes from agent curl command for Windows

--- cloud/pod_agent_util_test.go
+++ cloud/pod_agent_util_test.go
@@ -34,7 +34,7 @@ func TestAgentScript(t *testing.T) {
 
 			expected := []string{
 				"bash", "-c",
-				"curl -LO 'www.test.com/clients/linux_amd64/evergreen' --retry 10 --retry-max-time 100 && " +
+				"curl -fLO 'www.test.com/clients/linux_amd64/evergreen' --retry 10 --retry-max-time 100 && " +
 					"chmod +x evergreen && " +
 					"./evergreen agent --api_server=www.test.com --mode=pod --log_prefix=/data/mci/agent --working_directory=/data/mci",
 			}
@@ -55,7 +55,7 @@ func TestAgentScript(t *testing.T) {
 
 			expected := []string{
 				"cmd.exe", "/c",
-				"curl -LO 'www.test.com/clients/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100 && " +
+				"curl -fLO 'www.test.com/clients/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100 && " +
 					".\\evergreen.exe agent --api_server=www.test.com --mode=pod --log_prefix=/data/mci/agent --working_directory=/data/mci",
 			}
 			assert.Equal(t, expected, cmd)
@@ -83,7 +83,7 @@ func TestAgentScript(t *testing.T) {
 
 			expected := []string{
 				"bash", "-c",
-				fmt.Sprintf("(curl -LO 'https://foo.com/%s/linux_amd64/evergreen' --retry 10 --retry-max-time 100 || curl -LO 'www.test.com/clients/linux_amd64/evergreen' --retry 10 --retry-max-time 100) && "+
+				fmt.Sprintf("(curl -fLO 'https://foo.com/%s/linux_amd64/evergreen' --retry 10 --retry-max-time 100 || curl -fLO 'www.test.com/clients/linux_amd64/evergreen' --retry 10 --retry-max-time 100) && "+
 					"chmod +x evergreen && "+
 					"./evergreen agent --api_server=www.test.com --mode=pod --log_prefix=/data/mci/agent --working_directory=/data/mci", evergreen.BuildRevision),
 			}
@@ -103,7 +103,7 @@ func TestAgentScript(t *testing.T) {
 			require.NotZero(t, cmd)
 			expected := []string{
 				"cmd.exe", "/c",
-				fmt.Sprintf("(curl -LO 'https://foo.com/%s/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100 || curl -LO 'www.test.com/clients/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100) && "+
+				fmt.Sprintf("(curl -fLO 'https://foo.com/%s/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100 || curl -fLO 'www.test.com/clients/windows_amd64/evergreen.exe' --retry 10 --retry-max-time 100) && "+
 					".\\evergreen.exe agent --api_server=www.test.com --mode=pod --log_prefix=/data/mci/agent --working_directory=/data/mci", evergreen.BuildRevision),
 			}
 			assert.Equal(t, expected, cmd)
