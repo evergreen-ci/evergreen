8690d85b19b6754236ed4474a7cda6065539994b
malik.hadjri <malik.hadjri@mongodb.com>
2021-10-12 16:20:00 -0400

    EVG-15149: Do not hard code task details in checkResetDisplayTask

--- model/task/task.go
+++ model/task/task.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
-	"sort"
 	"strings"
 	"time"
 
@@ -1494,15 +1493,6 @@ func (t *Task) MarkEnd(finishTime time.Time, detail *apimodels.TaskEndDetail) er
 func (t *Task) GetDisplayStatus() string {
 	if t.DisplayStatus != "" {
 		return t.DisplayStatus
-	}
-	if t.DisplayOnly {
-		execTasks, err := Find(ByIds(t.ExecutionTasks))
-		if err != nil {
-			return evergreen.TaskFailed
-		}
-		sort.Sort(ByPriority(execTasks))
-		statusTask := execTasks[0]
-		return statusTask.ResultStatus()
 	}
 	if t.Aborted && t.IsFinished() {
 		return evergreen.TaskAborted
