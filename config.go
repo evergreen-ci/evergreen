1dfa107a9ed87d6cb8e5e55637b996947de42baa
Annie Black <annie.black@10gen.com>
2021-10-19 12:22:36 -0400

    EVG-15583 remove all-configs fetch flag (#5111)

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-09-28"
+	ClientVersion = "2021-10-19"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-10-05"
