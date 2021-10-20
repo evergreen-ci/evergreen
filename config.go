cda0e58828647e9724f8c5ca219f98d067b715db
Brian Samek <bsamek@users.noreply.github.com>
2021-10-20 16:31:36 -0400

    EVG-15267 Apply module patches consistently with cloning them (#5114)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2021-10-19"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-10-15"
+	AgentVersion = "2021-10-20"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
