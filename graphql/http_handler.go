e57cd418c45bed0d154c96624e748d161b93898e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2021-10-21 17:45:48 -0400

    Print stack trace on panic

--- graphql/http_handler.go
+++ graphql/http_handler.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/apollotracing"
+	"github.com/evergreen-ci/gimlet"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/message"
 	"github.com/vektah/gqlparser/v2/gqlerror"
@@ -16,17 +18,19 @@ import (
 // Handler returns a gimlet http handler func used as the gql route handler
 func Handler(apiURL string) func(w http.ResponseWriter, r *http.Request) {
 	srv := handler.NewDefaultServer(NewExecutableSchema(New(apiURL)))
-
 	// Apollo tracing support https://github.com/apollographql/apollo-tracing
 	srv.Use(apollotracing.Tracer{})
 
 	// Handler to log graphql panics to splunk
 	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) error {
 		queryPath := graphql.GetFieldContext(ctx).Path()
+
 		grip.Critical(message.Fields{
 			"path":    "/graphql/query",
 			"message": "unhandled panic",
 			"error":   err,
+			"stack":   string(debug.Stack()),
+			"request": gimlet.GetRequestID(ctx),
 			"query":   queryPath,
 		})
 		return errors.New("internal server error")
