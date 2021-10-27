1f26432024147ccd7b1644a553ccaf4c1aeb52eb
malik.hadjri <malik.hadjri@mongodb.com>
2021-10-27 14:42:11 -0400

    EVG-15219 Allow for unconfigured BB ticket search projects in project form

--- public/static/js/projects.js
+++ public/static/js/projects.js
@@ -200,7 +200,10 @@ mciModule.controller(
     };
 
     $scope.shouldDisableWebhook = function () {
-      return $scope.settingsFormData.build_baron_settings && (($scope.settingsFormData.build_baron_settings.ticket_search_projects !== undefined && $scope.settingsFormData.build_baron_settings.ticket_search_projects.length > 0)
+      return $scope.settingsFormData.build_baron_settings
+          && (($scope.settingsFormData.build_baron_settings.ticket_search_projects !== null
+              && $scope.settingsFormData.build_baron_settings.ticket_search_projects !== undefined
+              && $scope.settingsFormData.build_baron_settings.ticket_search_projects.length > 0)
           || $scope.settingsFormData.build_baron_settings.ticket_create_project
           || $scope.ticket_search_project);
     };
@@ -212,10 +215,14 @@ mciModule.controller(
 
     $scope.bbConfigIsValid = function () {
       if ($scope.settingsFormData.build_baron_settings.ticket_create_project){
-          return $scope.settingsFormData.build_baron_settings.ticket_search_projects !== undefined && $scope.settingsFormData.build_baron_settings.ticket_search_projects.length > 0
+          return $scope.settingsFormData.build_baron_settings.ticket_search_projects !== null
+              && $scope.settingsFormData.build_baron_settings.ticket_search_projects !== undefined
+              && $scope.settingsFormData.build_baron_settings.ticket_search_projects.length > 0
       }
       else {
-          return $scope.settingsFormData.build_baron_settings.ticket_search_projects === undefined || $scope.settingsFormData.build_baron_settings.ticket_search_projects.length <= 0
+          return $scope.settingsFormData.build_baron_settings.ticket_search_projects === null
+              || $scope.settingsFormData.build_baron_settings.ticket_search_projects === undefined
+              || $scope.settingsFormData.build_baron_settings.ticket_search_projects.length <= 0
       }
     };
 
