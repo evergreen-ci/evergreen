5a7e01939ad6cd0e65b5d327727a4c05ced94693
Annie Black <annie.black@10gen.com>
2021-11-04 14:21:28 -0400

    womp

--- units/generate_tasks.go
+++ units/generate_tasks.go
@@ -290,7 +290,7 @@ func (j *generateTasksJob) Run(ctx context.Context) {
 			"task":          t.Id,
 			"job":           j.ID(),
 			"version":       t.Version,
-			"is_save_error": strings.Contains(err.Error(), evergreen.SaveGenerateTasksError)
+			"is_save_error": strings.Contains(err.Error(), evergreen.SaveGenerateTasksError),
 		}))
 	}
 
