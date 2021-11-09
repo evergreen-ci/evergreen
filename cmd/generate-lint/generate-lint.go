4ccd4007dda8bbe8a56293c8ac0891d96608cf05
Kim Tao <kim.tao@mongodb.com>
2021-11-09 09:59:03 -0500

    Temporary hack to make generate-lint generate lint checks for all packages

--- cmd/generate-lint/generate-lint.go
+++ cmd/generate-lint/generate-lint.go
@@ -122,25 +122,32 @@ func getAllTargets() ([]string, error) {
 
 // generateTasks returns a map of tasks to generate.
 func generateTasks() (*shrub.Configuration, error) {
-	changes, err := whatChanged()
+	// TODO: remove this hack to get around diff-based lint generation before
+	// committing.
+	// changes, err := whatChanged()
+	// if err != nil {
+	//     return nil, err
+	// }
+	// var targets []string
+	// var maxHosts int
+	// if len(changes) == 0 {
+	//     maxHosts = commitMaxHosts
+	//     targets, err = getAllTargets()
+	//     if err != nil {
+	//         return nil, err
+	//     }
+	// } else {
+	//     maxHosts = patchMaxHosts
+	//     targets, err = targetsFromChangedFiles(changes)
+	//     if err != nil {
+	//         return nil, err
+	//     }
+	// }
+	targets, err := getAllTargets()
 	if err != nil {
 		return nil, err
 	}
-	var targets []string
-	var maxHosts int
-	if len(changes) == 0 {
-		maxHosts = commitMaxHosts
-		targets, err = getAllTargets()
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		maxHosts = patchMaxHosts
-		targets, err = targetsFromChangedFiles(changes)
-		if err != nil {
-			return nil, err
-		}
-	}
+	maxHosts := commitMaxHosts
 
 	conf := &shrub.Configuration{}
 	if len(targets) == 0 {
