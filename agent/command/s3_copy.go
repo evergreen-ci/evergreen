5bc5218bba0f0f9c8e6ea05b6ea7358c5cbc4dce
Bynn <bynn.lee@mongodb.com>
2021-11-10 11:11:10 -0500

    EVG-14648 Update s3copy to log destination path

--- agent/command/s3_copy.go
+++ agent/command/s3_copy.go
@@ -230,6 +230,8 @@ func (c *s3copy) s3Copy(ctx context.Context,
 			logger.Task().Warning("destination bucket names containing dots that are created after Sept. 30, 2020 are not guaranteed to have valid attached URLs")
 			foundDottedBucketName = true
 		}
+
+		logger.Task().Infof("successfully copied file to '%s'", s3CopyFile.Destination.Path)
 	}
 
 	return nil
