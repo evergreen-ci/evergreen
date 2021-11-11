950e3fbac082c772e256ccf952d64015b8aad40a
malik.hadjri <malik.hadjri@mongodb.com>
2021-11-11 18:44:10 -0500

    EVG-15467 Include git metadata in backport cli command

--- config.go
+++ config.go
@@ -32,11 +32,10 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2021-11-01"
+	ClientVersion = "2021-11-11"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2021-11-10"
-
 )
 
 // ConfigSection defines a sub-document in the evergreen config
