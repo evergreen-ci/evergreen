3ed91d6391861e9261c5ac12b11a43c4c88f21c7
Annie Black <annie.black@10gen.com>
2021-11-16 10:56:02 -0500

    EVG-15856 comment out parser project merge usage (#5205)

--- model/project_ref.go
+++ model/project_ref.go
@@ -740,12 +740,13 @@ func FindMergedProjectRef(identifier string, version string, includeParserProjec
 			return nil, errors.Wrapf(err, "error merging repo ref '%s' for project '%s'", repoRef.RepoRefId, pRef.Identifier)
 		}
 	}
-	if includeParserProject {
-		err = pRef.MergeWithParserProject(version)
-		if err != nil {
-			return nil, errors.Wrapf(err, "Unable to merge parser project with project ref %s", pRef.Identifier)
-		}
-	}
+	// Removing due to outage: EVG-15856
+	//if includeParserProject {
+	//	err = pRef.MergeWithParserProject(version)
+	//	if err != nil {
+	//		return nil, errors.Wrapf(err, "Unable to merge parser project with project ref %s", pRef.Identifier)
+	//	}
+	//}
 	return pRef, nil
 }
 
