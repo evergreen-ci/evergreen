88413626b258bf683fa2c5c2d2bd2b17c93be1b0
minnakt <47064971+minnakt@users.noreply.github.com>
2021-11-22 15:30:50 -0500

    EVG-15774: Respect admin permissions in SetPatchPriority resolver (#5212)

--- graphql/util.go
+++ graphql/util.go
@@ -621,17 +621,9 @@ func ModifyVersion(version model.Version, user user.DBUser, proj *model.ProjectR
 			}
 		}
 	case SetPriority:
-		var projId string
-		if proj == nil {
-			projId, err := model.GetIdForProject(version.Identifier)
-			if err != nil {
-				return http.StatusNotFound, errors.Errorf("error getting project ref: %s", err)
-			}
-			if projId == "" {
-				return http.StatusNotFound, errors.Errorf("project for %s came back nil: %s", version.Branch, err)
-			}
-		} else {
-			projId = proj.Id
+		projId := version.Identifier
+		if projId == "" {
+			return http.StatusNotFound, errors.Errorf("Could not find project for version %s", version.Id)
 		}
 		if modifications.Priority > evergreen.MaxTaskPriority {
 			requiredPermission := gimlet.PermissionOpts{
