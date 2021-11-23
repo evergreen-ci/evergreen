12653fa61c721122fe9adad6d77443e1e067fc37
malik.hadjri <malik.hadjri@mongodb.com>
2021-11-23 16:15:35 -0500

    EVG-15868 Refactor project config handling

--- model/project_aliases_test.go
+++ model/project_aliases_test.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
-	mgobson "github.com/evergreen-ci/evergreen/db/mgo/bson"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"go.mongodb.org/mongo-driver/bson"
