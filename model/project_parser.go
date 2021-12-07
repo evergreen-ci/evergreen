fac568f05757d3c41f804267639f3977324abea7
malik.hadjri <malik.hadjri@mongodb.com>
2021-12-07 13:01:28 -0500

    EVG-15868 Set config identifier using utility

--- model/project_parser.go
+++ model/project_parser.go
@@ -614,7 +614,7 @@ func LoadProjectInto(ctx context.Context, data []byte, opts *GetProjectOpts, ide
 		*project = *p
 	}
 	project.Identifier = identifier
-	config.Identifier = &identifier
+	config.Identifier = utility.ToStringPtr(identifier)
 	return intermediateProject, config, errors.Wrapf(err, LoadProjectError)
 }
 
