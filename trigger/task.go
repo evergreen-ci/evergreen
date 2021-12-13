6be5c65c340bb9a48bbeafd58459c6b95f0f56e1
Chukwurado Umeaka <chukwurado.umeaka98@gmail.com>
2021-12-13 10:30:19 -0500

    Fix bug to reassign `shouldSkipTicket` variable to value instead of create new variable in if block

--- trigger/task.go
+++ trigger/task.go
@@ -355,7 +355,7 @@ func (t *taskTriggers) generate(sub *event.Subscription, pastTenseOverride, test
 					return nil, errors.Wrapf(err, "error getting execution task")
 				}
 				if executionTask.Details.Status == evergreen.TaskFailed {
-					shouldSkipTicket := executionTask.Details.Description == evergreen.TaskDescriptionStranded ||
+					shouldSkipTicket = executionTask.Details.Description == evergreen.TaskDescriptionStranded ||
 						executionTask.Details.Type == evergreen.CommandTypeSetup ||
 						executionTask.IsSystemUnresponsive()
 					if !shouldSkipTicket {
