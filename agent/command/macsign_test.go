5f4b1b6de67ddab11984c18564adcf8e5505a469
Tural Farhadov <tural.ferhadov@gmail.com>
2021-12-16 10:21:11 -0800

    remove convey

--- agent/command/macsign_test.go
+++ agent/command/macsign_test.go
@@ -12,149 +12,168 @@ import (
 	"github.com/evergreen-ci/evergreen/model"
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/util"
-	. "github.com/smartystreets/goconvey/convey"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMacSignValidateParams(t *testing.T) {
+	for _, tc := range []struct {
+		name      string
+		params    map[string]interface{}
+		shouldErr bool
+	}{
+		{
+			name: "can detect missing key_id",
+			params: map[string]interface{}{
+				"secret":          "secret",
+				"local_zip_file":  "local",
+				"output_zip_file": "zip",
+				"service_url":     "url",
+			},
+			shouldErr: true,
+		},
+		{
+			name: "can detect missing secret",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"local_zip_file":  "local",
+				"output_zip_file": "zip",
+				"service_url":     "url",
+			},
+			shouldErr: true,
+		},
+		{
+			name: "can detect missing service_url",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"local_zip_file":  "local",
+				"output_zip_file": "zip",
+			},
+			shouldErr: true,
+		},
+		{
+			name: "can detect missing output_zip_file",
+			params: map[string]interface{}{
+				"key_id":         "keyId",
+				"secret":         "secret",
+				"local_zip_file": "local",
+				"service_url":    "url",
+			},
+			shouldErr: true,
+		},
+		{
+			name: "can detect missing local_zip_file",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"service_url":     "url",
+				"output_zip_file": "zip",
+			},
+			shouldErr: true,
+		},
+		{
+			name: "valid set of signing params produces no error",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"service_url":     "url",
+				"output_zip_file": "outputzip",
+				"local_zip_file":  "inputzip",
+			},
+			shouldErr: false,
+		},
+		// notary specific tests
+		{
+			name: "can detect missing bundleId on notarization",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"service_url":     "url",
+				"output_zip_file": "outputzip",
+				"local_zip_file":  "inputzip",
+				"notarize":        true,
+			},
+			shouldErr: true,
+		},
+		{
+			name: "valid set of notarization params produces no error",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"service_url":     "url",
+				"output_zip_file": "outputzip",
+				"local_zip_file":  "inputzip",
+				"notarize":        true,
+				"bundle_id":       "bundleId",
+			},
+			shouldErr: false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+			cmd := &macSign{}
+			err := cmd.ParseParams(tc.params)
 
-	Convey("With a mac signing and notarization command", t, func() {
-
-		var cmd *macSign
-
-		Convey("when validating signing command params", func() {
-			cmd = &macSign{}
-
-			Convey("a missing key id should cause an error", func() {
-
-				params := map[string]interface{}{
-					"secret":          "secret",
-					"local_zip_file":  "local",
-					"output_zip_file": "zip",
-					"service_url":     "url",
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("a missing secret should cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"local_zip_file":  "local",
-					"output_zip_file": "zip",
-					"service_url":     "url",
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("a missing service url should cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"secret":          "secret",
-					"local_zip_file":  "local",
-					"output_zip_file": "zip",
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("a missing output_zip_file should cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":         "keyId",
-					"secret":         "secret",
-					"local_zip_file": "local",
-					"service_url":    "url",
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("a missing local_zip_file should cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"secret":          "secret",
-					"service_url":     "url",
-					"output_zip_file": "zip",
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("valid set of params should not cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"secret":          "secret",
-					"service_url":     "url",
-					"output_zip_file": "outputzip",
-					"local_zip_file":  "inputzip",
-				}
-				So(cmd.ParseParams(params), ShouldBeNil)
-				So(cmd.KeyId, ShouldEqual, params["key_id"])
-				So(cmd.Secret, ShouldEqual, params["secret"])
-				So(cmd.ServiceUrl, ShouldEqual, params["service_url"])
-				So(cmd.OutputZipFile, ShouldEqual, params["output_zip_file"])
-				So(cmd.LocalZipFile, ShouldEqual, params["local_zip_file"])
-			})
+			if tc.shouldErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
-
-		Convey("when validating notarization command params", func() {
-			cmd = &macSign{}
-
-			Convey("notarizing requested bundleId not given should cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"secret":          "secret",
-					"service_url":     "url",
-					"output_zip_file": "outputzip",
-					"local_zip_file":  "inputzip",
-					"notarize":        true,
-				}
-				So(cmd.ParseParams(params), ShouldNotBeNil)
-			})
-			Convey("valid set of notarizing params should not cause an error", func() {
-
-				params := map[string]interface{}{
-					"key_id":          "keyId",
-					"secret":          "secret",
-					"service_url":     "url",
-					"output_zip_file": "outputzip",
-					"local_zip_file":  "inputzip",
-					"notarize":        true,
-					"bundle_id":       "bundleId",
-				}
-				So(cmd.ParseParams(params), ShouldBeNil)
-				So(cmd.KeyId, ShouldEqual, params["key_id"])
-				So(cmd.Secret, ShouldEqual, params["secret"])
-				So(cmd.ServiceUrl, ShouldEqual, params["service_url"])
-				So(cmd.OutputZipFile, ShouldEqual, params["output_zip_file"])
-				So(cmd.LocalZipFile, ShouldEqual, params["local_zip_file"])
-				So(cmd.Notarize, ShouldEqual, params["notarize"])
-				So(cmd.BundleId, ShouldEqual, params["bundle_id"])
-			})
-		})
-	})
+	}
 }
 
 func TestMacSignExecute(t *testing.T) {
 
-	Convey("With a mac signing and notarization command", t, func() {
-
-		Convey("when valid params given", func() {
-
-			Convey("failed macnotary command should cause an error", func() {
+	for _, tc := range []struct {
+		name              string
+		params            map[string]interface{}
+		executableContent string
+		shouldErr         bool
+	}{
+		{
+			name: "can detect failure from macnotary binary",
+			params: map[string]interface{}{
+				"key_id":            "keyId",
+				"secret":            "secret",
+				"local_zip_file":    "local",
+				"output_zip_file":   "zip",
+				"service_url":       "url",
+				"notarize":          true,
+				"bundleId":          "bundleId",
+				"working_directory": "shouldbecreated",
+			},
+			executableContent: "#!/bin/sh \nexit 1",
+			shouldErr:         true,
+		},
+		{
+			name: "can create file on successful macnotary execution",
+			params: map[string]interface{}{
+				"key_id":          "keyId",
+				"secret":          "secret",
+				"local_zip_file":  "local",
+				"output_zip_file": filepath.Join(t.TempDir(), "output.zip"),
+				"service_url":     "url",
+				"notarize":        true,
+				"bundleId":        "bundleId",
+			},
+			executableContent: fmt.Sprintf("#!/bin/sh \ntouch %s && exit 0", filepath.Join(t.TempDir(), "output.zip")),
+			shouldErr:         false,
+		},
+	} {
+		{
+			tc := tc
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
 				ctx := context.TODO()
 				mockClientBinary := filepath.Join(t.TempDir(), "client")
-				require.NoError(t,
-					ioutil.WriteFile(mockClientBinary, []byte("#!/bin/sh \nexit 1"), 0777),
+				assert.NoError(t,
+					ioutil.WriteFile(mockClientBinary, []byte(tc.executableContent), 0777),
 				)
-
-				cmd := &macSign{
-					KeyId:         "keyId",
-					Secret:        "secret",
-					ServiceUrl:    "url",
-					OutputZipFile: "output",
-					LocalZipFile:  "input",
-					Notarize:      true,
-					BundleId:      "bundleId",
-					ClientBinary:  mockClientBinary,
-				}
+				cmd := &macSign{}
+				tc.params["client_binary"] = mockClientBinary
+				cmd.ParseParams(tc.params)
 
 				comm := client.NewMock("http://localhost.com")
 				conf := &internal.TaskConfig{
@@ -164,41 +183,15 @@ func TestMacSignExecute(t *testing.T) {
 					BuildVariant: &model.BuildVariant{},
 				}
 				logger, err := comm.GetLoggerProducer(ctx, client.TaskData{ID: conf.Task.Id, Secret: conf.Task.Secret}, nil)
-				require.NoError(t, err)
+				assert.NoError(t, err)
 
-				require.Error(t, cmd.Execute(ctx, comm, logger, conf))
-			})
-			Convey("successful macnotary command should produce output file", func() {
-				ctx := context.TODO()
-
-				cmd := &macSign{
-					KeyId:         "keyId",
-					Secret:        "secret",
-					ServiceUrl:    "url",
-					OutputZipFile: filepath.Join(t.TempDir(), "output.zip"),
-					LocalZipFile:  "input",
-					Notarize:      true,
-					BundleId:      "bundleId",
-					ClientBinary:  filepath.Join(t.TempDir(), "client"),
+				if tc.shouldErr {
+					assert.Error(t, cmd.Execute(ctx, comm, logger, conf))
+				} else {
+					assert.NoError(t, cmd.Execute(ctx, comm, logger, conf))
 				}
-
-				require.NoError(t,
-					ioutil.WriteFile(cmd.ClientBinary, []byte(fmt.Sprintf("#!/bin/sh \ntouch %s && exit 0", cmd.OutputZipFile)), 0777),
-				)
-
-				comm := client.NewMock("http://localhost.com")
-				conf := &internal.TaskConfig{
-					Expansions:   &util.Expansions{},
-					Task:         &task.Task{Id: "mock_id", Secret: "mock_secret"},
-					Project:      &model.Project{},
-					BuildVariant: &model.BuildVariant{},
-				}
-				logger, err := comm.GetLoggerProducer(ctx, client.TaskData{ID: conf.Task.Id, Secret: conf.Task.Secret}, nil)
-				require.NoError(t, err)
-
-				require.NoError(t, cmd.Execute(ctx, comm, logger, conf))
-				require.FileExists(t, cmd.OutputZipFile)
 			})
-		})
-	})
+
+		}
+	}
 }
