7a191de5e59fbbb9d99f2e99db508aeacc3679f8
Chukwurado Umeaka <50035150+Chukwurado@users.noreply.github.com>
2021-12-21 11:10:15 -0500

    EVG 15012 Return warning errors if tasks have no timeout (#5245)
    * EVG 15012 Return warning errors if tasks have no timeout
    
    * Update project_validator.go
    
    * Update project_validator.go
    
    * Modified unit tests in project_validator_tests to pass new changes
    
    * Show task names in one statement instead of in multiple statements
    
    * Update project_validator_test.go
    
    * updated warning message when no timeout is defined for tasks
    
    * break out from loop if a task has not timeout set
    
    * Update project_validator.go

--- validator/project_validator.go
+++ validator/project_validator.go
@@ -131,6 +131,7 @@ var projectSemanticValidators = []projectValidator{
 	checkTaskCommands,
 	checkTaskGroups,
 	checkLoggerConfig,
+	checkTaskTimeout,
 }
 
 var projectSettingsValidators = []projectSettingsValidator{
@@ -1897,4 +1898,27 @@ func parseS3PullParameters(c model.PluginCommandConf) (task, bv string, err erro
 		return "", "", errors.Errorf("command '%s' was supplied parameter '%s' but is not a string argument, got %T", c.Command, paramName, i)
 	}
 	return task, bv, nil
+}
+
+// checkTaskTimeout checks if all tasks contain a timeout
+func checkTaskTimeout(project *model.Project) ValidationErrors {
+	errs := ValidationErrors{}
+	if project.ExecTimeoutSecs > 0 {
+		return errs
+	}
+	for _, task := range project.Tasks {
+		if task.ExecTimeoutSecs == 0 {
+			errs = append(errs,
+				ValidationError{
+					Message: fmt.Sprintf("project '%s' does not "+
+						"have an exec_timeout_secs defined on one or more tasks; these tasks will default to a timeout of 6 hours",
+						project.Identifier),
+					Level: Warning,
+				},
+			)
+			break
+		}
+	}
+
+	return errs
 }
