a836f0b4330dd4a7957d88dfc557a1fd6928315b
Chaya Malik <chaya.malik@10gen.com>
2021-12-21 07:08:32 -0500

    fix panic

--- model/project.go
+++ model/project.go
@@ -1202,6 +1202,10 @@ func FindLatestVersionWithValidProject(projectId string) (*Version, *Project, er
 		}))
 	}
 
+	if lastGoodVersion == nil {
+		return nil, nil, errors.WithStack(errors.Errorf("could not find a valid version for project %s", projectId))
+	}
+
 	return nil, nil, errors.Wrapf(err, "Error loading project from "+
 		"last good version for project, %s", lastGoodVersion.Identifier)
 }
