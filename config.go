5188c88ea323aa1599de4374902c48fe2664768f
Kim Tao <kim.tao@mongodb.com>
2022-01-04 09:54:47 -0500

    EVG-15955: fix gopsutil import

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2021-12-22"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2021-12-21"
+	AgentVersion = "2022-01-04"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
