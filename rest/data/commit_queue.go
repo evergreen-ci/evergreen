d9dd52558feab2a62a536a49c23bc5558ca444ef
malik.hadjri <malik.hadjri@mongodb.com>
2022-01-07 02:56:35 -0500

    EVG-16000 Fix compile err

--- rest/data/commit_queue.go
+++ rest/data/commit_queue.go
@@ -137,12 +137,12 @@ func getPatchInfo(ctx context.Context, githubToken string, patchDoc *patch.Patch
 	}
 
 	// fetch the latest config file
-	config, projectYaml, err := model.GetPatchedProject(ctx, patchDoc, githubToken)
+	config, patchConfig, err := model.GetPatchedProject(ctx, patchDoc, githubToken)
 	if err != nil {
 		return "", nil, nil, errors.Wrap(err, "can't get remote config file")
 	}
 
-	patchDoc.PatchedParserProject = projectYaml
+	patchDoc.PatchedParserProject = patchConfig.PatchedParserProject
 	return patchContent, summaries, config, nil
 }
 
