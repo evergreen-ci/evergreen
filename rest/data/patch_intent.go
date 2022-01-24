d83b47df36032034e5cc67411b1841b9684296f1
Bynn <bynn.lee@mongodb.com>
2022-01-24 10:13:23 -0500

    function name

--- rest/data/patch_intent.go
+++ rest/data/patch_intent.go
@@ -19,7 +19,7 @@ type DBPatchIntentConnector struct{}
 
 func (p *DBPatchIntentConnector) AddPatchIntent(intent patch.Intent, queue amboy.Queue) error {
 	patchDoc := intent.NewPatch()
-	projectRef, err := model.FindOneProjectRefByRepoAndBranchWithPRTesting(patchDoc.GithubPatchData.BaseOwner,
+	projectRef, err := model.FindOneProjectRefByRepoAndBranchWithAutoPRTesting(patchDoc.GithubPatchData.BaseOwner,
 		patchDoc.GithubPatchData.BaseRepo, patchDoc.GithubPatchData.BaseBranch)
 	if err != nil {
 		return gimlet.ErrorResponse{
