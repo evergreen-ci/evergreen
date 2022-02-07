c7a8f42af9573399bb6d241d162a0f1c8052ea1f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-02-07 18:28:35 -0500

    Start logging graphql requests to splunk

---
+++ graphql/splunktracing.go
@@ -0,0 +1,49 @@
+package graphql
+
+import (
+	"context"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/evergreen-ci/gimlet"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
+)
+
+type (
+	SplunkTracing struct{}
+)
+
+var _ interface {
+	graphql.HandlerExtension
+	graphql.ResponseInterceptor
+} = SplunkTracing{}
+
+func (SplunkTracing) ExtensionName() string {
+	return "SplunkTracing"
+}
+
+func (SplunkTracing) Validate(graphql.ExecutableSchema) error {
+	return nil
+}
+
+func (SplunkTracing) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+	rc := graphql.GetOperationContext(ctx)
+
+	start := graphql.Now()
+
+	defer func() {
+		end := graphql.Now()
+
+		duration := end.Sub(start)
+		grip.Info(message.Fields{
+			"message":     "graphql tracing",
+			"query":       rc.Operation.Name,
+			"variables":   rc.Variables,
+			"duration_ms": duration.Milliseconds(),
+			"request":     gimlet.GetRequestID(ctx),
+			"start":       start,
+			"end":         end,
+		})
+	}()
+	return next(ctx)
+}
