c691e821a9976073503945b85eca0612f1ee513e
Kim Tao <kim.tao@mongodb.com>
2022-02-15 16:52:07 -0500

    EVG-16066: remove spawn host modification fallback (#5394)

--- units/spawnhost_stop.go
+++ units/spawnhost_stop.go
@@ -24,10 +24,6 @@ func init() {
 }
 
 type spawnhostStopJob struct {
-	// TODO (EVG-16066): remove HostID and UserID since they are no longer
-	// necessary, except for temporary backward compatibility.
-	HostID                string `bson:"host_id" json:"host_id" yaml:"host_id"`
-	UserID                string `bson:"user_id" json:"user_id" yaml:"user_id"`
 	CloudHostModification `bson:"cloud_host_modification" json:"cloud_host_modification" yaml:"cloud_host_modification"`
 	job.Base              `bson:"job_base" json:"job_base" yaml:"job_base"`
 
@@ -60,17 +56,6 @@ func NewSpawnhostStopJob(h *host.Host, user, ts string) amboy.Job {
 
 func (j *spawnhostStopJob) Run(ctx context.Context) {
 	defer j.MarkComplete()
-
-	// Setting the base fields are for temporary backward compatibility with
-	// pending jobs already stored in the DB.
-	// TODO (EVG-16066): remove this check once all old versions of the job are
-	// complete.
-	if j.HostID != "" {
-		j.CloudHostModification.HostID = j.HostID
-	}
-	if j.UserID != "" {
-		j.CloudHostModification.UserID = j.UserID
-	}
 
 	stopCloudHost := func(mgr cloud.Manager, h *host.Host, user string) error {
 		if err := mgr.StopInstance(ctx, h, user); err != nil {
