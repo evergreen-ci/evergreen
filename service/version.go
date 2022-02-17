1a450005c15cfeebe949fb3e39e8f9b81319bcc8
Annie Black <annie.black@10gen.com>
2022-02-17 10:18:11 -0500

    EVG-14348 case correctly on patch requesters for fetch (#5401)

--- service/version.go
+++ service/version.go
@@ -221,7 +221,7 @@ func (uis *UIServer) versionPage(w http.ResponseWriter, r *http.Request) {
 	pluginContext := projCtx.ToPluginContext(uis.Settings, currentUser)
 	pluginContent := getPluginDataAndHTML(uis, plugin.VersionPage, pluginContext)
 	newUILink := ""
-	if len(uis.Settings.Ui.UIv2Url) > 0 && projCtx.Version.Requester == evergreen.PatchVersionRequester {
+	if len(uis.Settings.Ui.UIv2Url) > 0 && evergreen.IsPatchRequester(projCtx.Version.Requester) {
 		newUILink = fmt.Sprintf("%s/version/%s", uis.Settings.Ui.UIv2Url, projCtx.Version.Id)
 	}
 	uis.render.WriteResponse(w, http.StatusOK, struct {
