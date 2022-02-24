61aef095035cc4bb12f96fc7eb88a75960cd2cee
Bynn <bynn.lee@mongodb.com>
2022-02-24 15:17:27 -0500

    EVG-15934 Add hint to UnscheduleStaleUnderwaterTasks

--- model/task/task.go
+++ model/task/task.go
@@ -1075,7 +1075,9 @@ func UnscheduleStaleUnderwaterHostTasks(distroID string) (int, error) {
 		},
 	}
 
-	info, err := UpdateAll(query, update)
+	// Force the query to use 'distro_1_status_1_activated_1_priority_1'
+	// instead of defaulting to 'status_1_depends_on.status_1_depends_on.unattainable_1'.
+	info, err := UpdateAllWithHint(query, update, ActivatedTasksByDistroIndex)
 	if err != nil {
 		return 0, errors.Wrap(err, "problem unscheduling stale underwater tasks")
 	}
