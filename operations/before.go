ce3b7bf113e435f1983cd8b648fd216c9cb14621
malik.hadjri <malik.hadjri@mongodb.com>
2022-03-01 11:14:55 -0500

    Revert "Revert "EVG-6496 Auto update CLI if it is set in client settings before each command (#5416)""
    This reverts commit db8dd434f9d43da0512742127ce2c6c23c16aca6.

--- operations/before.go
+++ operations/before.go
@@ -1,6 +1,7 @@
 package operations
 
 import (
+	"context"
 	"os"
 	"strings"
 
@@ -94,6 +95,26 @@ var (
 		}
 
 		return catcher.Resolve()
+	}
+
+	// autoUpdateCLI is to be run before commonly used command line functions and will automatically update and install a newer
+	// CLI version if one is found, and then run the command on that new CLI version.
+	// Some functions that one would expect to return quickly have been omitted from having this as a 'before' function since downloading and installing
+	// takes time that would be cumbersome to the user (e.g. list functions, delete functions).
+	autoUpdateCLI = func(c *cli.Context) error {
+		confPath := c.String("conf")
+		conf, err := NewClientSettings(confPath)
+		if err != nil {
+			return errors.Wrap(err, "problem loading configuration")
+		}
+		if conf.AutoUpgradeCLI {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			if err = checkAndUpdateVersion(conf, ctx, true, false, true); err != nil {
+				grip.Errorf("Automatic CLI update failed! Continuing with command execution. Error: %s", err.Error())
+			}
+		}
+		return nil
 	}
 )
 
