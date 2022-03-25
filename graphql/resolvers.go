b0f5d546e8921fa0cc781da7a3884b5280f4af98
Bynn Lee <bynn.lee@mongodb.com>
2022-03-25 14:55:35 -0400

    EVG-15391 Allow non-admin users to override dependencies for patches (#5522)

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -435,7 +435,12 @@ func (r *taskResolver) CanOverrideDependencies(ctx context.Context, at *restMode
 		RequiredLevel: evergreen.TasksAdmin.Value,
 		Resource:      *at.ProjectId,
 	}
-	if len(at.DependsOn) > 0 && currentUser.HasPermission(requiredPermission) {
+	overrideRequesters := []string{
+		evergreen.PatchVersionRequester,
+		evergreen.GithubPRRequester,
+	}
+	if len(at.DependsOn) > 0 && (utility.StringSliceContains(overrideRequesters, utility.FromStringPtr(at.Requester)) ||
+		currentUser.HasPermission(requiredPermission)) {
 		return true, nil
 	}
 	return false, nil
