2ad472fcea30413ee72c81d0661a84c2b0405e1e
Jonathan Brill <jonathan.brill@10gen.com>
2022-04-04 16:08:52 -0400

    increment agent version

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ var (
 	ClientVersion = "2022-03-11"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2022-04-01"
+	AgentVersion = "2022-04-04"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
