f4e253016cd92d2476455ae9eb807753a24564f9
malik.hadjri <malik.hadjri@mongodb.com>
2022-05-05 12:01:35 -0400

    EVG-16103 Remove "Heartbeat ticker canceled, aborting task" from agent logs

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ var (
 	ClientVersion = "2022-04-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2022-05-04b"
+	AgentVersion = "2022-05-05"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
