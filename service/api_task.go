a931a7971f86f828bd85be81967894da4b5f2034
Annie Black <annie.black@10gen.com>
2022-05-06 11:16:59 -0400

    EVG-12923 block task group tasks before clearing host's running task (#5640)

--- service/api_task.go
+++ service/api_task.go
@@ -179,7 +179,26 @@ func (as *APIServer) EndTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// clear the running task on the host startPhaseAt that the task has finished
+	// For a single-host task group, if a task fails, block and dequeue later tasks in that group.
+	// Call before MarkEnd so the version is marked finished when this is the last task in the version to finish,
+	// and before clearing the running task from the host so later tasks in the group aren't picked up by the host.
+	if t.IsPartOfSingleHostTaskGroup() && details.Status != evergreen.TaskSucceeded {
+		// BlockTaskGroups is a recursive operation, which
+		// includes updating a large number of task
+		// documents.
+		if err := model.BlockTaskGroupTasks(t.Id); err != nil {
+			grip.Error(message.WrapError(err, message.Fields{
+				"message": "problem blocking task group tasks",
+				"task_id": t.Id,
+			}))
+		}
+		grip.Debug(message.Fields{
+			"message": "blocked task group tasks for task",
+			"task_id": t.Id,
+		})
+	}
+
+	// Clear the running task on the host now that the task has finished.
 	if err := currentHost.ClearRunningAndSetLastTask(t); err != nil {
 		err = errors.Wrapf(err, "error clearing running task %s for host %s", t.Id, currentHost.Id)
 		grip.Errorf(err.Error())
@@ -194,25 +213,6 @@ func (as *APIServer) EndTask(w http.ResponseWriter, r *http.Request) {
 	if projectRef == nil {
 		as.LoggedError(w, r, http.StatusNotFound, fmt.Errorf("empty projectRef for task"))
 		return
-	}
-
-	// For a single-host task group, if a task fails, block and dequeue later tasks in that group.
-	// Call before MarkEnd so the version is marked finished when this is the last task in the version
-	// to finish
-	if t.IsPartOfSingleHostTaskGroup() && details.Status != evergreen.TaskSucceeded {
-		// BlockTaskGroups is a recursive operation, which
-		// includes updating a large number of task
-		// documents.
-		if err = model.BlockTaskGroupTasks(t.Id); err != nil {
-			grip.Error(message.WrapError(err, message.Fields{
-				"message": "problem blocking task group tasks",
-				"task_id": t.Id,
-			}))
-		}
-		grip.Debug(message.Fields{
-			"message": "blocked task group tasks for task",
-			"task_id": t.Id,
-		})
 	}
 
 	// mark task as finished
