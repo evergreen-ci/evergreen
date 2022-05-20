97cdd8df0b1141aea59fe466089185b18d5222be
Annie Black <annie.black@10gen.com>
2022-05-20 12:05:09 -0400

    fix variable name

--- graphql/resolvers.go
+++ graphql/resolvers.go
@@ -1089,12 +1089,12 @@ func (r *queryResolver) ProjectEvents(ctx context.Context, identifier string, li
 	return res, err
 }
 
-func (r *queryResolver) RepoEvents(ctx context.Context, identifier string, limit *int, before *time.Time) (*ProjectEvents, error) {
+func (r *queryResolver) RepoEvents(ctx context.Context, id string, limit *int, before *time.Time) (*ProjectEvents, error) {
 	timestamp := time.Now()
 	if before != nil {
 		timestamp = *before
 	}
-	events, err := data.GetEventsById(identifier, timestamp, utility.FromIntPtr(limit))
+	events, err := data.GetEventsById(id, timestamp, utility.FromIntPtr(limit))
 	res := &ProjectEvents{
 		EventLogEntries: getPointerEventList(events),
 		Count:           len(events),
