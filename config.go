facdbda59e8471e8ebe904a7a5e58080f5ca93d0
Annie Black <annie.black@10gen.com>
2022-05-23 15:45:11 -0400

    EVG-15769 add patchable and patch_only to s3.put (#5675)

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ var (
 	ClientVersion = "2022-04-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2022-05-19"
+	AgentVersion = "2022-05-23"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
