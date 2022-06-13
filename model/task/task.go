62820234635fd95a9368090507231651518b11ad
Jonathan Brill <jonathan.brill@10gen.com>
2022-06-13 17:34:16 -0400

    EVG-16679 fix generate tasks retry loop (#5718)

--- model/task/task.go
+++ model/task/task.go
@@ -3318,7 +3318,7 @@ func (t *Task) FindAllMarkedUnattainableDependencies() ([]Task, error) {
 	return FindAll(query)
 }
 
-func (t *Task) toTaskNode() TaskNode {
+func (t *Task) ToTaskNode() TaskNode {
 	return TaskNode{
 		Name:    t.DisplayName,
 		Variant: t.BuildVariant,
