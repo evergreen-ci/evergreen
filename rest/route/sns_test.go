9ca4a6d47129b46dc7d44bee5bf5e915f1943c9c
Kim Tao <kim.tao@mongodb.com>
2022-06-15 16:11:50 -0400

    Fix lint-rest-route after merge

--- rest/route/sns_test.go
+++ rest/route/sns_test.go
@@ -431,8 +431,8 @@ func TestECSSNSHandleNotification(t *testing.T) {
 			)
 			rh.env.Settings().Providers.AWS.Pod.ECS.Clusters = []evergreen.ECSClusterConfig{
 				{
-					Name:     clusterID,
-					Platform: evergreen.ECSClusterPlatformLinux,
+					Name: clusterID,
+					OS:   evergreen.ECSOSLinux,
 				},
 			}
 			cocoaMock.GlobalECSService.Clusters[clusterID] = cocoaMock.ECSCluster{
