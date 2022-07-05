9fd8629bfc3a664307af5858fc46aab065889a70
Bynn Lee <bynn.lee@mongodb.com>
2022-07-05 12:22:00 -0400

    EVG-13262 Add check for CLI version string update (#5774)

---
+++ scripts/verify-client-version-update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [[ "${BRANCH_NAME}" == "" ]]; then
+    BRANCH_NAME=main;
+fi
+
+# Find the common ancestor between the current set of changes and the upstream branch, then see if any source code files
+# have changed in the operations directory.
+common_ancestor=$(git merge-base ${BRANCH_NAME}@{upstream} HEAD);
+files_changed="$(git diff --name-only "${common_ancestor}" -- 'operations/**.go')"
+if [[ "${files_changed}" == "" ]]; then
+    exit 0;
+fi
+
+# Get the list of commit revisions.
+committed_changes=$(git log --format=%H ${common_ancestor}..);
+
+# Find the latest commit when the client version was updated.
+client_version_line_num=$(git grep -n -e "ClientVersion =" config.go | cut -d ':' -f 2);
+last_commit_client_version_updated=$(git blame -l -L ${client_version_line_num},${client_version_line_num} config.go | cut -d ' ' -f 1);
+
+# Check if the last commit when the client version was updated is in the list of committed changes.
+if [[ "${committed_changes}" =~ "${last_commit_client_version_updated}" ]]; then
+    exit 0;
+fi
+
+# If the client version was updated but not committed yet, it still counts as a change.
+if [[ "${last_commit_client_version_updated}" == "0000000000000000000000000000000000000000" ]]; then
+    exit 0;
+fi
+
+echo -e "Files affected by client version update:\n${files_changed}" >&2
+echo "Evergreen CLI has been changed but client version has not been updated. Please update the client version." >&2
+exit 1
