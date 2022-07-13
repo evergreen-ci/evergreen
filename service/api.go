3e7f6ff39cee94dc5c90bf913d2ee67ef7f52937
Chaya Malik <chaya.malik@10gen.com>
2022-07-13 17:35:42 -0400

    undo initial changes

--- service/api.go
+++ service/api.go
@@ -159,38 +159,27 @@ func (as *APIServer) requireProject(next http.HandlerFunc) http.HandlerFunc {
 
 func (as *APIServer) requireHost(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		h, _, err := model.ValidateHost(gimlet.GetVars(r)["hostId"], r)
-		UpdateHost(h)
+		h, code, err := model.ValidateHost(gimlet.GetVars(r)["hostId"], r)
 		if err != nil {
-			// validate the host again after update in case the agent and app server were out of sync,
-			// and the update fixed it
-			var code int
-			h, code, err = model.ValidateHost(gimlet.GetVars(r)["hostId"], r)
-			if err != nil {
-				as.LoggedError(w, r, code, errors.Wrap(err, "host not assigned to run task"))
-				return
-			}
-
+			as.LoggedError(w, r, code, errors.Wrap(err, "host not assigned to run task"))
+			return
+		}
+		// update host access time
+		if err := h.UpdateLastCommunicated(); err != nil {
+			grip.Warningf("Could not update host last communication time for %s: %+v", h.Id, err)
+		}
+		// Since the host has contacted the app server, we should prevent the
+		// app server from attempting to deploy agents or agent monitors.
+		// Deciding whether or not we should redeploy agents or agent monitors
+		// is handled within the REST route handler.
+		if h.NeedsNewAgent {
+			grip.Warning(message.WrapError(h.SetNeedsNewAgent(false), "problem clearing host needs new agent"))
+		}
+		if h.NeedsNewAgentMonitor {
+			grip.Warning(message.WrapError(h.SetNeedsNewAgentMonitor(false), "problem clearing host needs new agent monitor"))
 		}
 		r = setAPIHostContext(r, h)
 		next(w, r)
-	}
-}
-
-func UpdateHost(h *host.Host) {
-	// update host access time
-	if err := h.UpdateLastCommunicated(); err != nil {
-		grip.Warningf("Could not update host last communication time for %s: %+v", h.Id, err)
-	}
-	// Since the host has contacted the app server, we should prevent the
-	// app server from attempting to deploy agents or agent monitors.
-	// Deciding whether or not we should redeploy agents or agent monitors
-	// is handled within the REST route handler.
-	if h.NeedsNewAgent {
-		grip.Warning(message.WrapError(h.SetNeedsNewAgent(false), "problem clearing host needs new agent"))
-	}
-	if h.NeedsNewAgentMonitor {
-		grip.Warning(message.WrapError(h.SetNeedsNewAgentMonitor(false), "problem clearing host needs new agent monitor"))
 	}
 }
 
@@ -376,8 +365,7 @@ func (as *APIServer) FetchExpansionsForTask(w http.ResponseWriter, r *http.Reque
 // AttachFiles updates file mappings for a task or build
 func (as *APIServer) AttachFiles(w http.ResponseWriter, r *http.Request) {
 	t := MustHaveTask(r)
-
-	grip.Infof("Attaching files to task:%v", t.Id)
+	grip.Infoln("Attaching files to task:", t.Id)
 
 	entry := &artifact.Entry{
 		TaskId:          t.Id,
