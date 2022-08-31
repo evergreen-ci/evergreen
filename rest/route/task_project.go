4ad9df9fe9ea9ef8015e047ce084744cc50b27c0
malik.hadjri <malik.hadjri@mongodb.com>
2022-08-31 11:50:16 -0400

    Fix lint

--- rest/route/task_project.go
+++ rest/route/task_project.go
@@ -2,12 +2,13 @@ package route
 
 import (
 	"context"
+	"net/http"
+
 	"github.com/evergreen-ci/evergreen/model/task"
 	"github.com/evergreen-ci/evergreen/rest/data"
 	"github.com/evergreen-ci/evergreen/rest/model"
 	"github.com/evergreen-ci/gimlet"
 	"github.com/pkg/errors"
-	"net/http"
 )
 
 // taskByProjectHandler implements the GET /projects/{project_id}/revisions/{commit_hash}/tasks.
