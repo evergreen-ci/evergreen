db6c2841db2939d3c18a9ff8c91dca6b0abe7290
annie.black <annie.black@mongodb.com>
2022-09-21 10:04:01 -0400

    cr

--- public/static/app/perf/perf.js
+++ public/static/app/perf/perf.js
@@ -1,6 +1,7 @@
 mciModule.controller('PerfController', function PerfController(
   $scope, $window, $http, $location, $filter, ChangePointsService, PERFORMANCE_ANALYSIS_AND_TRIAGE_API, $sce,
 ) {
+  $scope.conf = $window.plugins["perf"];
   $scope.task = $window.task_data;
   $scope.newTrendChartsUi = $sce.trustAsResourceUrl(PERFORMANCE_ANALYSIS_AND_TRIAGE_API.UI + "/task/" + ($scope.task ? $scope.task.id : null) + "/performanceData");
 });
