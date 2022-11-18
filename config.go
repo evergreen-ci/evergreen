ee4369fcc8ad8d90a2632ffe7754271602154a52
Annie Black <annie.black@mongodb.com>
2022-11-18 13:38:48 -0500

    EVG-1824 add option to automatically add module changes (#6069)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2022-11-10"
+	ClientVersion = "2022-11-16"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2022-11-18"
