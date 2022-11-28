6c5e326eb294959ed5d9c99d11b3cb871d799a8c
Annie Black <annie.black@mongodb.com>
2022-11-28 15:25:25 -0500

    EVG-16982 respect disabled for cron (#6087)

--- model/project.go
+++ model/project.go
@@ -1293,8 +1293,10 @@ func FindLatestVersionWithValidProject(projectId string) (*Version, *Project, er
 		"last good version for project '%s'", lastGoodVersion.Identifier)
 }
 
-func (bvt *BuildVariantTaskUnit) HasBatchTime() bool {
-	return bvt.CronBatchTime != "" || bvt.BatchTime != nil || bvt.Activate != nil
+// HasSpecificActivation returns if the build variant task specifies an activation condition that
+// overrides the default, such as cron/batchtime, disabling the task, or explicitly activating it.
+func (bvt *BuildVariantTaskUnit) HasSpecificActivation() bool {
+	return bvt.CronBatchTime != "" || bvt.BatchTime != nil || bvt.Activate != nil || bvt.IsDisabled()
 }
 
 func (p *Project) FindTaskForVariant(task, variant string) *BuildVariantTaskUnit {
