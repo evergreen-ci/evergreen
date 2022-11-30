24ed1b584076ce9f36cf93f956d536ecc1b7c6f4
bynn <bynn.lee@mongodb.com>
2022-11-29 19:20:06 -0500

    nit

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -627,7 +627,7 @@ func (r *mutationResolver) EditSpawnHost(ctx context.Context, spawnHost *EditSpa
 	}
 	if spawnHost.PublicKey != nil {
 		if h.Status != evergreen.HostRunning {
-			return nil, InputValidationError.Send(ctx, "Host must be running to add a public key")
+			return nil, InputValidationError.Send(ctx, fmt.Sprintf("Host must be running to add a public key but is '%s'", h.Status))
 		}
 		if utility.FromBoolPtr(spawnHost.SavePublicKey) {
 			if err = savePublicKey(ctx, *spawnHost.PublicKey); err != nil {
