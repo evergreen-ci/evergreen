223d41afaab2630d7276f4edc183a44efa2e15c2
Chaya Malik <chaya.malik@mongodb.com>
2022-12-12 10:16:42 -0500

    exit if already finalized

--- trigger/patch.go
+++ trigger/patch.go
@@ -200,6 +200,10 @@ func finalizeChildPatch(sub *event.Subscription) error {
 	if childPatch == nil {
 		return errors.Errorf("child patch '%s' not found", target.ChildPatchId)
 	}
+	// return if patch is already finalized
+	if childPatch.Version != "" {
+		return nil
+	}
 
 	ctx, cancel := evergreen.GetEnvironment().Context()
 	defer cancel()
