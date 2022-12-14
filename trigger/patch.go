f5f1ae4a618c19b48d7a6b79bccd58a131a548b8
Chaya Malik <chaya.malik@10gen.com>
2022-12-14 14:45:15 -0500

    EVG-16663 Suppress general notifications for aborted patches (#6130)

--- trigger/patch.go
+++ trigger/patch.go
@@ -104,7 +104,7 @@ func (t *patchTriggers) patchOutcome(sub *event.Subscription) (*notification.Not
 		if successOutcome || failureOutcome || anyOutcome {
 			aborted, err := model.IsAborted(t.patch.Id.Hex())
 			if err != nil {
-				return nil, errors.Errorf("getting aborted status for patch '%s'", t.patch.Id.Hex())
+				return nil, errors.Wrapf(err, "getting aborted status for patch '%s'", t.patch.Id.Hex())
 			}
 			if aborted {
 				return nil, nil
@@ -382,6 +382,16 @@ func (t *patchTriggers) patchFamilyOutcome(sub *event.Subscription) (*notificati
 	if t.event.EventType != event.PatchChildrenCompletion {
 		return nil, nil
 	}
+
+	// Don't notify the user of the patch outcome if they aborted the patch
+	aborted, err := model.IsAborted(t.patch.Id.Hex())
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting aborted status for patch '%s'", t.patch.Id.Hex())
+	}
+	if aborted {
+		return nil, nil
+	}
+
 	return t.generate(sub)
 }
 
