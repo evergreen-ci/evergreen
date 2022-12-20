a2c2714e63c7cd1fd870a854858fc8cf9552f9c7
Chaya Malik <chaya.malik@10gen.com>
2022-12-20 14:50:48 -0500

    EVG-17206 Add help text to set-module and patch-set-module (#6148)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2022-12-19"
+	ClientVersion = "2022-12-20"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2022-12-05"
