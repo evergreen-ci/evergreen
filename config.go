cbdbacecfd6b85577741678a9b145109f85ae904
malik.hadjri <malik.hadjri@mongodb.com>
2023-01-13 11:51:58 -0500

    Uptick CLI

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 	BuildRevision = ""
 
 	// Commandline Version String; used to control auto-updating.
-	ClientVersion = "2023-01-11"
+	ClientVersion = "2023-01-13"
 
 	// Agent version to control agent rollover.
 	AgentVersion = "2023-01-11"
