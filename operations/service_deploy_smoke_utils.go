c8b9845b3a84777b44f46d01c223e1fd3362aedb
Kim Tao <kim.tao@mongodb.com>
2023-01-13 15:20:06 -0500

    Refactor and document smoke test logic

--- operations/service_deploy_smoke_utils.go
+++ operations/service_deploy_smoke_utils.go
@@ -30,46 +30,55 @@ type smokeEndpointTestDefinitions struct {
 	API map[string][]string `yaml:"api,omitempty"`
 }
 
-func (tests smokeEndpointTestDefinitions) checkEndpoints(username, key string) error {
+func (td smokeEndpointTestDefinitions) checkEndpoints(username, key string) error {
 	client := utility.GetHTTPClient()
 	defer utility.PutHTTPClient(client)
 	client.Timeout = time.Second
 
 	// wait for web service to start
-	attempts := 10
-	for i := 1; i <= attempts; i++ {
-		grip.Infof("checking if Evergreen is up (attempt %d of %d)", i, attempts)
-		_, err := client.Get(smokeUrlPrefix + smokeUiPort)
-		if err != nil {
-			if i == attempts {
-				err = errors.Wrapf(err, "connecting to Evergreen after %d attempts", attempts)
-				grip.Error(err)
-				return err
-			}
-			grip.Infof("could not connect to Evergreen (attempt %d of %d)", i, attempts)
-			time.Sleep(time.Second)
-			continue
-		}
+	if err := td.waitForEvergreen(client); err != nil {
+		return errors.Wrap(err, "waiting for Evergreen to be up")
 	}
-	grip.Info("Evergreen is up")
 
 	// check endpoints
 	catcher := grip.NewSimpleCatcher()
 	grip.Info("Testing UI Endpoints")
-	for url, expected := range tests.UI {
+	for url, expected := range td.UI {
 		catcher.Wrap(makeSmokeGetRequestAndCheck(username, key, client, url, expected), "testing UI endpoints")
 	}
 
 	grip.Info("Testing API Endpoints")
-	for url, expected := range tests.API {
+	for url, expected := range td.API {
 		catcher.Wrap(makeSmokeGetRequestAndCheck(username, key, client, "/api"+url, expected), "testing API endpoints")
 	}
 
-	grip.InfoWhen(!catcher.HasErrors(), "success: all endpoints accessible")
+	grip.InfoWhen(!catcher.HasErrors(), "Success: all endpoints are accessible.")
 
 	return errors.Wrapf(catcher.Resolve(), "testing endpoints")
 }
 
+// waitForEvergreen waits for the Evergreen app server to be up and accepting
+// requests.
+func (td smokeEndpointTestDefinitions) waitForEvergreen(client *http.Client) error {
+	const attempts = 10
+	for i := 0; i < attempts; i++ {
+		grip.Infof("Checking if Evergreen is up. (%d/%d)", i, attempts)
+		if _, err := client.Get(smokeUrlPrefix + smokeUiPort); err != nil {
+			grip.Error(errors.Wrap(err, "connecting to Evergreen"))
+			time.Sleep(time.Second)
+			continue
+		}
+
+		grip.Info("Evergreen is up.")
+		return nil
+	}
+
+	return errors.Errorf("Evergreen was not up after %d check attempts.", attempts)
+}
+
+// getLatestGithubCommit gets the latest commit on the main branch of Evergreen.
+// The smoke test is implicitly assuming here that the commit the repotracker
+// picks up is the latest commit on the main branch of Evergreen.
 func getLatestGithubCommit() (string, error) {
 	client := utility.GetHTTPClient()
 	defer utility.PutHTTPClient(client)
@@ -98,142 +107,193 @@ func checkContainerTask(username, key string) error {
 	client := utility.GetHTTPClient()
 	defer utility.PutHTTPClient(client)
 
-	return checkForTask(client, agent.PodMode, []string{smokeContainerTaskID}, username, key)
+	return checkTaskStatusAndLogs(client, agent.PodMode, []string{smokeContainerTaskID}, username, key)
 }
 
 func checkHostTaskByCommit(username, key string) error {
 	client := utility.GetHTTPClient()
 	defer utility.PutHTTPClient(client)
 
-	var builds []apimodels.APIBuild
-	var build apimodels.APIBuild
+	// Triggering the repotracker causes the app server to pick up the latest
+	// available commits and create versions, builds, and tasks for that commit.
+	if err := triggerRepotracker(username, key, client); err != nil {
+		return errors.Wrap(err, "triggering repotracker to run")
+	}
 
-	// trigger repotracker to insert relevant builds and tasks from agent.yml definitions
-	for i := 0; i < 5; i++ {
-		if i == 5 {
-			return errors.Errorf("unable to trigger the repotracker after 5 attempts")
-		}
+	// Check that the builds should be eventually created after triggering the
+	// repotracker.
+	builds, err := getAndCheckBuilds(username, key, client)
+	if err != nil {
+		return errors.Wrap(err, "getting and checking builds")
+	}
+
+	// Check that the tasks eventually run after triggering the repotracker.
+	return checkTaskStatusAndLogs(client, agent.HostMode, builds[0].Tasks, username, key)
+}
+
+// triggerRepotracker makes a request to the Evergreen app server's REST API to
+// run the repotracker. Note that this returning success means that the
+// repotracker will run eventually. It does *not* guarantee that it has already
+// run, nor that it has actually managed to pick up the latest commits from
+// GitHub.
+func triggerRepotracker(username, key string, client *http.Client) error {
+	grip.Info("Attempting to trigger repotracker to run.")
+
+	const repotrackerAttempts = 5
+	for i := 0; i < repotrackerAttempts; i++ {
 		time.Sleep(2 * time.Second)
-		grip.Infof("running repotracker for evergreen project (%d/5)", i)
+		grip.Infof("Requesting repotracker for evergreen project. (%d/%d)", i+1, repotrackerAttempts)
 		_, err := makeSmokeRequest(username, key, http.MethodPost, client, "/rest/v2/projects/evergreen/repotracker")
 		if err != nil {
-			grip.Error(err)
+			grip.Error(errors.Wrap(err, "requesting repotracker to run"))
 			continue
 		}
-		break
+
+		grip.Info("Successfully triggered repotracker to run.")
+		return nil
 	}
-	for i := 0; i <= 30; i++ {
-		// get task id
-		if i == 30 {
-			return errors.New("ran out of attempts to get builds for version")
-		}
+
+	return errors.Errorf("could not successfully trigger repotracker after %d attempts", repotrackerAttempts)
+}
+
+// getAndCheckBuilds gets build information from the Evergreen app server's REST
+// API for the builds that it expects to be eventaully created. These checks are
+// assuming that, by triggering the repotracker to run, a version should
+// eventually be created for the latest commit to Evergreen.
+func getAndCheckBuilds(username, key string, client *http.Client) ([]apimodels.APIBuild, error) {
+	grip.Info("Attempting to get builds created by triggering the repotracker.")
+
+	const buildCheckAttempts = 30
+	for i := 0; i < buildCheckAttempts; i++ {
+		// Poll the app server until the builds exist and have tasks.
+		// This is implicitly assuming that the app server has proper
+		// integration with GitHub to pick up the latest commit and successfully
+		// creates versions, builds, and tasks based on that commit.
+
 		time.Sleep(10 * time.Second)
+
+		// The latest GitHub commit must match the one that the repotracker
+		// picks up.
 		latest, err := getLatestGithubCommit()
 		if err != nil {
 			grip.Error(errors.Wrap(err, "getting latest GitHub commit"))
 			continue
 		}
-		grip.Infof("checking for a build of %s (%d/30)", latest, i+1)
+		grip.Infof("Checking for a build of commit '%s'. (%d/%d)", latest, i+1, buildCheckAttempts)
 		body, err := makeSmokeRequest(username, key, http.MethodGet, client, "/rest/v2/versions/evergreen_"+latest+"/builds")
 		if err != nil {
-			grip.Error(err)
+			grip.Error(errors.Wrap(err, "requesting builds"))
 			continue
 		}
 
+		builds := []apimodels.APIBuild{}
 		err = json.Unmarshal(body, &builds)
 		if err != nil {
-			err = json.Unmarshal(body, &build)
-			if err != nil {
-				return errors.Wrap(err, "unmarshalling JSON response body into builds")
-			}
+			return nil, errors.Wrap(err, "unmarshalling JSON response body into builds")
 		}
-
 		if len(builds) == 0 {
-			builds = []apimodels.APIBuild{build}
-		}
-
-		if len(builds[0].Tasks) == 0 {
-			builds = []apimodels.APIBuild{}
-			build = apimodels.APIBuild{}
-
 			continue
 		}
-		break
+		if len(builds[0].Tasks) == 0 {
+			continue
+		}
+
+		grip.Infof("Successfully got %d builds for commit '%s' created by triggering the repotracker.", len(builds), latest)
+		return builds, nil
 	}
-	return checkForTask(client, agent.HostMode, builds[0].Tasks, username, key)
+
+	return nil, errors.Errorf("could not get builds after %d attempts - this might indicate that there was an issue in the repotracker that prevented the creation of builds", buildCheckAttempts)
 }
 
-func checkForTask(client *http.Client, mode agent.Mode, tasks []string, username, key string) error {
-	var taskStatus string
+// checkTaskStatusAndLogs checks that all the expected tasks are finished,
+// succeeded, and performed the expected operations based on the task log
+// contents.
+func checkTaskStatusAndLogs(client *http.Client, mode agent.Mode, tasks []string, username, key string) error {
+	grip.Infof("Checking task status and task logs for tasks: %s", strings.Join(tasks, ", "))
+	const taskCheckAttempts = 30
 OUTER:
-	for i := 0; i <= 30; i++ {
-		// check task
-		if i == 30 {
-			return errors.Errorf("task status is %s (expected %s)", taskStatus, evergreen.TaskSucceeded)
-		}
+	for i := 0; i < taskCheckAttempts; i++ {
+		// Poll the app server until the task is finished and check its task
+		// logs for the expected results.
+		// It's worth noting there that there is a substantial amount of
+		// heavy-lifting being done here by the Evergreen app server and agent
+		// under the covers. In the background, the app server must run the
+		// scheduler to create a task queue to run the new tasks, the agent must
+		// pick up those tasks from that task queue, and the agent must run
+		// those tasks and report the result back to the app server. If any of
+		// those operations go wrong (e.g. due to a modification to the app
+		// server, agent, or smoke configuration files), these checks can fail.
 		time.Sleep(10 * time.Second)
-		grip.Infof("checking for %d tasks (%d/30)", len(tasks), i+1)
+		grip.Infof("Checking %d tasks. (%d/%d)", len(tasks), i+1, taskCheckAttempts)
 
 		for _, taskId := range tasks {
-			task, err := checkTask(client, username, key, taskId)
+			task, err := getTaskInfo(client, username, key, taskId)
 			if err != nil {
 				return errors.WithStack(err)
 			}
 
-			if task.Status == evergreen.TaskFailed {
-				return errors.Errorf("task status is %s (expected %s)", task.Status, evergreen.TaskSucceeded)
-			}
-			if task.Status != evergreen.TaskSucceeded {
-				grip.Infof("found task is status %s", task.Status)
-				taskStatus = task.Status
+			if !evergreen.IsFinishedTaskStatus(task.Status) {
+				grip.Infof("Found task '%s' is not yet finished and has status '%s' (expected '%s').", taskId, task.Status, evergreen.TaskSucceeded)
 				continue OUTER
 			}
-			err = checkLog(task, client, mode, username, key)
-			if err != nil {
-				return err
+			if task.Status != evergreen.TaskSucceeded {
+				return errors.Errorf("finished task '%s' has non-successful status '%s' (expected '%s')", taskId, task.Status, evergreen.TaskSucceeded)
+			}
+			if err = getAndCheckTaskLog(task, client, mode, username, key); err != nil {
+				return errors.Wrapf(err, "getting and checking task log for task '%s'", taskId)
 			}
 		}
-		grip.Infof("Successfully checked %d %s tasks", len(tasks), string(mode))
+
+		grip.Infof("Successfully checked %d %s tasks and their task logs.", len(tasks), string(mode))
 		return nil
 	}
-	return errors.New("this code should be unreachable")
+
+	return errors.Errorf("task status and task log checks were incomplete after %d attempts - this might indicate an underlying issue with the app server, the agent, or the smoke test's configuration setup", taskCheckAttempts)
 }
 
-func checkLog(task apimodels.APITask, client *http.Client, mode agent.Mode, username, key string) error {
+// getAndCheckTaskLog gets the task logs from the task log URL and checks that
+// it has the expected content, indicating that the task executed the commands
+// properly.
+func getAndCheckTaskLog(task apimodels.APITask, client *http.Client, mode agent.Mode, username, key string) error {
 	// retry for *slightly* delayed logger closing
-	var err error
-	for i := 0; i < 3; i++ {
-		grip.Infof("checking for log %s (%d/3)", task.Logs["task_log"], i+1)
-		var body []byte
-		body, err = makeSmokeRequest(username, key, http.MethodGet, client, task.Logs["task_log"]+"&text=true")
+	const taskLogCheckAttempts = 3
+	for i := 0; i < taskLogCheckAttempts; i++ {
+		grip.Infof("Checking for task log from URL %s. (%d/%d)", task.Logs["task_log"], i+1, taskLogCheckAttempts)
+		body, err := makeSmokeRequest(username, key, http.MethodGet, client, task.Logs["task_log"]+"&text=true")
 		if err != nil {
-			err = errors.Wrap(err, "error getting log data")
-			grip.Debug(err)
+			grip.Error(errors.Wrap(err, "getting task log data"))
 			continue
 		}
-		if err = checkTaskLog(body, mode); err == nil {
-			break
+		if err := checkTaskLogContent(body, mode); err != nil {
+			grip.Error(errors.Wrap(err, "getting task log data"))
+			continue
 		}
+
+		return nil
 	}
-	return err
+
+	return errors.Errorf("task log check failed after %d attempts", taskLogCheckAttempts)
 }
 
-func checkTaskLog(body []byte, mode agent.Mode) error {
+// checkTaskLogContent compares the expected result of running the smoke test
+// project YAML (agent.yml) against the actual task log's text.
+func checkTaskLogContent(body []byte, mode agent.Mode) error {
 	page := string(body)
 
 	// Validate that task contains task completed message
 	if strings.Contains(page, "Task completed - SUCCESS") {
-		grip.Infof("Found task completed message in log:\n%s", page)
+		grip.Info("Found task completed message in task log")
 	} else {
-		grip.Errorf("did not find task completed message in log:\n%s", page)
-		return errors.New("did not find task completed message in log")
+		return errors.New("did not find task completed message in task logs")
 	}
 
+	// Note that these checks have a direct dependency on the task configuration
+	// in the smoke test's project YAML (agent.yml).
 	if mode == agent.HostMode {
 		// Validate that setup_group only runs in first task
 		if strings.Contains(page, "first") {
 			if !strings.Contains(page, "setup_group") {
-				return errors.New("did not find setup_group in logs for first task")
+				return errors.New("did not find setup_group in task logs for first task")
 			}
 		} else {
 			if strings.Contains(page, "setup_group") {
@@ -243,16 +303,16 @@ func checkTaskLog(body []byte, mode agent.Mode) error {
 
 		// Validate that setup_task and teardown_task run for all tasks
 		if !strings.Contains(page, "setup_task") {
-			return errors.New("did not find setup_task in logs")
+			return errors.New("did not find setup_task in task logs")
 		}
 		if !strings.Contains(page, "teardown_task") {
-			return errors.New("did not find teardown_task in logs")
+			return errors.New("did not find teardown_task in task logs")
 		}
 
 		// Validate that teardown_group only runs in last task
 		if strings.Contains(page, "fourth") {
 			if !strings.Contains(page, "teardown_group") {
-				return errors.New("did not find teardown_group in logs for last (fourth) task")
+				return errors.New("did not find teardown_group in task logs for last (fourth) task")
 			}
 		} else {
 			if strings.Contains(page, "teardown_group") {
@@ -260,7 +320,7 @@ func checkTaskLog(body []byte, mode agent.Mode) error {
 			}
 		}
 	} else if mode == agent.PodMode {
-		// TODO (PM-2617) Add task groups to the container task smoke test once they aree supported
+		// TODO (PM-2617) Add task groups to the container task smoke test once they are supported
 		if !strings.Contains(page, "container task") {
 			return errors.New("did not find container task in logs")
 		}
@@ -269,34 +329,34 @@ func checkTaskLog(body []byte, mode agent.Mode) error {
 	return nil
 }
 
-func checkTask(client *http.Client, username, key string, taskId string) (apimodels.APITask, error) {
+// getTaskInfo gets basic information about the current status and task logs for
+// the given task ID from the REST API.
+func getTaskInfo(client *http.Client, username, key string, taskId string) (apimodels.APITask, error) {
+	grip.Infof("Checking information for task '%s'.", taskId)
+
 	task := apimodels.APITask{}
-	grip.Infof("checking for task %s", taskId)
 	r, err := http.NewRequest(http.MethodGet, smokeUrlPrefix+smokeUiPort+"/rest/v2/tasks/"+taskId, nil)
 	if err != nil {
 		return task, errors.Wrap(err, "making request for task")
 	}
 	r.Header.Add(evergreen.APIUserHeader, username)
 	r.Header.Add(evergreen.APIKeyHeader, key)
+
 	resp, err := client.Do(r)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
 	if err != nil {
 		return task, errors.Wrap(err, "getting task data")
 	}
+	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		err = errors.Wrap(err, "reading response body")
-		grip.Error(err)
-		return task, err
+		return task, errors.Wrap(err, "reading response body")
 	}
 	if resp.StatusCode >= 400 {
 		return task, errors.Errorf("got HTTP response code %d with error %s", resp.StatusCode, string(body))
 	}
 
-	err = json.Unmarshal(body, &task)
-	if err != nil {
+	if err := json.Unmarshal(body, &task); err != nil {
 		return task, errors.Wrap(err, "unmarshalling JSON response body into task")
 	}
 
@@ -315,18 +375,14 @@ func makeSmokeRequest(username, key string, method string, client *http.Client, 
 	req.Header.Add(evergreen.APIUserHeader, username)
 	req.Header.Add(evergreen.APIKeyHeader, key)
 	resp, err := client.Do(req)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
 	if err != nil {
 		return nil, errors.Wrapf(err, "getting endpoint '%s'", url)
 	}
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		err = errors.Wrap(err, "reading response body")
-		grip.Error(err)
-		return nil, err
+		return nil, errors.Wrap(err, "reading response body")
 	}
 	if resp.StatusCode >= 400 {
 		return body, errors.Errorf("got HTTP response code %d with error %s", resp.StatusCode, string(body))
@@ -337,16 +393,14 @@ func makeSmokeRequest(username, key string, method string, client *http.Client, 
 
 func makeSmokeGetRequestAndCheck(username, key string, client *http.Client, url string, expected []string) error {
 	body, err := makeSmokeRequest(username, key, http.MethodGet, client, url)
-	grip.Error(err)
+	grip.Error(errors.Wrap(err, "making smoke request"))
 	page := string(body)
 	catcher := grip.NewSimpleCatcher()
 	for _, text := range expected {
 		if strings.Contains(page, text) {
-			grip.Infof("found '%s' in endpoint '%s'", text, url)
+			grip.Infof("Successfully found expected text '%s' from endpoint '%s'.", text, url)
 		} else {
-			logErr := errors.Errorf("did not find '%s' in endpoint '%s'", text, url)
-			grip.Error(logErr)
-			catcher.Add(logErr)
+			catcher.Errorf("did not find '%s' in endpoint '%s'", text, url)
 		}
 	}
 
