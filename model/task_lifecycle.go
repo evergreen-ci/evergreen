0f0b9dc7d45454bbc3a5e5e3c15d7687020379a4
malik.hadjri <malik.hadjri@mongodb.com>
2023-02-03 14:56:26 -0500

    Add log for dequeue/restart

--- model/task_lifecycle.go
+++ model/task_lifecycle.go
@@ -817,7 +817,13 @@ func DequeueAndRestartForTask(cq *commitqueue.CommitQueue, t *task.Task, githubS
 	if err := tryDequeueAndAbortCommitQueueVersion(p, *cq, t.Id, caller); err != nil {
 		return err
 	}
-
+	grip.Info(message.Fields{
+		"source":       "commit queue",
+		"version":      t.Version,
+		"project_id":   cq.ProjectID,
+		"queue_length": len(cq.Queue),
+		"message":      "commit queue item failed, dequeueing and restarting later versions",
+	})
 	err = SendCommitQueueResult(p, githubState, reason)
 	grip.Error(message.WrapError(err, message.Fields{
 		"message": "unable to send github status",
