216cff12f1c373a32ed164f7fbf7ddb617fe967e
Kim Tao <kim.tao@mongodb.com>
2023-02-21 13:50:48 -0500

    EVG-18820: clean up migrated agent REST routes for expansions/vars (#6275)

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ var (
 	ClientVersion = "2023-02-14"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-02-16"
+	AgentVersion = "2023-02-17"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
