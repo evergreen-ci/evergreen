1ff483897a10ee134940e44a64d9de9eae315141
Annie Black <annie.black@mongodb.com>
2023-03-14 20:27:26 -0400

    EVG-17595 remove gotest.parse_json (#6321)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-02-23"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-03-03"
+	AgentVersion = "2023-03-13"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
