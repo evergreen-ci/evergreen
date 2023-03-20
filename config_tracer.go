9d12a1a58c411d37fcbf7c2ffb98c3471cb27e1d
Jonathan Brill <jonathan.brill@10gen.com>
2023-03-20 13:00:09 -0400

    document config

--- config_tracer.go
+++ config_tracer.go
@@ -7,13 +7,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// TracerConfig configures the OpenTelemetry tracer provider. If not enabled traces will not be sent.
 type TracerConfig struct {
 	Enabled           bool   `yaml:"enabled" bson:"enabled" json:"enabled"`
 	CollectorEndpoint string `yaml:"collector_endpoint" bson:"collector_endpoint" json:"collector_endpoint"`
 }
 
+// SectionId returns the ID of this config section.
 func (c *TracerConfig) SectionId() string { return "tracer" }
 
+// Get populates the config from the database.
 func (c *TracerConfig) Get(env Environment) error {
 	ctx, cancel := env.Context()
 	defer cancel()
@@ -35,6 +38,7 @@ func (c *TracerConfig) Get(env Environment) error {
 	return nil
 }
 
+// Set sets the document in the database to match the in-memory config struct.
 func (c *TracerConfig) Set() error {
 	env := GetEnvironment()
 	ctx, cancel := env.Context()
@@ -51,6 +55,7 @@ func (c *TracerConfig) Set() error {
 	return errors.Wrapf(err, "updating config section '%s'", c.SectionId())
 }
 
+// ValidateAndDefault validates the tracer configuration.
 func (c *TracerConfig) ValidateAndDefault() error {
 	if c.Enabled && c.CollectorEndpoint == "" {
 		return errors.New("tracer can't be enabled without a collector endpoint")
