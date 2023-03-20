0ce69495a5ef41fa189488072ef85089978badbf
Jonathan Brill <jonathan.brill@10gen.com>
2023-03-20 10:39:07 -0400

    add settings to admin settings

--- testutil/config.go
+++ testutil/config.go
@@ -460,6 +460,10 @@ func MockConfig() *evergreen.Settings {
 			UnexpirableHostsPerUser:   2,
 			UnexpirableVolumesPerUser: 2,
 		},
+		Tracer: evergreen.TracerConfig{
+			Enabled:           true,
+			CollectorEndpoint: "localhost:4317",
+		},
 		ShutdownWaitSeconds: 15,
 	}
 }
