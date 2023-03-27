8604128c7f7116c160eece26314feb63ca4a3bd8
Kim Tao <kim.tao@mongodb.com>
2023-03-27 11:04:26 -0400

    EVG-18630: set default value for XDG_CACHE_HOME in agent commands (#6342)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-03-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-03-22"
+	AgentVersion = "2023-03-27"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
