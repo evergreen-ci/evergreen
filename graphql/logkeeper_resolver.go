da7f1eb68a795a6191e4ac7021238e0d27e0004c
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-04 13:34:53 -0400

    Fetch task for logkeeper build

---
+++ graphql/logkeeper_resolver.go
@@ -0,0 +1,28 @@
+package graphql
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/plank"
+)
+
+// Task is the resolver for the task field.
+func (r *logkeeperBuildResolver) Task(ctx context.Context, obj *plank.Build) (*model.APITask, error) {
+	task, err := getTask(ctx, obj.TaskID, &obj.TaskExecution, r.sc.GetURL())
+	if err != nil {
+		return nil, ResourceNotFound.Send(ctx, fmt.Sprintf("Finding task %s for buildId: %s: %s", obj.TaskID, obj.ID, err.Error()))
+	}
+	return task, nil
+}
+
+// LogkeeperBuild returns LogkeeperBuildResolver implementation.
+func (r *Resolver) LogkeeperBuild() LogkeeperBuildResolver { return &logkeeperBuildResolver{r} }
+
+type logkeeperBuildResolver struct{ *Resolver }
+
+type logkeeperTestResolver struct{ *Resolver }
