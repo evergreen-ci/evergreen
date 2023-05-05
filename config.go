4a82879c4c050e4cd7784e4789929faaefd988b5
malik.hadjri <malik.hadjri@mongodb.com>
2023-05-05 12:14:37 -0400

    Uptick

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-05-02"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-05-04"
+	AgentVersion = "2023-05-05"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
