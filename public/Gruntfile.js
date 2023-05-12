68013723f60ad829e587c653bc028c91d267e439
SupaJoon <arjun453@gmail.com>
2023-05-12 13:08:46 -0400

    EVG-19773: Repair js-test and stabilize Legacy Waterfall UI build script (#6489)

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -1,66 +1,66 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-  require('load-grunt-tasks')(grunt);
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        babel: {
-          options: {
-            sourceMap: true,
-            plugins: ['transform-react-jsx'],
-            presets: ['es2015', 'react'],
+    babel: {
+      options: {
+        sourceMap: true,
+        plugins: ["transform-react-jsx"],
+        presets: ["env"],
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: "static/app",
+            src: ["**/*.jsx"],
+            dest: "static/dist/js",
+            ext: ".js",
           },
-          dist: {
-            files: [{
-              expand: true,
-              cwd: 'static/app',
-              src: ['**/*.jsx'],
-              dest: 'static/dist/js',
-              ext: '.js',
-            }]
-          },
+        ],
+      },
+    },
+
+    watch: {
+      css: {
+        files: ["static/less/**"],
+        tasks: ["css"],
+      },
+      react: {
+        files: ["static/app/**/*.jsx"],
+        tasks: ["babel"],
+      },
+    },
+
+    less: {
+      main: {
+        options: {
+          paths: ["static/less"],
+          sourceMap: true,
+          sourceMapFilename: "static/dist/less.map",
+          sourceMapURL: "/static/dist/less.map",
+          sourceMapRootpath: "../../",
         },
-
-        watch: {
-          css: {
-            files: ['static/less/**'],
-            tasks: ['css']
-          },
-          react: {
-            files: ['static/app/**/*.jsx'],
-            tasks: ['babel']
-          }
+        files: {
+          "static/dist/css/styles.css": "static/less/main.less",
         },
-        
-        less: {
-            main: {
-                options: {
-                    paths: ['static/less'],
-                    sourceMap: true,
-                    sourceMapFilename: 'static/dist/less.map',
-                    sourceMapURL: '/static/dist/less.map',
-                    sourceMapRootpath: '../../'
-                },
-                files: {
-                    'static/dist/css/styles.css': 'static/less/main.less'
-                }
-            }
+      },
+    },
+
+    cssmin: {
+      combine: {
+        files: {
+          "static/dist/css/styles.min.css": "static/dist/css/styles.css",
         },
+      },
+    },
+  });
 
-        cssmin: {
-            combine: {
-                files: {
-                    'static/dist/css/styles.min.css': 'static/dist/css/styles.css'
-                }
-            }
-        }
-    });
+  grunt.loadNpmTasks("grunt-babel");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-react');
-
-    grunt.registerTask('css', ['less', 'cssmin']);
-    grunt.registerTask('default', ['css']);
+  grunt.registerTask("css", ["less", "cssmin"]);
+  grunt.registerTask("default", ["css"]);
 };
