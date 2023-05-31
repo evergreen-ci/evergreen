a7029ff95481b9ba11f1173f648de23bcbe83a5e
julianedwards <julian.edwards@10gen.com>
2023-05-31 14:44:09 -0400

    EVG-20013: new log model and interfaces (#6563)

---
+++ model/log/service_interface.go
@@ -0,0 +1,16 @@
+package log
+
+import (
+	"context"
+
+	"github.com/evergreen-ci/evergreen"
+	"github.com/pkg/errors"
+)
+
+type logService interface { //nolint:unused
+	GetTaskLogs(context.Context, TaskOptions, GetOptions) (LogIterator, error)
+}
+
+func getServiceImpl(env evergreen.Environment, serviceVersion int) (logService, error) { //nolint:unused
+	return nil, errors.New("not implemented")
+}
