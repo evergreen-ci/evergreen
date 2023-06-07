5f7ff6c5f3eda47b655e9e5329c662d581e5000a
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2023-06-07 12:08:50 -0400

    EVG-19915 Update err message for failure to apply commit queue patch (#6624)

--- config.go
+++ config.go
@@ -37,7 +37,7 @@ var (
 	ClientVersion = "2023-06-01"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-06-07"
+	AgentVersion = "2023-06-07a"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
