e27b81fdeda23cfdbacb1729f2a3cfa939f9ceeb
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2023-06-08 11:24:16 -0400

    EVG-19867 Allow host.create to use function vars (#6630)

--- rest/data/host_create.go
+++ rest/data/host_create.go
@@ -105,25 +105,25 @@ func CreateHostsFromTask(ctx context.Context, settings *evergreen.Settings, t *t
 	createHostCmds := []apimodels.CreateHost{}
 	catcher := grip.NewBasicCatcher()
 	for _, commandConf := range projectTask.Commands {
-		var createHost *apimodels.CreateHost
+		var cmds []model.PluginCommandConf
 		if commandConf.Function != "" {
-			cmds := proj.Functions[commandConf.Function]
-			for _, cmd := range cmds.List() {
-				createHost, err = createHostFromCommand(cmd)
-				if err != nil {
-					return err
-				}
-				if createHost == nil {
-					continue
-				}
-				createHostCmds = append(createHostCmds, *createHost)
-			}
+			cmds = proj.Functions[commandConf.Function].List()
 		} else {
-			createHost, err = createHostFromCommand(commandConf)
+			cmds = []model.PluginCommandConf{commandConf}
+		}
+		for _, cmd := range cmds {
+			createHost, err := createHostFromCommand(cmd)
 			if err != nil {
 				return err
 			}
 			if createHost == nil {
+				continue
+			}
+			cmdExpansions := util.NewExpansions(commandConf.Vars)
+			cmdExpansions.Update(expansions.Map())
+			err = createHost.Expand(cmdExpansions)
+			if err != nil {
+				catcher.Wrap(err, "handling expansions")
 				continue
 			}
 			createHostCmds = append(createHostCmds, *createHost)
@@ -135,11 +135,6 @@ func CreateHostsFromTask(ctx context.Context, settings *evergreen.Settings, t *t
 
 	hosts := []host.Host{}
 	for _, createHost := range createHostCmds {
-		err = createHost.Expand(expansions)
-		if err != nil {
-			catcher.Wrap(err, "handling expansions")
-			continue
-		}
 		err = createHost.Validate()
 		if err != nil {
 			catcher.Add(err)
