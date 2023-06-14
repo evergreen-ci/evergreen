7a6215257f45b8a9878ee968720ca0e37847d192
malik.hadjri <malik.hadjri@mongodb.com>
2023-06-14 15:05:05 -0400

    Revert "EVG-20161 Only request mergeable sha for merge-patch task"
    This reverts commit 56b08309a2024a56b69202a12d28640a5617e5b1.

--- agent/command/git.go
+++ agent/command/git.go
@@ -305,15 +305,11 @@ func (c *gitFetchProject) buildCloneCommand(ctx context.Context, comm client.Com
 		if conf.Task.Requester == evergreen.MergeTestRequester {
 			// Proceed if github has confirmed this pr is mergeable. If it hasn't checked, this request
 			// will make it check.
-			if conf.Task.DisplayName == evergreen.MergeTaskName {
-				commitToTest, err = c.waitForMergeableCheck(ctx, comm, logger, conf, opts)
-				if err != nil {
-					commitToTest = conf.GithubPatchData.HeadHash
-					logger.Task().Errorf("Error checking if pull request is mergeable: %s", err)
-					logger.Task().Warningf("Because errors were encountered trying to retrieve the pull request, we will use the last recorded hash to test (%s).", commitToTest)
-				}
-			} else {
+			commitToTest, err = c.waitForMergeableCheck(ctx, comm, logger, conf, opts)
+			if err != nil {
 				commitToTest = conf.GithubPatchData.HeadHash
+				logger.Task().Errorf("Error checking if pull request is mergeable: %s", err)
+				logger.Task().Warningf("Because errors were encountered trying to retrieve the pull request, we will use the last recorded hash to test (%s).", commitToTest)
 			}
 			ref = "merge"
 			branchName = fmt.Sprintf("evg-merge-test-%s", utility.RandomString())
--- config.go
+++ config.go
@@ -37,7 +37,7 @@ var (
 	ClientVersion = "2023-06-02"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-06-14"
+	AgentVersion = "2023-06-07a"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
