1efae082512dfa77605dbcb33aa00aed7a215787
bynn <bynn.lee@mongodb.com>
2023-06-21 13:27:55 -0400

    remove debug

--- config.go
+++ config.go
@@ -611,7 +611,7 @@ func (s *Settings) CreateInstallationToken(ctx context.Context, owner, repo stri
 func (s *Settings) CreateInstallationTokenWithDefaultOwnerRepo(ctx context.Context, opts *github.InstallationTokenOptions) (string, error) {
 	if s.AuthConfig.Github == nil || s.AuthConfig.Github.DefaultOwner == "" || s.AuthConfig.Github.DefaultRepo == "" {
 		// TODO EVG-19966: Return error here
-		return "", errors.New(fmt.Sprintf("default for '%s/%s' not found", s.AuthConfig.Github.DefaultOwner, s.AuthConfig.Github.DefaultRepo))
+		return "", nil
 	}
 	return s.CreateInstallationToken(ctx, s.AuthConfig.Github.DefaultOwner, s.AuthConfig.Github.DefaultRepo, opts)
 }
