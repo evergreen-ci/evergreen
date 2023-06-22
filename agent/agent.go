fe55d8a0dd565f8826c23e4b704482cfdbb5fa00
Kim Tao <kim.tao@mongodb.com>
2023-06-22 14:53:19 -0400

    Revert "Revert "EVG-20136: propagate panic from runTask to caller (#6669)""
    This reverts commit c3b9283abfe7c317bfab9bd822bb1f40acb277e3.

--- agent/agent.go
+++ agent/agent.go
@@ -507,16 +507,20 @@ func (a *Agent) startLogging(ctx context.Context, tc *taskContext) error {
 	return nil
 }
 
-// runTask returns true if the agent should exit, and separate an error if relevant
-func (a *Agent) runTask(ctx context.Context, tc *taskContext) (bool, error) {
+// runTask runs a task. It returns true if the agent should exit.
+func (a *Agent) runTask(ctx context.Context, tc *taskContext) (shouldExit bool, err error) {
 	// we want to have separate context trees for tasks and loggers, so
 	// when a task is canceled by a context, it can log its clean up.
 	tskCtx, tskCancel := context.WithCancel(ctx)
 	defer tskCancel()
 
-	var err error
 	defer func() {
-		err = recovery.HandlePanicWithError(recover(), err, "running task")
+		op := "running task"
+		pErr := recovery.HandlePanicWithError(recover(), nil, op)
+		if pErr == nil {
+			return
+		}
+		err = a.logPanic(tc.logger, pErr, err, op)
 	}()
 
 	// If the heartbeat aborts the task immediately, we should report that
@@ -925,4 +929,32 @@ func (a *Agent) shouldKill(tc *taskContext, ignoreTaskGroupCheck bool) bool {
 	}
 	// return true otherwise
 	return true
+}
+
+// logPanic logs and returns a panic error, along with the original error (if
+// any). If there was no panic error, this is a no-op.
+func (a *Agent) logPanic(logger client.LoggerProducer, pErr, originalErr error, op string) error {
+	if pErr == nil {
+		return nil
+	}
+
+	msg := message.Fields{
+		"message":   "programmatic error: agent panicked",
+		"operation": op,
+		"stack":     message.NewStack(2, "").Raw(),
+	}
+
+	catcher := grip.NewBasicCatcher()
+	catcher.Add(originalErr)
+	catcher.Add(pErr)
+	grip.Alert(message.WrapError(catcher.Resolve(), msg))
+	if logger != nil && !logger.Closed() {
+		logMsg := message.Fields{
+			"message":   "programmatic error: Evergreen agent hit a runtime panic",
+			"operation": op,
+		}
+		logger.Execution().Error(logMsg)
+	}
+
+	return catcher.Resolve()
 }
