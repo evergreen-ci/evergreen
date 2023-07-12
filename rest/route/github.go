aac42fc8c107cfeebc3244bcb1a18539e35a63d6
Brian Samek <brian.samek@mongodb.com>
2023-07-12 11:41:11 -0400

    Toggle merge queue with project ref

--- rest/route/github.go
+++ rest/route/github.go
@@ -39,9 +39,6 @@ const (
 	resetDefinitionsComment = "evergreen reset-definitions"
 
 	refTags = "refs/tags/"
-
-	// This will be removed when EVG-19964 is ready.
-	disableMergeGroup = true
 )
 
 // skipCILabels are a set of labels which will skip creating PR patch if part of the commit description or message.
@@ -230,37 +227,60 @@ func (gh *githubHookApi) Run(ctx context.Context) gimlet.Responder {
 		}
 
 	case *github.MergeGroupEvent:
-		var msg string
-		if disableMergeGroup {
-			msg = "merge group received, skipping"
-		} else {
-			msg = "merge group received, attempting to queue"
-		}
+		org := event.GetOrg().GetLogin()
+		repo := event.GetRepo().GetName()
+		branch := strings.TrimPrefix(event.MergeGroup.GetBaseRef(), "refs/heads")
 		grip.Info(message.Fields{
 			"source":   "GitHub hook",
 			"msg_id":   gh.msgID,
 			"event":    gh.eventType,
-			"org":      event.GetOrg().GetLogin(),
-			"repo":     event.GetRepo().GetName(),
+			"org":      org,
+			"repo":     repo,
 			"base_sha": event.GetMergeGroup().GetBaseSHA(),
 			"head_sha": event.GetMergeGroup().GetHeadSHA(),
-			"message":  msg,
+			"message":  "merge group received",
 		})
-
-		// This will be removed when EVG-19964 is ready.
-		if disableMergeGroup {
-			return gimlet.NewJSONResponse(struct{}{})
-		}
-		if err := gh.AddIntentForGithubMerge(event); err != nil {
+		ref, err := model.FindOneProjectRefWithCommitQueueByOwnerRepoAndBranch(org, repo, branch)
+		if err != nil {
 			grip.Error(message.WrapError(err, message.Fields{
 				"source":   "GitHub hook",
 				"msg_id":   gh.msgID,
 				"event":    gh.eventType,
-				"org":      event.GetOrg().GetLogin(),
-				"repo":     event.GetRepo().GetName(),
+				"org":      org,
+				"repo":     repo,
 				"base_sha": event.GetMergeGroup().GetBaseSHA(),
 				"head_sha": event.GetMergeGroup().GetHeadSHA(),
-				"message":  "can't add intent",
+				"message":  "finding project ref",
+			}))
+			return gimlet.NewJSONInternalErrorResponse(errors.Wrap(err, "finding project ref"))
+		}
+		if ref == nil {
+			grip.Error(message.Fields{
+				"source":   "GitHub hook",
+				"msg_id":   gh.msgID,
+				"event":    gh.eventType,
+				"org":      org,
+				"repo":     repo,
+				"base_sha": event.GetMergeGroup().GetBaseSHA(),
+				"head_sha": event.GetMergeGroup().GetHeadSHA(),
+				"message":  "no matching project ref",
+			})
+		}
+		if ref.CommitQueue.MergeQueue == model.MergeQueueGitHub {
+			err = gh.AddIntentForGithubMerge(event)
+		} else {
+			return gimlet.NewJSONResponse(struct{}{})
+		}
+		if err != nil {
+			grip.Error(message.WrapError(err, message.Fields{
+				"source":   "GitHub hook",
+				"msg_id":   gh.msgID,
+				"event":    gh.eventType,
+				"org":      org,
+				"repo":     repo,
+				"base_sha": event.GetMergeGroup().GetBaseSHA(),
+				"head_sha": event.GetMergeGroup().GetHeadSHA(),
+				"message":  "adding project intent",
 			}))
 			return gimlet.NewJSONInternalErrorResponse(errors.Wrap(err, "adding patch intent"))
 		}
