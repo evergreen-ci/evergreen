8f3ac0790e935670d4966bcf9cabfe923ebd6ea7
Chaya Malik <chaya.malik@10gen.com>
2023-07-17 07:46:30 -0700

    EVG-20311 Remove agent loop labels (#6781)

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 	ClientVersion = "2023-07-06"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-07-13"
+	AgentVersion = "2023-07-14"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
