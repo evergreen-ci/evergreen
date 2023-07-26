76d51c6cfed097580d143d2e3e9a8750489fca20
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-26 18:42:57 -0400

    Maintain list of mongodb error constants

--- db/errors.go
+++ db/errors.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/evergreen-ci/evergreen/db/mgo"
 	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func IsDuplicateKey(err error) bool {
@@ -29,3 +30,15 @@ func IsDocumentLimit(err error) bool {
 	}
 	return strings.Contains(errors.Cause(err).Error(), "an inserted document is too large")
 }
+
+func IsErrorCode(err error, errorCode int) bool {
+	var mongoError mongo.CommandError
+	if errors.As(err, &mongoError) && mongoError.HasErrorCode(errorCode) {
+		return true
+	}
+	return false
+}
+
+// ErrorCode constants for mongo errors
+
+const FACET_PIPELINE_STAGE_TOO_LARGE_CODE = 4031700 // https://github.com/mongodb/mongo/blob/a1732172ed5d66d98582ea1059c0ede9d8cd5065/src/mongo/db/pipeline/document_source_facet.cpp#L165
