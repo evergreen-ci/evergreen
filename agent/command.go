61119bd7d45a2dbf71f30c1d873d1d04a1569fea
Kim Tao <kim.tao@mongodb.com>
2023-07-28 11:31:26 -0400

    EVG-20283: let commands gracefully shut down (#6826)

--- agent/command.go
+++ agent/command.go
@@ -162,10 +162,13 @@ func (a *Agent) runCommand(ctx context.Context, tc *taskContext, logger client.L
 	a.comm.UpdateLastMessageTime()
 
 	start := time.Now()
-	// We have seen cases where calling exec.*Cmd.Wait() waits for too long if
-	// the process has called subprocesses. It will wait until a subprocess
-	// finishes, instead of returning immediately when the context is canceled.
-	// We therefore check both if the context is cancelled and if Wait() has finished.
+	// This method must return soon after the context errors (e.g. due to
+	// aborting the task). Even though commands ought to respect the context and
+	// finish up quickly when the context errors, we cannot guarantee that every
+	// command implementation will respect the context or will finish in a
+	// timely manner. Therefore, just in case the command hangs or is slow, run
+	// the command in a goroutine so it not stop the task from making forward
+	// progress.
 	cmdChan := make(chan error, 1)
 	go func() {
 		defer func() {
@@ -186,6 +189,16 @@ func (a *Agent) runCommand(ctx context.Context, tc *taskContext, logger client.L
 			}
 		}
 	case <-ctx.Done():
+		// Make a best-effort attempt to wait for the command to gracefully shut
+		// down. Either the command will respect the context and return, or this
+		// will time out waiting for the command.
+		timer := time.NewTimer(5 * time.Second)
+		defer timer.Stop()
+		select {
+		case <-timer.C:
+		case <-cmdChan:
+		}
+
 		if ctx.Err() == context.DeadlineExceeded {
 			tc.logger.Task().Errorf("Command %s stopped early because idle timeout duration of %d seconds has been reached.", displayName, int(tc.timeout.idleTimeoutDuration.Seconds()))
 		} else {
