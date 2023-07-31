9af840e65325df2081d0ddcb51c2cfa4c3d205d4
annie.black <annie.black@mongodb.com>
2023-07-31 11:07:15 -0400

    agent version

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 	ClientVersion = "2023-07-27"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-07-26"
+	AgentVersion = "2023-07-31"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
