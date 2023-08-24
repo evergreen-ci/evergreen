2f0f477845e10677c88726956e0f9be04941b2ba
Chaya Malik <chaya.malik@mongodb.com>
2023-08-24 11:08:56 -0400

    cr

--- units/host_termination.go
+++ units/host_termination.go
@@ -108,7 +108,6 @@ func (j *hostTerminationJob) Run(ctx context.Context) {
 			"status":   j.host.Status,
 			"provider": j.host.Distro.Provider,
 			"message":  "host termination for a non-spawnable distro",
-			"cause":    "programmer error",
 		})
 		if err := j.host.Terminate(ctx, evergreen.User, j.TerminationReason); err != nil {
 			j.AddError(errors.Wrapf(err, "terminating intent host '%s' in DB", j.host.Id))
