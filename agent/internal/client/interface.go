1bf97ee5945d6e1532926a90d671512970b3153d
Chaya Malik <chaya.malik@10gen.com>
2023-08-25 08:36:25 -0700

    EVG-20287 Remove TaskConflict  (#6952)

--- agent/internal/client/interface.go
+++ agent/internal/client/interface.go
@@ -58,9 +58,8 @@ type SharedCommunicator interface {
 	// GetProject loads the project using the task's version ID.
 	GetProject(context.Context, TaskData) (*model.Project, error)
 	// Heartbeat will return a non-empty task status if the agent should stop running the task.
-	// Returning evergreen.TaskConflict means the agent is no longer authorized to run this task and
-	// should move on to the next available one. Returning evergreen.TaskFailed means that the task
-	// has been aborted. An empty string indicates the heartbeat has succeeded.
+	// Returning evergreen.TaskFailed means that the task has been aborted. An empty string
+	// indicates the heartbeat has succeeded.
 	Heartbeat(context.Context, TaskData) (string, error)
 	// GetExpansionsAndVars returns the expansions, project variables, and
 	// version parameters. For expansions, all expansions are loaded except for
--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 	ClientVersion = "2023-08-23"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-08-25"
+	AgentVersion = "2023-08-26"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
