57971ccced4bcc1588c4c69b0f77bc2f74ca9d60
annie.black <annie.black@mongodb.com>
2023-09-07 16:22:10 -0400

    compile

--- model/task_lifecycle.go
+++ model/task_lifecycle.go
@@ -1534,10 +1534,7 @@ func updateVersionStatus(v *Version) (string, error) {
 
 // UpdatePatchStatus updates the status of a patch.
 func UpdatePatchStatus(p *patch.Patch, versionStatus string) error {
-	patchStatus, err := evergreen.VersionStatusToPatchStatus(versionStatus)
-	if err != nil {
-		return errors.Wrapf(err, "getting patch status from version status '%s'", versionStatus)
-	}
+	patchStatus := evergreen.VersionStatusToPatchStatus(versionStatus)
 
 	if patchStatus == p.Status {
 		return nil
@@ -1546,10 +1543,10 @@ func UpdatePatchStatus(p *patch.Patch, versionStatus string) error {
 	event.LogPatchStateChangeEvent(p.Version, patchStatus)
 
 	if evergreen.IsFinishedVersionStatus(patchStatus) {
-		if err = p.MarkFinished(patchStatus, time.Now()); err != nil {
+		if err := p.MarkFinished(patchStatus, time.Now()); err != nil {
 			return errors.Wrapf(err, "marking patch '%s' as finished with status '%s'", p.Id.Hex(), patchStatus)
 		}
-	} else if err = p.UpdateStatus(patchStatus); err != nil {
+	} else if err := p.UpdateStatus(patchStatus); err != nil {
 		return errors.Wrapf(err, "updating patch '%s' with status '%s'", p.Id.Hex(), patchStatus)
 	}
 
@@ -1633,10 +1630,7 @@ func UpdateBuildAndVersionStatusForTask(ctx context.Context, t *task.Task) error
 			if err != nil {
 				return errors.Wrapf(err, "getting collective status for patch '%s'", p.Id.Hex())
 			}
-			versionStatus, err := evergreen.PatchStatusToVersionStatus(collectiveStatus)
-			if err != nil {
-				return errors.Wrapf(err, "getting version status")
-			}
+			versionStatus := evergreen.PatchStatusToVersionStatus(collectiveStatus)
 			if parentPatch != nil {
 				event.LogVersionChildrenCompletionEvent(parentPatch.Id.Hex(), versionStatus, parentPatch.Author)
 			} else {
