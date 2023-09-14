81c938bd721b54949b6adc0cde8a6ae58344ae3a
julianedwards <julian.edwards@mongodb.com>
2023-09-14 13:53:25 -0400

    new task output type

--- taskoutput/task_output.go
+++ taskoutput/task_output.go
@@ -1,32 +1,19 @@
 package taskoutput
 
-import (
-	"github.com/evergreen-ci/evergreen/taskoutput/tasklogs"
-	"github.com/evergreen-ci/evergreen/taskoutput/testlogs"
-)
-
 // TaskOutput is the versioned entry point for coordinating persistent storage
-// of a task run's output data. The int value represents the centralized
-// version of the task output, which then maps to the underlying versions of
-// each recognized output type collected during a task run.
-type TaskOutput int
-
-// TaskLogs returns the versioned entry point of the task logs output type.
-func (o TaskOutput) TaskLogs() tasklogs.TaskLogs {
-	switch {
-	case o >= 0:
-		return tasklogs.TaskLogs(0)
-	default:
-		return tasklogs.TaskLogs(-1)
-	}
+// of a task run's output data.
+type TaskOutput struct {
+	TaskLogs TaskLogOutput
+	TestLogs TestLogOutput
 }
 
-// TestLogs returns the versioned entry point of the test logs output type.
-func (o TaskOutput) TestLogs() testlogs.TestLogs {
-	switch {
-	case o >= 0:
-		return testlogs.TestLogs(0)
-	default:
-		return testlogs.TestLogs(-1)
-	}
+// TaskOptions represents the task-level information required for accessing
+// task logs belonging to a task run.
+type TaskOptions struct {
+	// ProjectID is the project ID of the task run.
+	ProjectID string
+	// TaskID is the task ID of the task run.
+	TaskID string
+	// Execution is the execution number of the task run.
+	Execution int
 }
