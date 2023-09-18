b640f8754847be9ced7953c8dd4e0a1a8a479145
malik.hadjri <malik.hadjri@mongodb.com>
2023-09-17 23:20:44 -0400

    Use mock for job test

--- units/provisioning_create_host_test.go
+++ units/provisioning_create_host_test.go
@@ -7,13 +7,11 @@ import (
 
 	"github.com/evergreen-ci/birch"
 	"github.com/evergreen-ci/evergreen"
-	"github.com/evergreen-ci/evergreen/apimodels"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/evergreen-ci/evergreen/mock"
 	"github.com/evergreen-ci/evergreen/model/distro"
 	"github.com/evergreen-ci/evergreen/model/host"
 	"github.com/evergreen-ci/evergreen/testutil"
-	jmock "github.com/mongodb/jasper/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -23,8 +21,8 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 	defer cancel()
 	ctx = testutil.TestSpan(ctx, t)
 
-	for testName, testCase := range map[string]func(ctx context.Context, t *testing.T, env *mock.Environment, mgr *jmock.Manager, h *host.Host){
-		"PopulatesFields": func(ctx context.Context, t *testing.T, env *mock.Environment, mgr *jmock.Manager, h *host.Host) {
+	for testName, testCase := range map[string]func(ctx context.Context, t *testing.T, env *mock.Environment, h *host.Host){
+		"PopulatesFields": func(ctx context.Context, t *testing.T, env *mock.Environment, h *host.Host) {
 			j := NewHostCreateJob(env, *h, "job-id", 0, true)
 			hostCreateJob, ok := j.(*createHostJob)
 			require.True(t, ok)
@@ -33,26 +31,8 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 			assert.Equal(t, h.Id, hostCreateJob.HostID)
 			assert.Equal(t, *h, *hostCreateJob.host)
 		},
-		"SucceedsForAMICreateHost": func(ctx context.Context, t *testing.T, env *mock.Environment, mgr *jmock.Manager, h *host.Host) {
+		"SucceedsForAMICreateHost": func(ctx context.Context, t *testing.T, env *mock.Environment, h *host.Host) {
 			require.NoError(t, h.Insert(ctx))
-
-			c := apimodels.CreateHost{
-				AMI:                 "ami-654321",
-				CloudProvider:       "ec2",
-				NumHosts:            "1",
-				Scope:               "task",
-				SetupTimeoutSecs:    600,
-				TeardownTimeoutSecs: 21600,
-				AWSKeyID:            "my_aws_key",
-				AWSSecret:           "my_secret_key",
-				InstanceType:        "t1.micro",
-				Subnet:              "subnet-123456",
-				SecurityGroups:      []string{"1234"},
-			}
-			d, err := distro.GetHostCreateDistro(ctx, c)
-			require.NoError(t, err)
-			h.Distro = *d
-
 			j := NewHostCreateJob(env, *h, "job-id", 0, true)
 			hostCreateJob, ok := j.(*createHostJob)
 			require.True(t, ok)
@@ -60,34 +40,9 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 			assert.False(t, hostCreateJob.HasErrors())
 			foundHost, err := host.FindOneId(ctx, h.Id)
 			require.NoError(t, err)
-			assert.Equal(t, evergreen.HostBuilding, foundHost.Status)
+			assert.Equal(t, evergreen.HostStarting, foundHost.Status)
 		},
-		"SucceedsForDistroCreateHost": func(ctx context.Context, t *testing.T, env *mock.Environment, mgr *jmock.Manager, h *host.Host) {
-			require.NoError(t, h.Insert(ctx))
-
-			c := apimodels.CreateHost{
-				Distro:              "distro-id",
-				CloudProvider:       "ec2",
-				NumHosts:            "1",
-				Scope:               "task",
-				SetupTimeoutSecs:    600,
-				TeardownTimeoutSecs: 21600,
-				KeyName:             "mock_key",
-			}
-			d, err := distro.GetHostCreateDistro(ctx, c)
-			require.NoError(t, err)
-			h.Distro = *d
-
-			j := NewHostCreateJob(env, *h, "job-id", 0, true)
-			hostCreateJob, ok := j.(*createHostJob)
-			require.True(t, ok)
-			hostCreateJob.Run(ctx)
-			assert.False(t, hostCreateJob.HasErrors())
-			foundHost, err := host.FindOneId(ctx, h.Id)
-			require.NoError(t, err)
-			assert.Equal(t, evergreen.HostBuilding, foundHost.Status)
-		},
-		"NoopsForTerminatedHost": func(ctx context.Context, t *testing.T, env *mock.Environment, mgr *jmock.Manager, h *host.Host) {
+		"NoopsForTerminatedHost": func(ctx context.Context, t *testing.T, env *mock.Environment, h *host.Host) {
 			h.Status = evergreen.HostTerminated
 			require.NoError(t, h.Insert(ctx))
 
@@ -110,7 +65,6 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 
 			env := &mock.Environment{}
 			require.NoError(t, env.Configure(tctx))
-			mgr := &jmock.Manager{}
 
 			h := host.Host{
 				Id:     "id",
@@ -125,11 +79,11 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 				},
 				Host:     "localhost",
 				User:     evergreen.User,
-				ParentID: "parent-id",
+				UserHost: true,
 			}
 			d := distro.Distro{
 				Id:       "distro-id",
-				Provider: evergreen.ProviderNameEc2OnDemand,
+				Provider: evergreen.ProviderNameMock,
 				ProviderSettingsList: []*birch.Document{birch.NewDocument(
 					birch.EC.String("region", evergreen.DefaultEC2Region),
 					birch.EC.String("aws_access_key_id", "key"),
@@ -137,13 +91,8 @@ func TestProvisioningCreateHostJob(t *testing.T) {
 				)},
 			}
 			require.NoError(t, d.Insert(tctx))
-			parentHost := host.Host{
-				Id:            "parent-id",
-				HasContainers: true,
-			}
-			require.NoError(t, parentHost.Insert(tctx))
-
-			testCase(tctx, t, env, mgr, &h)
+			h.Distro = d
+			testCase(tctx, t, env, &h)
 		})
 	}
 }
