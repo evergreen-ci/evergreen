401e8ad577adc84d7cc9920cf5b7f26e0707bc97
ZackarySantana <zackzackyack@gmail.com>
2023-09-20 15:58:42 -0400

    Remove update retry info (not introduced in this PR)

--- units/spawnhost_stop.go
+++ units/spawnhost_stop.go
@@ -3,12 +3,10 @@ package units
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/evergreen-ci/evergreen/cloud"
 	"github.com/evergreen-ci/evergreen/model/event"
 	"github.com/evergreen-ci/evergreen/model/host"
-	"github.com/evergreen-ci/utility"
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/job"
 	"github.com/mongodb/amboy/registry"
@@ -53,11 +51,6 @@ func NewSpawnhostStopJob(h *host.Host, user, ts string) amboy.Job {
 	j.SetEnqueueAllScopes(true)
 	j.CloudHostModification.HostID = h.Id
 	j.CloudHostModification.UserID = user
-	j.UpdateRetryInfo(amboy.JobRetryOptions{
-		Retryable:   utility.TruePtr(),
-		MaxAttempts: utility.ToIntPtr(stopHostRetryLimit),
-		WaitUntil:   utility.ToTimeDurationPtr(30 * time.Second),
-	})
 	return j
 }
 
