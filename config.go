153d1aadad3c44b37fc3a9efcb6267dacdae6034
annie.black <annie.black@mongodb.com>
2023-09-26 16:22:48 -0400

    Revert "Revert "EVG-20317 Stop on error if module cloning fails (#7076)" (#7078)"
    This reverts commit 2dd969a8c597ba2ce401c673bafdf3e112e38078.

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 	ClientVersion = "2023-09-06"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-09-19"
+	AgentVersion = "2023-09-26"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
