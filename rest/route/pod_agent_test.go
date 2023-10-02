c5359780ac46adca324d0ed76708287f473684b1
ZackarySantana <zackzackyack@gmail.com>
2023-10-02 12:22:19 -0400

    Fix RunSuccessfullyFinishesTask

--- rest/route/pod_agent_test.go
+++ rest/route/pod_agent_test.go
@@ -472,6 +472,10 @@ func TestPodAgentEndTask(t *testing.T) {
 				Identifier: "identifier",
 			}
 			require.NoError(t, projectToInsert.Insert())
+			parserProjectToInsert := model.ParserProject{
+				Id: projectToInsert.Id,
+			}
+			require.NoError(t, parserProjectToInsert.Insert())
 			rh.podID = podID
 			rh.taskID = taskID
 			rh.details = *td
