4af8c8dda43a105cc66b4cfad9772491bcaa318c
julianedwards <julian.edwards@mongodb.com>
2023-10-02 14:28:49 -0400

    cr

--- apimodels/cedar_models.go
+++ apimodels/cedar_models.go
@@ -57,7 +57,7 @@ func GetBuildloggerLogsV2(ctx context.Context, opts GetBuildloggerLogsOptionsV2)
 		start = time.Unix(0, opts.Start).UTC()
 	}
 	if opts.End > 0 {
-		end = time.Unix(0, opts.Start).UTC()
+		end = time.Unix(0, opts.End).UTC()
 	}
 
 	getOpts := buildlogger.GetOptions{
@@ -111,6 +111,14 @@ func (it *buildloggerIterator) Next() bool {
 	if err != nil {
 		it.exhausted = err == io.EOF
 		it.catcher.AddWhen(err != io.EOF, errors.Wrap(err, "reading log lines"))
+		return false
+	}
+
+	// Each log line is expected to have the format:
+	//     [P:3%d] [2006/01/02 15:04:05.000] %s
+	// Fail if we cannot parse the first 34 characters to avoid panicking.
+	if len(line) < 34 {
+		it.catcher.Errorf("malformed line '%s'", line)
 		return false
 	}
 
