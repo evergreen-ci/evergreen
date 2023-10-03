aa0e43e97cb364567e7ddc755948f67a0c974b04
ZackarySantana <zackzackyack@gmail.com>
2023-10-03 13:23:37 -0400

    Getting a pointer to a pointer rather than dereferncing the pointer...

--- model/project_ref.go
+++ model/project_ref.go
@@ -2025,7 +2025,7 @@ func SaveProjectPageForSection(projectId string, p *ProjectRef, section ProjectP
 		if err != nil {
 			return false, errors.Wrapf(err, "error getting project")
 		}
-		if &p.PerfEnabled != &pRef.PerfEnabled && pRef.Identifier != pRef.Id {
+		if *p.PerfEnabled != *pRef.PerfEnabled && pRef.Identifier != pRef.Id {
 			return false, errors.New("project id and identifier are different when setting performance plugin")
 		}
 		err = db.Update(coll,
