41f813cc07566fc955c3be4f0f0db57b1f432d2b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-10-04 18:17:41 -0400

    Add tests

--- rest/model/artifact_test.go
+++ rest/model/artifact_test.go
@@ -3,7 +3,9 @@ package model
 import (
 	"testing"
 
+	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/model/artifact"
+	"github.com/evergreen-ci/utility"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,4 +32,25 @@ func TestArtifactModels(t *testing.T) {
 
 	origEntry := apiEntry.ToService()
 	assert.EqualValues(entry, origEntry)
+}
+
+func TestAPIFileBuildFromService(t *testing.T) {
+	assert := assert.New(t)
+	file := artifact.File{
+		Name:           "file1",
+		Link:           "l1",
+		Visibility:     "public",
+		IgnoreForFetch: true,
+		ContentType:    "text/plain",
+	}
+	apiFile := APIFile{}
+	apiFile.BuildFromService(file)
+
+	origFile := apiFile.ToService()
+	assert.EqualValues(file, origFile)
+
+	env := evergreen.GetEnvironment()
+	apiFile.GetLogURL(env, "t1", 1)
+	assert.Equal(utility.FromStringPtr(apiFile.URLParsley), "https://localhost:4173/taskFile/t1/1/file1")
+
 }
