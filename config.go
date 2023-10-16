3a520b8d267092c6bbebcf0a9dc9b9ef587e3e4c
Bynn Lee <bynn.lee@mongodb.com>
2023-10-16 14:14:17 -0400

    EVG-20916 Remove Unused GitHub hook code (#7113)

--- config.go
+++ config.go
@@ -10,11 +10,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/evergreen-ci/evergreen/util"
 	"github.com/evergreen-ci/utility"
-	"github.com/golang-jwt/jwt"
-	"github.com/google/go-github/v52/github"
 	"github.com/mongodb/amboy/logger"
 	"github.com/mongodb/anser/bsonutil"
 	"github.com/mongodb/grip"
@@ -38,7 +35,7 @@ var (
 	ClientVersion = "2023-09-06"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-10-04"
+	AgentVersion = "2023-10-09"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
@@ -531,105 +528,6 @@ func (s *Settings) GetSender(ctx context.Context, env Environment) (send.Sender,
 	}
 
 	return send.NewConfiguredMultiSender(senders...), nil
-}
-
-type githubAppAuth struct {
-	AppId      int64
-	privateKey []byte
-}
-
-// getGithubAppAuth returns app id and app private key if it exists.
-func (s *Settings) getGithubAppAuth() *githubAppAuth {
-	if s.AuthConfig.Github == nil || s.AuthConfig.Github.AppId == 0 {
-		return nil
-	}
-
-	key := s.Expansions[githubAppPrivateKey]
-	if key == "" {
-		return nil
-	}
-
-	return &githubAppAuth{
-		AppId:      s.AuthConfig.Github.AppId,
-		privateKey: []byte(key),
-	}
-}
-
-// CreateInstallationToken uses the owner/repo information to request an github app installation id
-// and uses that id to create an installation token.
-func (s *Settings) CreateInstallationToken(ctx context.Context, owner, repo string, opts *github.InstallationTokenOptions) (string, error) {
-	const (
-		maxDelay   = 10 * time.Second
-		minDelay   = time.Second
-		maxRetries = 5
-	)
-
-	if owner == "" || repo == "" {
-		return "", errors.New("no owner/repo specified to create installation token")
-	}
-	authFields := s.getGithubAppAuth()
-	if authFields == nil {
-		// TODO EVG-19966: Return error here
-		grip.Debug(message.Fields{
-			"message": "no auth",
-			"owner":   owner,
-			"repo":    repo,
-			"ticket":  "EVG-19966",
-		})
-		return "", nil
-	}
-
-	retryConf := utility.NewDefaultHTTPRetryConf()
-	retryConf.MaxDelay = maxDelay
-	retryConf.BaseDelay = minDelay
-	retryConf.MaxRetries = maxRetries
-
-	httpClient := utility.GetHTTPRetryableClient(retryConf)
-	defer utility.PutHTTPClient(httpClient)
-
-	key, err := jwt.ParseRSAPrivateKeyFromPEM(authFields.privateKey)
-	if err != nil {
-		return "", errors.Wrap(err, "parsing private key")
-	}
-
-	itr := ghinstallation.NewAppsTransportFromPrivateKey(httpClient.Transport, authFields.AppId, key)
-	httpClient.Transport = itr
-	client := github.NewClient(httpClient)
-	installationId, _, err := client.Apps.FindRepositoryInstallation(ctx, owner, repo)
-	if err != nil {
-		// TODO EVG-19966: Return error here
-		grip.Debug(message.WrapError(err, message.Fields{
-			"message": "error finding installation id",
-			"owner":   owner,
-			"repo":    repo,
-			"appId":   authFields.AppId,
-			"ticket":  "EVG-19966",
-		}))
-		return "", errors.Wrap(err, "finding installation id")
-	}
-	if installationId == nil {
-		return "", errors.New(fmt.Sprintf("Installation id for '%s/%s' not found", owner, repo))
-	}
-	token, _, err := client.Apps.CreateInstallationToken(ctx, installationId.GetID(), opts)
-	if err != nil || token == nil {
-		return "", errors.Wrapf(err, "creating installation token for installation id: %d", installationId.GetID())
-	}
-	return token.GetToken(), nil
-}
-
-// CreateInstallationTokenWithDefaultOwnerRepo returns an installation token when we do not care about
-// the owner/repo that we are calling the GitHub function with (i.e. checking rate limit).
-// It will use the default owner/repo specified in the admin settings and error if it's not set.
-func (s *Settings) CreateInstallationTokenWithDefaultOwnerRepo(ctx context.Context, opts *github.InstallationTokenOptions) (string, error) {
-	if s.AuthConfig.Github == nil || s.AuthConfig.Github.DefaultOwner == "" || s.AuthConfig.Github.DefaultRepo == "" {
-		// TODO EVG-19966: Return error here
-		grip.Debug(message.Fields{
-			"message": "no default owner/repo",
-			"ticket":  "EVG-19966",
-		})
-		return "", nil
-	}
-	return s.CreateInstallationToken(ctx, s.AuthConfig.Github.DefaultOwner, s.AuthConfig.Github.DefaultRepo, opts)
 }
 
 func (s *Settings) makeSplunkSender(ctx context.Context, client *http.Client, levelInfo send.LevelInfo, fallback send.Sender) (send.Sender, error) {
--- rest/route/repo_test.go
+++ rest/route/repo_test.go
@@ -81,8 +81,7 @@ func TestPatchRepoIDHandler(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	require.NoError(t, db.ClearCollections(dbModel.RepoRefCollection, dbModel.ProjectVarsCollection,
-		dbModel.ProjectAliasCollection, dbModel.GithubHooksCollection, commitqueue.Collection,
-		dbModel.ProjectRefCollection))
+		dbModel.ProjectAliasCollection, commitqueue.Collection, dbModel.ProjectRefCollection, evergreen.GitHubAppCollection))
 
 	repoRef := &dbModel.RepoRef{
 		ProjectRef: dbModel.ProjectRef{
@@ -92,12 +91,14 @@ func TestPatchRepoIDHandler(t *testing.T) {
 		},
 	}
 	assert.NoError(t, repoRef.Upsert())
-	hook := dbModel.GithubHook{
-		HookID: 1,
-		Owner:  repoRef.Owner,
-		Repo:   repoRef.Repo,
+
+	installation := evergreen.GitHubAppInstallation{
+		Owner:          repoRef.Owner,
+		Repo:           repoRef.Repo,
+		InstallationID: 1234,
 	}
-	assert.NoError(t, hook.Insert())
+	assert.NoError(t, installation.Upsert(ctx))
+
 	repoVars := &dbModel.ProjectVars{
 		Id:          repoRef.Id,
 		Vars:        map[string]string{"a": "hello", "b": "world"},
@@ -218,12 +219,12 @@ func TestPatchRepoIDHandler(t *testing.T) {
 	assert.Equal(t, http.StatusBadRequest, resp.Status())
 	assert.Contains(t, resp.Data().(gimlet.ErrorResponse).Message, "must enable GitHub webhooks first")
 
-	hook = dbModel.GithubHook{
-		HookID: 2,
-		Owner:  "10gen",
-		Repo:   repoRef.Repo,
+	installation = evergreen.GitHubAppInstallation{
+		Owner:          "10gen",
+		Repo:           repoRef.Repo,
+		InstallationID: 1234,
 	}
-	assert.NoError(t, hook.Insert())
+	assert.NoError(t, installation.Upsert(ctx))
 
 	resp = h.Run(ctx)
 	assert.Equal(t, http.StatusOK, resp.Status())
@@ -244,8 +245,8 @@ func TestPatchHandlersWithRestricted(t *testing.T) {
 	defer cancel()
 	env := testutil.NewEnvironment(ctx, t)
 	require.NoError(t, db.ClearCollections(dbModel.RepoRefCollection, dbModel.ProjectVarsCollection,
-		dbModel.ProjectAliasCollection, dbModel.GithubHooksCollection, commitqueue.Collection, user.Collection,
-		dbModel.ProjectRefCollection, evergreen.ScopeCollection, evergreen.RoleCollection, evergreen.ConfigCollection))
+		dbModel.ProjectAliasCollection, commitqueue.Collection, user.Collection, dbModel.ProjectRefCollection,
+		evergreen.ScopeCollection, evergreen.RoleCollection, evergreen.ConfigCollection, evergreen.GitHubAppCollection))
 
 	independentProject := &dbModel.ProjectRef{
 		Id:         "branch1",
@@ -269,12 +270,12 @@ func TestPatchHandlersWithRestricted(t *testing.T) {
 	assert.NoError(t, independentProject.Insert())
 	assert.NoError(t, branchProject.Insert())
 
-	hook := dbModel.GithubHook{
-		HookID: 1,
-		Owner:  branchProject.Owner,
-		Repo:   branchProject.Repo,
+	installation := evergreen.GitHubAppInstallation{
+		Owner:          branchProject.Owner,
+		Repo:           branchProject.Repo,
+		InstallationID: 1234,
 	}
-	assert.NoError(t, hook.Insert())
+	assert.NoError(t, installation.Upsert(ctx))
 
 	u := &user.DBUser{Id: "branch1_admin"}
 	assert.NoError(t, u.Insert())
