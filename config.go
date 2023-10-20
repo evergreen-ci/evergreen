a8b6c647e9b1567ae74cffecb088ec3630ad5f0d
annie.black <annie.black@mongodb.com>
2023-10-20 14:22:30 -0400

    EVG-19801 add optional param to s3.get

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-10-02"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-10-09"
+	AgentVersion = "2023-10-20ssh"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
