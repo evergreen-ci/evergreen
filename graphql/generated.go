9165aa2fadd12cd6c237ec3bf77d7b2c1e025ee9
Jonathan Brill <jonathan.brill@10gen.com>
2023-11-20 17:36:16 -0500

    make the field optional

--- graphql/generated.go
+++ graphql/generated.go
@@ -17206,14 +17206,11 @@ func (ec *executionContext) _Distro_mountpoints(ctx context.Context, field graph
 		return graphql.Null
 	}
 	if resTmp == nil {
-		if !graphql.HasFieldError(ctx, fc) {
-			ec.Errorf(ctx, "must not be null")
-		}
 		return graphql.Null
 	}
 	res := resTmp.([]string)
 	fc.Result = res
-	return ec.marshalNString2ᚕstring(ctx, field.Selections, res)
+	return ec.marshalOString2ᚕstring(ctx, field.Selections, res)
 }
 
 func (ec *executionContext) fieldContext_Distro_mountpoints(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
@@ -72572,9 +72569,6 @@ func (ec *executionContext) _Distro(ctx context.Context, sel ast.SelectionSet, o
 			}
 		case "mountpoints":
 			out.Values[i] = ec._Distro_mountpoints(ctx, field, obj)
-			if out.Values[i] == graphql.Null {
-				atomic.AddUint32(&out.Invalids, 1)
-			}
 		default:
 			panic("unknown field " + strconv.Quote(field.Name))
 		}
