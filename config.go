748663368a054ecc10a6973cda05ed8a2420ed63
Kim Tao <kim.tao@mongodb.com>
2023-11-27 12:07:15 -0500

    DEVPROD-1966: create auto-archive command (#7244)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-11-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-11-20"
+	AgentVersion = "2023-11-27"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
