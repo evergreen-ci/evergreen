f3ace63dad6d3b93aad39231f17fdc98e5c93987
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2023-11-27 14:49:25 -0500

    DEVPROD-1931 Error on module clone failure (#7275)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-11-20"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-11-27a"
+	AgentVersion = "2023-11-27b"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
