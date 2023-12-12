ce732330f92e315642108dce556ac0cb87b69c7b
ZackarySantana <zackzackyack@gmail.com>
2023-12-12 15:02:27 -0500

    Switch upstream in git merge base

--- operations/patch_util.go
+++ operations/patch_util.go
@@ -628,7 +628,7 @@ func loadGitData(dir, branch, ref, commits string, format bool, extraArgs ...str
 	// In the case a range is passed, we only need one commit to determine the base, so we use the first commit.
 	// For details see: https://git-scm.com/docs/gitrevisions
 
-	mergeBase, err := gitMergeBase(dir, branch+"@{upstream}", ref, commits)
+	mergeBase, err := gitMergeBase(dir, "upstream/"+branch, ref, commits)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Error getting merge base, "+
 			"may need to create local branch '%s' and have it track upstream", branch)
