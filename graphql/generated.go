29853a9f235cf049cdfd43908dd5802f1c82565a
Jonathan Brill <jonathan.brill@10gen.com>
2023-12-13 10:30:48 -0500

    make field required

--- graphql/generated.go
+++ graphql/generated.go
@@ -54544,11 +54544,14 @@ func (ec *executionContext) _TaskEndDetail_diskDevices(ctx context.Context, fiel
 		return graphql.Null
 	}
 	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
 		return graphql.Null
 	}
 	res := resTmp.([]string)
 	fc.Result = res
-	return ec.marshalOString2ᚕstring(ctx, field.Selections, res)
+	return ec.marshalNString2ᚕstringᚄ(ctx, field.Selections, res)
 }
 
 func (ec *executionContext) fieldContext_TaskEndDetail_diskDevices(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
@@ -82610,6 +82613,9 @@ func (ec *executionContext) _TaskEndDetail(ctx context.Context, sel ast.Selectio
 			out.Values[i] = ec._TaskEndDetail_traceID(ctx, field, obj)
 		case "diskDevices":
 			out.Values[i] = ec._TaskEndDetail_diskDevices(ctx, field, obj)
+			if out.Values[i] == graphql.Null {
+				out.Invalids++
+			}
 		default:
 			panic("unknown field " + strconv.Quote(field.Name))
 		}
