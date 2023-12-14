7dcfe3d8d6cacee27f079dc4e9e94c65d45c2cf1
Kim Tao <kim.tao@mongodb.com>
2023-12-14 12:20:05 -0500

    Add max tasks per version admin setting

---
+++ config_task_limits.go
@@ -0,0 +1,57 @@
+package evergreen
+
+import (
+	"context"
+
+	"github.com/mongodb/anser/bsonutil"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// TaskLimitsConfig holds relevant settings for Evergreen task limitations.
+// These are usually protections against inputs that can cause issues like
+// service instability.
+type TaskLimitsConfig struct {
+	// MaxTasksPerVersion is the maximum number of tasks that a single version
+	// can have.
+	MaxTasksPerVersion int `bson:"max_tasks_per_version" json:"max_tasks_per_version" yaml:"max_tasks_per_version"`
+}
+
+var (
+	maxTasksPerVersionKey = bsonutil.MustHaveTag(TaskLimitsConfig{}, "MaxTasksPerVersion")
+)
+
+func (c *TaskLimitsConfig) SectionId() string { return "task_limits" }
+
+func (c *TaskLimitsConfig) Get(ctx context.Context) error {
+	res := GetEnvironment().DB().Collection(ConfigCollection).FindOne(ctx, byId(c.SectionId()))
+	if err := res.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			*c = TaskLimitsConfig{}
+			return nil
+		}
+		return errors.Wrapf(err, "getting config section '%s'", c.SectionId())
+	}
+
+	if err := res.Decode(&c); err != nil {
+		return errors.Wrapf(err, "decoding config section '%s'", c.SectionId())
+	}
+
+	return nil
+}
+
+func (c *TaskLimitsConfig) Set(ctx context.Context) error {
+	_, err := GetEnvironment().DB().Collection(ConfigCollection).UpdateOne(ctx, byId(c.SectionId()), bson.M{
+		"$set": bson.M{
+			maxTasksPerVersionKey: c.MaxTasksPerVersion,
+		},
+	}, options.Update().SetUpsert(true))
+
+	return errors.Wrapf(err, "updating config section '%s'", c.SectionId())
+}
+
+func (c *TaskLimitsConfig) ValidateAndDefault() error {
+	return nil
+}
