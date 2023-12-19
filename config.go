a1da19f670e360e23a60535f7f54d9b163ab8763
Chaya Malik <chaya.malik@mongodb.com>
2023-12-19 14:58:30 -0500

    fix test and rename killProcsOverride to killProcsAdditionalCheck

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 	ClientVersion = "2023-11-30"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2023-12-14"
+	AgentVersion = "2023-12-19"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
