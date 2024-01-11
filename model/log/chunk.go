c4a6db76ed0a71b3e365acff3a381bd2b106131c
julianedwards <julian.edwards@10gen.com>
2024-01-10 21:22:52 -0500

    DEVPROD-2717: Merge logs deterministically (#7378)

---
+++ model/log/chunk.go
@@ -0,0 +1,21 @@
+package log
+
+// A chunk is a storage abstraction representing a sequential, continuous
+// section of a single log. Logs can be coherently represented by a set of
+// ordered chunks.
+
+// chunkInfo represents a log chunk file's metadata that enables optimized
+// fetching of log files stored as a set of chunks in pail-backed bucket
+// storage.
+type chunkInfo struct {
+	key      string
+	numLines int
+	start    int64
+	end      int64
+}
+
+// chunkGroup represents a set of chunks belonging to a single log.
+type chunkGroup struct {
+	name   string
+	chunks []chunkInfo
+}
