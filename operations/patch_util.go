718c627607f61928a0ff0e81cb24ee4318174f95
malik.hadjri <malik.hadjri@mongodb.com>
2024-01-16 10:23:12 -0500

    Revert loadgitdata

--- operations/patch_util.go
+++ operations/patch_util.go
@@ -621,10 +621,11 @@ Continue?`, uncommittedChangesFlag), true), nil
 // If no dir is provided, we use the current working directory.
 // The branch argument is used to determine where to generate the merge base from, and any extra
 // arguments supplied are passed directly in as additional args to git diff.
+// TODO: DEVPROD-3740 Re-implement the changes for this function from https://github.com/evergreen-ci/evergreen/pull/7311
 func loadGitData(dir, remote, branch, ref, commits string, format bool, extraArgs ...string) (*localDiff, error) {
 	// branch@{upstream} refers to the branch that the branch specified by branchname is set to
-	// build on top of. This allows automatically detecting a branch based on the correct remote,
-	// if the user's repo is a fork, for example. This also works with a commit hash, if given.
+	// build on top of. For example, if a user's repo is a fork, this allows automatic detection
+	// of a branch based on the correct remote. This also works with a commit hash, if given.
 	// In the case a range is passed, we only need one commit to determine the base, so we use the first commit.
 	// For details see: https://git-scm.com/docs/gitrevisions
 
