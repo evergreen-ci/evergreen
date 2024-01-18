12b6be8e9a3ab3b202e04204da9384f24b3dc999
Bynn Lee <bynn.lee@mongodb.com>
2024-01-18 10:02:07 -0800

    DEVPROD-1931 Error on module clone failure (#7424)

--- agent/command/git.go
+++ agent/command/git.go
@@ -877,7 +877,7 @@ func (c *gitFetchProject) fetch(ctx context.Context,
 		}
 		err = c.fetchModuleSource(ctx, comm, conf, logger, jpm, td, opts.token, opts.method, p, moduleName)
 		if err != nil {
-			logger.Execution().Error(errors.Wrap(err, "fetching module source"))
+			return errors.Wrapf(err, "fetching module source '%s'", moduleName)
 		}
 	}
 
