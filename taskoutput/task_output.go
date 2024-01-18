3edf29bea9cbe51257b9db4325c707b588f8de05
julianedwards <julian.edwards@mongodb.com>
2024-01-17 22:32:22 -0500

    evergreen

--- taskoutput/task_output.go
+++ taskoutput/task_output.go
@@ -27,7 +27,7 @@ func InitializeTaskOutput(env evergreen.Environment, opts TaskOptions) *TaskOutp
 	output := &TaskOutput{}
 	// TODO (DEVPROD-1723): Remove special logic for the evergreen project
 	// once we set the global default logger to "evergreen".
-	if settings.LoggerConfig.DefaultLogger != "buildlogger" || opts.ProjectID == "mci" {
+	if settings.LoggerConfig.DefaultLogger != "buildlogger" || opts.ProjectID == "evergreen" {
 		output.TaskLogs.Version = 1
 		output.TaskLogs.BucketConfig = settings.Buckets.LogBucket
 		output.TestLogs.Version = 1
