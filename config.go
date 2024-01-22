8589ee757406aa9d1c466b761c3de576d38b291e
julianedwards <julian.edwards@mongodb.com>
2024-01-22 16:25:14 -0500

    bump agent version

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ var (
 	ClientVersion = "2024-01-18"
 
 	// Agent version to control agent rollover.
-	AgentVersion = "2024-01-22"
+	AgentVersion = "2024-01-22-a"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
