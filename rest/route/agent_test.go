4de3fe697875e7c59814906f04f1263ab6a047fe
julianedwards <julian.edwards@mongodb.com>
2024-01-23 14:12:23 -0500

    fix tests

--- rest/route/agent_test.go
+++ rest/route/agent_test.go
@@ -607,20 +607,6 @@ func TestAgentGetProjectRef(t *testing.T) {
 	projRef1 := &model.ProjectRef{Id: "project1"}
 	require.NoError(t, task1.Insert())
 	require.NoError(t, projRef1.Insert())
-	// Set the default logger after inserting into the DB since this should
-	// be set dynamically by the route handler when not set.
-	projRef1.DefaultLogger = "buildlogger"
-
-	task2 := &task.Task{
-		Id:      "task2",
-		Project: "project2",
-	}
-	projRef2 := &model.ProjectRef{
-		Id:            "project2",
-		DefaultLogger: "evergreen",
-	}
-	require.NoError(t, task2.Insert())
-	require.NoError(t, projRef2.Insert())
 
 	task3 := &task.Task{
 		Id:      "task3",
@@ -649,12 +635,6 @@ func TestAgentGetProjectRef(t *testing.T) {
 			taskID:         task1.Id,
 			expectedStatus: http.StatusOK,
 			expectedData:   projRef1,
-		},
-		{
-			name:           "ProjectLogger",
-			taskID:         task2.Id,
-			expectedStatus: http.StatusOK,
-			expectedData:   projRef2,
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
