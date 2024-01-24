32bca9a61dc192f8cde087b8c0f631c62713ea70
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-01-24 10:08:29 -0500

    Remove unused code

--- graphql/task_resolver.go
+++ graphql/task_resolver.go
@@ -679,13 +679,3 @@ func (r *taskResolver) VersionMetadata(ctx context.Context, obj *restModel.APITa
 func (r *Resolver) Task() TaskResolver { return &taskResolver{r} }
 
 type taskResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *taskResolver) HasTestResults(ctx context.Context, obj *restModel.APITask) (bool, error) {
-	panic(fmt.Errorf("not implemented: HasTestResults - hasTestResults"))
-}
