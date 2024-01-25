11eeedcc87e01b5a4c9e58a092755236662ca377
Kim Tao <kim.tao@mongodb.com>
2024-01-25 18:20:42 -0500

    DEVPROD-983: clean up GitHub app usages in the agent (#7447)

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-01-23-a"
+	AgentVersion = "2024-01-25"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
--- rest/route/agent.go
+++ rest/route/agent.go
@@ -449,14 +449,7 @@ func (h *getExpansionsAndVarsHandler) Run(ctx context.Context) gimlet.Responder 
 
 	appToken, err := h.settings.CreateInstallationToken(ctx, pRef.Owner, pRef.Repo, nil)
 	if err != nil {
-		grip.Debug(message.WrapError(err, message.Fields{
-			"ticket":  "EVG-19966",
-			"message": "error creating GitHub app token",
-			"caller":  "getExpansionsAndVarsHandler",
-			"owner":   pRef.Owner,
-			"repo":    pRef.Repo,
-			"task":    t.Id,
-		}))
+		return gimlet.NewJSONInternalErrorResponse(errors.Wrap(err, "creating GitHub app token"))
 	}
 
 	knownHosts := h.settings.Expansions[evergreen.GithubKnownHosts]
