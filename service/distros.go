bce3d2e77ff7870b0bf00e06d546460ae9dd6fae
minnakt <minna.kt@mongodb.com>
2024-02-02 13:28:51 -0500

    Fix smoke test

--- service/distros.go
+++ service/distros.go
@@ -8,5 +8,4 @@ import (
 func (uis *UIServer) distrosPage(w http.ResponseWriter, r *http.Request) {
 	spruceLink := fmt.Sprintf("%s/distros", uis.Settings.Ui.UIv2Url)
 	http.Redirect(w, r, spruceLink, http.StatusPermanentRedirect)
-	return
 }
