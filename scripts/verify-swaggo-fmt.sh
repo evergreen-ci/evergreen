780ce8370d33a052de865f6777e566ef1a6b1325
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-02-05 11:38:47 -0500

    feat: use go installed swaggo command

--- scripts/verify-swaggo-fmt.sh
+++ scripts/verify-swaggo-fmt.sh
@@ -2,11 +2,20 @@
 # Swaggo is either installed in ../swag or as a go module. If it's installed as a go module, the swaggo command is
 # available in the PATH. If it's installed in ../swag, the swaggo command is available at ../swag.
 
-swaggo="../swag"
+swaggo="swag"
 
-# Test if swaggo is installed as a go module.
+# Test if swaggo is installed as a binary.
 if command -v swag &> /dev/null; then
     swaggo=$(command -v swag)
+elif [ -n "$GOROOT" ] && [ -f "$GOROOT/bin/swag" ]; then
+    # Test if swaggo is installed as a go module.
+    swaggo="$GOROOT/bin/swag"
+fi
+
+# If swaggo is not installed, exit with an error.
+if [ ! -f "$swaggo" ]; then
+    echo "swaggo is not installed."
+    exit 1
 fi
 
 before=$(git diff --diff-filter=M)
