e859aa574b7bcf8a372bfb4278617d2e41c142d3
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-02-05 13:59:33 -0500

    fix: add comments to script

--- scripts/verify-swaggo-fmt.sh
+++ scripts/verify-swaggo-fmt.sh
@@ -1,18 +1,13 @@
+#!/usr/bin/env bash
 
-# Swaggo is either installed in ../swag or as a go module. If it's installed as a go module, the swaggo command is
-# available in the PATH. If it's installed in ../swag, the swaggo command is available at ../swag.
+# This script checks for swaggo and then uses the found swaggo to run swaggo fmt, failing if the command makes any changes to the files.
 
 swaggo="swag"
 
-echo $GOROOT
-echo $GOBIN
-echo $GOPATH
-
 # Test if swaggo is installed as a binary or a go module.
 if command -v swag &> /dev/null; then
+    # If the user has installed swaggo as a binary, use the binary.
     swaggo=$(command -v swag)
-elif [ -n "$GOROOT" ] && [ -f "$GOROOT/bin/swag" ]; then
-    swaggo="$GOROOT/bin/swag"
 elif [ -n "$GOPATH" ] && [ -f "$GOPATH/bin/swag" ]; then
     swaggo="$GOPATH/bin/swag"
 elif [ -n "$GOBIN" ] && [ -f "$GOBIN/swag" ]; then
@@ -25,12 +20,13 @@ if ! command -v "$swaggo" &> /dev/null; then
     exit 1
 fi
 
+# Compare the diff before and after running swaggo fmt. We only care about if the modified files has changed.
 before=$(git diff --diff-filter=M)
 $swaggo fmt -g service/service.go
 after=$(git diff --diff-filter=M)
 if [ "$before" = "$after" ]; then
     exit 0
 else
-    echo "Please run 'make swaggo-format' in your local environment to fix the lint errors."
+    echo "Please run 'make swaggo-format' in your local environment to fix the lint errors. If this is your local environment, please commit the changes this command made."
     exit 1
 fi
