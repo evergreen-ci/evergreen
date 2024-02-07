c3806e3b120bd11c9a01fc0d5c93339a6f8822f7
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-02-07 18:01:43 -0500

    Redact fields from otel traces

--- graphql/http_handler.go
+++ graphql/http_handler.go
@@ -3,17 +3,24 @@ package graphql
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"runtime/debug"
 	"strings"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/evergreen-ci/evergreen/util"
 	"github.com/evergreen-ci/gimlet"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/message"
 	"github.com/ravilushqa/otelgqlgen"
 	"github.com/vektah/gqlparser/v2/gqlerror"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+const (
+	requestVariablesPrefix = "gql.request.variables"
 )
 
 // Handler returns a gimlet http handler func used as the gql route handler
@@ -23,7 +30,33 @@ func Handler(apiURL string) func(w http.ResponseWriter, r *http.Request) {
 	// Send OTEL traces for each request.
 	// Only create spans for resolved fields.
 	srv.Use(otelgqlgen.Middleware(
-		otelgqlgen.WithCreateSpanFromFields(func(fieldCtx *graphql.FieldContext) bool { return fieldCtx.IsResolver }),
+		otelgqlgen.WithCreateSpanFromFields(func(fieldCtx *graphql.FieldContext) bool {
+			return fieldCtx.IsMethod
+		}),
+		otelgqlgen.WithRequestVariablesAttributesBuilder(
+			otelgqlgen.RequestVariablesBuilderFunc(func(requestVariables map[string]interface{}) []attribute.KeyValue {
+				// Deep clone the request variables to avoid modifying the original map.
+				redactedRequestVariables := map[string]interface{}{}
+				registeredTypes := []interface{}{
+					map[string]interface{}{},
+				}
+				err := util.DeepCopy(requestVariables, &redactedRequestVariables, registeredTypes)
+				if err != nil {
+					grip.Error(message.WrapError(err, message.Fields{
+						"message": "failed to deep copy request variables",
+					}))
+					return nil
+				}
+				RedactFieldsInMap(redactedRequestVariables)
+				variables := make([]attribute.KeyValue, 0, len(redactedRequestVariables))
+
+				for k, v := range redactedRequestVariables {
+					variables = append(variables, attribute.String(fmt.Sprintf("%s.%s", requestVariablesPrefix, k), fmt.Sprintf("%+v", v)))
+
+				}
+				return variables
+			}),
+		),
 	))
 
 	// Log graphql requests to splunk
