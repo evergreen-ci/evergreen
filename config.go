d4c238305f7c5e9e132aa84764e5ac2e03c630b3
Brian Samek <brian.samek@mongodb.com>
2024-02-07 15:19:23 -0500

    mfw

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-02-06"
+	AgentVersion = "2024-02-07"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
