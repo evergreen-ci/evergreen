47984eb8d7f418908b4d27ec23b83fdd75ed8da8
Chaya Malik <chaya.malik@10gen.com>
2024-02-20 14:42:56 -0500

    DEVPROD-966 Save check run ID on Task (#7543)

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-02-16"
+	AgentVersion = "2024-02-20"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
