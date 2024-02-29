98601ceb172fae0686e6fb38cdfb8f69e4505dc8
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-02-29 14:57:37 -0500

    fix: export go version

--- scripts/check-go-vulnerabilities.sh
+++ scripts/check-go-vulnerabilities.sh
@@ -1,10 +1,8 @@
-
 #!/usr/bin/env bash
-set -x
-
 # This script checks for go vulnerabilities.
 
 govul="govulncheck"
+
 
 # Test if govulncheck is installed as a binary or a go module.
 if command -v govulncheck &> /dev/null; then
@@ -22,7 +20,11 @@ if ! command -v "$govul" &> /dev/null; then
     exit 1
 fi
 
-result=$($govul ./...)
+# We re-export the go binary in the front to make sure agents pick up the latest version. Without this,
+# the agent picks up an older version that isn't compatible with govulncheck.
+export PATH="$GOROOT/bin:$PATH"
+
+result=$($govul -C $(pwd) ./...)
 if [ $? -eq 0 ]; then
     echo "No vulnerabilities found."
     exit 0
