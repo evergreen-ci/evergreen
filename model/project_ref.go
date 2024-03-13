1dfb055292745c3c16921073d7f5568a4cc08f81
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-03-13 12:21:10 -0400

    FIx comment

--- model/project_ref.go
+++ model/project_ref.go
@@ -1342,7 +1342,7 @@ func FindFirstProjectRef() (*ProjectRef, error) {
 // FindAllMergedTrackedProjectRefsWithRestrictedProjects returns all project refs in the db
 // that are currently being tracked (i.e. their project files
 // still exist and the project is not hidden) and are not restricted projects.
-// You can optionally pass in a list of restricted projects to include in the results.
+// You can optionally pass in a list of restricted projects the user has permission to view to include in the results.
 func FindAllMergedTrackedProjectRefsWithRestrictedProjects(restrictedProjectRefIds []string) ([]ProjectRef, error) {
 	projectRefs := []ProjectRef{}
 	q := db.Query(bson.M{
