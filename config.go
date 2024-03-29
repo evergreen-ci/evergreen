9a2cbb4db28e9f2f368e1afcf5e4df5dc941a7ad
dylrich <dylan.richardson@mongodb.com>
2024-03-29 15:13:06 +0000

    DEVPROD-5626: add papertrail.trace command
    This commit adds the papertrail.trace Evergreen command. Projects will
    use this command to simplify their interaction with the Papertrail
    service when tracking artifact publishing. The command takes the
    following parameters:
    
      key_id:      The Papertrail Key ID for authentication
      secret_key:  The Papertrail Secret Key for authentication
      product:     The procut name to associate with each trace
      version:     The product version to associate with each trace
      filenames:   A list of files present locally to trace
      work_dir:    An optional working directory to search for files in

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-04-02"
+	AgentVersion = "2024-04-03"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
