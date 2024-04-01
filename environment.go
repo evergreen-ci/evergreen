b0ac5b869a34c2eba54fca1848883df3cccdc583
Jonathan Brill <jonathan.brill@10gen.com>
2024-04-01 15:58:27 -0400

    use the same version of semconv

--- environment.go
+++ environment.go
@@ -41,7 +41,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
