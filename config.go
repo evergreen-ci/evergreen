d366eede1c2355ad245744f9496474cbd218a790
bynn <bynn.lee@mongodb.com>
2024-04-01 18:26:04 -0400

    agent version

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-04-01"
+	AgentVersion = "2024-04-01a"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
