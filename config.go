dc6f62766fbbaf63bb7e92a65f5a869b49799089
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2024-04-10 16:00:08 -0500

    DEVPROD-708 Redact host secret from all logs (#7714)

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-04-08"
+	AgentVersion = "2024-04-10"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
