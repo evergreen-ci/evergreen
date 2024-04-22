089c8e7cde74807341f6d72449ddb2290400fbd2
Kim Tao <kim.tao@mongodb.com>
2024-04-22 18:07:37 -0400

    DEVPROD-6729: reduce agent critical logs (#7780)

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-04-19"
+	AgentVersion = "2024-04-22"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
