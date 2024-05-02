dd61761d31f5e35e9a4beb87e4692cb3d73c6ab1
annie.black <annie.black@mongodb.com>
2024-05-02 15:08:29 -0400

    DEVPROD-3681 remove anser migration feature

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2024-05-02"
+	ClientVersion = "2024-05-02-a"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
