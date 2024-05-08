6bea2061e7ab5627a29df2e5306b3008d8ad3b33
Brian Samek <brian.samek@mongodb.com>
2024-05-08 09:45:09 -0400

    DEVPROD-5605 Bump version strings

--- config.go
+++ config.go
@@ -34,11 +34,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2024-05-07"
+	ClientVersion = "2024-05-08"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-05-03-a"
+	AgentVersion = "2024-05-08"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
