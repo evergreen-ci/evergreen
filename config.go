b44f54d583c2265a406dd01f0783b18e18184d40
Jeff Zambory <jeff.zambory@mongodb.com>
2024-05-09 12:49:40 -0400

    remove additional string

--- config.go
+++ config.go
@@ -38,7 +38,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2024-05-09-a"
+	AgentVersion = "2024-05-09"
 )
 
 // ConfigSection defines a sub-document in the evergreen config
