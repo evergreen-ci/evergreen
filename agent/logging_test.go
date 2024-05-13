07401958ade4c1f05f26aa6855bc63536325ff3f
annie.black <annie.black@mongodb.com>
2024-05-13 11:22:12 -0400

    remove irrelevant test

--- agent/logging_test.go
+++ agent/logging_test.go
@@ -2,25 +2,14 @@ package agent
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"os"
 	"testing"
 
 	"github.com/evergreen-ci/evergreen/agent/globals"
-	"github.com/evergreen-ci/evergreen/agent/internal"
 	"github.com/evergreen-ci/evergreen/agent/internal/client"
-	"github.com/evergreen-ci/evergreen/agent/internal/testutil"
-	agentutil "github.com/evergreen-ci/evergreen/agent/util"
-	"github.com/evergreen-ci/evergreen/model"
-	"github.com/evergreen-ci/evergreen/model/task"
 	_ "github.com/evergreen-ci/evergreen/testutil"
-	"github.com/evergreen-ci/evergreen/util"
-	"github.com/mongodb/jasper"
 	"github.com/mongodb/jasper/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/otel"
 )
 
 func TestGetSenderLocal(t *testing.T) {
@@ -29,88 +18,6 @@ func TestGetSenderLocal(t *testing.T) {
 	defer cancel()
 	_, err := (&Agent{}).GetSender(ctx, globals.LogOutputStdout, "agent", "task_id", 2)
 	assert.NoError(err)
-}
-
-func TestAgentFileLogging(t *testing.T) {
-	assert := assert.New(t)
-	require := require.New(t)
-
-	tmpDirName := t.TempDir()
-	require.NoError(os.Mkdir(fmt.Sprintf("%s/tmp", tmpDirName), 0666))
-
-	agt := &Agent{
-		opts: Options{
-			HostID:           "host",
-			HostSecret:       "secret",
-			StatusPort:       2286,
-			LogOutput:        globals.LogOutputStdout,
-			LogPrefix:        "agent",
-			WorkingDirectory: tmpDirName,
-		},
-		comm:   client.NewHostCommunicator("www.example.com", "host", "secret"),
-		tracer: otel.GetTracerProvider().Tracer("noop_tracer"),
-	}
-	jpm, err := jasper.NewSynchronizedManager(false)
-	require.NoError(err)
-	agt.jasper = jpm
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Run a task with a command logger specified as a file.
-	taskID := "logging"
-	taskSecret := "mock_task_secret"
-	task := &task.Task{
-		Project:        "project",
-		Id:             "t1",
-		Execution:      0,
-		DisplayName:    "task1",
-		TaskOutputInfo: testutil.InitializeTaskOutput(t),
-	}
-	tc := &taskContext{
-		task: client.TaskData{
-			ID:     taskID,
-			Secret: taskSecret,
-		},
-		ranSetupGroup: false,
-		oomTracker:    &mock.OOMTracker{},
-		taskConfig: &internal.TaskConfig{
-			Task:         *task,
-			BuildVariant: model.BuildVariant{Name: "bv"},
-			Project: model.Project{
-				Tasks: []model.ProjectTask{
-					{Name: "task1", Commands: []model.PluginCommandConf{
-						{
-							Command: "shell.exec",
-							Params: map[string]interface{}{
-								"shell":  "bash",
-								"script": "echo 'hello world'",
-							},
-						},
-					}},
-				},
-				BuildVariants: model.BuildVariants{
-					{Name: "bv", Tasks: []model.BuildVariantTaskUnit{{Name: "task1", Variant: "bv"}}},
-				},
-			},
-			Timeout:       internal.Timeout{IdleTimeoutSecs: 15, ExecTimeoutSecs: 15},
-			WorkDir:       tmpDirName,
-			NewExpansions: agentutil.NewDynamicExpansions(util.Expansions{}),
-		},
-	}
-	require.NoError(agt.startLogging(ctx, tc))
-	defer func() {
-		agt.removeTaskDirectory(tc)
-		assert.NoError(tc.logger.Close())
-	}()
-	require.NoError(agt.runTaskCommands(ctx, tc))
-
-	// Verify log contents.
-	f, err := os.Open(fmt.Sprintf("%s/%s/%s/task.log", tmpDirName, taskLogDirectory, "shell.exec"))
-	require.NoError(err)
-	bytes, err := io.ReadAll(f)
-	assert.NoError(f.Close())
-	require.NoError(err)
-	assert.Contains(string(bytes), "hello world")
 }
 
 func TestStartLogging(t *testing.T) {
