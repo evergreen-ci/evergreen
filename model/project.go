b7a571dc55cf0c52e87deef41d391f02424d54bd
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-05-21 12:49:55 -0400

    DEVPROD-6943 Unmatching tasks/tags in variants resulting in a validation warning rather than a translation error (#7821)

--- model/project.go
+++ model/project.go
@@ -414,9 +414,10 @@ type BuildVariant struct {
 	Tasks        []BuildVariantTaskUnit `yaml:"tasks,omitempty" bson:"tasks"`
 	DisplayTasks []patch.DisplayTask    `yaml:"display_tasks,omitempty" bson:"display_tasks,omitempty"`
 
-	// EmptyTaskSelectors stores task selectors that don't target any tasks for this build variant.
-	// This is only for validation purposes.
-	EmptyTaskSelectors []string `yaml:"-" bson:"-"`
+	// TranslationWarnings are validation warnings that are only detectable during project translation.
+	// e.g. task selectors that don't target any tasks in a build variant but the build
+	// variant still has tasks.
+	TranslationWarnings []string `yaml:"-" bson:"-"`
 }
 
 // CheckRun is used to provide information about a github check run.
