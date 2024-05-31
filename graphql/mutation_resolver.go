5662914abf1a56694c61c69a103ee9c8850770ca
Sophie Stadler <sophie.stadler@mongodb.com>
2024-05-31 12:15:38 -0400

    DEVPROD-7046: Remove deprecated updateSpawnHostStatus fields, add testdata (#7928)

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -864,16 +864,15 @@ func (r *mutationResolver) RemoveVolume(ctx context.Context, volumeID string) (b
 }
 
 // UpdateSpawnHostStatus is the resolver for the updateSpawnHostStatus field.
-func (r *mutationResolver) UpdateSpawnHostStatus(ctx context.Context, hostID *string, action *SpawnHostStatusActions, updateSpawnHostStatusInput *UpdateSpawnHostStatusInput) (*restModel.APIHost, error) {
-	shouldKeepOff := false
-	// TODO: Use input object throughout resolver once deprecated fields are removed
-	if updateSpawnHostStatusInput != nil {
-		hostID = &updateSpawnHostStatusInput.HostID
-		action = &updateSpawnHostStatusInput.Action
-		shouldKeepOff = utility.FromBoolPtr(updateSpawnHostStatusInput.ShouldKeepOff)
+func (r *mutationResolver) UpdateSpawnHostStatus(ctx context.Context, updateSpawnHostStatusInput UpdateSpawnHostStatusInput) (*restModel.APIHost, error) {
+	hostID := updateSpawnHostStatusInput.HostID
+	action := updateSpawnHostStatusInput.Action
+	shouldKeepOff := utility.FromBoolPtr(updateSpawnHostStatusInput.ShouldKeepOff)
 
+	h, err := host.FindOneByIdOrTag(ctx, hostID)
+	if h == nil {
+		return nil, ResourceNotFound.Send(ctx, fmt.Sprintf("Unable to find host %s", hostID))
 	}
-	h, err := host.FindOneByIdOrTag(ctx, *hostID)
 	if err != nil {
 		return nil, ResourceNotFound.Send(ctx, fmt.Sprintf("Error finding host by id: %s", err))
 	}
@@ -885,7 +884,7 @@ func (r *mutationResolver) UpdateSpawnHostStatus(ctx context.Context, hostID *st
 	}
 
 	var httpStatus int
-	switch *action {
+	switch action {
 	case SpawnHostStatusActionsStart:
 		httpStatus, err = data.StartSpawnHost(ctx, env, usr, h)
 	case SpawnHostStatusActionsStop:
