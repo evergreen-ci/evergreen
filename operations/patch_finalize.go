a63167e51e22ca849607dd2382dec2f2fd6d9916
malik.hadjri <malik.hadjri@mongodb.com>
2024-06-07 17:14:30 -0400

    comment

--- operations/patch_finalize.go
+++ operations/patch_finalize.go
@@ -37,23 +37,6 @@ func PatchFinalize() cli.Command {
 				return errors.Wrap(err, "setting up legacy Evergreen client")
 			}
 
-			existingPatch, err := ac.GetPatch(patchID)
-			if err != nil {
-				return errors.Wrapf(err, "getting patch '%s'", patchID)
-			}
-			if existingPatch == nil {
-				return errors.Wrapf(err, "patch '%s' not found", patchID)
-			}
-			numTasksToFinalize := 0
-			for _, vt := range existingPatch.VariantsTasks {
-				numTasksToFinalize += len(vt.Tasks)
-			}
-			if numTasksToFinalize > largeNumFinalizedTasksThreshold {
-				if !confirm(fmt.Sprintf("This is a large patch build, expected to schedule %d tasks. Continue?", numTasksToFinalize), true) {
-					return errors.New("patch aborted")
-				}
-			}
-
 			if err = ac.FinalizePatch(patchID); err != nil {
 				return err
 			}
