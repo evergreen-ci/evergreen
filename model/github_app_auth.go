66f8b1aa6b620e8d18e1e6b4306bef10e9e0ac28
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-06-10 11:15:10 -0400

    fix: add github app auth connection to agent

--- model/github_app_auth.go
+++ model/github_app_auth.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/mongodb/anser/bsonutil"
 	adb "github.com/mongodb/anser/db"
@@ -67,6 +68,18 @@ func (githubAppAuth *GithubAppAuth) Upsert() error {
 		},
 	)
 	return err
+}
+
+// GetGitHubAppAuth fulfills the GithubAppAuthProvider interface and returns the app auth
+// for the given project.
+func (githubAppAuth *GithubAppAuth) GetGitHubAppAuth() *evergreen.GithubAppAuth {
+	if githubAppAuth == nil {
+		return nil
+	}
+	return &evergreen.GithubAppAuth{
+		AppID:      githubAppAuth.AppId,
+		PrivateKey: githubAppAuth.PrivateKey,
+	}
 }
 
 // RemoveGithubAppAuth deletes the app auth for the given project id from the database
