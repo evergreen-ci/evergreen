077b527a4f2448e248c7632309891f01a0e39767
= <=>
2024-06-26 14:44:54 -0400

    Changed tests to avoid suite

--- thirdparty/runtime_environments_test.go
+++ thirdparty/runtime_environments_test.go
@@ -3,44 +3,18 @@ package thirdparty
 import (
 	"context"
 	"testing"
-	"time"
 
-	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/testutil"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestRuntimeEnvironmentsSuite(t *testing.T) {
-	config := testutil.TestConfig()
-	testutil.ConfigureIntegrationTest(t, config, "TestRuntimeEnvironmentsSuite")
-
-	suite.Run(t, &runtimeEnvironmentsSuite{config: config})
-}
-
-type runtimeEnvironmentsSuite struct {
-	suite.Suite
-	config *evergreen.Settings
-
-	ctx    context.Context
-	cancel func()
-}
-
-func (s *runtimeEnvironmentsSuite) SetupTest() {
-	var err error
-	s.NoError(err)
-
-	s.ctx, s.cancel = context.WithTimeout(context.Background(), 30*time.Second)
-}
-
-func (s *runtimeEnvironmentsSuite) TearDownTest() {
-	s.NoError(s.ctx.Err())
-	s.cancel()
-}
-
 // TODO: Uncomment when DEVPROD-6983 is resolved. Right now, the API does not work on task hosts.
-// func (s *runtimeEnvironmentsSuite) TestGetImageNames() {
-// 	result, err := getImageNames(s.ctx, s.config.RuntimeEnvironments.BaseURL, s.config.RuntimeEnvironments.APIKey)
-// 	s.NoError(err)
-// 	s.NotEmpty(result)
-// 	s.NotContains(result, "")
-// }
+func TestGetImageNames(t *testing.T) {
+	assert := assert.New(t)
+	config := testutil.TestConfig()
+	testutil.ConfigureIntegrationTest(t, config, "TestGetImageNames")
+	result, err := getImageNames(context.TODO(), config.RuntimeEnvironments.BaseURL, config.RuntimeEnvironments.APIKey)
+	assert.NoError(err)
+	assert.NotEmpty(result)
+	assert.NotContains(result, "")
+}
