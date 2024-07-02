fa32bc1e8a0c7326abfe22339835a1a3fc16151a
= <=>
2024-07-02 11:28:15 -0400

    started writing getOSInfo

--- thirdparty/runtime_environments.go
+++ thirdparty/runtime_environments.go
@@ -16,6 +16,18 @@ type RuntimeEnvironmentsClient struct {
 	APIKey  string
 }
 
+type ImageInfo struct {
+}
+type OSInfo struct {
+	Id      string
+	AMIId   string
+	Distro  string
+	Version string
+	Type    string
+	Manager string
+	Name    string
+}
+
 func NewRuntimeEnvironmentsClient(baseURL string, apiKey string) *RuntimeEnvironmentsClient {
 	c := RuntimeEnvironmentsClient{
 		Client:  &http.Client{},
@@ -57,4 +69,23 @@ func (c *RuntimeEnvironmentsClient) getImageNames(ctx context.Context) ([]string
 		}
 	}
 	return filteredImages, nil
+}
+
+func (c *RuntimeEnvironmentsClient) getOSInfo(ctx context.Context, amiId string, page int, limit int) ([]ImageInfo, error) {
+	apiURL := fmt.Sprintf("%s/rest/api/v1/image", c.BaseURL)
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, apiURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Api-Key", c.APIKey)
+	resp, err := c.Client.Do(request)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		msg, _ := io.ReadAll(resp.Body)
+		return nil, errors.Errorf("HTTP request returned unexpected status '%s': %s", resp.Status, string(msg))
+	}
 }
