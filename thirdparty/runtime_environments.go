1194edea21d891806021508b02a42f79143f697b
= <=>
2024-07-02 14:44:10 -0400

    added getPackages function

--- thirdparty/runtime_environments.go
+++ thirdparty/runtime_environments.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/evergreen-ci/gimlet"
 	"github.com/pkg/errors"
@@ -14,6 +15,12 @@ type RuntimeEnvironmentsClient struct {
 	Client  *http.Client
 	BaseURL string
 	APIKey  string
+}
+
+type Package struct {
+	Name    string
+	Version string
+	Manager string
 }
 
 func NewRuntimeEnvironmentsClient(baseURL string, apiKey string) *RuntimeEnvironmentsClient {
@@ -57,4 +64,39 @@ func (c *RuntimeEnvironmentsClient) getImageNames(ctx context.Context) ([]string
 		}
 	}
 	return filteredImages, nil
+}
+
+// getPackages returns a list of packages changes (name and version) from the corresponding AMI id.
+func (c *RuntimeEnvironmentsClient) getPackages(ctx context.Context, amiId string, page string, limit string) ([]Package, error) {
+	apiURL := fmt.Sprintf("%s/rest/api/v1/image", c.BaseURL)
+	params := url.Values{}
+	params.Set("ami", amiId)
+	params.Set("page", page)
+	params.Set("limit", limit)
+	params.Set("type", "Packages")
+	reqURL, err := url.Parse(apiURL)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing API url")
+	}
+	reqURL.RawQuery = params.Encode()
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, reqURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Api-Key", c.APIKey)
+	resp, err := c.Client.Do(request)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		msg, _ := io.ReadAll(resp.Body)
+		return nil, errors.Errorf("HTTP request returned unexpected status '%s': %s", resp.Status, string(msg))
+	}
+	var decodedPackages []Package
+	if err := gimlet.GetJSON(resp.Body, &decodedPackages); err != nil {
+		return nil, errors.Wrap(err, "decoding http body")
+	}
+	return decodedPackages, nil
 }
