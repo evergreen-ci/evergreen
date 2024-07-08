65b006074d51b467628a0158f1c63421e6a5d322
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2024-07-08 08:12:15 -0500

    DEVPROD-6073 Implement degraded mode route (#8031)

---
+++ rest/route/degraded_mode_test.go
@@ -0,0 +1,52 @@
+package route
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/evergreen-ci/evergreen"
+	"github.com/evergreen-ci/evergreen/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetDegradedMode(t *testing.T) {
+	assert := assert.New(t)
+
+	routeManager := makeSetDegradedMode()
+	assert.NotNil(routeManager)
+	assert.IsType(&degradedModeHandler{}, routeManager)
+
+	ctx := context.Background()
+
+	settings := testutil.MockConfig()
+	assert.NoError(settings.Set(ctx))
+	assert.True(settings.ServiceFlags.CPUDegradedModeDisabled)
+	json := []byte(`{
+    "receiver": "webhook-devprod-evergreen",
+    "status": "firing"
+    }`)
+	request, err := http.NewRequest(http.MethodPost, "/degraded_mode", bytes.NewBuffer(json))
+	assert.NoError(err)
+	err = routeManager.Parse(ctx, request)
+	assert.NoError(err)
+	resp := routeManager.Run(ctx)
+	assert.NoError(err)
+	assert.NotNil(resp)
+	assert.Equal(http.StatusOK, resp.Status())
+
+	settings, err = evergreen.GetConfig(ctx)
+	assert.NoError(err)
+	assert.False(settings.ServiceFlags.CPUDegradedModeDisabled)
+
+	json = []byte(`{
+    "receiver": "webhook-devprod-evergreen",
+    "status": "resolved"
+    }`)
+	request, err = http.NewRequest(http.MethodPost, "/degraded_mode", bytes.NewBuffer(json))
+	assert.NoError(err)
+	err = routeManager.Parse(ctx, request)
+	assert.Error(err)
+
+}
