95152bfefd4d5ba8f29c492dfe6631526a101e1c
Chaya Malik <chaya.malik@10gen.com>
2024-07-10 13:47:30 -0400

    DEVPROD-760 Don't overwrite tasks when re-configuring a patch  (#8051)

--- rest/route/patch_test.go
+++ rest/route/patch_test.go
@@ -901,6 +901,11 @@ buildvariants:
 	assert.True(t, foundCompile)
 	assert.True(t, foundPassing)
 
+	dbPatch, err := patch.FindOneId(unfinalized.Id.Hex())
+	require.NotNil(t, dbPatch)
+	assert.NoError(t, err)
+	assert.Equal(t, len(dbPatch.VariantsTasks[0].Tasks), len(tasks))
+
 	// valid request, reconfiguring a finalized patch
 	handler = makeSchedulePatchHandler(env).(*schedulePatchHandler)
 	body = patchTasks{
@@ -936,6 +941,13 @@ buildvariants:
 	assert.True(t, foundFailing)
 	assert.True(t, foundPassing)
 	assert.True(t, foundCompile)
+
+	// ensure that the patch contains both the previously-scheduled and newly-scheduled tasks,
+	// and didn't overwrite the previous tasks with the new tasks
+	dbPatch, err = patch.FindOneId(unfinalized.Id.Hex())
+	require.NotNil(t, dbPatch)
+	assert.NoError(t, err)
+	assert.Equal(t, len(dbPatch.VariantsTasks[0].Tasks), len(tasks))
 
 	// * should select all tasks
 	patch2 := patch.Patch{
