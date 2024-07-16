4e4e48f8b5b095ef46d2e4c51446ff50d61d90e5
Arjun Patel <arjun453@gmail.com>
2024-07-16 12:54:54 -0400

    hasLogViewPermission usage

--- graphql/task_resolver.go
+++ graphql/task_resolver.go
@@ -548,6 +548,13 @@ func (r *taskResolver) Status(ctx context.Context, obj *restModel.APITask) (stri
 
 // TaskLogs is the resolver for the taskLogs field.
 func (r *taskResolver) TaskLogs(ctx context.Context, obj *restModel.APITask) (*TaskLogs, error) {
+	canView, err := hasLogViewPermission(ctx, obj)
+	if err != nil {
+		return nil, err
+	}
+	if !canView {
+		return nil, Forbidden.Send(ctx, "insufficient permission for viewing task logs")
+	}
 	// Let the individual TaskLogs resolvers handle fetching logs for the task
 	// We can avoid the overhead of fetching task logs that we will not view
 	// and we can avoid handling errors that we will not see
