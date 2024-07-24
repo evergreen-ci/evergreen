4069d7da9e018cd2c8f6e36aee1efb842a531f33
sophiabarness <sophia.barnes@mongodb.com>
2024-07-24 16:01:12 -0400

    changed testing function

--- graphql/image_test.go
+++ graphql/image_test.go
@@ -9,20 +9,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test(t *testing.T) {
+func init() {
+	testutil.Setup()
+}
+
+func TestPackages(t *testing.T) {
 	config := New("/graphql")
 	ctx := getContext(t)
 	testConfig := testutil.TestConfig()
 	testutil.ConfigureIntegrationTest(t, testConfig, "TestPackages")
 	require.NoError(t, testConfig.RuntimeEnvironments.Set(ctx))
 	manager := "pip"
+	testPackage := "Automat"
 	image := thirdparty.Image{
-		ID: "ubuntu2204",
+		AMI: "ami-0f6b89500372d4a06",
 	}
 	opts := PackageOpts{
 		Manager: &manager,
+		Name:    &testPackage,
 	}
 	res, err := config.Resolvers.Image().Packages(ctx, &image, opts)
 	require.NoError(t, err)
 	assert.NotEmpty(t, res)
+	assert.Equal(t, res[0].Name, testPackage)
 }
