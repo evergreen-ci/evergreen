be24cb102671dde2199de59d4027e81b69a501bb
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-08-28 13:21:43 -0400

    fix: finish rebase

--- environment.go
+++ environment.go
@@ -368,22 +368,6 @@ func (e *envState) initDB(ctx context.Context, settings DBSettings, tracer trace
 	ctx, span := tracer.Start(ctx, "InitDB")
 	defer span.End()
 
-<<<<<<< HEAD
-=======
-	opts := options.Client().ApplyURI(settings.Url).SetWriteConcern(settings.WriteConcernSettings.Resolve()).
-		SetReadConcern(settings.ReadConcernSettings.Resolve()).
-		SetTimeout(5 * time.Minute).
-		SetConnectTimeout(5 * time.Second).
-		SetMonitor(apm.NewMonitor(apm.WithCommandAttributeDisabled(false), apm.WithCommandAttributeTransformer(redactSensitiveCollections)))
-
-	if settings.AWSAuthEnabled {
-		opts.SetAuth(options.Credential{
-			AuthMechanism: awsAuthMechanism,
-			AuthSource:    mongoExternalAuthSource,
-		})
-	}
-
->>>>>>> d4d685a79 (fix: remove todo comments)
 	var err error
 	e.client, err = mongo.Connect(settings.mongoOptions(settings.Url))
 	if err != nil {
@@ -408,28 +392,8 @@ func (e *envState) createRemoteQueues(ctx context.Context, tracer trace.Tracer) 
 	if url == "" {
 		url = DefaultAmboyDatabaseURL
 	}
-<<<<<<< HEAD
-	client, err := mongo.Connect(ctx, e.settings.Database.mongoOptions(url))
-=======
 
-	opts := options.Client().
-		ApplyURI(url).
-		SetTimeout(10 * time.Second).
-		SetConnectTimeout(5 * time.Second).
-		SetReadPreference(readpref.Primary()).
-		SetReadConcern(e.settings.Database.ReadConcernSettings.Resolve()).
-		SetWriteConcern(e.settings.Database.WriteConcernSettings.Resolve()).
-		SetMonitor(apm.NewMonitor(apm.WithCommandAttributeDisabled(false)))
-
-	if e.settings.Database.AWSAuthEnabled {
-		opts.SetAuth(options.Credential{
-			AuthMechanism: awsAuthMechanism,
-			AuthSource:    mongoExternalAuthSource,
-		})
-	}
-
-	client, err := mongo.Connect(opts)
->>>>>>> a8bf6d028 (feat: update libraries to mostly use mongo-driver v2 beta)
+	client, err := mongo.Connect(e.settings.Database.mongoOptions(url))
 	if err != nil {
 		return errors.Wrap(err, "connecting to the Amboy database")
 	}
