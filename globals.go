e4f585698234bde532756e64e34fdc6c80cd0aad
Brian Samek <brian.samek@mongodb.com>
2024-08-29 11:13:20 -0400

    DEVPROD-9693 Remove host tenancy from host.create

--- globals.go
+++ globals.go
@@ -627,11 +627,10 @@ type EC2Tenancy string
 const (
 	EC2TenancyDefault   EC2Tenancy = "default"
 	EC2TenancyDedicated EC2Tenancy = "dedicated"
-	EC2TenancyHost      EC2Tenancy = "host"
 )
 
 // ValidEC2Tenancies represents valid EC2 tenancy values.
-var ValidEC2Tenancies = []EC2Tenancy{EC2TenancyDefault, EC2TenancyDedicated, EC2TenancyHost}
+var ValidEC2Tenancies = []EC2Tenancy{EC2TenancyDefault, EC2TenancyDedicated}
 
 // IsValidEC2Tenancy returns if the given EC2 tenancy is valid.
 func IsValidEC2Tenancy(tenancy EC2Tenancy) bool {
