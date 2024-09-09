a073f7e52e2aff1e0d4248fe996abe86d970373e
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-09-09 10:21:25 -0400

    fix: add display tasks rather than remove execution tasks

--- model/project.go
+++ model/project.go
@@ -1752,9 +1752,6 @@ func (p *Project) ResolvePatchVTs(patchDoc *patch.Patch, requester, alias string
 			tasks = append(tasks, p.findMatchingProjectTasks(tRegex)...)
 		}
 	}
-	// This is used to remove execution tasks that are part of a display task when
-	// the specified tasks is 'all'.
-	executionTasksToRemove := map[string][]string{}
 	var pairs TaskVariantPairs
 	for _, v := range bvs {
 		for _, t := range tasks {
@@ -1765,19 +1762,14 @@ func (p *Project) ResolvePatchVTs(patchDoc *patch.Patch, requester, alias string
 				pairs.ExecTasks = append(pairs.ExecTasks, TVPair{Variant: v, TaskName: t})
 			} else if dt := p.GetDisplayTask(v, t); dt != nil {
 				pairs.DisplayTasks = append(pairs.DisplayTasks, TVPair{Variant: v, TaskName: t})
-				if isAllTasks {
-					executionTasksToRemove[v] = append(executionTasksToRemove[v], dt.ExecTasks...)
-				}
 			}
 		}
-	}
-	for v, tasksToRemove := range executionTasksToRemove {
-		for _, t := range tasksToRemove {
-			for i, pair := range pairs.ExecTasks {
-				if pair.Variant == v && pair.TaskName == t {
-					pairs.ExecTasks = append(pairs.ExecTasks[:i], pairs.ExecTasks[i+1:]...)
-					break
-				}
+		// If the selected tasks were 'all', we have to manually add the display tasks
+		// so the execution tasks get grouped together.
+		if isAllTasks {
+			dt := p.FindDisplayTasksForVariant(v)
+			for _, displayTask := range dt {
+				pairs.DisplayTasks = append(pairs.DisplayTasks, TVPair{Variant: v, TaskName: displayTask})
 			}
 		}
 	}
