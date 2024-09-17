e09fda95c05dd70d7f62ecf5dce801acdc24a1be
malik.hadjri <malik.hadjri@mongodb.com>
2024-09-17 17:18:36 -0400

    comment

--- units/host_termination.go
+++ units/host_termination.go
@@ -225,6 +225,7 @@ func (j *hostTerminationJob) Run(ctx context.Context) {
 						lastActivatedTaskGroupTask = t
 					}
 				}
+				// We want to block all subsequent tasks from running, regardless of what status the dependencies are waiting on.
 				j.AddError(errors.Wrapf(model.UpdateBlockedDependencies(ctx, []task.Task{*latestTask}, true), "updating blocked dependencies for task '%s'", latestTask.Id))
 				if lastActivatedTaskGroupTask.Id != latestTask.Id {
 					// If we aren't looking at the last task in the group, then we should mark the whole thing for restart,
