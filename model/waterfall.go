f17414550d2aec58c6fbe42b1b58e48db0c6e923
minnakt <47064971+minnakt@users.noreply.github.com>
2024-10-30 13:04:08 -0400

    DEVPROD-12405: Update Waterfall resolver to help with pagination investigation (#8441)

--- model/waterfall.go
+++ model/waterfall.go
@@ -37,6 +37,7 @@ type WaterfallBuildVariant struct {
 	Id          string           `bson:"_id" json:"_id"`
 	DisplayName string           `bson:"display_name" json:"display_name"`
 	Builds      []WaterfallBuild `bson:"builds" json:"builds"`
+	Version     string           `bson:"version" json:"version"`
 }
 
 type WaterfallOptions struct {
@@ -204,6 +205,9 @@ func GetWaterfallBuildVariants(ctx context.Context, versionIds []string) ([]Wate
 	})
 	pipeline = append(pipeline, bson.M{
 		"$project": bson.M{
+			build.VersionKey: bson.M{
+				"$first": "$" + bsonutil.GetDottedKeyName(buildsKey, build.VersionKey),
+			},
 			build.DisplayNameKey: bson.M{
 				"$first": "$" + bsonutil.GetDottedKeyName(buildsKey, build.DisplayNameKey),
 			},
@@ -223,4 +227,39 @@ func GetWaterfallBuildVariants(ctx context.Context, versionIds []string) ([]Wate
 	}
 
 	return res, nil
+}
+
+// GetNextRecentActiveWaterfallVersion returns the next recent active version on the waterfall, i.e. a newer
+// activated version than the version with the given minOrder.
+func GetNextRecentActiveWaterfallVersion(ctx context.Context, projectId string, minOrder int) (*Version, error) {
+	match := bson.M{
+		VersionIdentifierKey: projectId,
+		VersionRequesterKey: bson.M{
+			"$in": evergreen.SystemVersionRequesterTypes,
+		},
+		VersionRevisionOrderNumberKey: bson.M{
+			"$gt": minOrder,
+		},
+		VersionActivatedKey: true,
+	}
+	pipeline := []bson.M{
+		{"$match": match},
+		{"$sort": bson.M{VersionRevisionOrderNumberKey: 1}},
+		{"$limit": 1},
+	}
+
+	res := []Version{}
+	env := evergreen.GetEnvironment()
+	cursor, err := env.DB().Collection(VersionCollection).Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, errors.Wrap(err, "aggregating versions")
+	}
+	err = cursor.All(ctx, &res)
+	if err != nil {
+		return nil, err
+	}
+	if len(res) == 0 {
+		return nil, nil
+	}
+	return &res[0], nil
 }
