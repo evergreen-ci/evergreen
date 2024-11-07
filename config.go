572d7739874b9c96897bc3fc457a4000fca21507
Bynn Lee <bynn.lee@mongodb.com>
2024-11-07 14:36:00 -0500

    DEVPROD-9824 Repeat patch command should work with local modules (#8452)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2024-10-30"
+	ClientVersion = "2024-11-07"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
