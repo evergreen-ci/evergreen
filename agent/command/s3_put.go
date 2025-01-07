ad6a962065f458452ffda653c758610ce43b410b
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-01-07 14:34:50 -0500

    feat: add expanded remote files

--- agent/command/s3_put.go
+++ agent/command/s3_put.go
@@ -36,6 +36,7 @@ var (
 	s3PutVisibilityAttribute           = fmt.Sprintf("%s.visibility", s3PutAttribute)
 	s3PutPermissionsAttribute          = fmt.Sprintf("%s.permissions", s3PutAttribute)
 	s3PutRemotePathAttribute           = fmt.Sprintf("%s.remote_path", s3PutAttribute)
+	s3PutExpandedRemotePathAttribute   = fmt.Sprintf("%s.expanded_remote_path", s3PutAttribute)
 )
 
 // s3pc is a command to put a resource to an S3 bucket and download it to
@@ -311,6 +312,7 @@ func (s3pc *s3put) Execute(ctx context.Context,
 		attribute.String(s3PutVisibilityAttribute, s3pc.Visibility),
 		attribute.String(s3PutPermissionsAttribute, s3pc.Permissions),
 		attribute.String(s3PutRemotePathAttribute, s3pc.remoteFile),
+		attribute.String(s3PutExpandedRemotePathAttribute, s3pc.RemoteFile),
 	)
 
 	s3pc.internalBuckets = conf.InternalBuckets
