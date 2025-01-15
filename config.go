1adef9f14575eb0f3c8db1129908cc517e38a12e
Kim Tao <kim.tao@mongodb.com>
2025-01-15 15:54:10 -0500

    DEVPROD-13406: silence patch info logs for JSON output (#8631)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-01-15"
+	ClientVersion = "2025-01-16"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
