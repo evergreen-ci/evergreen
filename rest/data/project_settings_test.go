42303e77996b6718e4e4e89d8c0a0fd6f4a44c05
Kim Tao <kim.tao@mongodb.com>
2025-01-15 13:40:37 -0500

    WIP deleting code and fixing GQL test data

--- rest/data/project_settings_test.go
+++ rest/data/project_settings_test.go
@@ -214,7 +214,6 @@ func TestSaveProjectSettingsForSectionForRepo(t *testing.T) {
 			PrivateVars: map[string]bool{"hello": true},
 		}
 		assert.NoError(t, pVars.Insert())
-		checkAndSetProjectVarsSynced(t, &repoRef.ProjectRef, true)
 
 		// add scopes
 		allProjectsScope := gimlet.Scope{
@@ -1020,7 +1019,6 @@ func TestSaveProjectSettingsForSection(t *testing.T) {
 			PrivateVars: map[string]bool{"hello": true, "private": true, "change": true},
 		}
 		assert.NoError(t, pVars.Insert())
-		checkAndSetProjectVarsSynced(t, &pRef, false)
 
 		// add scopes
 		allProjectsScope := gimlet.Scope{
@@ -1259,7 +1257,6 @@ func TestPromoteVarsToRepo(t *testing.T) {
 			AdminOnlyVars: map[string]bool{"d": true},
 		}
 		assert.NoError(t, rVars.Insert())
-		checkAndSetProjectVarsSynced(t, &repoRef.ProjectRef, true)
 
 		pRef := model.ProjectRef{
 			Id:                    "pId",
@@ -1289,7 +1286,6 @@ func TestPromoteVarsToRepo(t *testing.T) {
 			AdminOnlyVars: map[string]bool{},
 		}
 		assert.NoError(t, pVars.Insert())
-		checkAndSetProjectVarsSynced(t, &pRef, false)
 
 		usr := user.DBUser{
 			Id:          "u",
@@ -1433,7 +1429,6 @@ func TestCopyProject(t *testing.T) {
 			PrivateVars: map[string]bool{"hello": true, "private": true, "change": true},
 		}
 		assert.NoError(t, pVars.Insert())
-		checkAndSetProjectVarsSynced(t, &pRef, false)
 		// add scopes
 		allProjectsScope := gimlet.Scope{
 			ID:        evergreen.AllProjectsScope,
