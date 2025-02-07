6bbd5468ab64f8d7dbfc50e95dd0879cd8a09c65
Kim Tao <kim.tao@mongodb.com>
2025-02-07 12:04:22 -0500

    DEVPROD-13567: improve retry and traceability for GitHub app token creation errors (#8693)

--- model/githubapp/github_app_installation.go
+++ model/githubapp/github_app_installation.go
@@ -2,6 +2,7 @@ package githubapp
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"sync"
 	"time"
@@ -11,9 +12,12 @@ import (
 	"github.com/evergreen-ci/utility"
 	"github.com/golang-jwt/jwt"
 	"github.com/google/go-github/v52/github"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 const (
@@ -105,22 +109,90 @@ func (g *GitHubClient) Close() {
 // This function cannot be moved to thirdparty because it is needed to set up the environment.
 // Couple this with a defered call with Close() to clean up the client.
 func getGitHubClientForAuth(authFields *GithubAppAuth) (*GitHubClient, error) {
-	retryConf := utility.NewDefaultHTTPRetryConf()
-	retryConf.MaxDelay = GitHubRetryMaxDelay
-	retryConf.BaseDelay = GitHubRetryMinDelay
-	retryConf.MaxRetries = GitHubMaxRetries
-
 	key, err := jwt.ParseRSAPrivateKeyFromPEM(authFields.PrivateKey)
 	if err != nil {
 		return nil, errors.Wrap(err, "parsing private key")
 	}
 
-	httpClient := utility.GetHTTPRetryableClient(retryConf)
-	itr := ghinstallation.NewAppsTransportFromPrivateKey(httpClient.Transport, authFields.AppID, key)
-	httpClient.Transport = itr
+	itr := ghinstallation.NewAppsTransportFromPrivateKey(utility.DefaultTransport(), authFields.AppID, key)
+	httpClient := utility.GetCustomHTTPRetryableClientWithTransport(itr, githubClientShouldRetry(), utility.RetryHTTPDelay(utility.RetryOptions{
+		MinDelay:    GitHubRetryMinDelay,
+		MaxDelay:    GitHubRetryMaxDelay,
+		MaxAttempts: GitHubMaxRetries + 1,
+	}))
+
 	client := github.NewClient(httpClient)
 	wrappedClient := GitHubClient{Client: client}
 	return &wrappedClient, nil
+}
+
+func githubClientShouldRetry() utility.HTTPRetryFunction {
+	defaultRetryableStatuses := utility.NewDefaultHTTPRetryConf().Statuses
+
+	return func(index int, req *http.Request, resp *http.Response, err error) bool {
+		const op = "githubClientShouldRetry"
+		_, span := tracer.Start(req.Context(), op)
+		defer span.End()
+
+		span.SetAttributes(attribute.Int(githubAppAttemptAttribute, index))
+		span.SetAttributes(attribute.String(githubAppURLAttribute, req.URL.String()))
+		span.SetAttributes(attribute.String(githubAppMethodAttribute, req.Method))
+
+		makeLogMsg := func(extraFields map[string]any) message.Fields {
+			msg := message.Fields{
+				"url":     req.URL.String(),
+				"method":  req.Method,
+				"attempt": index,
+				"op":      op,
+			}
+			for k, v := range extraFields {
+				msg[k] = v
+			}
+			return msg
+		}
+
+		if err != nil {
+			if errors.Is(err, io.EOF) || errors.Is(err, io.ErrUnexpectedEOF) {
+				return true
+			}
+			if utility.IsTemporaryError(err) {
+				return true
+			}
+
+			// TODO (DEVPROD-13567): retry in situations where there's no
+			// response but the error is still retryable (e.g. connection reset
+			// by peer).
+
+			grip.Error(message.WrapError(err, makeLogMsg(map[string]any{
+				"message": "GitHub endpoint encountered unretryable error",
+			})))
+
+			return false
+		}
+
+		if resp == nil {
+			grip.Error(message.WrapError(err, makeLogMsg(map[string]any{
+				"message": "GitHub app endpoint returned nil response",
+			})))
+			return true
+		}
+
+		for _, statusCode := range defaultRetryableStatuses {
+			if resp.StatusCode == statusCode {
+				return true
+			}
+		}
+
+		// TODO (DEVPROD-13567): retry when response from GitHub is non-OK due
+		// to a transient problem that is still retryable (e.g. secondary rate
+		// limit exceeded).
+		grip.ErrorWhen(resp.StatusCode >= http.StatusBadRequest, makeLogMsg(map[string]any{
+			"message":     "GitHub app endpoint returned response but is not retryable",
+			"status_code": resp.StatusCode,
+		}))
+
+		return false
+	}
 }
 
 // getInstallationIDFromGitHub returns an installation ID from GitHub given an owner and a repo.
