a8c001ed484c7c30f051d7f7ba985b2eddf2e23d
Kim Tao <kim.tao@mongodb.com>
2025-02-24 17:07:51 -0500

    DEVPROD-3611: upgrade Go version  (#8740)

--- config.go
+++ config.go
@@ -33,11 +33,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-02-21"
+	ClientVersion = "2025-02-24"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-02-21"
+	AgentVersion = "2025-02-24"
 )
 
 const (
