ba8131b244f0a159a5d4fea69b60e10bbce6811e
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-02-24 16:28:22 -0500

    feat: add test for merge queue invalid ref

--- agent/command/git_test.go
+++ agent/command/git_test.go
@@ -230,6 +230,27 @@ func (s *GitGetProjectSuite) TestRetryFetchAttemptsOnceOnSuccess() {
 	s.Require().NoError(err)
 }
 
+func (s *GitGetProjectSuite) TestRetryFetchStopsOnInvalidGitHubMergeQueueRef() {
+	c := &gitFetchProject{
+		Directory: "dir",
+		Token:     projectGitHubToken,
+	}
+	conf := s.taskConfig2
+	logger, err := s.comm.GetLoggerProducer(s.ctx, &conf.Task, nil)
+	s.Require().NoError(err)
+
+	opts := cloneOpts{}
+
+	attempt := 0
+	err = c.retryFetch(s.ctx, logger, true, opts, func(o cloneOpts) error {
+		attempt++
+		return errors.Errorf("fatel: %s", githubMergeQueueInvalidRefError)
+	})
+
+	s.Equal(1, attempt)
+	s.Require().ErrorContains(err, githubMergeQueueInvalidRefExternalError)
+}
+
 func (s *GitGetProjectSuite) TestBuildSourceCommandCloneDepth() {
 	c := &gitFetchProject{
 		Directory: "dir",
