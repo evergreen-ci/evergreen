3cb6d654a97a27158edca6d1b5b5e6a179c1c137
Bynn Lee <bynn.lee@mongodb.com>
2025-03-11 10:49:39 -0400

    DEVPROD-13572 Update agent to end after running single task host task (#8789)

--- config.go
+++ config.go
@@ -37,7 +37,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-03-11-b"
+	AgentVersion = "2025-03-11-c"
 )
 
 const (
