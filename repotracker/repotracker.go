e4d5922755c6503bc12107c7133818524999604f
bynn <bynn.lee@mongodb.com>
2025-03-19 18:44:48 -0400

    comment

--- repotracker/repotracker.go
+++ repotracker/repotracker.go
@@ -613,6 +613,7 @@ func CreateVersionFromConfig(ctx context.Context, projectInfo *model.ProjectInfo
 			return nil, errors.Wrap(err, "translating intermediate project")
 		}
 	}
+	// Make sure identifier exists for single task distro validation.
 	if projectInfo.Project.Identifier == "" {
 		projectInfo.Project.Identifier = projectInfo.Ref.Identifier
 	}
