69bcc7622c7dad13e084151245635e5fb77451a7
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-03-19 10:22:40 -0400

    fix: remove cancelled context

--- agent/command.go
+++ agent/command.go
@@ -196,6 +196,11 @@ func (a *Agent) runCommandOrFunc(ctx context.Context, tc *taskContext, commandIn
 			commandSpan.End()
 			if cmd.RetryOnFailure() {
 				tc.logger.Task().Infof("Command is set to automatically restart on completion, this can be done %d total times per task.", evergreen.MaxAutomaticRestarts)
+				// Sometimes the command is cancelled due to timeouts but we still want the MarkFailedTaskToRestart to be called
+				// with a valid context.
+				if errors.Is(err, context.Canceled) {
+					ctx = context.WithoutCancel(ctx)
+				}
 				if restartErr := a.comm.MarkFailedTaskToRestart(ctx, tc.task); restartErr != nil {
 					tc.logger.Task().Errorf("Encountered error marking task to restart upon completion: %s", restartErr)
 				}
