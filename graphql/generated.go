9ac8619e700e086027d42c85e5b19e8c1741b27c
Arjun Patel <arjun453@gmail.com>
2025-03-26 16:46:03 -0400

    resolve allowBVs on spruceConfig

--- graphql/generated.go
+++ graphql/generated.go
@@ -1040,6 +1040,7 @@ type ComplexityRoot struct {
 	}
 
 	ProjectTasksPair struct {
+		AllowedBVs   func(childComplexity int) int
 		AllowedTasks func(childComplexity int) int
 		ProjectID    func(childComplexity int) int
 	}
@@ -6746,6 +6747,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.ProjectSettings.Vars(childComplexity), true
+
+	case "ProjectTasksPair.allowedBVs":
+		if e.complexity.ProjectTasksPair.AllowedBVs == nil {
+			break
+		}
+
+		return e.complexity.ProjectTasksPair.AllowedBVs(childComplexity), true
 
 	case "ProjectTasksPair.allowedTasks":
 		if e.complexity.ProjectTasksPair.AllowedTasks == nil {
@@ -48330,6 +48338,50 @@ func (ec *executionContext) fieldContext_ProjectTasksPair_allowedTasks(_ context
 	return fc, nil
 }
 
+func (ec *executionContext) _ProjectTasksPair_allowedBVs(ctx context.Context, field graphql.CollectedField, obj *model.APIProjectTasksPair) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_ProjectTasksPair_allowedBVs(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (any, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.AllowedBVs, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.([]string)
+	fc.Result = res
+	return ec.marshalNString2ᚕstringᚄ(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_ProjectTasksPair_allowedBVs(_ context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "ProjectTasksPair",
+		Field:      field,
+		IsMethod:   false,
+		IsResolver: false,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type String does not have child fields")
+		},
+	}
+	return fc, nil
+}
+
 func (ec *executionContext) _ProjectVars_adminOnlyVars(ctx context.Context, field graphql.CollectedField, obj *model.APIProjectVars) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_ProjectVars_adminOnlyVars(ctx, field)
 	if err != nil {
@@ -55459,6 +55511,8 @@ func (ec *executionContext) fieldContext_SingleTaskDistroConfig_projectTasksPair
 				return ec.fieldContext_ProjectTasksPair_projectId(ctx, field)
 			case "allowedTasks":
 				return ec.fieldContext_ProjectTasksPair_allowedTasks(ctx, field)
+			case "allowedBVs":
+				return ec.fieldContext_ProjectTasksPair_allowedBVs(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type ProjectTasksPair", field.Name)
 		},
@@ -89797,6 +89851,11 @@ func (ec *executionContext) _ProjectTasksPair(ctx context.Context, sel ast.Selec
 			}
 		case "allowedTasks":
 			out.Values[i] = ec._ProjectTasksPair_allowedTasks(ctx, field, obj)
+			if out.Values[i] == graphql.Null {
+				out.Invalids++
+			}
+		case "allowedBVs":
+			out.Values[i] = ec._ProjectTasksPair_allowedBVs(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
 				out.Invalids++
 			}
