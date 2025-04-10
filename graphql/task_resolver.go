d1dbac36f842da52cf75509a47bda301109b7093
SupaJoon <arjun453@gmail.com>
2025-04-10 10:14:10 -0400

    DEVPROD-16171: Optimize Task.test query for test failure filtering (#8907)

--- graphql/task_resolver.go
+++ graphql/task_resolver.go
@@ -579,6 +579,17 @@ func (r *taskResolver) TaskLogs(ctx context.Context, obj *restModel.APITask) (*T
 
 // Tests is the resolver for the tests field.
 func (r *taskResolver) Tests(ctx context.Context, obj *restModel.APITask, opts *TestFilterOptions) (*TaskTestResult, error) {
+	// Return early if it is known that there are no test results to return.
+	if opts != nil && len(opts.Statuses) > 0 {
+		diffFailureStatuses := utility.GetSetDifference(opts.Statuses, evergreen.TestFailureStatuses)
+		if len(diffFailureStatuses) == 0 && !obj.ResultsFailed {
+			return &TaskTestResult{
+				TestResults:       []*restModel.APITest{},
+				TotalTestCount:    0,
+				FilteredTestCount: 0,
+			}, nil
+		}
+	}
 	taskID := utility.FromStringPtr(obj.Id)
 	dbTask, err := task.FindOneIdAndExecution(ctx, taskID, obj.Execution)
 	if err != nil {
