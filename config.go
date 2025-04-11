1fc9efb64cddae9a6287530d4a58019e34ea4f62
Chaya Malik <chaya.malik@10gen.com>
2025-04-11 12:45:03 -0400

    DEVPROD-15806 Fix home directory not defined on windows  (#8927)

--- config.go
+++ config.go
@@ -35,7 +35,7 @@ var (
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-04-11-a"
+	AgentVersion = "2025-04-11b"
 )
 
 const (
