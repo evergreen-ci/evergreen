d7851a944b08cc061a8340acec4b3af5d19a535a
Chaya Malik <chaya.malik@10gen.com>
2025-04-17 14:11:32 -0400

    DEVPROD-15806 Fix home directory not found on windows (#8940)

--- util/filepath.go
+++ util/filepath.go
@@ -1,12 +1,27 @@
 package util
 
 import (
+	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+
+	"github.com/mitchellh/go-homedir"
 )
 
 // ConsistentFilepath returns a filepath that always uses forward slashes ('/')
 // rather than platform-dependent file path separators.
 func ConsistentFilepath(parts ...string) string {
 	return strings.Replace(filepath.Join(parts...), "\\", "/", -1)
+}
+
+func GetUserHome() (string, error) {
+	userHome, err := homedir.Dir()
+	if err != nil {
+		// workaround for cygwin if we're on windows but couldn't get a homedir
+		if runtime.GOOS == "windows" && len(os.Getenv("HOME")) > 0 {
+			userHome = os.Getenv("HOME")
+		}
+	}
+	return userHome, err
 }
