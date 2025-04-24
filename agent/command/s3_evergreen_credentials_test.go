76decab643c0080d9a300c5e9f65538192bfbe4c
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-04-24 13:07:07 -0400

    fix: reformat for backwards compatability

--- agent/command/s3_evergreen_credentials_test.go
+++ agent/command/s3_evergreen_credentials_test.go
@@ -21,24 +21,7 @@ func TestEvergreenCredentials(t *testing.T) {
 	})
 
 	t.Run("RoleARN", func(t *testing.T) {
-		expires := time.Now().Add(time.Hour).Format(time.RFC3339)
-		comm.AssumeRoleResponse = &apimodels.AWSCredentials{
-			AccessKeyID:     "assume_access_key",
-			SecretAccessKey: "secret_access_key",
-			SessionToken:    "session_token",
-			Expiration:      expires,
-		}
-
-		provider := createEvergreenCredentials(comm, taskData, "role_arn")
-
-		creds, err := provider.Retrieve(t.Context())
-		require.NoError(t, err)
-		assert.Equal(t, "assume_access_key", creds.AccessKeyID)
-		assert.Equal(t, "secret_access_key", creds.SecretAccessKey)
-		assert.Equal(t, "session_token", creds.SessionToken)
-		assert.Equal(t, expires, creds.Expires.Format(time.RFC3339))
-
-		t.Run("FailsWithInvalidTimeFormat", func(t *testing.T) {
+		t.Run("PassesWithValidTimeFormat", func(t *testing.T) {
 			expires := time.Now().Add(time.Hour).String()
 			comm.AssumeRoleResponse = &apimodels.AWSCredentials{
 				AccessKeyID:     "assume_access_key",
@@ -52,6 +35,25 @@ func TestEvergreenCredentials(t *testing.T) {
 			creds, err := provider.Retrieve(t.Context())
 			require.Error(t, err)
 			assert.Empty(t, creds)
+		})
+
+		t.Run("FailsWithInvalidTimeFormat", func(t *testing.T) {
+			expires := time.Now().Add(time.Hour).Format(time.RFC3339)
+			comm.AssumeRoleResponse = &apimodels.AWSCredentials{
+				AccessKeyID:     "assume_access_key",
+				SecretAccessKey: "secret_access_key",
+				SessionToken:    "session_token",
+				Expiration:      expires,
+			}
+
+			provider := createEvergreenCredentials(comm, taskData, "role_arn")
+
+			creds, err := provider.Retrieve(t.Context())
+			require.NoError(t, err)
+			assert.Equal(t, "assume_access_key", creds.AccessKeyID)
+			assert.Equal(t, "secret_access_key", creds.SecretAccessKey)
+			assert.Equal(t, "session_token", creds.SessionToken)
+			assert.Equal(t, expires, creds.Expires.Format(time.RFC3339))
 		})
 	})
 }
