568d4c0aaf91dcbd7f22fde415f5aa1a5ab4adc2
minnakt <minna.kt@mongodb.com>
2025-04-28 11:35:14 -0400

    DEVPROD-17238: Correctly log error in `Task.VersionMetadata` resolver

--- graphql/task_resolver.go
+++ graphql/task_resolver.go
@@ -647,7 +647,7 @@ func (r *taskResolver) VersionMetadata(ctx context.Context, obj *restModel.APITa
 	versionID := utility.FromStringPtr(obj.Version)
 	v, err := model.VersionFindOne(ctx, model.VersionById(versionID).Project(bson.M{model.VersionBuildVariantsKey: 0}))
 	if err != nil {
-		return nil, InternalServerError.Send(ctx, fmt.Sprintf("fetching version '%s': %s", versionID, utility.FromStringPtr(obj.Id)))
+		return nil, InternalServerError.Send(ctx, fmt.Sprintf("fetching version '%s': %s", versionID, err.Error()))
 	}
 	if v == nil {
 		return nil, ResourceNotFound.Send(ctx, fmt.Sprintf("version '%s' not found", versionID))
