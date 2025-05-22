b310ae39b8b40b227144473fd29ed52a55c0da19
aldo-aguilar <57504606+aldo-aguilar@users.noreply.github.com>
2025-05-22 10:22:38 -0400

    DEVPROD-17088: Add AdminSettings GraphQL type (#8970)

--- graphql/config_resolver.go
+++ graphql/config_resolver.go
@@ -2,10 +2,27 @@ package graphql
 
 import (
 	"context"
+	"fmt"
 	"sort"
+	"strings"
 
+	"github.com/evergreen-ci/evergreen"
 	"github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/utility"
 )
+
+// BannerTheme is the resolver for the bannerTheme field.
+func (r *adminSettingsResolver) BannerTheme(ctx context.Context, obj *model.APIAdminSettings) (*evergreen.BannerTheme, error) {
+	if obj == nil {
+		return nil, InternalServerError.Send(ctx, "admin settings object undefined when attempting to resolve banner theme")
+	}
+	themeString := strings.ToUpper(utility.FromStringPtr(obj.BannerTheme))
+	valid, theme := evergreen.IsValidBannerTheme(themeString)
+	if !valid {
+		return nil, InputValidationError.Send(ctx, fmt.Sprintf("invalid banner theme '%s'", themeString))
+	}
+	return &theme, nil
+}
 
 // Port is the resolver for the port field.
 func (r *containerPoolResolver) Port(ctx context.Context, obj *model.APIContainerPool) (int, error) {
@@ -24,11 +41,15 @@ func (r *spruceConfigResolver) SecretFields(ctx context.Context, obj *model.APIA
 	return redactedFieldsAsSlice, nil
 }
 
+// AdminSettings returns AdminSettingsResolver implementation.
+func (r *Resolver) AdminSettings() AdminSettingsResolver { return &adminSettingsResolver{r} }
+
 // ContainerPool returns ContainerPoolResolver implementation.
 func (r *Resolver) ContainerPool() ContainerPoolResolver { return &containerPoolResolver{r} }
 
 // SpruceConfig returns SpruceConfigResolver implementation.
 func (r *Resolver) SpruceConfig() SpruceConfigResolver { return &spruceConfigResolver{r} }
 
+type adminSettingsResolver struct{ *Resolver }
 type containerPoolResolver struct{ *Resolver }
 type spruceConfigResolver struct{ *Resolver }
