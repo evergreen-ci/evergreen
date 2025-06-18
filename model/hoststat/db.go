296ad4c88e5c9d2f81730083befead759dc409cf
Kim Tao <kim.tao@mongodb.com>
2025-06-18 10:25:31 -0400

    Add basic logic to get distro, check flag, and get stats

--- model/hoststat/db.go
+++ model/hoststat/db.go
@@ -1,4 +1,39 @@
 package hoststat
 
+import (
+	"context"
+	"time"
+
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/db/mgo/bson"
+	"github.com/mongodb/anser/bsonutil"
+)
+
+var (
+	distroIDKey  = bsonutil.MustHaveTag(HostStat{}, "DistroID")
+	timestampKey = bsonutil.MustHaveTag(HostStat{}, "Timestamp")
+)
+
 // Collection stores host usage statistics as time series data.
 const Collection = "host_stats"
+
+// Find finds all host stats that match the given query.
+func Find(ctx context.Context, q db.Q) ([]HostStat, error) {
+	stats := []HostStat{}
+	err := db.FindAllQ(ctx, Collection, q, &stats)
+	if err != nil {
+		return nil, err
+	}
+	return stats, nil
+}
+
+// FindByDistroSince finds all host stats for a given distro since the start
+// timestamp.
+// kim: TODO: add unit test
+func FindByDistroSince(ctx context.Context, distroID string, startAt time.Time) ([]HostStat, error) {
+	q := db.Query(bson.M{
+		distroIDKey:  distroID,
+		timestampKey: bson.M{"$gte": startAt},
+	})
+	return Find(ctx, q)
+}
--- units/distro_autotune.go
+++ units/distro_autotune.go
@@ -3,10 +3,15 @@ package units
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/evergreen-ci/evergreen/model/distro"
+	"github.com/evergreen-ci/evergreen/model/hoststat"
+	"github.com/evergreen-ci/utility"
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/amboy/job"
 	"github.com/mongodb/amboy/registry"
+	"github.com/pkg/errors"
 )
 
 const distroAutoTuneJobName = "distro-auto-tune"
@@ -20,6 +25,7 @@ func init() {
 type distroAutoTuneJob struct {
 	job.Base `bson:"job_base" json:"job_base" yaml:"job_base"`
 	DistroID string `bson:"distro_id" json:"distro_id" yaml:"distro_id"`
+	distro   distro.Distro
 }
 
 func makeDistroAutoTuneJob() *distroAutoTuneJob {
@@ -49,4 +55,34 @@ func NewDistroAutoTuneJob(distroID, ts string) amboy.Job {
 func (j *distroAutoTuneJob) Run(ctx context.Context) {
 	defer j.MarkComplete()
 
+	if err := j.populate(ctx); err != nil {
+		j.AddError(errors.Wrapf(err, "populating job for distro '%s'", j.DistroID))
+		return
+	}
+
+	// kim: TODO: needs distro feature flag merged.
+	if !j.distro.HostAllocatorSettings.AutoTuneMaximumHosts {
+		return
+	}
+
+	const recentStatsWindow = 7 * utility.Day
+	stats, err := hoststat.FindByDistroSince(ctx, j.DistroID, time.Now().Add(-recentStatsWindow))
+	if err != nil {
+		j.AddError(errors.Wrapf(err, "getting host stats for distro '%s'", j.DistroID))
+		return
+	}
+
+	// kim: TODO: use stats to decide whether to adjust up/down.
+	fmt.Println(stats)
+}
+
+func (j *distroAutoTuneJob) populate(ctx context.Context) error {
+	d, err := distro.FindOneId(j.DistroID)
+	if err != nil {
+		return errors.Wrapf(err, "finding distro '%s'", j.DistroID)
+	}
+	if d == nil {
+		return errors.Errorf("distro '%s' not found", j.DistroID)
+	}
+	j.distro = d
 }
