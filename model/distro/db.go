87bc73add19f392cb3bdce1ad8095aaf94eb11c8
Kim Tao <kim.tao@mongodb.com>
2025-06-20 17:14:12 -0400

    Rename distro ID -> distro for consistency with BSON key

--- model/distro/db.go
+++ model/distro/db.go
@@ -46,6 +46,8 @@ var (
 	// ImageID is not equivalent to AMI. It is the identifier of the base image for the distro.
 	ImageIDKey          = bsonutil.MustHaveTag(Distro{}, "ImageID")
 	SingleTaskDistroKey = bsonutil.MustHaveTag(Distro{}, "SingleTaskDistro")
+
+	hostAllocatorAutoTuneMaxHostsKey = bsonutil.MustHaveTag(HostAllocatorSettings{}, "AutoTuneMaximumHosts")
 )
 
 var (
@@ -215,4 +217,15 @@ func FindByIdWithDefaultSettings(ctx context.Context, id string) (*Distro, error
 		d.ProviderSettingsList = []*birch.Document{providerSettings}
 	}
 	return d, nil
+}
+
+// FindByNeedsAutoTune finds all distros that can auto-tune their maximum hosts.
+func FindByNeedsAutoTune(ctx context.Context) ([]Distro, error) {
+	autoTuneMaxHostsKey := bsonutil.GetDottedKeyName(HostAllocatorSettingsKey, hostAllocatorAutoTuneMaxHostsKey)
+	q := bson.M{
+		DisabledKey:         bson.M{"$ne": true},
+		ProviderKey:         bson.M{"$in": []string{evergreen.ProviderNameEc2Fleet, evergreen.ProviderNameEc2OnDemand}},
+		autoTuneMaxHostsKey: true,
+	}
+	return Find(ctx, q)
 }
