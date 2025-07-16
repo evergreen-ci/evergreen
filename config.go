c8bbd112a1b83a10fba3ce9544dd5816b08dd657
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2025-07-16 16:32:06 -0400

    DEVPROD-16879 Remove omitempty from all json tags (#9211)

--- config.go
+++ config.go
@@ -31,11 +31,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-07-16"
+	ClientVersion = "2025-07-16a"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-07-16"
+	AgentVersion = "2025-07-16a"
 )
 
 const (
