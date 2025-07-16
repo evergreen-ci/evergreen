4c3d7c72428a8be469f2bbc0a2457511156558c3
malik.hadjri <malik.hadjri@mongodb.com>
2025-07-16 16:53:01 -0400

    Uptick

--- config.go
+++ config.go
@@ -31,7 +31,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-07-16b"
+	ClientVersion = "2025-07-16c"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
