60ffa96b1ae7cf6d6926d37df9020bd483aa3e2c
Kim Tao <kim.tao@mongodb.com>
2025-07-29 15:08:49 -0400

    Bump CLI version

--- config.go
+++ config.go
@@ -31,7 +31,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-07-16c"
+	ClientVersion = "2025-07-29"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
