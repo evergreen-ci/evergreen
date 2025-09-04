e71807a635c65a8cb7306ba92cef4c8282384e1f
Kim Tao <kim.tao@mongodb.com>
2025-09-04 11:04:12 -0400

    Use proper context and log ImagePull response to debug why it's not succeeding just in PR patch

--- thirdparty/docker/docker_cleanup_test.go
+++ thirdparty/docker/docker_cleanup_test.go
@@ -1,7 +1,6 @@
 package docker
 
 import (
-	"context"
 	"io"
 	"testing"
 	"time"
@@ -19,93 +18,97 @@ import (
 func TestCleanup(t *testing.T) {
 	dockerClient, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
 	require.NoError(t, err)
-	ctx := context.Background()
 
 	// check if the Docker daemon is running
-	_, err = dockerClient.Ping(ctx)
+	_, err = dockerClient.Ping(t.Context())
 	if err != nil {
 		// the daemon isn't running. Make sure Cleanup noops
-		assert.NoError(t, Cleanup(context.Background(), grip.NewJournaler("")))
+		assert.NoError(t, Cleanup(t.Context(), grip.NewJournaler("")))
 		return
 	}
 
 	const imageName = "public.ecr.aws/docker/library/hello-world:latest"
 	for name, test := range map[string]func(*testing.T){
-		"cleanContainers": func(*testing.T) {
+		"cleanContainers": func(t *testing.T) {
 			var resp container.CreateResponse
-			resp, err = dockerClient.ContainerCreate(ctx, &container.Config{
+			resp, err = dockerClient.ContainerCreate(t.Context(), &container.Config{
 				Image: imageName,
 			}, nil, nil, nil, "")
 			require.NoError(t, err)
-			require.NoError(t, dockerClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}))
+			require.NoError(t, dockerClient.ContainerStart(t.Context(), resp.ID, types.ContainerStartOptions{}))
 			var info types.Info
-			info, err = dockerClient.Info(ctx)
+			info, err = dockerClient.Info(t.Context())
 			require.NoError(t, err)
 			require.Positive(t, info.ContainersRunning)
 
-			assert.NoError(t, cleanContainers(context.Background(), dockerClient))
+			assert.NoError(t, cleanContainers(t.Context(), dockerClient))
 
-			info, err = dockerClient.Info(ctx)
+			info, err = dockerClient.Info(t.Context())
 			assert.NoError(t, err)
 			assert.Zero(t, info.Containers)
 		},
-		"cleanImages": func(*testing.T) {
-			assert.NoError(t, cleanImages(context.Background(), dockerClient))
+		"cleanImages": func(t *testing.T) {
+			assert.NoError(t, cleanImages(t.Context(), dockerClient))
 
 			var info types.Info
-			info, err = dockerClient.Info(ctx)
+			info, err = dockerClient.Info(t.Context())
 			assert.NoError(t, err)
 			assert.Zero(t, info.Images)
 		},
-		"cleanVolumes": func(*testing.T) {
-			_, err = dockerClient.VolumeCreate(ctx, volume.CreateOptions{})
+		"cleanVolumes": func(t *testing.T) {
+			_, err = dockerClient.VolumeCreate(t.Context(), volume.CreateOptions{})
 			require.NoError(t, err)
-			volumes, err := dockerClient.VolumeList(ctx, volume.ListOptions{})
+			volumes, err := dockerClient.VolumeList(t.Context(), volume.ListOptions{})
 			require.NoError(t, err)
 			require.NotEmpty(t, volumes.Volumes)
 
-			assert.NoError(t, cleanVolumes(context.Background(), dockerClient, grip.NewJournaler("")))
+			assert.NoError(t, cleanVolumes(t.Context(), dockerClient, grip.NewJournaler("")))
 
-			volumes, err = dockerClient.VolumeList(ctx, volume.ListOptions{})
+			volumes, err = dockerClient.VolumeList(t.Context(), volume.ListOptions{})
 			assert.NoError(t, err)
 			assert.Empty(t, volumes.Volumes)
 		},
-		"Cleanup": func(*testing.T) {
-			resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
+		"Cleanup": func(t *testing.T) {
+			resp, err := dockerClient.ContainerCreate(t.Context(), &container.Config{
 				Image: imageName,
 			}, nil, nil, nil, "")
 			require.NoError(t, err)
-			require.NoError(t, dockerClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}))
-			info, err := dockerClient.Info(ctx)
+			require.NoError(t, dockerClient.ContainerStart(t.Context(), resp.ID, types.ContainerStartOptions{}))
+			info, err := dockerClient.Info(t.Context())
 			require.NoError(t, err)
 			require.Positive(t, info.ContainersRunning)
 			require.Positive(t, info.Images)
 
-			_, err = dockerClient.VolumeCreate(ctx, volume.CreateOptions{})
+			_, err = dockerClient.VolumeCreate(t.Context(), volume.CreateOptions{})
 			require.NoError(t, err)
-			volumes, err := dockerClient.VolumeList(ctx, volume.ListOptions{})
+			volumes, err := dockerClient.VolumeList(t.Context(), volume.ListOptions{})
 			require.NoError(t, err)
 			require.NotEmpty(t, volumes.Volumes)
 
-			assert.NoError(t, Cleanup(context.Background(), grip.NewJournaler("")))
+			assert.NoError(t, Cleanup(t.Context(), grip.NewJournaler("")))
 
-			info, err = dockerClient.Info(ctx)
+			info, err = dockerClient.Info(t.Context())
 			assert.NoError(t, err)
 			assert.Zero(t, info.Containers)
 			assert.Zero(t, info.Images)
 
-			volumes, err = dockerClient.VolumeList(ctx, volume.ListOptions{})
+			volumes, err = dockerClient.VolumeList(t.Context(), volume.ListOptions{})
 			assert.NoError(t, err)
 			assert.Empty(t, volumes.Volumes)
 		},
 	} {
 		// Retry pulling the Docker image to work around rate limits on
 		// unauthenciated pulls.
-		require.NoError(t, utility.Retry(ctx, func() (bool, error) {
-			out, err := dockerClient.ImagePull(ctx, imageName, types.ImagePullOptions{})
+		require.NoError(t, utility.Retry(t.Context(), func() (bool, error) {
+			out, err := dockerClient.ImagePull(t.Context(), imageName, types.ImagePullOptions{})
 			if err != nil {
 				return true, err
 			}
+			b, err := io.ReadAll(out)
+			if err != nil {
+				return true, err
+			}
+			grip.Infof("ImagePull response: %s\n", string(b))
 			if _, err := io.Copy(io.Discard, out); err != nil {
 				return true, err
 			}
@@ -119,7 +122,7 @@ func TestCleanup(t *testing.T) {
 			MaxDelay:    10 * time.Minute,
 		}))
 
-		images, err := dockerClient.ImageList(ctx, types.ImageListOptions{All: true})
+		images, err := dockerClient.ImageList(t.Context(), types.ImageListOptions{All: true})
 		require.NoError(t, err)
 		require.Len(t, images, 1)
 
