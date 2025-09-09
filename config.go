c317d8128813cd3b5fdbb375fd0c84553ba1d2f2
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-09 11:00:55 -0400

    DEVPROD-687 Remove is_oauth from git.get_project command (#9360)

--- config.go
+++ config.go
@@ -32,11 +32,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-08-26"
+	ClientVersion = "2025-09-08"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-09-04"
+	AgentVersion = "2025-09-08"
 )
 
 const (
