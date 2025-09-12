41202df8ba83a6c3c204de5f08609b2e95e19326
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-12 09:37:00 -0400

    Update patch resolver

--- graphql/patch_resolver.go
+++ graphql/patch_resolver.go
@@ -266,7 +266,12 @@ func (r *patchResolver) VersionFull(ctx context.Context, obj *restModel.APIPatch
 
 // IncludedLocalModules is the resolver for the includedLocalModules field.
 func (r *patchResolver) IncludedLocalModules(ctx context.Context, obj *restModel.APIPatch) ([]*restModel.APILocalModuleInclude, error) {
-	return obj.LocalModuleIncludes, nil
+	// Convert []APILocalModuleInclude to []*APILocalModuleInclude
+	result := make([]*restModel.APILocalModuleInclude, len(obj.LocalModuleIncludes))
+	for i, module := range obj.LocalModuleIncludes {
+		result[i] = &module
+	}
+	return result, nil
 }
 
 // Patch returns PatchResolver implementation.
