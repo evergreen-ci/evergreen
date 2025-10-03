6f6ea7dda4150f2c433dca55d51f32ddcc9e8c2b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-10-03 14:36:38 -0400

    Use verify-gqlgen shell script

---
+++ scripts/verify-gqlgen.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script runs gqlgen generate and fails if the command makes any changes to the files.
+
+# Compare the diff before and after running gqlgen generate. We only care about if the modified files has changed.
+before=$(git diff --diff-filter=M)
+make gqlgen
+after=$(git diff --diff-filter=M)
+if [ "$before" = "$after" ]; then
+    echo "No changes found after running gqlgen."
+    exit 0
+else
+    echo "Error: make gqlgen generated changes. Please run 'make gqlgen' and commit the changes."
+    exit 1
+fi
