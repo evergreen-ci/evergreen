34f269c7457d190e96062fb46866983c5bd485eb
malik.hadjri <malik.hadjri@mongodb.com>
2025-10-24 12:33:40 -0400

    DEVPROD-23141 Ensure module includes are processed together for a path

--- config.go
+++ config.go
@@ -32,7 +32,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-10-10"
+	ClientVersion = "2025-10-24"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
