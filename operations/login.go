1800f5c22d102416903a780f80c6e9da02260b00
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-11-17 10:33:32 -0500

    feat: use helper func

--- operations/login.go
+++ operations/login.go
@@ -12,25 +12,34 @@ func Login() cli.Command {
 		Name:  "login",
 		Usage: "authenticate the CLI with evergreen",
 		Action: func(c *cli.Context) error {
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			confPath := c.Parent().String(confFlagName)
-			conf, err := NewClientSettings(confPath)
-			if err != nil {
-				return errors.Wrap(err, "loading configuration")
-			}
-			comm, err := conf.setupRestCommunicator(ctx, false)
-			if err != nil {
-				return errors.Wrap(err, "setting up REST communicator")
-			}
-			defer comm.Close()
-
-			if err = conf.SetOAuthToken(ctx, comm); err != nil {
-				return errors.Wrap(err, "setting OAuth token")
+			if _, err := login(c); err != nil {
+				return errors.Wrap(err, "logging in")
 			}
 
 			return nil
 		},
 	}
+}
+
+// login has a user to authenticate using oauth and saves the token.
+func login(c *cli.Context) (*ClientSettings, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	confPath := c.Parent().String(confFlagName)
+	conf, err := NewClientSettings(confPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "loading configuration")
+	}
+	comm, err := conf.setupRestCommunicator(ctx, false)
+	if err != nil {
+		return nil, errors.Wrap(err, "setting up REST communicator")
+	}
+	defer comm.Close()
+
+	if err = conf.SetOAuthToken(ctx, comm); err != nil {
+		return nil, errors.Wrap(err, "setting OAuth token")
+	}
+
+	return conf, nil
 }
