8c6c50ede571546c9f990ead0b3c0876d7513270
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-12-02 10:13:58 -0500

    feat: better comment

--- rest/client/methods.go
+++ rest/client/methods.go
@@ -1760,15 +1760,17 @@ func (c *communicatorImpl) GetOAuthToken(ctx context.Context, doNotUseBrowser bo
 	}
 	defer client.Close()
 
-	// This attempt tries to get a token or refresh using the refresh token.
+	// This attempt tries to get a token or refreshes using the refresh token.
 	token, err := client.Token()
+	// We have to explicitly check the expiry is valid because the OAuth
+	// library doesn't consider a token with a zero time expiry as expired.
 	if err == nil && token.Expiry.After(time.Now()) {
 		return token, client.TokenFilePath(), nil
 	}
 
 	shouldRetry := false
 	if token.Expiry.Before(time.Now()) {
-		// Dex doesn't count zero time as expired, so we double check if it's expired.
+		// Retry if the token is expired.
 		shouldRetry = true
 	} else {
 		// Otherwise, check the error to see if we should retry.
