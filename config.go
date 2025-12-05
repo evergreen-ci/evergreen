fcceaa7fb1cdc879810e2d1d06154d7728750f5f
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-12-05 15:27:41 -0500

    ci: agent and client bump

--- config.go
+++ config.go
@@ -32,11 +32,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2025-12-02"
+	ClientVersion = "2025-12-05"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2025-12-01"
+	AgentVersion = "2025-12-05"
 )
 
 const (
