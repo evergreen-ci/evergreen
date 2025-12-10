002add59265fdd866781243026f55a71bac101bf
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-12-10 15:33:17 -0500

    fix: return error rather than exit

--- operations/panic.go
+++ operations/panic.go
@@ -3,7 +3,6 @@ package operations
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/evergreen-ci/evergreen/rest/model"
 )
@@ -11,20 +10,17 @@ import (
 // SendPanicReport sends a panic report to the Evergreen service.
 func SendPanicReport(ctx context.Context, report *model.PanicReport) error {
 	if report == nil || report.ConfigFilePath == "" {
-		fmt.Fprintln(os.Stderr, "unexpected error occured, could not reach out to Evergreen service:", report.Panic)
-		os.Exit(1)
+		return fmt.Errorf("could not reach Evergreen service: %v", report.Panic)
 	}
 
 	conf, err := NewClientSettings(report.ConfigFilePath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "unexpected error occured, could not load config file '%s': %v\n", report.ConfigFilePath, err)
-		os.Exit(1)
+		return fmt.Errorf("could not load config file '%s': %v", report.ConfigFilePath, err)
 	}
 
 	comm, err := conf.setupRestCommunicator(ctx, false)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "unexpected error occured, could not set up REST communicator: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("could not set up REST communicator: %v", err)
 	}
 	defer comm.Close()
 
