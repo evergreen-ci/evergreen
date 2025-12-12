abd1770d6bc208987b2652a00ee14aa87f91ef0e
maddaak <akhilesh.maddali@mongodb.com>
2025-12-12 13:57:36 -0500

    Updates to dependency changes

--- service/ui.go
+++ service/ui.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	csrf "filippo.io/csrf/gorilla"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/PuerkitoBio/rehttp"
 	"github.com/evergreen-ci/evergreen"
@@ -13,7 +14,6 @@ import (
 	"github.com/evergreen-ci/evergreen/rest/route"
 	"github.com/evergreen-ci/evergreen/thirdparty"
 	"github.com/evergreen-ci/gimlet"
-	"github.com/gorilla/csrf"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/sessions"
 	"github.com/mongodb/amboy"
