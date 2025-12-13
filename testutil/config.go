3368eb91143081841a2b638354f56365a20899fc
bynn <bynn.lee@mongodb.com>
2025-12-13 00:26:43 -0500

    DEVPROD-25114 Add service flag for debuggable spawn host

--- testutil/config.go
+++ testutil/config.go
@@ -403,6 +403,7 @@ func MockConfig() *evergreen.Settings {
 			ElasticIPsDisabled:              true,
 			ReleaseModeDisabled:             true,
 			LegacyUIAdminPageDisabled:       true,
+			DebugSpawnHostDisabled:          true,
 		},
 		SingleTaskDistro: evergreen.SingleTaskDistroConfig{
 			ProjectTasksPairs: []evergreen.ProjectTasksPair{
