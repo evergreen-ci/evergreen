1ad9d35dedbb531a5a05f3fc81fe6016267e1a48
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2026-01-13 14:30:38 -0500

    fix: context in test

--- service/api_test.go
+++ service/api_test.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"net/http"
 	"testing"
@@ -18,16 +17,13 @@ import (
 )
 
 func TestLimitedProjectEndPoint(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	assert := assert.New(t)
 	assert.NoError(db.Clear(model.ProjectRefCollection))
 
 	testutil.DisablePermissionsForTests()
 	defer testutil.EnablePermissionsForTests()
 	testConfig := testutil.TestConfig()
-	testApiServer, err := CreateTestServer(ctx, testConfig, nil, false)
+	testApiServer, err := CreateTestServer(t.Context(), testConfig, nil, false)
 	require.NoError(t, err, "failed to create new API server")
 	defer testApiServer.Close()
 
