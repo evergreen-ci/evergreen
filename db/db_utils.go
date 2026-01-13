361b373ca7d4e544532d3c006d3e33892f8104b7
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2026-01-13 16:44:42 -0500

    fix: remove old evg get session

--- db/db_utils.go
+++ db/db_utils.go
@@ -43,21 +43,6 @@ func GetGlobalSessionFactory() SessionFactory {
 	}
 }
 
-// GetSession creates a database connection using the global environment's
-// session (and context through the session).
-func (s *shimFactoryImpl) GetSession() (db.Session, db.Database, error) {
-	if s.env == nil {
-		return nil, nil, errors.New("undefined environment")
-	}
-
-	session := s.env.Session()
-	if session == nil {
-		return nil, nil, errors.New("session is not defined")
-	}
-
-	return session, session.DB(s.db), nil
-}
-
 // GetContextSession creates a database session and connection that uses the associated
 // context in its operations.
 func (s *shimFactoryImpl) GetContextSession(ctx context.Context) (db.Session, db.Database, error) {
