f5f123f594c00967fcbe74fc8c8d6d7babee2722
Kim Tao <kim.tao@mongodb.com>
2026-01-13 17:56:07 -0500

    DEVPROD-26289: log project GitHub app rate limits (#9678)

--- model/project_ref.go
+++ model/project_ref.go
@@ -558,6 +558,7 @@ var (
 	projectRefLastAutoRestartedTaskAtKey            = bsonutil.MustHaveTag(ProjectRef{}, "LastAutoRestartedTaskAt")
 	projectRefNumAutoRestartedTasksKey              = bsonutil.MustHaveTag(ProjectRef{}, "NumAutoRestartedTasks")
 	projectRefTestSelectionKey                      = bsonutil.MustHaveTag(ProjectRef{}, "TestSelection")
+	projectRefUseGitHubAppForAPIKey                 = bsonutil.MustHaveTag(ProjectRef{}, "UseGitHubAppForAPI")
 
 	commitQueueEnabledKey          = bsonutil.MustHaveTag(CommitQueueParams{}, "Enabled")
 	triggerDefinitionProjectKey    = bsonutil.MustHaveTag(TriggerDefinition{}, "Project")
@@ -3739,4 +3740,40 @@ func ProjectCanDispatchTask(pRef *ProjectRef, t *task.Task) (canDispatch bool, r
 // GetProjectAdminRole returns the project admin role ID for the given project.
 func GetProjectAdminRole(projectId string) string {
 	return fmt.Sprintf("admin_project_%s", projectId)
+}
+
+// FindProjectAndRepoRefsUsingGitHubAppForAPI returns all branch project refs
+// and repo refs that use GitHub app authentication for internal GitHub API
+// requests. This does not take into account whether a branch project ref
+// inherits settings from the repo ref, so if a repo ref has the GitHub app
+// enabled for internal API usage, this function will return that repo ref but
+// will not return the branch projects that inherit that setting.
+func FindProjectAndRepoRefsUsingGitHubAppForAPI(ctx context.Context) ([]ProjectRef, error) {
+	pRefs := []ProjectRef{}
+	if err := db.FindAllQ(ctx,
+		ProjectRefCollection,
+		db.Query(bson.M{
+			projectRefUseGitHubAppForAPIKey: true,
+		}),
+		&pRefs,
+	); err != nil {
+		return nil, errors.Wrap(err, "finding project refs using GitHub app for API")
+	}
+
+	repoRefs := []RepoRef{}
+	if err := db.FindAllQ(ctx,
+		RepoRefCollection,
+		db.Query(bson.M{
+			projectRefUseGitHubAppForAPIKey: true,
+		}),
+		&repoRefs,
+	); err != nil {
+		return nil, errors.Wrap(err, "finding repo refs using GitHub app for API")
+	}
+	repoRefsAsProjectRefs := make([]ProjectRef, 0, len(repoRefs))
+	for _, repoRef := range repoRefs {
+		repoRefsAsProjectRefs = append(repoRefsAsProjectRefs, repoRef.ProjectRef)
+	}
+
+	return append(pRefs, repoRefsAsProjectRefs...), nil
 }
