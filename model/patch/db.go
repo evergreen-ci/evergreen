7f14376c6e58c662662e7a9a94ba5a6a6dc68beb
minnakt <minna.kt@mongodb.com>
2026-01-16 11:03:27 -0500

    Revert "DEVPROD-12099 fix: Remove patch diff, add hint (#9696)"
    This reverts commit 2325c52334a16df496e9d56809e40d70ee7b5fa5.

--- model/patch/db.go
+++ model/patch/db.go
@@ -16,7 +16,6 @@ import (
 	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.opentelemetry.io/otel/attribute"
 )
 
@@ -246,9 +245,6 @@ func ByPatchNameStatusesMergeQueuePaginated(ctx context.Context, opts ByPatchNam
 		pipeline = append(pipeline, bson.M{"$match": bson.M{"requester": bson.M{"$in": validatedRequesters}}})
 	}
 
-	// Exclude large patch diff data to avoid exceeding MongoDB's 16MB document limit in the $facet output.
-	pipeline = append(pipeline, bson.M{"$project": ExcludePatchDiff})
-
 	resultPipeline := []bson.M{}
 	if opts.Page > 0 {
 		resultPipeline = append(resultPipeline, bson.M{"$skip": opts.Page * opts.Limit})
@@ -272,13 +268,7 @@ func ByPatchNameStatusesMergeQueuePaginated(ctx context.Context, opts ByPatchNam
 	}
 
 	env := evergreen.GetEnvironment()
-
-	var aggregateOpts *options.AggregateOptions
-	if onlyMergeQueue {
-		aggregateOpts = options.Aggregate().SetHint("branch_1_create_time_-1")
-	}
-
-	cursor, err := env.DB().Collection(Collection).Aggregate(ctx, pipeline, aggregateOpts)
+	cursor, err := env.DB().Collection(Collection).Aggregate(ctx, pipeline)
 	if err != nil {
 		return nil, 0, err
 	}
