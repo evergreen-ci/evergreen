484a2807c01bf0763aa0c609864e4e2a5139177c
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2026-01-20 14:57:46 -0500

    DEVPROD-24945 New command to select a task to debug (#9700)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-01-13"
+	ClientVersion = "2026-01-16"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
