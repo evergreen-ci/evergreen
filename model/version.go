47cd58873bd38b973a4946eb3b6391a9d3387d6d
maddaak <akhilesh.maddali@mongodb.com>
2026-02-11 12:19:56 -0500

    Adding test cases and address code review comment

--- model/version.go
+++ model/version.go
@@ -818,6 +818,7 @@ func constructManifest(ctx context.Context, v *Version, projectRef *ProjectRef, 
 	for _, module := range moduleList {
 		if shouldUseBaseRevision && !module.AutoUpdate && baseManifest != nil {
 			if baseModule, ok := baseManifest.Modules[module.Name]; ok {
+				// Use base module revision unless YAML specifies otherwise.
 				if module.Ref == "" || module.Ref == baseModule.Revision {
 					modules[module.Name] = baseModule
 					continue
