67675cbfc2e07e49467bad1dcdb56515b53ef272
Malik Hadjri <19805673+hadjri@users.noreply.github.com>
2026-03-04 11:36:08 -0600

    DEVPROD-24952 Support manually setting debug expansions (#9870)

--- config.go
+++ config.go
@@ -33,11 +33,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-03-02a"
+	ClientVersion = "2026-03-04"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2026-03-02a"
+	AgentVersion = "2026-03-04"
 )
 
 const (
