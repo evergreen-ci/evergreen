2552a9c470bf6e30a510f9480ec1b1bf10b382db
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2026-03-09 10:28:19 -0400

    Revert "DEVPROD-29442 Get raw patch rather than diff for GitHub patches (#9895)"
    This reverts commit 7da525f6f6b5cbf383029c6e6cded817b0913615.

--- thirdparty/github.go
+++ thirdparty/github.go
@@ -1569,7 +1569,7 @@ func GetGithubPullRequestDiff(ctx context.Context, gh GithubPatch) (string, []Su
 	githubClient := getGithubClient(token, caller, retryConfig{retry404: true})
 	defer githubClient.Close()
 
-	diff, resp, err := githubClient.PullRequests.GetRaw(ctx, gh.BaseOwner, gh.BaseRepo, gh.PRNumber, github.RawOptions{Type: github.Patch})
+	diff, resp, err := githubClient.PullRequests.GetRaw(ctx, gh.BaseOwner, gh.BaseRepo, gh.PRNumber, github.RawOptions{Type: github.Diff})
 	if resp != nil {
 		defer resp.Body.Close()
 		span.SetAttributes(attribute.Bool(githubCachedAttribute, respFromCache(resp.Response)))
