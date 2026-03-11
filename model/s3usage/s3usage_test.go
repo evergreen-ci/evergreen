d0eaf116329e6e1b373bf820182af6e9ccf57181
Akhilesh Maddali <akhilesh.maddali@mongodb.com>
2026-03-11 09:53:22 -0400

    DEVPROD-25591: Calculate Artifacts S3 PUT costs and Save to Task Document (#9920)

--- model/s3usage/s3usage_test.go
+++ model/s3usage/s3usage_test.go
@@ -3,6 +3,7 @@ package s3usage
 import (
 	"testing"
 
+	"github.com/evergreen-ci/evergreen"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,6 +26,7 @@ func TestS3Usage(t *testing.T) {
 		s3Usage = S3Usage{}
 		s3Usage.NumPutRequests = 5
 		assert.False(t, s3Usage.IsZero())
+
 	})
 
 	t.Run("IncrementUserFiles", func(t *testing.T) {
@@ -137,5 +139,48 @@ func TestCalculatePutRequestsWithContext(t *testing.T) {
 		assert.Equal(t, 1, CalculatePutRequestsWithContext(S3BucketTypeLarge, S3UploadMethodPut, 5*MB-1))
 		assert.Equal(t, 4, CalculatePutRequestsWithContext(S3BucketTypeSmall, S3UploadMethodPut, 5*MB+1))
 		assert.Equal(t, 4, CalculatePutRequestsWithContext(S3BucketTypeLarge, S3UploadMethodPut, 5*MB+1))
+	})
+}
+
+func TestCalculateS3PutCostWithConfig(t *testing.T) {
+	validConfig := &evergreen.CostConfig{
+		S3Cost: evergreen.S3CostConfig{
+			Upload: evergreen.S3UploadCostConfig{
+				UploadCostDiscount: 0.3,
+			},
+		},
+	}
+
+	t.Run("WithValidConfig", func(t *testing.T) {
+		cost := CalculateS3PutCostWithConfig(1000, validConfig)
+		// 1000 * 0.000005 * 0.7 = 0.0035
+		assert.InDelta(t, 0.0035, cost, 0.000001)
+	})
+
+	t.Run("WithNilConfig", func(t *testing.T) {
+		cost := CalculateS3PutCostWithConfig(1000, nil)
+		assert.Equal(t, 0.0, cost)
+	})
+
+	t.Run("WithZeroPutRequests", func(t *testing.T) {
+		cost := CalculateS3PutCostWithConfig(0, validConfig)
+		assert.Equal(t, 0.0, cost)
+	})
+
+	t.Run("WithInvalidDiscount", func(t *testing.T) {
+		invalidConfig := &evergreen.CostConfig{
+			S3Cost: evergreen.S3CostConfig{
+				Upload: evergreen.S3UploadCostConfig{
+					UploadCostDiscount: 1.5,
+				},
+			},
+		}
+		cost := CalculateS3PutCostWithConfig(1000, invalidConfig)
+		assert.Equal(t, 0.0, cost)
+	})
+
+	t.Run("WithNegativePutRequests", func(t *testing.T) {
+		cost := CalculateS3PutCostWithConfig(-5, validConfig)
+		assert.Equal(t, 0.0, cost)
 	})
 }
