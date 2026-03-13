c8fb1ac2d485cdf17a3d9f87be266a6d3963bdea
Bynn Lee <bynn.lee@mongodb.com>
2026-03-13 16:32:03 -0400

    DEVPROD-25033 Add warning for include module (#9930)

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-03-12a"
+	ClientVersion = "2026-03-13"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
