beeefce5d41cf13bb1029b377a371b75e368dc85
minnakt <minna.kt@mongodb.com>
2026-03-18 14:47:37 -0400

    idk why, but don't update this condition I guess

--- model/project_vars.go
+++ model/project_vars.go
@@ -586,7 +586,7 @@ func (projectVars *ProjectVars) FindAndModify(ctx context.Context, varsToDelete 
 		update["$unset"] = unsetUpdate
 	}
 
-	if len(projectVars.PrivateVars) != 0 || len(projectVars.AdminOnlyVars) != 0 || len(projectVars.VarsDescriptions) != 0 {
+	if len(projectVars.PrivateVars) != 0 && len(projectVars.AdminOnlyVars) != 0 {
 		initializeUpdate := bson.M{}
 		originalProjectVars, err := FindOneProjectVars(ctx, projectVars.Id)
 		if err != nil {
@@ -601,9 +601,6 @@ func (projectVars *ProjectVars) FindAndModify(ctx context.Context, varsToDelete 
 		}
 		if originalProjectVars.AdminOnlyVars == nil {
 			initializeUpdate[adminOnlyVarsMapKey] = bson.M{}
-		}
-		if originalProjectVars.VarsDescriptions == nil {
-			initializeUpdate[varsDescriptionsMapKey] = bson.M{}
 		}
 		if len(initializeUpdate) > 0 {
 			err := db.Update(ctx,
