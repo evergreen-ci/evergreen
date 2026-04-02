78ef1d936e258f4622185c8827c58ab93758db52
malik.hadjri <malik.hadjri@mongodb.com>
2026-04-02 15:51:56 -0400

    simplify

--- operations/daemon_proc_windows.go
+++ operations/daemon_proc_windows.go
@@ -5,8 +5,7 @@ package operations
 import "syscall"
 
 // daemonSysProcAttr returns the SysProcAttr for the daemon child process on
-// Windows. In practice, the debug daemon is only supported on Linux spawn hosts
-// and this code path is unreachable; it exists only to satisfy cross-compilation.
+// Windows
 func daemonSysProcAttr() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{CreationFlags: 0x00000008} // CREATE_NEW_PROCESS_GROUP
 }
