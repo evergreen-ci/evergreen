fe6da32f9a2e26feca8fbcb91e6a672bbf710358
malik.hadjri <malik.hadjri@mongodb.com>
2026-04-08 12:14:54 -0400

    no-op bug fix

--- agent/taskexec/local_executor.go
+++ agent/taskexec/local_executor.go
@@ -319,9 +319,9 @@ func (e *LocalExecutor) stepNext(ctx context.Context) error {
 
 	startTime := time.Now()
 
-	_, isNoOp := noOpCommands[targetCmd.Command.Command]
+	_, isNoOp := noOpCommands[targetCmd.CommandName]
 	if isNoOp {
-		noOpMsg := e.getNoOpMessage(targetCmd.Command.Command)
+		noOpMsg := e.getNoOpMessage(targetCmd.CommandName)
 		if e.streamWriter != nil {
 			e.streamWriter.WriteChannelMessage(ExecChannel, noOpMsg)
 		}
@@ -752,6 +752,7 @@ func (e *LocalExecutor) rebuildCommandList() error {
 				e.debugState.CommandList = append(e.debugState.CommandList, CommandInfo{
 					Index:          globalIndex,
 					Command:        cmd,
+					CommandName:    cmd.Command,
 					DisplayName:    cmd.GetDisplayName(),
 					IsFunction:     cmd.Function != "",
 					FunctionName:   cmd.Function,
@@ -773,6 +774,7 @@ func (e *LocalExecutor) rebuildCommandList() error {
 				info := CommandInfo{
 					Index:          globalIndex,
 					Command:        cmd,
+					CommandName:    rcmd.Name(),
 					DisplayName:    displayName,
 					IsFunction:     cmd.Function != "",
 					FunctionName:   cmd.Function,
