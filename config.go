9eccde50ea13d27beeb30ff14cea54e5f3f92d73
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2026-04-30 09:48:54 -0400

    ci: agent and client version

--- config.go
+++ config.go
@@ -33,11 +33,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-04-29b"
+	ClientVersion = "2026-04-30"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2026-04-28"
+	AgentVersion = "2026-04-30"
 )
 
 const (
