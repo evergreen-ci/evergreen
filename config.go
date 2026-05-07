cd6f4f84a9337bbf8310f08f9402cf6a9f1803ae
minnakt <minna.kt@mongodb.com>
2026-05-07 10:54:32 -0400

    bump `ClientVersion`

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-05-04"
+	ClientVersion = "2026-05-07"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
