748085bb33baa9183f83991fdb9a6921b46e4149
malik.hadjri <malik.hadjri@mongodb.com>
2026-06-02 14:30:48 -0400

    nit

--- cloud/spawn.go
+++ cloud/spawn.go
@@ -165,7 +165,7 @@ func CreateSpawnHost(ctx context.Context, so SpawnOptions, settings *evergreen.S
 		}
 	}
 	if so.IsDebug {
-		debugScript, err := getDebugSetupScript(ctx, so, settings, d.WorkDir, d.HomeDir())
+		debugScript, err := getDebugSetupScript(ctx, so, settings, d.WorkDir)
 		if err != nil {
 			return nil, errors.Wrap(err, "generating debug setup script")
 		}
@@ -237,12 +237,9 @@ func CreateSpawnHost(ctx context.Context, so SpawnOptions, settings *evergreen.S
 // getDebugSetupScript returns the debug setup script to use. The
 // admin-configured debug script always runs. If a step number is also
 // specified, the debug setup-phase script is appended after it.
-func getDebugSetupScript(ctx context.Context, so SpawnOptions, settings *evergreen.Settings, workDir, homeDir string) (string, error) {
+func getDebugSetupScript(ctx context.Context, so SpawnOptions, settings *evergreen.Settings, workDir string) (string, error) {
 	script := settings.DebugSpawnHosts.SetupScript
 	configBaseDir := workDir
-	if configBaseDir == "" {
-		configBaseDir = homeDir
-	}
 	configScript, configPath, err := generateConfigScript(ctx, so.ProvisionOptions.TaskId, settings, configBaseDir)
 	if err != nil {
 		return "", errors.Wrap(err, "generating config YAML script")
