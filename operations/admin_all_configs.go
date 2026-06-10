5a808c75c2c7b9d41d771c56c0ee65e541782240
maddaak <akhilesh.maddali@mongodb.com>
2026-06-10 11:40:05 -0400

    Fix warning logged as catcher error in admin all configs

--- operations/admin_all_configs.go
+++ operations/admin_all_configs.go
@@ -92,7 +92,7 @@ func fetchAndWriteConfigs(ctx context.Context, c *legacyClient, projects []model
 			continue
 		}
 		if len(versions) == 0 {
-			catcher.Errorf("WARNING: project '%s' has no versions", p.Identifier)
+			grip.Warningf(ctx, "project '%s' has no versions", p.Identifier)
 			continue
 		}
 
