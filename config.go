6959e7d62a4ade64d276325435ac7d2ecb70b111
Akhilesh Maddali <akhilesh.maddali@mongodb.com>
2026-06-17 11:25:09 -0400

    DEVPROD-35238 - Change Error in admin all configs to warning for missing Version Path (#10352)

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-06-16"
+	ClientVersion = "2026-06-16a"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
