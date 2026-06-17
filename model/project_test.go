12ed4ef7ed3bd61f6aa117019c7fe809b5b99149
Annie Black <annie.black@mongodb.com>
2026-06-17 15:30:07 -0400

    DEVPROD-33516: productionize cross-file YAML anchors
    Make cross-file YAML anchor support always-on rather than behind a
    --yaml-anchors flag. Remove EnableYAMLAnchors from GetProjectOpts,
    validate, and evaluate CLI commands. Simplify createIntermediateProject
    by removing the nil-registry legacy branch. Add tests covering functions
    anchors, reverse-direction errors, single-file regression, and a
    benchmark. Update docs to drop beta language.

--- model/project_test.go
+++ model/project_test.go
@@ -1548,7 +1548,7 @@ tasks:
   depends_on:
     - name: dist-test
 `
-	intermediate, err := createIntermediateProject([]byte(projYml), false, nil)
+	intermediate, err := createIntermediateProject([]byte(projYml), false, &anchorEntries{})
 	s.NoError(err)
 	marshaled, err := yaml.Marshal(intermediate)
 	s.NoError(err)
