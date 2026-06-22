4825cf4b37a87d72b29c15ff7bb902354f0f08c2
Shreeya Chand <sachand@usc.edu>
2026-06-22 12:15:00 -0400

    wire aliases

--- units/patch_intent.go
+++ units/patch_intent.go
@@ -350,7 +350,7 @@ func (j *patchIntentProcessor) finishPatch(ctx context.Context, patchDoc *patch.
 			}
 		}
 	}
-	if err = j.verifyValidAlias(ctx, pref.Id, patchDoc.PatchedProjectConfig); err != nil {
+	if err = j.verifyValidAliases(ctx, pref.Id, patchDoc.AliasesToResolve(), patchDoc.PatchedProjectConfig); err != nil {
 		j.gitHubError = invalidAlias
 		return err
 	}
@@ -1360,9 +1360,8 @@ func fetchTriggerVersionInfo(ctx context.Context, patchDoc *patch.Patch) (*model
 	return v, project, pp, nil
 }
 
-func (j *patchIntentProcessor) verifyValidAlias(ctx context.Context, projectId string, configStr string) error {
-	alias := j.intent.GetAlias()
-	if alias == "" {
+func (j *patchIntentProcessor) verifyValidAliases(ctx context.Context, projectId string, aliases []string, configStr string) error {
+	if len(aliases) == 0 {
 		return nil
 	}
 	var projectConfig *model.ProjectConfig
@@ -1373,14 +1372,16 @@ func (j *patchIntentProcessor) verifyValidAlias(ctx context.Context, projectId s
 			return errors.Wrap(err, "creating project config")
 		}
 	}
-	aliases, err := model.FindAliasInProjectRepoOrProjectConfig(ctx, projectId, alias, projectConfig)
-	if err != nil {
-		return errors.Wrapf(err, "retrieving aliases for project '%s'", projectId)
+	for _, alias := range aliases {
+		found, err := model.FindAliasInProjectRepoOrProjectConfig(ctx, projectId, alias, projectConfig)
+		if err != nil {
+			return errors.Wrapf(err, "retrieving aliases for project '%s'", projectId)
+		}
+		if len(found) == 0 {
+			return errors.Errorf("alias '%s' could not be found on project '%s'", alias, projectId)
+		}
 	}
-	if len(aliases) > 0 {
-		return nil
-	}
-	return errors.Errorf("alias '%s' could not be found on project '%s'", alias, projectId)
+	return nil
 }
 
 func findEvergreenUserForPR(ctx context.Context, githubUID int) (*user.DBUser, error) {
