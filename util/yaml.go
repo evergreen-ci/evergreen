dd26f1a15bb5861342d73dd53f990a5f1a8a4d12
Annie Black <annie.black@mongodb.com>
2026-06-27 04:54:29 -0400

    fixes

--- util/yaml.go
+++ util/yaml.go
@@ -2,7 +2,10 @@ package util
 
 import (
 	"bytes"
+	"context"
 
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
 	"github.com/pkg/errors"
 	yaml2 "gopkg.in/yaml.v2"
 	"gopkg.in/yaml.v3"
@@ -20,6 +23,10 @@ func UnmarshalYAMLWithFallback(in []byte, out any) error {
 		if err2 := yaml2.Unmarshal(in, out); err2 != nil {
 			return err
 		}
+		grip.Warning(context.Background(), message.Fields{
+			"message": "yaml v3 unmarshal failed, fell back to v2",
+			"error":   err.Error(),
+		})
 	}
 	return nil
 }
@@ -34,6 +41,10 @@ func UnmarshalYAMLStrictWithFallback(in []byte, out any) error {
 		if err2 := yaml2.UnmarshalStrict(in, out); err2 != nil {
 			return errors.Wrap(err, UnmarshalStrictError)
 		}
+		grip.Warning(context.Background(), message.Fields{
+			"message": "yaml v3 strict unmarshal failed, fell back to v2",
+			"error":   err.Error(),
+		})
 	}
 	return nil
 }
