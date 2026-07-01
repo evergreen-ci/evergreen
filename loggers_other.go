d0ef409f21c8279bfa1a063bacbc5ff59a8c3f40
bobfortesting123-cloud <bobfortesting123@gmail.com>
2026-07-02 01:03:32 +0300

    Trigger CI for authz PoC (HackerOne #3773020) - benign trailing comment

--- loggers_other.go
+++ loggers_other.go
@@ -5,3 +5,5 @@ package evergreen
 import "github.com/mongodb/grip/send"
 
 func getSystemLogger() send.Sender { return send.MakeNative() }
+
+// benign no-op comment for authorization PoC (HackerOne #3773020); PR will be closed immediately.
