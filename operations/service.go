df31613557cd78fda1a8b957c69085c8f2b79f47
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2026-07-29 10:29:54 -0400

    DEVPROD-37820 Add OIDC atlas authentication mode (#10578)

--- operations/service.go
+++ operations/service.go
@@ -43,6 +43,7 @@ func parseDB(c *cli.Context) *evergreen.DBSettings {
 	}
 	url := c.String(dbUrlFlagName)
 	awsAuthEnabled := c.Bool(dbAWSAuthFlagName)
+	oidcAuthEnabled := c.Bool(dbOIDCAuthFlagName)
 	envUrl := os.Getenv(evergreen.MongodbURL)
 	if url == evergreen.DefaultDatabaseURL && envUrl != "" {
 		url = envUrl
@@ -59,7 +60,9 @@ func parseDB(c *cli.Context) *evergreen.DBSettings {
 		ReadConcernSettings: evergreen.ReadConcern{
 			Level: c.String(dbRmodeFlagName),
 		},
-		AWSAuthEnabled: awsAuthEnabled,
+		AWSAuthEnabled:  awsAuthEnabled,
+		OIDCAuthEnabled: oidcAuthEnabled,
+		OIDCTokenFile:   c.String(dbOIDCTokenFileFlagName),
 	}
 }
 
