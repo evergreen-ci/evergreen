67e9bd8a6fa6582bfb6d003405f2f62977b44a84
Annie Black <annie.black@mongodb.com>
2026-08-31 18:19:31 -0400

    DEVPROD-42403 Add spec validation, tests, and OpenAPI docs

--- cmd/swagger-to-openapi/swagger-to-openapi.go
+++ cmd/swagger-to-openapi/swagger-to-openapi.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"os"
 
-	"github.com/getkin/kin-openapi/openapi2"
-	"github.com/getkin/kin-openapi/openapi2conv"
+	"github.com/evergreen-ci/evergreen/rest/openapi"
 	"github.com/pkg/errors"
 )
 
-// swagger-to-openapi converts a Swagger 2.0 spec into an OpenAPI 3 spec.
-// swaggo, which generates Evergreen's REST v2 spec from code comments, only
-// emits Swagger 2.0, so the generated spec is converted as a post-processing
-// step before it is rendered and published.
+// swagger-to-openapi converts the generated Swagger 2.0 REST v2 spec into an
+// OpenAPI 3 spec, in place by default. It is run as part of the API doc build.
 func main() {
 	var input, output string
 	flag.StringVar(&input, "input", "", "path to the Swagger 2.0 spec to convert")
@@ -34,27 +30,17 @@ func run(input, output string) error {
 		output = input
 	}
 
-	contents, err := os.ReadFile(input)
+	swaggerSpec, err := os.ReadFile(input)
 	if err != nil {
 		return errors.Wrapf(err, "reading Swagger spec '%s'", input)
 	}
 
-	var swagger openapi2.T
-	if err := json.Unmarshal(contents, &swagger); err != nil {
-		return errors.Wrapf(err, "parsing Swagger spec '%s'", input)
-	}
-
-	openapi, err := openapi2conv.ToV3(&swagger)
+	openapiSpec, err := openapi.Convert(swaggerSpec)
 	if err != nil {
-		return errors.Wrap(err, "converting Swagger 2.0 spec to OpenAPI 3")
+		return err
 	}
 
-	converted, err := json.MarshalIndent(openapi, "", "    ")
-	if err != nil {
-		return errors.Wrap(err, "marshalling OpenAPI 3 spec")
-	}
-
-	if err := os.WriteFile(output, append(converted, '\n'), 0644); err != nil {
+	if err := os.WriteFile(output, openapiSpec, 0644); err != nil {
 		return errors.Wrapf(err, "writing OpenAPI 3 spec '%s'", output)
 	}
 
