3788c22d87d33391104a34144ea6533d4612f9f4
Annie Black <annie.black@mongodb.com>
2026-09-16 15:39:42 -0400

    DEVPROD-41718 support per-module clone_depth in git.get_project (#10777)

--- config.go
+++ config.go
@@ -35,11 +35,11 @@ var (
 
 	// ClientVersion is the commandline version string used to control updating
 	// the CLI. The format is the calendar date (YYYY-MM-DD).
-	ClientVersion = "2026-09-14a"
+	ClientVersion = "2026-09-10"
 
 	// Agent version to control agent rollover. The format is the calendar date
 	// (YYYY-MM-DD).
-	AgentVersion = "2026-09-15"
+	AgentVersion = "2026-09-16"
 )
 
 const (
