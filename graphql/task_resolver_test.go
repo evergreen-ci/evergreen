0a12e9725f9faf449c35a6adb59931f12ad42851
winstonzmdb <winston.zhao@mongodb.com>
2026-09-21 20:47:01 -0700

    Added resolver tests

--- graphql/task_resolver_test.go
+++ graphql/task_resolver_test.go
@@ -5,9 +5,77 @@ import (
 	"testing"
 
 	gqlgen "github.com/99designs/gqlgen/graphql"
+	"github.com/evergreen-ci/evergreen/db"
+	"github.com/evergreen-ci/evergreen/model/distro"
+	"github.com/evergreen-ci/evergreen/model/task"
+	restModel "github.com/evergreen-ci/evergreen/rest/model"
+	"github.com/evergreen-ci/utility"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/vektah/gqlparser/v2/ast"
 )
+
+func TestTaskResolverErrors(t *testing.T) {
+	ctx := t.Context()
+	require.NoError(t, db.ClearCollections(task.Collection, distro.Collection))
+
+	validDistro := distro.Distro{
+		Id:      "valid-distro",
+		Aliases: []string{"valid-distro-alias"},
+	}
+	require.NoError(t, validDistro.Insert(ctx))
+
+	warningDistro := distro.Distro{
+		Id:          "warning-distro",
+		WarningNote: "being deprecated",
+	}
+	require.NoError(t, warningDistro.Insert(ctx))
+
+	r := &taskResolver{}
+
+	t.Run("NoErrorsForValidPrimaryDistro", func(t *testing.T) {
+		obj := &restModel.APITask{
+			Id:       utility.ToStringPtr("task-with-valid-distro"),
+			DistroId: utility.ToStringPtr(validDistro.Id),
+		}
+		errs, err := r.Errors(ctx, obj)
+		require.NoError(t, err)
+		assert.Empty(t, errs)
+	})
+
+	t.Run("NoErrorsForValidPrimaryDistroReferencedByAlias", func(t *testing.T) {
+		obj := &restModel.APITask{
+			Id:       utility.ToStringPtr("task-with-distro-alias"),
+			DistroId: utility.ToStringPtr("valid-distro-alias"),
+		}
+		errs, err := r.Errors(ctx, obj)
+		require.NoError(t, err)
+		assert.Empty(t, errs)
+	})
+
+	t.Run("ReportsInvalidPrimaryDistro", func(t *testing.T) {
+		obj := &restModel.APITask{
+			Id:       utility.ToStringPtr("task-with-invalid-distro"),
+			DistroId: utility.ToStringPtr("nonexistent-distro"),
+		}
+		errs, err := r.Errors(ctx, obj)
+		require.NoError(t, err)
+		require.Len(t, errs, 1)
+		assert.Equal(t, distro.DistroNotFoundMessage("nonexistent-distro"), errs[0])
+	})
+
+	t.Run("ReportsWarningForDistroWithWarningNote", func(t *testing.T) {
+		obj := &restModel.APITask{
+			Id:       utility.ToStringPtr("task-with-warning-distro"),
+			DistroId: utility.ToStringPtr(warningDistro.Id),
+		}
+		errs, err := r.Errors(ctx, obj)
+		require.NoError(t, err)
+		expectedMsg, hasWarning := warningDistro.WarningNoteMessage()
+		require.True(t, hasWarning)
+		assert.Equal(t, []string{expectedMsg}, errs)
+	})
+}
 
 func TestShouldDecorateTestQuarantineStatus(t *testing.T) {
 	// The tests field resolves to a TaskTestResult whose immediate children are testResults,
